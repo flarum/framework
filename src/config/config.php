5a98eb0f06d6ed1f5c22967413061ff8b078c64e
Toby Zerner <toby.zerner@gmail.com>
2014-12-20 17:19:00 +1030

    Use Laravel's database config.

--- src/config/config.php
+++ src/config/config.php
@@ -4,16 +4,6 @@ return array(
 
 	'installed' => true,
 	'title' => 'Flarum Prototype Forum',
-	'database' => array(
-		'driver'    => 'mysql',
-		'host'      => 'localhost',
-		'database'  => 'flarum',
-		'username'  => '',
-		'password'  => '',
-		'charset'   => 'utf8',
-		'collation' => 'utf8_unicode_ci',
-		'prefix'    => '',
-	),
 
 	'route_rules' => array(
         // 'prefix' => 'blog',
