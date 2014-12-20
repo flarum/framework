1d8cda3c17f4c417bea084414d9d1b30820d89b7
Toby Zerner <toby.zerner@gmail.com>
2014-12-20 21:06:15 +1030

    Get rid of some troublesome dependencies for now.

--- ember/app/routes/application.js
+++ ember/app/routes/application.js
@@ -1,28 +1,30 @@
 import Ember from 'ember';
-import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
+// import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend(ApplicationRouteMixin, {
+// export default Ember.Route.extend(ApplicationRouteMixin, {
 
-	actions: {
+// 	actions: {
 
-		login: function() {
-			return this.render('login', {
-				into: 'application',
-				outlet: 'modal'
-			});
-		},
+// 		login: function() {
+// 			return this.render('login', {
+// 				into: 'application',
+// 				outlet: 'modal'
+// 			});
+// 		},
 
-		doLogin: function() {
-			this.get('session').authenticate('authenticator:custom', {});
-		},
+// 		doLogin: function() {
+// 			this.get('session').authenticate('authenticator:custom', {});
+// 		},
 
-		closeModal: function() {
-			return this.disconnectOutlet({
-				outlet: 'modal',
-				parentView: 'application'
-			});
-		}
+// 		closeModal: function() {
+// 			return this.disconnectOutlet({
+// 				outlet: 'modal',
+// 				parentView: 'application'
+// 			});
+// 		}
 
-	}
+// 	}
 
-});
+// });
+
+export default Ember.Route.extend();
