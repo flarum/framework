be7c116c0cf05fdd26aaf5282e669e830a95eb74
Toby Zerner <toby.zerner@gmail.com>
2014-12-23 12:45:07 +1030

    Upgrade ember-cli to 0.1.4 and fix related breakage. closes #18

--- packages/core/ember/Brocfile.js
+++ packages/core/ember/Brocfile.js
@@ -4,16 +4,15 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
-app.import('vendor/bootstrap/dist/js/bootstrap.js');
-app.import('vendor/spin.js/spin.js');
-app.import('vendor/spin.js/jquery.spin.js');
-app.import('vendor/moment/moment.js');
-app.import('vendor/json-api.js');
+app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+app.import('bower_components/spin.js/spin.js');
+app.import('bower_components/spin.js/jquery.spin.js');
+app.import('bower_components/moment/moment.js');
 
-app.import('vendor/font-awesome/fonts/fontawesome-webfont.eot');
-app.import('vendor/font-awesome/fonts/fontawesome-webfont.svg');
-app.import('vendor/font-awesome/fonts/fontawesome-webfont.ttf');
-app.import('vendor/font-awesome/fonts/fontawesome-webfont.woff');
-app.import('vendor/font-awesome/fonts/FontAwesome.otf');
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.eot');
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.svg');
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.ttf');
+app.import('bower_components/font-awesome/fonts/fontawesome-webfont.woff');
+app.import('bower_components/font-awesome/fonts/FontAwesome.otf');
 
 module.exports = app.toTree();
--- packages/core/src/views/index.blade.php
+++ packages/core/src/views/index.blade.php
@@ -9,19 +9,16 @@
     <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes">
 
+    <meta name="flarum/config/environment" content="%7B%22modulePrefix%22%3A%22flarum%22%2C%22environment%22%3A%22development%22%2C%22baseURL%22%3A%22/%22%2C%22locationType%22%3A%22hash%22%2C%22EmberENV%22%3A%7B%22FEATURES%22%3A%7B%7D%7D%2C%22APP%22%3A%7B%22LOG_ACTIVE_GENERATION%22%3Atrue%2C%22LOG_VIEW_LOOKUPS%22%3Atrue%7D%2C%22contentSecurityPolicyHeader%22%3A%22Content-Security-Policy-Report-Only%22%2C%22contentSecurityPolicy%22%3A%7B%22default-src%22%3A%22%27none%27%22%2C%22script-src%22%3A%22%27self%27%20%27unsafe-eval%27%22%2C%22font-src%22%3A%22%27self%27%22%2C%22connect-src%22%3A%22%27self%27%22%2C%22img-src%22%3A%22%27self%27%22%2C%22style-src%22%3A%22%27self%27%22%2C%22media-src%22%3A%22%27self%27%22%7D%2C%22exportApplicationGlobal%22%3Atrue%7D" />
+
     <base href="/">
 
     {{ app('flarum.web.assetManager')->styles() }}
 
   </head>
   <body>
-    <script>
-      window.FlarumENV = {"environment":"development","baseURL":"/","EmberENV":{"FEATURES":{"query-params-new":true}},"APP":{"LOG_MODULE_RESOLVER":true,"LOG_TRANSITIONS":true,"LOG_TRANSITIONS_INTERNAL":true},"LOG_MODULE_RESOLVER":true};
-      window.EmberENV = window.FlarumENV.EmberENV;
-    </script>
     {{ app('flarum.web.assetManager')->scripts() }}
     <script>
-      window.Flarum = require('flarum/app')['default'].create(FlarumENV.APP);
       // window.Flarum.registerPlugin(require('flarum/categories')['default']); // todo: make dynamic
     </script>
   </body>
