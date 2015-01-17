3ea695973a4a85dca76c7286f5ad8c063324844c
tdsbaaron <aaron.hong@student.tdsb.on.ca>
2015-01-17 13:29:05 -0500

    Update Forum.php
    Removed the unnecessary spaces, and corrected some mistakes.

--- src/Flarum/Core/Forum.php
+++ src/Flarum/Core/Forum.php
@@ -1,14 +1,13 @@
 <?php namespace Flarum\Core;
 
 use Tobscure\Permissible\Permissible;
-
 use Flarum\Core\Support\Exceptions\PermissionDeniedException;
 
 class Forum extends Entity
 {
     use Permissible;
 
-    public static function boot()
+    public static function boot();
     {
         parent::boot();
 
