ad269fdb5ad02b6896b6a0fa631473c9053ba79b
Toby Zerner <toby.zerner@gmail.com>
2015-01-22 14:44:33 +1030

    Implement token-based auth API

--- tests/_support/ApiHelper.php
+++ tests/_support/ApiHelper.php
@@ -1,10 +1,35 @@
 <?php
 namespace Codeception\Module;
 
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
+use Laracasts\TestDummy\Factory;
+use Auth;
+use DB;
 
 class ApiHelper extends \Codeception\Module
 {
+	public function haveAnAccount($data = [])
+	{
+		return Factory::create('Flarum\Core\Users\User', $data);
+	}
 
+	public function login($identifier, $password)
+	{
+		$this->getModule('REST')->sendPOST('/api/auth/login', ['identifier' => $identifier, 'password' => $password]);
+
+		$response = json_decode($this->getModule('REST')->grabResponse(), true);
+		if ($response && is_array($response) && isset($response['token'])) {
+			return $response['token'];
+		}
+
+		return false;
+	}
+
+	public function amAuthenticated()
+	{
+		$user = $this->haveAnAccount();
+		$user->groups()->attach(3); // Add member group
+		Auth::onceUsingId($user->id);
+
+        return $user;
+	}
 }
