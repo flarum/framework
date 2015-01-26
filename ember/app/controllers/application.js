49a28eca148a91b4a695df1159c3417e3b140d5b
Toby Zerner <toby.zerner@gmail.com>
2015-01-26 12:06:54 +1030

    Change forum title for demo

--- ember/app/controllers/application.js
+++ ember/app/controllers/application.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
 
 	// The title of the forum.
 	// TODO: Preload this value in the index.html payload from Laravel config.
-	forumTitle: 'Ninetech Support Forum',
+	forumTitle: 'Flarum Demo Forum',
 
 	// The title of the current page. This should be set as appropriate in
 	// controllers/views.
