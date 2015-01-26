5f49f270aa06d438069435e3c40fa124a972b77c
Toby Zerner <toby.zerner@gmail.com>
2015-01-26 12:07:24 +1030

    Change another instance of forum title

--- packages/core/src/routes.php
+++ packages/core/src/routes.php
@@ -3,5 +3,5 @@
 Route::get('/', function()
 {
 	return View::make('flarum.web::index')
-		->with('title', Config::get('flarum::forum_title', 'Flarum Support Forum'));
+		->with('title', Config::get('flarum::forum_title', 'Flarum Demo Forum'));
 });
