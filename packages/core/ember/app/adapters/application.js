9e2097eb69139137074a6801714864978e16c4b4
Toby Zerner <toby.zerner@gmail.com>
2015-01-30 12:05:57 +1030

    JsonApiAdapter fix to make index "load more" work

--- packages/core/ember/app/adapters/application.js
+++ packages/core/ember/app/adapters/application.js
@@ -3,7 +3,16 @@ import config from '../config/environment';
 
 export default JsonApiAdapter.extend({
 
-	// Todo: make this loaded via an environment variable or something
-	host: config.apiURL
-	
+  // Todo: make this loaded via an environment variable or something
+  host: config.apiURL,
+
+  // We can get rid of this after
+  // https://github.com/kurko/ember-json-api/pull/59 is merged.
+  extractMeta: function(store, type, payload) {
+    if (payload && payload.meta) {
+      store.metaForType(type, payload.meta);
+      delete payload.meta;
+    }
+  }
+  
 });
