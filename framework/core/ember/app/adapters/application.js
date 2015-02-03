aa665cfd7937cf9d5355ced2ce22f1a39473834f
Toby Zerner <toby.zerner@gmail.com>
2015-02-03 18:40:34 +1030

    Better handling of AJAX errors

--- framework/core/ember/app/adapters/application.js
+++ framework/core/ember/app/adapters/application.js
@@ -1,9 +1,8 @@
+import DS from 'ember-data';
 import JsonApiAdapter from 'ember-json-api/json-api-adapter';
 import config from '../config/environment';
 
 export default JsonApiAdapter.extend({
-
-  // Todo: make this loaded via an environment variable or something
   host: config.apiURL,
 
   // We can get rid of this after
@@ -13,6 +12,22 @@ export default JsonApiAdapter.extend({
       store.metaForType(type, payload.meta);
       delete payload.meta;
     }
+  },
+
+  ajaxError: function(jqXHR) {
+    var errors = this._super(jqXHR);
+    if (errors instanceof DS.InvalidError) {
+      var newErrors = {};
+      for (var i in errors.errors) {
+        var error = errors.errors[i];
+        newErrors[error.path] = error.detail;
+      }
+      errors = new DS.InvalidError(newErrors);
+    } else if (errors instanceof JsonApiAdapter.ServerError) {
+      // @todo show an alert message
+      console.log(errors);
+    }
+    return errors;
   }
   
 });
