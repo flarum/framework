0953d4e11f955e5b986895c74d47ad29c18674e6
Toby Zerner <toby.zerner@gmail.com>
2015-02-06 12:22:08 +1030

    Make discussion in view when returning to index

--- packages/core/ember/app/views/index/index.js
+++ packages/core/ember/app/views/index/index.js
@@ -4,7 +4,20 @@ export default Ember.View.extend({
 
     didInsertElement: function() {
         this.updateTitle();
-        $(window).scrollTop(this.get('controller.scrollTop'));
+        var scrollTop = this.get('controller.scrollTop');
+        $(window).scrollTop(scrollTop);
+
+        var lastDiscussion = this.get('controller.lastDiscussion');
+        if (lastDiscussion) {
+            var $discussion = $('.index-area .discussion-summary[data-id='+lastDiscussion.get('id')+']');
+            if ($discussion.length) {
+                var indexTop = $('#header').outerHeight();
+                var discussionTop = $discussion.offset().top;
+                if (discussionTop < scrollTop + indexTop || discussionTop + $discussion.outerHeight() > scrollTop + $(window).height()) {
+                    $(window).scrollTop(discussionTop - indexTop);
+                }
+            }
+        }
     },
 
     willDestroyElement: function() {
