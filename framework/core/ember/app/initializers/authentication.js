a5be161d6173d9275f1882c8da31cec8de6873d0
Toby Zerner <toby.zerner@gmail.com>
2015-02-06 10:35:46 +1030

    Cleanup, remove some old code

--- framework/core/ember/app/initializers/authentication.js
+++ framework/core/ember/app/initializers/authentication.js
@@ -1,8 +1,8 @@
 import FlarumAuthorizer from '../authorizers/flarum';
 
 export default {
-  name:       'authentication',
-  before:     'simple-auth',
+  name: 'authentication',
+  before: 'simple-auth',
   initialize: function(container) {
     container.register('authorizer:flarum', FlarumAuthorizer);
   }
