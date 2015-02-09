eec50f687cc1a317a61f107ec3f16313d7ef5d52
Toby Zerner <toby.zerner@gmail.com>
2015-02-09 20:14:18 +1030

    Move extractMeta override to serializer

--- ember/app/adapters/application.js
+++ ember/app/adapters/application.js
@@ -5,15 +5,6 @@ import config from '../config/environment';
 export default JsonApiAdapter.extend({
   host: config.apiURL,
 
-  // We can get rid of this after
-  // https://github.com/kurko/ember-json-api/pull/59 is merged.
-  extractMeta: function(store, type, payload) {
-    if (payload && payload.meta) {
-      store.metaForType(type, payload.meta);
-      delete payload.meta;
-    }
-  },
-
   ajaxError: function(jqXHR) {
     var errors = this._super(jqXHR);
     if (errors instanceof DS.InvalidError) {
