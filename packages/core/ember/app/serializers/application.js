39af318087d9a7bf6d694ceffd95bba8f375adde
Toby Zerner <toby.zerner@gmail.com>
2015-02-09 20:14:18 +1030

    Move extractMeta override to serializer

--- packages/core/ember/app/serializers/application.js
+++ packages/core/ember/app/serializers/application.js
@@ -1,13 +1,22 @@
 import JsonApiSerializer from 'ember-json-api/json-api-serializer';
 
 export default JsonApiSerializer.extend({
-	normalize: function(type, hash, property) {
-		var json = {};
+  normalize: function(type, hash, property) {
+    var json = {};
 
-		for (var prop in hash) {
-			json[prop.camelize()] = hash[prop]; 
-		}
+    for (var prop in hash) {
+      json[prop.camelize()] = hash[prop];
+    }
 
-		return this._super(type, json, property);
-	}
+    return this._super(type, json, property);
+  },
+
+  // We can get rid of this after
+  // https://github.com/kurko/ember-json-api/pull/59 is merged.
+  extractMeta: function(store, type, payload) {
+    if (payload && payload.meta) {
+      store.setMetadataFor(type, payload.meta);
+      delete payload.meta;
+    }
+  }
 });
