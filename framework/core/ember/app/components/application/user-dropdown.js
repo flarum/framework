d89bcef8165619463c5e15e1981573c6273bbe80
Toby Zerner <toby.zerner@gmail.com>
2015-02-12 15:16:15 +1030

    Helper method for adding menu separators

--- framework/core/ember/app/components/application/user-dropdown.js
+++ framework/core/ember/app/components/application/user-dropdown.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 
 import HasItemLists from 'flarum/mixins/has-item-lists';
 import DropdownButton from 'flarum/components/ui/dropdown-button';
-import SeparatorItem from 'flarum/components/ui/separator-item';
 
 export default DropdownButton.extend(HasItemLists, {
   layoutName: 'components/application/user-dropdown',
@@ -13,15 +12,12 @@ export default DropdownButton.extend(HasItemLists, {
   label: Ember.computed.alias('user.username'),
 
   populateItems: function(items) {
+    var self = this;
     this.addActionItem(items, 'profile', 'Profile', 'user');
     this.addActionItem(items, 'settings', 'Settings', 'cog');
-    items.pushObject(SeparatorItem.create());
-    this.addActionItem(items, 'logout', 'Log Out', 'sign-out', null, null, this);
-  },
-
-  actions: {
-    logout: function() {
-      this.logout();
-    }
+    this.addSeparatorItem(items);
+    this.addActionItem(items, 'logout', 'Log Out', 'sign-out', null, function() {
+      self.sendAction('logout');
+    });
   }
 })
