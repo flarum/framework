247518cb550a9a23b389380561112be828440b5c
Toby Zerner <toby.zerner@gmail.com>
2015-03-05 12:07:16 +1030

    Add badges API + styles for discussions and users

--- packages/core/ember/app/components/discussion/post-header/user.js
+++ packages/core/ember/app/components/discussion/post-header/user.js
@@ -8,5 +8,5 @@ var precompileTemplate = Ember.Handlebars.compile;
 export default Ember.Component.extend({
   tagName: 'h3',
   classNames: ['post-user'],
-  layout: precompileTemplate('{{#if post.user}}{{#link-to "user" post.user}}{{user-avatar post.user}} {{user-name post.user}}{{/link-to}}{{else}}{{user-avatar post.user}} {{user-name post.user}}{{/if}}')
+  layout: precompileTemplate('{{#if post.user}}{{#link-to "user" post.user}}{{user-avatar post.user}} {{user-name post.user}}{{/link-to}}{{else}}{{user-avatar post.user}} {{user-name post.user}}{{/if}}{{ui/item-list items=post.user.badges class="badges"}}')
 });
