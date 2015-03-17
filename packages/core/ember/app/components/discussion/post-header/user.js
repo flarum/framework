e1a00a35bdf5e631922dee381965066faf99a296
Toby Zerner <toby.zerner@gmail.com>
2015-03-18 09:12:38 +1030

    Animate user card popovers

--- packages/core/ember/app/components/discussion/post-header/user.js
+++ packages/core/ember/app/components/discussion/post-header/user.js
@@ -7,7 +7,7 @@ var precompileTemplate = Ember.Handlebars.compile;
  */
 export default Ember.Component.extend({
   classNames: ['post-user'],
-  layout: precompileTemplate('{{#if post.user}}<h3>{{#link-to "user" post.user}}{{user-avatar post.user}} {{user-name post.user}}{{/link-to}} {{ui/item-list items=post.user.badges class="badges"}}</h3>{{#if showCard}}{{user/user-card user=post.user class="user-card-popover" controlsButtonClass="btn btn-default btn-icon btn-sm btn-naked"}}{{/if}}{{else}}<h3>{{user-avatar post.user}} {{user-name post.user}}</h3>{{/if}}'),
+  layout: precompileTemplate('{{#if post.user}}<h3>{{#link-to "user" post.user}}{{user-avatar post.user}} {{user-name post.user}}{{/link-to}} {{ui/item-list items=post.user.badges class="badges"}}</h3>{{#if showCard}}{{user/user-card user=post.user class="user-card-popover fade" controlsButtonClass="btn btn-default btn-icon btn-sm btn-naked"}}{{/if}}{{else}}<h3>{{user-avatar post.user}} {{user-name post.user}}</h3>{{/if}}'),
 
   didInsertElement: function() {
     var component = this;
@@ -16,11 +16,16 @@ export default Ember.Component.extend({
       clearTimeout(timeout);
       timeout = setTimeout(function() {
         component.set('showCard', true);
+        Ember.run.scheduleOnce('afterRender', function() {
+          Ember.run.next(function() { component.$('.user-card').addClass('in'); });
+        });
       }, 250);
     }).bind('mouseout', '> a, .user-card', function() {
       clearTimeout(timeout);
       timeout = setTimeout(function() {
-        component.set('showCard', false);
+        component.$('.user-card').removeClass('in').one('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd', function() {
+          component.set('showCard', false);
+        });
       }, 250);
     });
   }
