e3110454aedef2118eca69b179cad08c2fecac5b
Toby Zerner <toby.zerner@gmail.com>
2015-05-02 08:46:38 +0930

    Allow extensions to easily override a function and call super

--- framework/core/js/lib/extension-utils.js
+++ framework/core/js/lib/extension-utils.js
@@ -1,8 +1,16 @@
 export function extend(object, func, extension) {
   var oldFunc = object[func];
   object[func] = function() {
-    var value = oldFunc.apply(this, arguments);
     var args = [].slice.apply(arguments);
+    var value = oldFunc.apply(this, args);
     return extension.apply(this, [value].concat(args));
   }
 };
+
+export function override(object, func, override) {
+  var parent = object[func];
+  object[func] = function() {
+    var args = [].slice.apply(arguments);
+    return override.apply(this, [parent].concat(args));
+  }
+}
