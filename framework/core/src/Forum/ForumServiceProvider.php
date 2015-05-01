40715203a525c6fa0f1514a164bf2c5455f38584
Toby Zerner <toby.zerner@gmail.com>
2015-05-02 08:10:06 +0930

    Define assets in a more appropriate spot, make extensible

--- framework/core/src/Forum/ForumServiceProvider.php
+++ framework/core/src/Forum/ForumServiceProvider.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\ServiceProvider;
 use Flarum\Support\AssetManager;
-use Flarum\Forum\Events\BootForum;
 
 class ForumServiceProvider extends ServiceProvider
 {
@@ -16,15 +15,6 @@ class ForumServiceProvider extends ServiceProvider
         $root = __DIR__.'/../..';
 
         $this->loadViewsFrom($root.'/views', 'flarum.forum');
-
-        $assetManager = $this->app['flarum.forum.assetManager'];
-
-        $assetManager->addFile([
-            $root.'/js/forum/dist/app.js',
-            $root.'/less/forum/app.less'
-        ]);
-
-        event(new BootForum($this->app));
 
         $this->publishes([
             $root.'/public/fonts' => public_path('flarum/fonts')
