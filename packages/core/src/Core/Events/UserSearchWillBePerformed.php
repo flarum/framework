16876e600bb517b4cb87c08670d74cfaeca97901
Toby Zerner <toby.zerner@gmail.com>
2015-05-03 12:07:15 +0930

    Add event to modify user search

--- packages/core/src/Core/Events/UserSearchWillBePerformed.php
+++ packages/core/src/Core/Events/UserSearchWillBePerformed.php
@@ -1,14 +1,14 @@
 <?php namespace Flarum\Core\Events;
 
-use Flarum\Core\Search\SearcherInterface;
+use Flarum\Core\Search\Users\UserSearcher;
 
-class SearchWillBePerformed
+class UserSearchWillBePerformed
 {
     public $searcher;
 
     public $criteria;
 
-    public function __construct(SearcherInterface $searcher, $criteria)
+    public function __construct(UserSearcher $searcher, $criteria)
     {
         $this->searcher = $searcher;
         $this->criteria = $criteria;
