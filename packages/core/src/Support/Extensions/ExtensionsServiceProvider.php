99c6c1964085c7aac338962917868bcba438072d
Toby Zerner <toby.zerner@gmail.com>
2015-05-04 08:56:19 +0930

    Add ExtensionsServiceProvider to load enabled extensions

---
+++ packages/core/src/Support/Extensions/ExtensionsServiceProvider.php
@@ -0,0 +1,33 @@
+<?php namespace Flarum\Support\Extensions;
+
+use Illuminate\Support\ServiceProvider;
+use DB;
+
+class ExtensionsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $extensions = json_decode(DB::table('config')->where('key', 'extensions_enabled')->pluck('value'), true);
+
+        foreach ($extensions as $extension) {
+            if (file_exists($file = base_path().'/extensions/'.$extension.'/bootstrap.php')) {
+                require $file;
+            }
+        }
+    }
+}
