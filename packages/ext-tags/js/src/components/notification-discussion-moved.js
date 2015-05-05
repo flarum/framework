f4fb1cca3fe5427fe133e26dbbf0deba157bfa6e
Toby Zerner <toby.zerner@gmail.com>
2015-05-05 17:31:42 +0930

    Update post/notification components

--- packages/ext-tags/js/src/components/notification-discussion-moved.js
+++ packages/ext-tags/js/src/components/notification-discussion-moved.js
@@ -1,28 +1,23 @@
 import Notification from 'flarum/components/notification';
-import avatar from 'flarum/helpers/avatar';
-import icon from 'flarum/helpers/icon';
 import username from 'flarum/helpers/username';
-import humanTime from 'flarum/helpers/human-time';
 import categoryLabel from 'categories/helpers/category-label';
 
 export default class NotificationDiscussionMoved extends Notification {
-  content() {
+  view() {
     var notification = this.props.notification;
     var discussion = notification.subject();
     var category = discussion.category();
 
-    return m('a', {href: app.route('discussion.near', {
-      id: discussion.id(),
-      slug: discussion.slug(),
-      near: notification.content().postNumber
-    }), config: m.route}, [
-      avatar(notification.sender()),
-      m('h3.notification-title', discussion.title()),
-      m('div.notification-info', [
-        icon('arrow-right'),
-        ' Moved to ', categoryLabel(category), ' by ', username(notification.sender()),
-        ' ', humanTime(notification.time())
-      ])
-    ]);
+    return super.view({
+      href: app.route('discussion.near', {
+        id: discussion.id(),
+        slug: discussion.slug(),
+        near: notification.content().postNumber
+      }),
+      config: m.route,
+      title: discussion.title(),
+      icon: 'arrow-right',
+      content: ['Moved to ', categoryLabel(category), ' by ', username(notification.sender())]
+    });
   }
 }
