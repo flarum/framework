9661aacdee83069dc3e24ca692eac31ea0c012c4
Toby Zerner <toby.zerner@gmail.com>
2015-05-07 09:20:19 +0930

    Fix moving discussions from uncategorized

--- extensions/tags/js/src/components/move-discussion-modal.js
+++ extensions/tags/js/src/components/move-discussion-modal.js
@@ -12,17 +12,18 @@ export default class MoveDiscussionModal extends Component {
 
   view() {
     var discussion = this.props.discussion;
+    var discussionCategory = discussion && discussion.category();
 
     return m('div.modal-dialog.modal-move-discussion', [
       m('div.modal-content', [
         m('button.btn.btn-icon.btn-link.close.back-control', {onclick: app.modal.close.bind(app.modal)}, icon('times')),
         m('div.modal-header', m('h3.title-control', discussion
-          ? ['Move ', m('em', discussion.title()), ' from ', categoryLabel(discussion.category()), ' to...']
+          ? ['Move ', m('em', discussion.title()), ' from ', categoryLabel(discussionCategory), ' to...']
           : ['Start a Discussion In...'])),
         m('div', [
           m('ul.category-list', [
             this.categories().map(category =>
-              (discussion && category.id() === discussion.category().id()) ? '' : m('li.category-tile', {style: 'background-color: '+category.color()}, [
+              (discussion && discussionCategory && category.id() === discussionCategory.id()) ? '' : m('li.category-tile', {style: 'background-color: '+category.color()}, [
                 m('a[href=javascript:;]', {onclick: this.save.bind(this, category)}, [
                   m('h3.title', category.title()),
                   m('p.description', category.description()),
