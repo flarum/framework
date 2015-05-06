f3738853f1a0bbdc3a1fa8597c661dfd76c243c7
Toby Zerner <toby.zerner@gmail.com>
2015-05-07 09:20:19 +0930

    Fix moving discussions from uncategorized

--- packages/ext-tags/js/src/helpers/category-label.js
+++ packages/ext-tags/js/src/helpers/category-label.js
@@ -1,3 +1,12 @@
-export default function categoryLabel(category) {
-  return m('span.category-label', {style: {backgroundColor: category.color()}}, category.title());
+export default function categoryLabel(category, attrs) {
+  attrs = attrs || {};
+
+  if (category) {
+    attrs.style = attrs.style || {};
+    attrs.style.backgroundColor = category.color();
+  } else {
+    attrs.className = (attrs.className || '')+' uncategorized';
+  }
+
+  return m('span.category-label', attrs, category ? category.title() : 'Uncategorized');
 }
