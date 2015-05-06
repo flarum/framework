a38efe6e9053fd36e39101506213f65b09ef6f91
Toby Zerner <toby.zerner@gmail.com>
2015-05-06 12:13:40 +0930

    Use absolute autoload path

--- extensions/tags/bootstrap.php
+++ extensions/tags/bootstrap.php
@@ -2,7 +2,7 @@
 
 // Require the extension's composer autoload file. This will enable all of our
 // classes in the src directory to be autoloaded.
-require 'vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Register our service provider with the Flarum application. In here we can
 // register bindings and execute code when the application boots.
