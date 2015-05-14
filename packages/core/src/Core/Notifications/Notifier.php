9a83c689e6de4e847d6c253b0cd33c0334513b6a
Toby Zerner <toby.zerner@gmail.com>
2015-05-14 22:39:57 +0930

    Rework notifications architecture
    - The recipient(s) are the concern of the notifier/sender, not the
    notification itself
    - Allow “retraction” of notifications (e.g. if a discussion is
    stickied, but then it is unstickied)
    - Misc. cleanup

--- packages/core/src/Core/Notifications/Notifier.php
+++ packages/core/src/Core/Notifications/Notifier.php
@@ -1,6 +1,7 @@
 <?php namespace Flarum\Core\Notifications;
 
 use Flarum\Core\Notifications\Types\Notification;
+use Flarum\Core\Notifications\Senders\RetractableSender;
 use Flarum\Core\Models\Notification as NotificationModel;
 use Flarum\Core\Models\User;
 use Illuminate\Container\Container;
@@ -18,12 +19,28 @@ class Notifier
         $this->container = $container;
     }
 
-    public function send(Notification $notification)
+    public function send(Notification $notification, array $users)
     {
         foreach ($this->methods as $method => $sender) {
             $sender = $this->container->make($sender);
-            if ($notification->getRecipient()->shouldNotify($notification::getType(), $method) && $sender::compatibleWith($notification)) {
-                $sender->send($notification);
+
+            if ($sender::compatibleWith($notification)) {
+                foreach ($users as $user) {
+                    if ($user->shouldNotify($notification::getType(), $method)) {
+                        $sender->send($notification, $user);
+                    }
+                }
+            }
+        }
+    }
+
+    public function retract(Notification $notification)
+    {
+        foreach ($this->methods as $method => $sender) {
+            $sender = $this->container->make($sender);
+
+            if ($sender instanceof RetractableSender && $sender::compatibleWith($notification)) {
+                $sender->retract($notification);
             }
         }
     }
--- packages/core/src/Core/Notifications/Types/AlertableNotification.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Flarum\Core\Notifications\Types;
-
-interface AlertableNotification
-{
-    public function getAlertData();
-
-    public static function getType();
-}
