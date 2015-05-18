a09c1b11a92532c4fb1d654d3594b76ce85f39df
Franz Liedke <franz@develophp.org>
2015-05-19 01:22:09 +0200

    Fix migrations to comply with PSR-2

--- packages/core/migrations/2015_02_24_000000_create_config_table.php
+++ packages/core/migrations/2015_02_24_000000_create_config_table.php
@@ -3,30 +3,30 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateConfigTable extends Migration {
+class CreateConfigTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('config', function(Blueprint $table)
-		{
-			$table->string('key', 100)->primary();
-			$table->binary('value')->nullable();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+        
+            $table->string('key', 100)->primary();
+            $table->binary('value')->nullable();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('config');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('config');
+    }
 }
--- packages/core/migrations/2015_02_24_000000_create_posts_table.php
+++ packages/core/migrations/2015_02_24_000000_create_posts_table.php
@@ -3,45 +3,45 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePostsTable extends Migration {
+class CreatePostsTable extends Migration
+{
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('posts', function(Blueprint $table)
-		{
-			$table->increments('id');
-			$table->integer('discussion_id')->unsigned();
-			$table->integer('number')->unsigned()->nullable();
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+        
+            $table->increments('id');
+            $table->integer('discussion_id')->unsigned();
+            $table->integer('number')->unsigned()->nullable();
 
-			$table->dateTime('time');
-			$table->integer('user_id')->unsigned()->nullable();
-			$table->string('type', 100)->nullable();
-			$table->text('content')->nullable();
-			$table->text('content_html')->nullable();
+            $table->dateTime('time');
+            $table->integer('user_id')->unsigned()->nullable();
+            $table->string('type', 100)->nullable();
+            $table->text('content')->nullable();
+            $table->text('content_html')->nullable();
 
-			$table->dateTime('edit_time')->nullable();
-			$table->integer('edit_user_id')->unsigned()->nullable();
-			$table->dateTime('hide_time')->nullable();
-			$table->integer('hide_user_id')->unsigned()->nullable();
-		});
+            $table->dateTime('edit_time')->nullable();
+            $table->integer('edit_user_id')->unsigned()->nullable();
+            $table->dateTime('hide_time')->nullable();
+            $table->integer('hide_user_id')->unsigned()->nullable();
+        });
 
-		// add fulltext index to content (and title?)
-		// add unique index on [discussion_id, number] !!!
-	}
+        // add fulltext index to content (and title?)
+        // add unique index on [discussion_id, number] !!!
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('posts');
-	}
-
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
 }
