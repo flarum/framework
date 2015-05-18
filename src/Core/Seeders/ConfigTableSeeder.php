1ab1631849a3ce8d76bd0d607e06809044add067
Franz Liedke <franz@develophp.org>
2015-05-19 01:53:37 +0200

    Fix the config table seeder
    It should include the "extensions_enabled" key which is read
    when initializing all extensions.

--- src/Core/Seeders/ConfigTableSeeder.php
+++ src/Core/Seeders/ConfigTableSeeder.php
@@ -14,11 +14,12 @@ class ConfigTableSeeder extends Seeder
     public function run()
     {
         $config = [
-            'api_url'         => 'http://flarum.dev/api',
-            'base_url'        => 'http://flarum.dev',
-            'forum_title'     => 'Flarum Demo Forum',
-            'welcome_message' => 'Flarum is now at a point where you can have basic conversations, so here is a little demo for you to break.',
-            'welcome_title'   => 'Welcome to Flarum Demo Forum'
+            'api_url'            => 'http://flarum.dev/api',
+            'base_url'           => 'http://flarum.dev',
+            'forum_title'        => 'Flarum Demo Forum',
+            'welcome_message'    => 'Flarum is now at a point where you can have basic conversations, so here is a little demo for you to break.',
+            'welcome_title'      => 'Welcome to Flarum Demo Forum',
+            'extensions_enabled' => '[]',
         ];
 
         DB::table('config')->insert(array_map(function ($key, $value) {
