1dceee6f84019fe608ede0e2d82ca2a92ad59770
Toby Zerner <toby.zerner@gmail.com>
2015-05-18 13:51:30 +0930

    Add some extra optional functionality to the Extend\Permission API
    - Automatically serialise the attribute
    - Apply Permissible grant callbacks
    
    Need to consider splitting the $permission property into two arguments
    (currently have to explode by ‘.’)

--- packages/core/src/Extend/Permission.php
+++ packages/core/src/Extend/Permission.php
@@ -2,18 +2,55 @@
 
 use Illuminate\Foundation\Application;
 use Flarum\Core\Models\Permission as PermissionModel;
+use Flarum\Extend\SerializeAttributes;
 
 class Permission implements ExtenderInterface
 {
     protected $permission;
+
+    protected $serialize = false;
+
+    protected $grant = [];
 
     public function __construct($permission)
     {
         $this->permission = $permission;
     }
 
+    public function serialize($serialize = true)
+    {
+        $this->serialize = $serialize;
+
+        return $this;
+    }
+
+    public function grant($callback)
+    {
+        $this->grant[] = $callback;
+
+        return $this;
+    }
+
     public function extend(Application $app)
     {
         PermissionModel::addPermission($this->permission);
+
+        list($entity, $permission) = explode('.', $this->permission);
+
+        if ($this->serialize) {
+            $extender = new SerializeAttributes(
+                'Flarum\Api\Serializers\\'.ucfirst($entity).'Serializer',
+                function (&$attributes, $model, $serializer) use ($permission) {
+                    $attributes['can'.ucfirst($permission)] = (bool) $model->can($serializer->actor->getUser(), $permission);
+                }
+            );
+
+            $extender->extend($app);
+        }
+
+        foreach ($this->grant as $callback) {
+            $model = 'Flarum\Core\Models\\'.ucfirst($entity);
+            $model::grantPermission($permission, $callback);
+        }
     }
 }
