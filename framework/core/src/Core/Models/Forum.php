53357ad56f1098c47966f8ba5c03e16d6826c3a8
Toby Zerner <toby.zerner@gmail.com>
2015-05-19 10:59:57 +0930

    Experimenting with some new ways to handle config
    For now I’ve chucked it on Flarum\Core as a static method, but
    ultimately I think we will need a ConfigRepository abstraction (whether
    it replaces or sits underneath the Flarum\Core static method I’m not
    sure).
    
    Also starting to think about multisite scenarios, I think this is
    important. The Forum model could actually end up with a database table
    behind it, and each forum would have its own config settings? Haven’t
    really thought about it too hard yet…

--- framework/core/src/Core/Models/Forum.php
+++ framework/core/src/Core/Models/Forum.php
@@ -1,8 +1,14 @@
 <?php namespace Flarum\Core\Models;
 
 use Tobscure\Permissible\Permissible;
+use Flarum\Core;
 
 class Forum extends Model
 {
     use Permissible;
+
+    public function getTitleAttribute()
+    {
+        return Core::config('forum_title');
+    }
 }
