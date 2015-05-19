248c19de734c882864baad0abf6719c4864ca27c
Toby Zerner <toby.zerner@gmail.com>
2015-05-19 10:59:57 +0930

    Experimenting with some new ways to handle config
    For now I’ve chucked it on Flarum\Core as a static method, but
    ultimately I think we will need a ConfigRepository abstraction (whether
    it replaces or sits underneath the Flarum\Core static method I’m not
    sure).
    
    Also starting to think about multisite scenarios, I think this is
    important. The Forum model could actually end up with a database table
    behind it, and each forum would have its own config settings? Haven’t
    really thought about it too hard yet…

--- src/Core.php
+++ src/Core.php
@@ -1,9 +1,20 @@
 <?php namespace Flarum;
 
+use DB;
+
 class Core
 {
     public static function isInstalled()
     {
         return file_exists(base_path('../config.php'));
+    }
+
+    public static function config($key, $default = null)
+    {
+        if (is_null($value = DB::table('config')->where('key', $key)->pluck('value'))) {
+            $value = $default;
+        }
+
+        return $value;
     }
 }
