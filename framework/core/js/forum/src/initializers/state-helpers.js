b1693f9537e91c940deaddabe8eeb5657185d29a
Toby Zerner <toby.zerner@gmail.com>
2015-06-01 10:26:23 +0930

    Add 'state helpers', shortcuts to make querying app state easier

---
+++ framework/core/js/forum/src/initializers/state-helpers.js
@@ -0,0 +1,14 @@
+import ReplyComposer from 'flarum/components/reply-composer';
+import DiscussionPage from 'flarum/components/discussion-page';
+
+export default function(app) {
+  app.composingReplyTo = function(discussion) {
+    return this.composer.component instanceof ReplyComposer &&
+      this.composer.component.props.discussion === discussion;
+  };
+
+  app.viewingDiscussion = function(discussion) {
+    return this.current instanceof DiscussionPage &&
+      this.current.discussion() === discussion;
+  };
+};
