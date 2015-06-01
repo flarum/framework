8af333a7d2a26959069ab550aa8b585998ed3f2e
Toby Zerner <toby.zerner@gmail.com>
2015-06-01 17:54:37 +0930

    Tweak reply placeholder appearance

--- packages/core/js/forum/src/components/reply-placeholder.js
+++ packages/core/js/forum/src/components/reply-placeholder.js
@@ -3,7 +3,7 @@ import avatar from 'flarum/helpers/avatar';
 
 export default class ReplyPlaceholder extends Component {
   view() {
-    return m('article.post.reply-post', {onclick: () => this.props.discussion.replyAction(true)}, [
+    return m('article.post.reply-post', {onmousedown: () => this.props.discussion.replyAction(true)}, [
       m('header.post-header', avatar(app.session.user()), 'Write a Reply...'),
     ]);
   }
