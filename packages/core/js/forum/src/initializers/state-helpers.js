5a1c3a4fb3cdb6c167e0f215a0b1e2c52e2c398e
Toby Zerner <toby.zerner@gmail.com>
2015-06-08 09:47:28 +0930

    Make composer sliding animation less jumpy when replying

--- packages/core/js/forum/src/initializers/state-helpers.js
+++ packages/core/js/forum/src/initializers/state-helpers.js
@@ -1,10 +1,12 @@
+import Composer from 'flarum/components/composer';
 import ReplyComposer from 'flarum/components/reply-composer';
 import DiscussionPage from 'flarum/components/discussion-page';
 
 export default function(app) {
   app.composingReplyTo = function(discussion) {
     return this.composer.component instanceof ReplyComposer &&
-      this.composer.component.props.discussion === discussion;
+      this.composer.component.props.discussion === discussion &&
+      this.composer.position() !== Composer.PositionEnum.HIDDEN;
   };
 
   app.viewingDiscussion = function(discussion) {
