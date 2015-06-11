b2c617ebb6a750813acc1dca3f69f5a2d862161f
Toby Zerner <toby.zerner@gmail.com>
2015-06-11 18:45:31 +0930

    Add English locale template to extension stub

--- stubs/extension/src/ServiceProvider.php
+++ stubs/extension/src/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 use Flarum\Support\ServiceProvider;
 use Flarum\Extend\ForumAssets;
+use Flarum\Extend\Locale;
+use Flarum\Extend\ForumTranslations;
 
 class {{classPrefix}}ServiceProvider extends ServiceProvider
 {
@@ -16,7 +18,14 @@ class {{classPrefix}}ServiceProvider extends ServiceProvider
             new ForumAssets([
                 __DIR__.'/../js/dist/extension.js',
                 __DIR__.'/../less/extension.less'
-            ])
+            ]),
+
+            (new Locale('en'))->translations(__DIR__.'/../locale/en.yml'),
+
+            new ForumTranslations([
+                // Add the keys of translations you would like to be available
+                // for use by the JS client application.
+            ]),
         );
     }
 
