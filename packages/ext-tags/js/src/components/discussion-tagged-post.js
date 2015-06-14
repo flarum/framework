9ba0d2ece220c4af6ed4f816fe9d4602b91d010f
Toby Zerner <toby.zerner@gmail.com>
2015-06-15 09:00:30 +0930

    Add event post when a discussion's tags are changed

--- packages/ext-tags/js/src/components/discussion-tagged-post.js
+++ packages/ext-tags/js/src/components/discussion-tagged-post.js
@@ -1,12 +1,25 @@
 import EventPost from 'flarum/components/event-post';
-import categoryLabel from 'flarum-categories/helpers/category-label';
+import tagsLabel from 'flarum-tags/helpers/tags-label';
 
-export default class DiscussionMovedPost extends EventPost {
+export default class DiscussionTaggedPost extends EventPost {
   view() {
     var post = this.props.post;
-    var oldCategory = app.store.getById('categories', post.content()[0]);
-    var newCategory = app.store.getById('categories', post.content()[1]);
+    var oldTags = post.content()[0];
+    var newTags = post.content()[1];
 
-    return super.view('arrow-right', ['moved the discussion from ', categoryLabel(oldCategory), ' to ', categoryLabel(newCategory), '.']);
+    var added = newTags.filter(tag => oldTags.indexOf(tag) === -1).map(id => app.store.getById('tags', id));
+    var removed = oldTags.filter(tag => newTags.indexOf(tag) === -1).map(id => app.store.getById('tags', id));
+    var total = added.concat(removed);
+
+    var build = function(verb, tags, only) {
+      return tags.length ? [verb, ' ', only && tags.length == 1 ? 'the ' : '', tagsLabel(tags)] : '';
+    };
+
+    return super.view('tag', [
+      build('added', added, !removed.length),
+      added.length && removed.length ? ' and ' : '',
+      build('removed', removed, !added.length),
+      total.length ? (total.length == 1 ? ' tag.' : ' tags.') : ''
+    ]);
   }
 }
