a5b08d1552e6c98beb8fdb09562f25b64db6cac3
Toby Zerner <toby.zerner@gmail.com>
2015-06-17 12:48:22 +0930

    Use new event name

--- packages/ext-tags/src/Handlers/TagLoader.php
+++ packages/ext-tags/src/Handlers/TagLoader.php
@@ -1,6 +1,6 @@
 <?php namespace Flarum\Tags\Handlers;
 
-use Flarum\Api\Events\WillRespond;
+use Flarum\Api\Events\WillSerializeData;
 use Flarum\Api\Actions\Forum\ShowAction as ForumShowAction;
 use Flarum\Tags\Tag;
 
@@ -8,10 +8,10 @@ class TagLoader
 {
     public function subscribe($events)
     {
-        $events->listen('Flarum\Api\Events\WillRespond', __CLASS__.'@whenWillRespond');
+        $events->listen('Flarum\Api\Events\WillSerializeData', __CLASS__.'@whenWillSerializeData');
     }
 
-    public function whenWillRespond(WillRespond $event)
+    public function whenWillSerializeData(WillSerializeData $event)
     {
         if ($event->action instanceof ForumShowAction) {
             $forum = $event->data;
