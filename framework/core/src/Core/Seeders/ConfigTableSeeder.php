46920468d0656fa4837ff23b25f0cac9d6ae65b2
Toby Zerner <toby.zerner@gmail.com>
2015-06-17 17:58:43 +0930

    Back to rewritten URLs

--- framework/core/src/Core/Seeders/ConfigTableSeeder.php
+++ framework/core/src/Core/Seeders/ConfigTableSeeder.php
@@ -13,8 +13,8 @@ class ConfigTableSeeder extends Seeder
     public function run()
     {
         $config = [
-            'api_url'               => 'http://flarum.dev/api.php',
-            'base_url'              => 'http://flarum.dev/index.php',
+            'api_url'               => 'http://flarum.dev/api',
+            'base_url'              => 'http://flarum.dev',
             'forum_title'           => 'Flarum Demo Forum',
             'welcome_message'       => 'Flarum is now at a point where you can have basic conversations, so here is a little demo for you to break.',
             'welcome_title'         => 'Welcome to Flarum Demo Forum',
