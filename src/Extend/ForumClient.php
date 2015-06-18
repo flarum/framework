137f55317beab3f95935f9ed3c94edbd361a9a17
Toby Zerner <toby.zerner@gmail.com>
2015-06-18 17:43:41 +0930

    Add API to add routes

--- src/Extend/ForumClient.php
+++ src/Extend/ForumClient.php
@@ -2,12 +2,15 @@
 
 use Illuminate\Contracts\Container\Container;
 use Flarum\Forum\Actions\IndexAction;
+use Psr\Http\Message\ServerRequestInterface;
 
 class ForumClient implements ExtenderInterface
 {
     protected $assets = [];
 
     protected $translations = [];
+
+    protected $routes = [];
 
     public function assets($assets)
     {
@@ -23,12 +26,36 @@ class ForumClient implements ExtenderInterface
         return $this;
     }
 
+    public function route($method, $url, $name, $action = 'Flarum\Forum\Actions\IndexAction')
+    {
+        $this->routes[] = compact('method', 'url', 'name', 'action');
+
+        return $this;
+    }
+
     public function extend(Container $container)
     {
+        if ($container->make('type') !== 'forum') {
+            return;
+        }
+
         $container->make('events')->listen('Flarum\Forum\Events\RenderView', function ($event) {
             $event->assets->addFiles($this->assets);
         });
 
         IndexAction::$translations = array_merge(IndexAction::$translations, $this->translations);
+
+        if (count($this->routes)) {
+            $routes = $container->make('flarum.forum.routes');
+
+            foreach ($this->routes as $route) {
+                $method = $route['method'];
+                $routes->$method($route['url'], $route['name'], function (ServerRequestInterface $httpRequest, $routeParams) use ($container, $route) {
+                    $action = $container->make($route['action']);
+
+                    return $action->handle($httpRequest, $routeParams);
+                });
+            }
+        }
     }
 }
