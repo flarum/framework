1c359b1041d54a0515ed6a9a5ffd77d1dd0c1d34
Toby Zerner <toby.zerner@gmail.com>
2015-06-18 12:38:24 +0930

    Update core permissions stuff

--- framework/core/src/Core/Models/Permission.php
+++ framework/core/src/Core/Models/Permission.php
@@ -2,15 +2,4 @@
 
 class Permission extends Model
 {
-    protected static $permissions = [];
-
-    public static function getPermissions()
-    {
-        return static::$permissions;
-    }
-
-    public static function addPermission($permission)
-    {
-        static::$permissions[] = $permission;
-    }
 }
