473963087b9ab052dc1e60700e700e6e8a3abaae
Toby Zerner <toby.zerner@gmail.com>
2015-06-19 15:29:27 +0930

    Default to 404 code for RouteNotFoundException
    Just want to get Stratigility’s FinalHandler showing something more apt
    than “Internal server error”

--- src/Http/RouteNotFoundException.php
+++ src/Http/RouteNotFoundException.php
@@ -6,4 +6,8 @@ use Exception;
 
 class RouteNotFoundException extends Exception
 {
+    public function __construct($message = null, $code = 404, Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
