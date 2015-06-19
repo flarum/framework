1f2e16c4bb23682c6e6d6a5e58011c40546a22ce
Toby Zerner <toby.zerner@gmail.com>
2015-06-19 11:19:49 +0930

    Get login/forgot password working again

--- framework/core/src/Forum/Actions/LoginAction.php
+++ framework/core/src/Forum/Actions/LoginAction.php
@@ -4,6 +4,7 @@ use Flarum\Api\Client;
 use Flarum\Forum\Events\UserLoggedIn;
 use Flarum\Core\Repositories\UserRepositoryInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Zend\Diactoros\Response;
 
 class LoginAction extends BaseAction
 {
@@ -25,15 +26,21 @@ class LoginAction extends BaseAction
 
         $data = $this->apiClient->send('Flarum\Api\Actions\TokenAction', $params);
 
-        event(new UserLoggedIn($this->users->findOrFail($data->userId), $data->token));
+        // TODO: The client needs to pass through exceptions(?) or the whole
+        // response so we can look at the response code. For now if there isn't
+        // any useful data we just assume it's a 401.
+        if (isset($data->userId)) {
+            event(new UserLoggedIn($this->users->findOrFail($data->userId), $data->token));
 
-        // TODO: The client needs to pass through exceptions
-        $response = $this->success();
-        $response->getBody()->write(json_encode($data));
+            $response = $this->success();
+            $response->getBody()->write(json_encode($data));
 
-        return $this->withRememberCookie(
-            $response,
-            $data->token
-        );
+            return $this->withRememberCookie(
+                $response,
+                $data->token
+            );
+        } else {
+            return new Response('php://memory', 401);
+        }
     }
 }
