e472f911b5584a32c092d1e44d717b1921eb4c09
Toby Zerner <toby.zerner@gmail.com>
2015-06-19 14:34:52 +0930

    Don't destroy discussion list in case of a redraw
    i.e. the one in index-page’s onunload → composer.minimize

--- framework/core/js/forum/src/components/index-nav-item.js
+++ framework/core/js/forum/src/components/index-nav-item.js
@@ -3,7 +3,9 @@ import NavItem from 'flarum/components/nav-item'
 export default class IndexNavItem extends NavItem {
   static props(props) {
     props.onclick = props.onclick || function() {
-      app.cache.discussionList = null;
+      if (app.cache.discussionList) {
+        app.cache.discussionList.forceReload = true;
+      }
       m.redraw.strategy('none');
     };
   }
