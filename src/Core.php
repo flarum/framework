74536d71623c2f280f149393123edd08ec49acd6
Franz Liedke <franz@develophp.org>
2015-06-20 20:38:44 +0200

    Add method for checking debug mode

--- src/Core.php
+++ src/Core.php
@@ -1,12 +1,15 @@
 <?php namespace Flarum;
 
-use DB;
-
 class Core
 {
     public static function isInstalled()
     {
         return file_exists(base_path('../config.php'));
+    }
+
+    public static function inDebugMode()
+    {
+        return env('APP_DEBUG');
     }
 
     public static function config($key, $default = null)
