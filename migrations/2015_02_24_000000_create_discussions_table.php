64e5d505332c6ba1569bbb9140107b1afd19e334
Toby Zerner <toby.zerner@gmail.com>
2015-06-23 10:34:33 +0930

    Allow author to delete discussion if there are no replies
    Also disallow the first post in a discussion to be deleted or hidden
    (thus preventing discussions with zero posts)
    
    closes flarum/core#90 closes flarum/core#92

--- migrations/2015_02_24_000000_create_discussions_table.php
+++ migrations/2015_02_24_000000_create_discussions_table.php
@@ -15,16 +15,17 @@ class CreateDiscussionsTable extends Migration
     public function up()
     {
         Schema::create('discussions', function (Blueprint $table) {
-        
+
             $table->increments('id');
             $table->string('title', 200);
             $table->integer('comments_count')->unsigned()->default(0);
+            $table->integer('participants_count')->unsigned()->default(0);
             $table->integer('number_index')->unsigned()->default(0);
 
             $table->dateTime('start_time');
             $table->integer('start_user_id')->unsigned()->nullable();
             $table->integer('start_post_id')->unsigned()->nullable();
-            
+
             $table->dateTime('last_time')->nullable();
             $table->integer('last_user_id')->unsigned()->nullable();
             $table->integer('last_post_id')->unsigned()->nullable();
