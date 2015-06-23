6596fb473416df9e4f9e85b12cbe92712e48da6d
Toby Zerner <toby.zerner@gmail.com>
2015-06-23 11:01:25 +0930

    Fix migration

--- extensions/likes/migrations/2015_05_11_000000_create_posts_likes_table.php
+++ extensions/likes/migrations/2015_05_11_000000_create_posts_likes_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLikesTable extends Migration
+class CreatePostsLikesTable extends Migration
 {
     /**
      * Run the migrations.
