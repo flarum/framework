0d5927d81231476c18f9fdf29df60843d0996cfe
Toby Zerner <toby.zerner@gmail.com>
2015-06-25 15:35:11 +0930

    Prevent incorrect badge redraw diffing

--- framework/core/js/lib/components/badge.js
+++ framework/core/js/lib/components/badge.js
@@ -6,10 +6,12 @@ export default class Badge extends Component {
     var iconName = this.props.icon;
     var label = this.props.title = this.props.label;
     delete this.props.icon, this.props.label;
-    this.props.config = function(element) {
+    this.props.config = function(element, isInitialized) {
+      if (isInitialized) return;
       $(element).tooltip();
     };
     this.props.className = 'badge '+(this.props.className || '');
+    this.props.key = this.props.className;
 
     return m('span', this.props, [
       icon(iconName+' icon-glyph'),
