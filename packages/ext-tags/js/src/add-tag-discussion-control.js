683112cf62a85eb3dd90e9730b63897c12c54d01
Toby Zerner <toby.zerner@gmail.com>
2015-06-25 15:40:15 +0930

    Update for discussion controls API

--- packages/ext-tags/js/src/add-tag-discussion-control.js
+++ packages/ext-tags/js/src/add-tag-discussion-control.js
@@ -6,13 +6,13 @@ import TagDiscussionModal from 'flarum-tags/components/tag-discussion-modal';
 
 export default function() {
   // Add a control allowing the discussion to be moved to another category.
-  extend(Discussion.prototype, 'controls', function(items) {
+  extend(Discussion.prototype, 'moderationControls', function(items) {
     if (this.canTag()) {
       items.add('tags', ActionButton.component({
         label: 'Edit Tags',
         icon: 'tag',
         onclick: () => app.modal.show(new TagDiscussionModal({ discussion: this }))
-      }), {after: 'rename'});
+      }));
     }
   });
 };
