5cfb87020220af039c4b8a98b30f9fec3974f834
Toby Zerner <toby.zerner@gmail.com>
2015-07-08 10:51:16 +0930

    Make admin open in a new window

--- framework/core/js/lib/models/group.js
+++ framework/core/js/lib/models/group.js
@@ -7,4 +7,8 @@ Group.prototype.namePlural = Model.attribute('namePlural');
 Group.prototype.color = Model.attribute('color');
 Group.prototype.icon = Model.attribute('icon');
 
+Group.ADMINISTRATOR_ID = 1;
+Group.GUEST_ID = 2;
+Group.MEMBER_ID = 3;
+
 export default Group;
