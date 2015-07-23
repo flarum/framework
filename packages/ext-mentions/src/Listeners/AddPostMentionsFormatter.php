ab0bed7bf890190057e1cede0b08afe59cd43521
Toby Zerner <toby.zerner@gmail.com>
2015-07-23 20:50:41 +0930

    Make mention formatter work in live preview 🎉

--- packages/ext-mentions/src/Listeners/AddPostMentionsFormatter.php
+++ packages/ext-mentions/src/Listeners/AddPostMentionsFormatter.php
@@ -19,11 +19,16 @@ class AddPostMentionsFormatter
         $tagName = 'POSTMENTION';
 
         $tag = $configurator->tags->add($tagName);
+
         $tag->attributes->add('username');
         $tag->attributes->add('number');
         $tag->attributes->add('id')->filterChain->append('#uint');
+        $tag->attributes['id']->required = false;
+
         $tag->template = '<a href="{$DISCUSSION_URL}{@number}" class="PostMention" data-number="{@number}"><xsl:value-of select="@username"/></a>';
-        $tag->filterChain->prepend([static::class, 'addId'])->addParameterByName('post');
+        $tag->filterChain->prepend([static::class, 'addId'])
+            ->addParameterByName('post')
+            ->setJS('function() { return true; }');
 
         $configurator->Preg->match('/\B@(?<username>[a-z0-9_-]+)#(?<number>\d+)/i', $tagName);
     }
@@ -40,8 +45,10 @@ class AddPostMentionsFormatter
             ->where('number', $tag->getAttribute('number'))
             ->pluck('id');
 
-        $tag->setAttribute('id', $id);
+        if ($id) {
+            $tag->setAttribute('id', $id);
 
-        return true;
+            return true;
+        }
     }
 }
