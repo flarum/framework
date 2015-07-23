e22c79775036aca9b1fed32a2dcdc83561032fa3
Toby Zerner <toby.zerner@gmail.com>
2015-07-23 20:35:03 +0930

    Initial commit

---
+++ extensions/markdown/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+// Require the extension's composer autoload file. This will enable all of our
+// classes in the src directory to be autoloaded.
+require __DIR__.'/vendor/autoload.php';
+
+// Return the name of our Extension class. Flarum will register it as a service
+// provider, allowing it to register bindings and execute code when the
+// application boots.
+return 'Flarum\Markdown\Extension';
