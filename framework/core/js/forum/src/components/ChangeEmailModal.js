bc4a000078669b2992d9ce2832b6c8d7e40c6412
Toby Zerner <toby.zerner@gmail.com>
2015-07-23 14:30:55 +0930

    Fix sign up modal

--- framework/core/js/forum/src/components/ChangeEmailModal.js
+++ framework/core/js/forum/src/components/ChangeEmailModal.js
@@ -38,7 +38,7 @@ export default class ChangeEmailModal extends Modal {
       return (
         <div className="Modal-body">
           <div class="Form Form--centered">
-            <p class="helpText">{m.trust(app.trans('core.confirmation_email_sent', {email: this.email()}))}</p>
+            <p class="helpText">{m.trust(app.trans('core.confirmation_email_sent', {email: <strong>{this.email()}</strong>}))}</p>
             <div class="Form-group">
               <a href={'http://' + emailProviderName} className="Button Button--primary Button--block">
                 {app.trans('core.go_to', {location: emailProviderName})}
