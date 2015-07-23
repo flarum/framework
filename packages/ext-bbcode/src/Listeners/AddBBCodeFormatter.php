107d3dc2f3379317e51f49add9a1a9db99b2c684
Toby Zerner <toby.zerner@gmail.com>
2015-07-23 20:25:52 +0930

    Initial commit

---
+++ packages/ext-bbcode/src/Listeners/AddBBCodeFormatter.php
@@ -0,0 +1,26 @@
+<?php namespace Flarum\BBCode\Listeners;
+
+use Flarum\Events\FormatterConfigurator;
+use Illuminate\Contracts\Events\Dispatcher;
+
+class AddBBCodeFormatter
+{
+    public function subscribe(Dispatcher $events)
+    {
+        $events->listen(FormatterConfigurator::class, __CLASS__.'@addBBCodeFormatter');
+    }
+
+    public function addBBCodeFormatter(FormatterConfigurator $event)
+    {
+        $event->configurator->BBCodes->addFromRepository('B');
+        $event->configurator->BBCodes->addFromRepository('I');
+        $event->configurator->BBCodes->addFromRepository('U');
+        $event->configurator->BBCodes->addFromRepository('S');
+        $event->configurator->BBCodes->addFromRepository('URL');
+        $event->configurator->BBCodes->addFromRepository('EMAIL');
+        $event->configurator->BBCodes->addFromRepository('CODE');
+        $event->configurator->BBCodes->addFromRepository('QUOTE');
+        $event->configurator->BBCodes->addFromRepository('LIST');
+        $event->configurator->BBCodes->addFromRepository('*');
+    }
+}
