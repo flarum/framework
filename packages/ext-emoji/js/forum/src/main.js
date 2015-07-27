c7910e3c11cc8b1d98221c2b25fe7ad37eb3fcf3
Toby Zerner <toby.zerner@gmail.com>
2015-07-27 14:19:08 +0930

    Better way to override formatting util

--- packages/ext-emoji/js/forum/src/main.js
+++ packages/ext-emoji/js/forum/src/main.js
@@ -3,14 +3,14 @@
 import { override } from 'flarum/extend';
 import app from 'flarum/app';
 import Post from 'flarum/models/Post';
+import Formatter from 'flarum/utils/Formatter';
 
 app.initializers.add('emoji', () => {
   override(Post.prototype, 'contentHtml', original => {
     return twemoji.parse(original());
   });
 
-  override(s9e.TextFormatter, 'preview', (original, content, elm) => {
-    original(content, elm);
-    twemoji.parse(elm);
+  override(Formatter, 'format', (original, text) => {
+    return twemoji.parse(original(text));
   });
 });
