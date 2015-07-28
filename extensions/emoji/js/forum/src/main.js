cb358a90846f354d16394741a0cb6d8e3c3a2b56
Toby Zerner <toby.zerner@gmail.com>
2015-07-28 16:06:37 +0930

    Update for live preview refactor

--- extensions/emoji/js/forum/src/main.js
+++ extensions/emoji/js/forum/src/main.js
@@ -3,14 +3,15 @@
 import { override } from 'flarum/extend';
 import app from 'flarum/app';
 import Post from 'flarum/models/Post';
-import Formatter from 'flarum/utils/Formatter';
 
 app.initializers.add('emoji', () => {
   override(Post.prototype, 'contentHtml', original => {
     return twemoji.parse(original());
   });
 
-  override(Formatter, 'format', (original, text) => {
-    return twemoji.parse(original(text));
+  override(s9e.TextFormatter, 'preview', (original, text, element) => {
+    original(text, element);
+
+    twemoji.parse(element);
   });
 });
