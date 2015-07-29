42fd8e26c15bc2420fce025588a869dfce2d7ec5
Toby Zerner <toby.zerner@gmail.com>
2015-07-29 21:00:27 +0930

    Begin implementing permissions page

---
+++ src/Core/Groups/Permission.php
@@ -0,0 +1,55 @@
+<?php namespace Flarum\Core\Groups;
+
+use Flarum\Core\Model;
+use Illuminate\Database\Eloquent\Builder;
+
+/**
+ * @todo document database columns with @property
+ */
+class Permission extends Model
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $table = 'permissions';
+
+    /**
+     * Define the relationship with the group that this permission is for.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function group()
+    {
+        return $this->belongsTo('Flarum\Core\Groups\Group', 'group_id');
+    }
+
+    /**
+     * Set the keys for a save update query.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $query->where('group_id', $this->group_id)
+              ->where('permission', $this->permission);
+
+        return $query;
+    }
+
+    /**
+     * Get a map of permissions to the group IDs that have them.
+     *
+     * @return array[]
+     */
+    public static function map()
+    {
+        $permissions = [];
+
+        foreach (static::get() as $permission) {
+            $permissions[$permission->permission][] = $permission->group_id;
+        }
+
+        return $permissions;
+    }
+}
