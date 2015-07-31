e204794b91642760ce01934298b321975f5f9d76
Toby Zerner <toby.zerner@gmail.com>
2015-07-31 20:08:27 +0930

    Allow custom variables to be set on the client app

---
+++ js/admin/src/utils/saveConfig.js
@@ -0,0 +1,14 @@
+export default function saveConfig(config) {
+  const oldConfig = JSON.parse(JSON.stringify(app.config));
+
+  Object.assign(app.config, config);
+
+  return app.request({
+    method: 'POST',
+    url: app.forum.attribute('adminUrl') + '/config',
+    data: {config}
+  }).catch(error => {
+    app.config = oldConfig;
+    throw error;
+  });
+}
