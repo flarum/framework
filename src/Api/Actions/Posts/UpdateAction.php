eee34598f128597c6dee475911bb299acb963c0f
Franz Liedke <franz@develophp.org>
2015-08-04 13:15:56 +0200

    Include editUser relationship when editing posts.
    Closes flarum/core#214. Hopefully. :)

--- src/Api/Actions/Posts/UpdateAction.php
+++ src/Api/Actions/Posts/UpdateAction.php
@@ -21,7 +21,9 @@ class UpdateAction extends SerializeResourceAction
     /**
      * @inheritdoc
      */
-    public $include = [];
+    public $include = [
+        'editUser' => true,
+    ];
 
     /**
      * @inheritdoc
