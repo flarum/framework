3b9278fa25c44fea2b7753241bbff632ac4439c3
Toby Zerner <toby.zerner@gmail.com>
2015-08-04 17:14:54 +0930

    Tweak some text/comments

--- framework/core/js/admin/src/components/AddExtensionModal.js
+++ framework/core/js/admin/src/components/AddExtensionModal.js
@@ -12,7 +12,7 @@ export default class AddExtensionModal extends Modal {
   content() {
     return (
       <div className="Modal-body">
-        <p>One day, this dialog will allow you to add an extension to your forum with ease. We're building an ecosystem as we speak!</p>
+        <p>One day in the not-too-distant future, this dialog will allow you to add an extension to your forum with ease. We're building an ecosystem as we speak!</p>
         <p>In the meantime, if you manage to get your hands on a new extension, simply drop it in your forum's <code>extensions</code> directory.</p>
         <p>If you're a developer, you can <a href="">read the docs</a> and have a go at building your own.</p>
       </div>
