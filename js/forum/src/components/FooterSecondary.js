f49d0e53419eb22b73a04f861f1a891d2cbd8355
Toby Zerner <toby.zerner@gmail.com>
2015-08-05 09:50:57 +0930

    Allow locale to be selected in footer

--- js/forum/src/components/FooterSecondary.js
+++ js/forum/src/components/FooterSecondary.js
@@ -1,4 +1,6 @@
 import Component from 'flarum/Component';
+import SelectDropdown from 'flarum/components/SelectDropdown';
+import Button from 'flarum/components/Button';
 import ItemList from 'flarum/utils/ItemList';
 import listItems from 'flarum/helpers/listItems';
 
@@ -23,6 +25,31 @@ export default class FooterSecondary extends Component {
    */
   items() {
     const items = new ItemList();
+
+    if (Object.keys(app.locales).length > 1) {
+      const locales = [];
+
+      for (const locale in app.locales) {
+        locales.push(Button.component({
+          active: app.locale === locale,
+          children: app.locales[locale],
+          icon: app.locale === locale ? 'check' : true,
+          onclick: () => {
+            if (app.session.user) {
+              app.session.user.savePreferences({locale}).then(() => window.location.reload());
+            } else {
+              document.cookie = `locale=${locale}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT`;
+              window.location.reload();
+            }
+          }
+        }));
+      }
+
+      items.add('locale', SelectDropdown.component({
+        children: locales,
+        buttonClassName: 'Button Button--text'
+      }));
+    }
 
     items.add('poweredBy', (
       <a href="http://flarum.org?r=forum" target="_blank">
