357833beb023b27bbf8cacfd2249120ff27195d7
Toby Zerner <toby.zerner@gmail.com>
2015-08-13 12:51:15 +0930

    Use new listen API

--- packages/ext-subscriptions/src/Extension.php
+++ packages/ext-subscriptions/src/Extension.php
@@ -5,14 +5,17 @@ use Illuminate\Events\Dispatcher;
 
 class Extension extends BaseExtension
 {
-    public function boot(Dispatcher $events)
+    public function listen(Dispatcher $events)
     {
-        $this->loadViewsFrom(__DIR__.'/../views', 'subscriptions');
-
         $events->subscribe('Flarum\Subscriptions\Listeners\AddClientAssets');
         $events->subscribe('Flarum\Subscriptions\Listeners\AddApiAttributes');
         $events->subscribe('Flarum\Subscriptions\Listeners\PersistSubscriptionData');
         $events->subscribe('Flarum\Subscriptions\Listeners\NotifyNewPosts');
         $events->subscribe('Flarum\Subscriptions\Listeners\HideIgnoredDiscussions');
+    }
+
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../views', 'subscriptions');
     }
 }
