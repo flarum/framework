ceea8dcee9216882c238c6e33a831354af028b5c
Toby Zerner <toby.zerner@gmail.com>
2015-08-13 12:50:12 +0930

    Fix error in post mention emails

--- packages/ext-mentions/views/emails/postMentioned.blade.php
+++ packages/ext-mentions/views/emails/postMentioned.blade.php
@@ -1,6 +1,6 @@
 Hey {{ $user->username }}!
 
-{{ $blueprint->sender->username }} replied to your post (#{{ $blueprint->post->number }}) in {{ $blueprint->post->discussion->title }}.
+{{ $blueprint->reply->user->username }} replied to your post (#{{ $blueprint->post->number }}) in {{ $blueprint->post->discussion->title }}.
 
 {{ \Flarum\Core::config('base_url') }}/d/{{ $blueprint->reply->discussion_id }}/-/{{ $blueprint->reply->number }}
 
