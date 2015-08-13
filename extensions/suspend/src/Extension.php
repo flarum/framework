ea5dead830b2ebb485bc66ead2405aa826c9384c
Toby Zerner <toby.zerner@gmail.com>
2015-08-13 12:51:24 +0930

    Use new listen API

--- extensions/suspend/src/Extension.php
+++ extensions/suspend/src/Extension.php
@@ -5,25 +5,10 @@ use Illuminate\Events\Dispatcher;
 
 class Extension extends BaseExtension
 {
-    /**
-     * Bootstrap the application events.
-     *
-     * @return void
-     */
-    public function boot(Dispatcher $events)
+    public function listen(Dispatcher $events)
     {
         $events->subscribe('Flarum\Suspend\Listeners\AddClientAssets');
         $events->subscribe('Flarum\Suspend\Listeners\AddApiAttributes');
         $events->subscribe('Flarum\Suspend\Listeners\PersistData');
-    }
-
-    /**
-     * Register the service provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
     }
 }
