92e902afc184680f891597c4827f482c68663cd8
Toby Zerner <toby.zerner@gmail.com>
2015-08-13 12:52:43 +0930

    Add permission setting for editing tags

--- packages/ext-tags/js/admin/src/main.js
+++ packages/ext-tags/js/admin/src/main.js
@@ -1,10 +1,12 @@
 import Tag from 'tags/models/Tag';
 import addTagsPermissionScope from 'tags/addTagsPermissionScope';
+import addTagPermission from 'tags/addTagPermission';
 import addTagsPane from 'tags/addTagsPane';
 
 app.initializers.add('tags', app => {
   app.store.models.tags = Tag;
 
   addTagsPermissionScope();
+  addTagPermission();
   addTagsPane();
 });
