7483b28afac2fd0a35b7a39500e4d8dcfb9cc253
Franz Liedke <franz@develophp.org>
2015-08-27 00:49:24 +0200

    Use new method for retrieving base URL

--- packages/ext-mentions/views/emails/postMentioned.blade.php
+++ packages/ext-mentions/views/emails/postMentioned.blade.php
@@ -2,7 +2,7 @@ Hey {{ $user->username }}!
 
 {{ $blueprint->reply->user->username }} replied to your post (#{{ $blueprint->post->number }}) in {{ $blueprint->post->discussion->title }}.
 
-{{ \Flarum\Core::config('base_url') }}/d/{{ $blueprint->reply->discussion_id }}/{{ $blueprint->reply->number }}
+{{ \Flarum\Core::url() }}/d/{{ $blueprint->reply->discussion_id }}/{{ $blueprint->reply->number }}
 
 ---
 
