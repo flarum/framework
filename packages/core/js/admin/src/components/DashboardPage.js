96a9234c2e17fc2696f4549845b0e87a4bad3610
Toby Zerner <toby.zerner@gmail.com>
2015-08-27 10:59:50 +0930

    Reorganise dashboard help text

--- packages/core/js/admin/src/components/DashboardPage.js
+++ packages/core/js/admin/src/components/DashboardPage.js
@@ -6,9 +6,14 @@ export default class DashboardPage extends Component {
       <div className="DashboardPage">
         <div className="container">
           <h2>Welcome to Flarum Beta</h2>
-          <p>Thanks for trying out Flarum! You are running version <strong>{app.forum.attribute('version')}</strong>.</p>
-          <p>This is beta software, and should not be used in production. If you run into any trouble, please read the <a href="http://flarum.org/docs/troubleshooting" target="_blank">Troubleshooting docs</a>. You can get further help on the <a href="http://discuss.flarum.org" target="_blank">support forum</a> and report bugs on <a href="https://github.com/flarum/core/issues" target="_blank">GitHub</a>.</p>
-          <p>If you'd like to contribute, check out the <a href="http://flarum.org/docs/contributing" target="_blank">Contributing page</a>.</p>
+          <p>Thanks for trying out Flarum! You are running version <strong>{app.forum.attribute('version')}</strong>. This is beta software, and should not be used in production.</p>
+          <ul>
+            <li>Having problems? Read the <a href="http://flarum.org/docs/troubleshooting" target="_blank">Troubleshooting docs</a>.</li>
+            <li>Found a bug? Please <a href="https://github.com/flarum/core/issues" target="_blank">report it on GitHub</a>.</li>
+            <li>Got some feedback? Let us know what you think on the <a href="http://discuss.flarum.org" target="_blank">Support Forum</a>.</li>
+            <li>Want to contribute? Read the <a href="http://flarum.org/docs/contributing" target="_blank">Contributing docs</a>.</li>
+            <li>Interested in developing extensions? Read the <a href="http://flarum.org/docs/extend" target="_blank">Extension docs</a>.</li>
+          </ul>
         </div>
       </div>
     );
