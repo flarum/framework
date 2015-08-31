5e2245801417c1d2524365d0233f4150528e69d1
Toby Zerner <toby.zerner@gmail.com>
2015-08-31 14:25:16 +0930

    Installer: Prevent crash when views directory is not writable
    Use plain PHP templates instead of Blade templates so there is nothing
    that needs to be written.
    
    closes #376

--- src/Install/Actions/IndexAction.php
+++ src/Install/Actions/IndexAction.php
@@ -38,8 +38,6 @@ class IndexAction extends HtmlAction
     {
         $view = $this->view->make('flarum.install::app');
 
-        $view->logo = $this->view->make('flarum.install::logo');
-
         $errors = [];
 
         if (version_compare(PHP_VERSION, '5.5.0', '<')) {
