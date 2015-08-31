2f72ff3bc1a865bb85e032df7d8dc846ffc49b85
Toby Zerner <toby.zerner@gmail.com>
2015-08-31 12:05:33 +0930

    Extract base Page class

--- framework/core/js/forum/src/components/NotificationsPage.js
+++ framework/core/js/forum/src/components/NotificationsPage.js
@@ -1,21 +1,20 @@
-import Component from 'flarum/Component';
+import Page from 'flarum/components/Page';
 import NotificationList from 'flarum/components/NotificationList';
 
 /**
  * The `NotificationsPage` component shows the notifications list. It is only
  * used on mobile devices where the notifications dropdown is within the drawer.
  */
-export default class NotificationsPage extends Component {
+export default class NotificationsPage extends Page {
   constructor(...args) {
     super(...args);
 
-    app.current = this;
     app.history.push('notifications');
-    app.drawer.hide();
-    app.modal.close();
 
     this.list = new NotificationList();
     this.list.load();
+
+    this.bodyClass = 'App--notifications';
   }
 
   view() {
