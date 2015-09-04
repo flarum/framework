3ab62fb0ca07ed8634ca178bcfc05a72c48fa7b6
Toby Zerner <toby.zerner@gmail.com>
2015-09-04 12:30:25 +0930

    Add LICENSE information

--- extensions/sticky/src/Gambits/StickyGambit.php
+++ extensions/sticky/src/Gambits/StickyGambit.php
@@ -1,4 +1,14 @@
-<?php namespace Flarum\Sticky\Gambits;
+<?php 
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Sticky\Gambits;
 
 use Flarum\Core\Search\Search;
 use Flarum\Core\Search\RegexGambit;
