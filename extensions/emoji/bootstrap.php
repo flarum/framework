9a342be49558caffb315779dc9bcc5eb594020b2
Toby Zerner <toby.zerner@gmail.com>
2015-09-04 12:32:52 +0930

    Add LICENSE information

--- extensions/emoji/bootstrap.php
+++ extensions/emoji/bootstrap.php
@@ -1,10 +1,14 @@
 <?php
 
-// Require the extension's composer autoload file. This will enable all of our
-// classes in the src directory to be autoloaded.
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require __DIR__.'/vendor/autoload.php';
 
-// Return the name of our Extension class. Flarum will register it as a service
-// provider, allowing it to register bindings and execute code when the
-// application boots.
 return 'Flarum\Emoji\Extension';
