8798f073fe3f6f84f84805e84804f1eb526beac1
Toby Zerner <toby.zerner@gmail.com>
2015-09-04 11:34:54 +0930

    Add copyright

--- packages/ext-tags/src/Commands/CreateTagHandler.php
+++ packages/ext-tags/src/Commands/CreateTagHandler.php
@@ -1,4 +1,14 @@
-<?php namespace Flarum\Tags\Commands;
+<?php 
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Tags\Commands;
 
 use Flarum\Tags\Tag;
 use Flarum\Core\Forum;
--- packages/ext-tags/src/Listeners/UpdateTagMetadata.php
+++ packages/ext-tags/src/Listeners/UpdateTagMetadata.php
@@ -1,4 +1,14 @@
-<?php namespace Flarum\Tags\Listeners;
+<?php 
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Tags\Listeners;
 
 use Flarum\Tags\Tag;
 use Flarum\Tags\Events\DiscussionWasTagged;
