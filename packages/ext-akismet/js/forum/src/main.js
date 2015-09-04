60e246bbcb7c7badb96b9849e992a02b6c72daa9
Toby Zerner <toby.zerner@gmail.com>
2015-09-04 12:44:02 +0930

    Initial commit

---
+++ packages/ext-akismet/js/forum/src/main.js
@@ -0,0 +1,24 @@
+import { extend } from 'flarum/extend';
+import app from 'flarum/app';
+
+import Button from 'flarum/components/Button';
+import CommentPost from 'flarum/components/CommentPost';
+import PostControls from 'flarum/utils/PostControls';
+
+app.initializers.add('akismet', () => {
+  extend(CommentPost.prototype, 'reportActionItems', function(items) {
+    if (this.props.post.reports()[0].reporter() === 'Akismet') {
+      items.add('notSpam',
+        <Button className="Button"
+          icon="check"
+          onclick={() => {
+            this.dismissReport({akismet: 'ham'}).then(
+              () => PostControls.restoreAction.apply(this.props.post)
+            );
+          }}>
+          Not Spam
+        </Button>
+      );
+    }
+  });
+}, -10); // set initializer priority to run after reports
