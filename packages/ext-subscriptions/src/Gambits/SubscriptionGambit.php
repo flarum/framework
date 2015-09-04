29c0607660e3ec03576976d5130a08cdfe7e724c
Toby Zerner <toby.zerner@gmail.com>
2015-09-04 12:30:01 +0930

    Add LICENSE information

--- packages/ext-subscriptions/src/Gambits/SubscriptionGambit.php
+++ packages/ext-subscriptions/src/Gambits/SubscriptionGambit.php
@@ -1,4 +1,14 @@
-<?php namespace Flarum\Subscriptions\Gambits;
+<?php 
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Subscriptions\Gambits;
 
 use Flarum\Core\Search\Search;
 use Flarum\Core\Search\RegexGambit;
