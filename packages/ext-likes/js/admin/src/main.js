2fa2a1ee2002ea6683d7f370899a9fd3ac386ab3
Toby Zerner <toby.zerner@gmail.com>
2015-09-22 18:08:49 +0930

    Add permission icon

--- packages/ext-likes/js/admin/src/main.js
+++ packages/ext-likes/js/admin/src/main.js
@@ -5,6 +5,7 @@ import PermissionGrid from 'flarum/components/PermissionGrid';
 app.initializers.add('likes', () => {
   extend(PermissionGrid.prototype, 'replyItems', items => {
     items.add('likePosts', {
+      icon: 'thumbs-o-up',
       label: 'Like posts',
       permission: 'discussion.likePosts'
     });
