d610ea663f3ad257090af47d44b3f8152a194d7d
Toby Zerner <toby.zerner@gmail.com>
2015-09-22 17:05:14 +0930

    Keep post actions visible when controls dropdown is open
    Also show without hover on touch devices

--- js/forum/src/initializers/boot.js
+++ js/forum/src/initializers/boot.js
@@ -77,7 +77,11 @@ export default function boot(app) {
 
   // Initialize FastClick, which makes links and buttons much more responsive on
   // touch devices.
-  $(() => FastClick.attach(document.body));
+  $(() => {
+    FastClick.attach(document.body)
+
+    $('body').addClass('ontouchstart' in window ? 'touch' : 'no-touch');
+  });
 
   app.booted = true;
 }
