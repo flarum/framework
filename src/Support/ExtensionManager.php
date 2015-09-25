02e40f7c47aa36130ff745538fb3031867d8daa1
Toby Zerner <toby.zerner@gmail.com>
2015-09-25 16:01:34 +0930

    Allow extensions to return a callback instead of a provider name
    This is useful for very simple extensions like language packs, because it means no Composer/namespacing and thus bootstrap.php doesn't have to be changed at all.

--- src/Support/ExtensionManager.php
+++ src/Support/ExtensionManager.php
@@ -53,8 +53,6 @@ class ExtensionManager
 
             $enabled[] = $extension;
 
-            $this->load($extension);
-
             $this->migrate($extension);
 
             $this->setEnabled($enabled);
@@ -75,8 +73,6 @@ class ExtensionManager
     public function uninstall($extension)
     {
         $this->disable($extension);
-
-        $this->load($extension);
 
         $this->migrateDown($extension);
     }
@@ -123,15 +119,6 @@ class ExtensionManager
     public function isEnabled($extension)
     {
         return in_array($extension, $this->getEnabled());
-    }
-
-    protected function load($extension)
-    {
-        if (file_exists($file = $this->getExtensionsDir() . '/' . $extension . '/bootstrap.php')) {
-            $className = require $file;
-
-            $ext = new $className($this->app);
-        }
     }
 
     protected function getExtensionsDir()
