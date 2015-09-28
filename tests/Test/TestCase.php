7706714ad902c7fa9a22d930c0a6d25add4fb32c
kirkbushell <torm3nt@gmail.com>
2015-09-28 15:09:13 +0100

    Removed phpsec as the testing library, added phpunit and converted the first spec test to phpunit format. Also added mockery.

---
+++ tests/Test/TestCase.php
@@ -0,0 +1,20 @@
+<?php
+namespace tests\Test;
+
+use Mockery;
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        Mockery::close();
+
+        $this->init();
+    }
+
+    protected function init()
+    {
+        // To be overloaded by children - saves having to do setUp/mockery::close every time
+    }
+}
