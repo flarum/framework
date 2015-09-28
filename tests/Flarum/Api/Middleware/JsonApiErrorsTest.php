bfc62554d4a98ede985379de01ad309e3de2164e
kirkbushell <torm3nt@gmail.com>
2015-09-28 16:43:38 +0100

    Added tests for the validation exception handling.

--- tests/Flarum/Api/Middleware/JsonApiErrorsTest.php
+++ tests/Flarum/Api/Middleware/JsonApiErrorsTest.php
@@ -2,6 +2,8 @@
 namespace tests\Flarum\Api\Middleware;
 
 use Flarum\Api\Middleware\JsonApiErrors;
+use Illuminate\Contracts\Support\MessageProvider;
+use Illuminate\Contracts\Validation\ValidationException;
 use tests\Test\Stubs\ApiException;
 use tests\Test\TestCase;
 
@@ -20,5 +22,17 @@ class JsonApiErrorsTest extends TestCase
 
         $this->assertEquals(599, $response->getStatusCode());
         $this->assertEquals('{"errors":["error1","error2"]}', (string) $response->getBody());
+    }
+    
+    public function test_it_should_handle_validation_exceptions()
+    {
+        $messageProvider = m::mock(MessageProvider::class);
+        $messageProvider->shouldReceive('getMessageBag')->once()->andReturn(collect(['field' => ['message1', 'message2']]));
+
+        $exception = new ValidationException($messageProvider);
+
+        $response = $this->middleware->handle($exception);
+
+        $this->assertEquals(400, $response->getStatusCode());
     }
 }
