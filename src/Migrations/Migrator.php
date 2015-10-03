8c7cdb184f13a73cddfa764704aa65e921d509e7
Toby Zerner <toby.zerner@gmail.com>
2015-10-03 17:38:23 +0930

    Fix installation

--- src/Migrations/Migrator.php
+++ src/Migrations/Migrator.php
@@ -246,7 +246,7 @@ class Migrator
     {
         $file = implode('_', array_slice(explode('_', $file), 4));
 
-        $class = ($extension ? str_replace('-', '\\', $extension) : 'Core') . '\\Migrations\\';
+        $class = ($extension ? str_replace('-', '\\', $extension) : 'Flarum\\Core') . '\\Migrations\\';
 
         $class .= Str::studly($file);
 
