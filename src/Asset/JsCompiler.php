18def302d679180876cfa6a98ea8d043faf2912d
Toby Zerner <toby.zerner@gmail.com>
2015-10-09 00:33:53 +1030

    Bundle unminified JS; minify via ClosureCompilerService when in production
    Falls back to a less effective minification library if ClosureCompilerService errors or is unavailable. Minification takes a while (20 seconds or so), but it only happens when assets are modified. Still, this means enabling/disabling extensions is taking far too long. Possible solutions:
    
    - Don't minify initially; set a process running in the background to do minification, and server unminified assets in the meantime.
    - Refactor compiler to send each JS file to CCS individually, only if that particular file has been modified.
    
    flarum/gulp has also been updated to no longer support uglification.
    
    closes #582

--- src/Asset/JsCompiler.php
+++ src/Asset/JsCompiler.php
@@ -10,15 +10,89 @@
 
 namespace Flarum\Asset;
 
-use Flarum\Asset\RevisionCompiler;
+use Exception;
+use MatthiasMullie\Minify;
+use s9e\TextFormatter\Configurator\JavaScript\Minifiers\ClosureCompilerService;
 
 class JsCompiler extends RevisionCompiler
 {
+    /**
+     * @var bool
+     */
+    protected $minify;
+
+    /**
+     * @param string $path
+     * @param string $filename
+     * @param bool $minify
+     */
+    public function __construct($path, $filename, $minify = false)
+    {
+        parent::__construct($path, $filename);
+
+        $this->minify = $minify;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function format($string)
     {
         return $string.";\n";
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function compile()
+    {
+        $output = parent::compile();
+
+        if ($this->minify) {
+            set_time_limit(60);
+
+            try {
+                $output = $this->minifyWithClosureCompilerService($output);
+            } catch (Exception $e) {
+                $output = $this->minifyWithFallback($output);
+            }
+        }
+
+        return $output;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getCacheDifferentiator()
+    {
+        return $this->minify;
+    }
+
+    /**
+     * @param string $source
+     * @return string
+     */
+    protected function minifyWithClosureCompilerService($source)
+    {
+        $minifier = new ClosureCompilerService;
+
+        $minifier->compilationLevel = 'SIMPLE_OPTIMIZATIONS';
+        $minifier->timeout = 60;
+
+        $output = $minifier->minify($source);
+
+        return $output;
+    }
+
+    /**
+     * @param string $source
+     * @return string
+     */
+    protected function minifyWithFallback($source)
+    {
+        $minifier = new Minify\JS($source);
+
+        return $minifier->minify();
     }
 }
