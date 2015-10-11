1aa24af95bceb9ef97cda9c1fa0fc3f76af6fb58
Toby Zerner <toby.zerner@gmail.com>
2015-10-11 13:01:53 +1030

    Rename initializer

--- packages/ext-sticky/js/admin/src/main.js
+++ packages/ext-sticky/js/admin/src/main.js
@@ -2,7 +2,7 @@ import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 import PermissionGrid from 'flarum/components/PermissionGrid';
 
-app.initializers.add('sticky', () => {
+app.initializers.add('flarum-sticky', () => {
   extend(PermissionGrid.prototype, 'moderateItems', items => {
     items.add('sticky', {
       icon: 'thumb-tack',
