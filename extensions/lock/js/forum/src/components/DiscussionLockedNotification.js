ae7baa1e7f3770411e4c1c9588f24b9d5cfcc213
Toby Zerner <toby.zerner@gmail.com>
2015-10-21 10:28:59 +1030

    Change `app.trans` calls to `app.translator.trans` calls

--- extensions/lock/js/forum/src/components/DiscussionLockedNotification.js
+++ extensions/lock/js/forum/src/components/DiscussionLockedNotification.js
@@ -12,6 +12,6 @@ export default class DiscussionLockedNotification extends Notification {
   }
 
   content() {
-    return app.trans('flarum-lock.forum.discussion_locked_notification', {user: this.props.notification.sender()});
+    return app.translator.trans('flarum-lock.forum.discussion_locked_notification', {user: this.props.notification.sender()});
   }
 }
