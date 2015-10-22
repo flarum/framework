8f8cfb5718ea1c2652779db3999008d484a962c2
Toby Zerner <toby.zerner@gmail.com>
2015-10-22 12:28:17 +1030

    Improve embed architecture
    - Fix composer preview button not working
    - Fix inappropriate alert message when reply is submitted

--- packages/ext-embed/js/forum/src/components/DiscussionPage.js
+++ packages/ext-embed/js/forum/src/components/DiscussionPage.js
@@ -1,27 +1,11 @@
-import Component from 'flarum/Component';
+import BaseDiscussionPage from 'flarum/components/DiscussionPage';
 import PostStream from 'flarum/components/PostStream';
 
-export default class DiscussionPage extends Component {
+export default class DiscussionPage extends BaseDiscussionPage {
   init() {
     super.init();
 
-    /**
-     * The discussion that is being viewed.
-     *
-     * @type {Discussion}
-     */
-    const discussion = this.discussion = app.preloadedDocument();
-
-    let includedPosts = [];
-    if (discussion.payload && discussion.payload.included) {
-      includedPosts = discussion.payload.included
-        .filter(record => record.type === 'posts' && record.relationships && record.relationships.discussion)
-        .map(record => app.store.getById('posts', record.id))
-        .sort((a, b) => a.id() - b.id())
-        .slice(0, 20);
-    }
-
-    this.stream = new PostStream({discussion, includedPosts});
+    this.bodyClass = null;
   }
 
   view() {
@@ -30,7 +14,7 @@ export default class DiscussionPage extends Component {
         <div class="container">
           <div className="DiscussionPage-discussion">
             <div className="DiscussionPage-stream">
-              {this.stream.render()}
+              {this.stream ? this.stream.render() : ''}
             </div>
           </div>
         </div>
