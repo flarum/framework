2c94819144c0dd685c2de3dde694be523b0f1f21
Toby Zerner <toby.zerner@gmail.com>
2015-10-31 18:19:45 +1030

    Reduce length of slug field to prevent error when creating unique index

--- extensions/tags/migrations/2015_10_19_061223_make_slug_unique.php
+++ extensions/tags/migrations/2015_10_19_061223_make_slug_unique.php
@@ -10,6 +10,7 @@ class MakeSlugUnique extends AbstractMigration
     public function up()
     {
         $this->schema->table('tags', function (Blueprint $table) {
+            $table->string('slug', 100)->change();
             $table->unique('slug');
         });
     }
@@ -17,6 +18,7 @@ class MakeSlugUnique extends AbstractMigration
     public function down()
     {
         $this->schema->table('tags', function (Blueprint $table) {
+            $table->string('slug', 255)->change();
             $table->dropUnique('tags_slug_unique');
         });
     }
