25e8c53af36752919d4c07c76fddcc8cebf735b0
Toby Zerner <toby.zerner@gmail.com>
2015-11-01 17:13:48 +1030

    Temporary fix for email URLs

--- extensions/mentions/views/emails/userMentioned.blade.php
+++ extensions/mentions/views/emails/userMentioned.blade.php
@@ -2,7 +2,7 @@ Hey {{ $user->username }}!
 
 {{ $blueprint->post->user->username }} mentioned you in a post in {{ $blueprint->post->discussion->title }}.
 
-{{ \Flarum\Core::url() }}/d/{{ $blueprint->post->discussion_id }}/{{ $blueprint->post->number }}
+{{ app()->url() }}/d/{{ $blueprint->post->discussion_id }}/{{ $blueprint->post->number }}
 
 ---
 
