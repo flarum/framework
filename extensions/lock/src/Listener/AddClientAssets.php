b7b8f4f90e3d22974c2d1742f1fe4f7070601f60
Toby Zerner <toby.zerner@gmail.com>
2015-11-02 18:52:33 +1030

    Translations are now automatically included

--- extensions/lock/src/Listener/AddClientAssets.php
+++ extensions/lock/src/Listener/AddClientAssets.php
@@ -34,7 +34,6 @@ class AddClientAssets
                 __DIR__.'/../../less/forum/extension.less'
             ]);
             $event->addBootstrapper('flarum/lock/main');
-            $event->addTranslations('flarum-lock.forum');
         }
 
         if ($event->isAdmin()) {
@@ -42,7 +41,6 @@ class AddClientAssets
                 __DIR__.'/../../js/admin/dist/extension.js'
             ]);
             $event->addBootstrapper('flarum/lock/main');
-            $event->addTranslations('flarum-lock.admin');
         }
     }
 }
