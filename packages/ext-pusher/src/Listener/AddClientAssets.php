1972286301d1f4586897fe3ad93333264db7c615
Toby Zerner <toby.zerner@gmail.com>
2015-11-02 18:52:47 +1030

    Translations are now automatically included

--- packages/ext-pusher/src/Listener/AddClientAssets.php
+++ packages/ext-pusher/src/Listener/AddClientAssets.php
@@ -34,7 +34,6 @@ class AddClientAssets
                 __DIR__.'/../../less/forum/extension.less'
             ]);
             $event->addBootstrapper('flarum/pusher/main');
-            $event->addTranslations('flarum-pusher.forum');
         }
 
         if ($event->isAdmin()) {
@@ -42,7 +41,6 @@ class AddClientAssets
                 __DIR__ . '/../../js/admin/dist/extension.js'
             ]);
             $event->addBootstrapper('flarum/pusher/main');
-            $event->addTranslations('flarum-pusher.admin');
         }
     }
 }
