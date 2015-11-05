b2cba12982a8974a44161921d75e1c50e0bc84a5
Toby Zerner <toby.zerner@gmail.com>
2015-11-05 16:41:01 +1030

    Release 0.1.0-beta.4

--- packages/ext-flags/js/admin/dist/extension.js
+++ packages/ext-flags/js/admin/dist/extension.js
@@ -1,7 +1,56 @@
-System.register('flarum/flags/main', ['flarum/extend', 'flarum/app', 'flarum/components/PermissionGrid'], function (_export) {
+System.register('flarum/flags/components/FlagsSettingsModal', ['flarum/components/SettingsModal'], function (_export) {
   'use strict';
 
-  var extend, app, PermissionGrid;
+  var SettingsModal, FlagsSettingsModal;
+  return {
+    setters: [function (_flarumComponentsSettingsModal) {
+      SettingsModal = _flarumComponentsSettingsModal['default'];
+    }],
+    execute: function () {
+      FlagsSettingsModal = (function (_SettingsModal) {
+        babelHelpers.inherits(FlagsSettingsModal, _SettingsModal);
+
+        function FlagsSettingsModal() {
+          babelHelpers.classCallCheck(this, FlagsSettingsModal);
+          babelHelpers.get(Object.getPrototypeOf(FlagsSettingsModal.prototype), 'constructor', this).apply(this, arguments);
+        }
+
+        babelHelpers.createClass(FlagsSettingsModal, [{
+          key: 'className',
+          value: function className() {
+            return 'FlagsSettingsModal Modal--small';
+          }
+        }, {
+          key: 'title',
+          value: function title() {
+            return app.translator.trans('flarum-flags.admin.settings.title');
+          }
+        }, {
+          key: 'form',
+          value: function form() {
+            return [m(
+              'div',
+              { className: 'Form-group' },
+              m(
+                'label',
+                null,
+                app.translator.trans('flarum-flags.admin.settings.guidelines_url_label')
+              ),
+              m('input', { className: 'FormControl', bidi: this.setting('flarum-flags.guidelines_url') })
+            )];
+          }
+        }]);
+        return FlagsSettingsModal;
+      })(SettingsModal);
+
+      _export('default', FlagsSettingsModal);
+    }
+  };
+});;
+System.register('flarum/flags/main', ['flarum/extend', 'flarum/app', 'flarum/components/PermissionGrid', 'flarum/flags/components/FlagsSettingsModal'], function (_export) {
+  'use strict';
+
+  var extend, app, PermissionGrid, FlagsSettingsModal;
   return {
     setters: [function (_flarumExtend) {
       extend = _flarumExtend.extend;
@@ -9,10 +58,16 @@ System.register('flarum/flags/main', ['flarum/extend', 'flarum/app', 'flarum/com
       app = _flarumApp['default'];
     }, function (_flarumComponentsPermissionGrid) {
       PermissionGrid = _flarumComponentsPermissionGrid['default'];
+    }, function (_flarumFlagsComponentsFlagsSettingsModal) {
+      FlagsSettingsModal = _flarumFlagsComponentsFlagsSettingsModal['default'];
     }],
     execute: function () {
 
       app.initializers.add('flarum-flags', function () {
+        app.extensionSettings['flarum-flags'] = function () {
+          return app.modal.show(new FlagsSettingsModal());
+        };
+
         extend(PermissionGrid.prototype, 'moderateItems', function (items) {
           items.add('viewFlags', {
             icon: 'flag',
