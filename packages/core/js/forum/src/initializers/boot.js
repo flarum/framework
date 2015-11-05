ee55a46ee19ab820cc13f87fe2f0664b0fc256dc
Toby Zerner <toby.zerner@gmail.com>
2015-11-05 16:24:04 +1030

    Revert back button behaviour
    As of 6ddab58, the back button was no longer shown if the user came in directly to a discussion. This caused problems on mobile where it was kind of hard to get back home without the button.

--- packages/core/js/forum/src/initializers/boot.js
+++ packages/core/js/forum/src/initializers/boot.js
@@ -4,7 +4,7 @@ import ScrollListener from 'flarum/utils/ScrollListener';
 import Pane from 'flarum/utils/Pane';
 import Drawer from 'flarum/utils/Drawer';
 import mapRoutes from 'flarum/utils/mapRoutes';
-
+import icon from 'flarum/helpers/icon';
 import Navigation from 'flarum/components/Navigation';
 import HeaderPrimary from 'flarum/components/HeaderPrimary';
 import HeaderSecondary from 'flarum/components/HeaderSecondary';
@@ -31,6 +31,7 @@ export default function boot(app) {
   }
 
   app.routes[defaultAction].path = '/';
+  app.history.push(defaultAction, icon('bars'), '/');
 
   m.startComputation();
 
