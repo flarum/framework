6d895e6d77bcc1f92b0b29c7af50368302c79e74
Franz Liedke <franz@develophp.org>
2015-11-11 19:30:35 +0100

    Inject hardcoded prerequisite parameters
    This affects version numbers, extensions and paths, which might be
    skeleton-specific. This commit moves those hardcoded values out of
    the classes and instead injects them through the constructor. This
    way, all prerequisites can be configured in the service provider.

--- src/Install/Prerequisite/PhpVersion.php
+++ src/Install/Prerequisite/PhpVersion.php
@@ -12,12 +12,19 @@ namespace Flarum\Install\Prerequisite;
 
 class PhpVersion extends AbstractPrerequisite
 {
+    protected $minVersion;
+
+    public function __construct($minVersion)
+    {
+        $this->minVersion = $minVersion;
+    }
+
     public function check()
     {
         if (version_compare(PHP_VERSION, '5.5.0', '<')) {
             $this->errors[] = [
-                'message' => 'PHP 5.5+ is required.',
-                'detail' => 'You are running version '.PHP_VERSION.'. Talk to your hosting provider about upgrading to the latest PHP version.'
+                'message' => "PHP $this->minVersion is required.",
+                'detail' => 'You are running version '.PHP_VERSION.'. Talk to your hosting provider about upgrading to the latest PHP version.',
             ];
         }
     }
