flarum/framework

commit e5a7013c2caaf25864cb0bd6640696c6ef9cf19f

Key item lists to maintain identity across redraws
Fixes #667. This issue was due to the fact that Mithril would change the "Lock" badge into a "Sticky" badge, but the tooltip initialization would not be triggered because it was using the same element. By maintaining element identity, the "Lock" badge will remain untouched, and a new element for the "Sticky" badge will be inserted before it. See https://lhorie.github.io/mithril/mithril.html#dealing-with-focus for more information.

js/lib/helpers/listItems.js
@@ -41,7 +41,8 @@ export default function listItems(items) {
             (item.itemName ? 'item-' + item.itemName : ''),
             className,
             (active ? 'active' : '')
-          ])}>
+          ])}
+            key={item.itemName}>
             {item}
           </li>,
       ' '

js/lib/components/Badge.js
@@ -24,11 +24,6 @@ export default class Badge extends Component {
     attrs.className = 'Badge ' + (type ? 'Badge--' + type : '') + ' ' + (attrs.className || '');
     attrs.title = extract(attrs, 'label') || '';
 
-    // Give the badge a unique key so that when badges are displayed together,
-    // and then one is added/removed, Mithril will correctly redraw the series
-    // of badges.
-    attrs.key = attrs.type;
-
     return (
       <span {...attrs}>
         {iconName ? icon(iconName, {className: 'Badge-icon'}) : m.trust('&nbsp;')}

commit 3cec7e8b46b76bc5f35824dc054832b101724369

Patch Mithril bug causing redraws to fail
Turns out there's a little more to the regression in e5a7013. First, we need to give the spaces in between list items a key too. Second, there's a bug in the latest Mithril code where using string keys can break the diffing algorithm. I've patched it manually in our dist JS files for now, and reported the issue: https://github.com/lhorie/mithril.js/issues/934

js/admin/dist/app.js
@@ -1051,11 +1051,9 @@ var m = (function app(window, undefined) {
 
 	function checkView(data, view, cached, cachedControllers, controllers, views) {
 		var controller = getController(cached.views, view, cachedControllers, data.controller);
-		//Faster to coerce to number and check for NaN
-		var key = +(data && data.attrs && data.attrs.key);
 		data = pendingRequests === 0 || forcing || cachedControllers && cachedControllers.indexOf(controller) > -1 ? data.view(controller) : {tag: "placeholder"};
 		if (data.subtree === "retain") return data;
-		if (key === key) (data.attrs = data.attrs || {}).key = key;
+		(data.attrs = data.attrs || {}).key = key;
 		updateLists(views, controllers, view, controller);
 		return data;
 	}
@@ -20671,15 +20669,19 @@ System.register('flarum/helpers/listItems', ['flarum/components/Separator', 'fla
       var className = item.props ? item.props.itemClassName : item.itemClassName;
 
       if (isListItem) {
-        item.props.key = item.itemName;
+        item.attrs = item.attrs || {};
+        item.attrs.key = item.attrs.key || item.itemName;
       }
+
+      var space = new String(' ');
+      space.attrs = { key: '_space_' + item.itemName };
 
       return [isListItem ? item : m(
         'li',
         { className: classList([item.itemName ? 'item-' + item.itemName : '', className, active ? 'active' : '']),
           key: item.itemName },
         item
-      ), ' '];
+      ), space];
     });
   }
 

js/forum/dist/app.js
@@ -1051,11 +1051,9 @@ var m = (function app(window, undefined) {
 
 	function checkView(data, view, cached, cachedControllers, controllers, views) {
 		var controller = getController(cached.views, view, cachedControllers, data.controller);
-		//Faster to coerce to number and check for NaN
-		var key = +(data && data.attrs && data.attrs.key);
 		data = pendingRequests === 0 || forcing || cachedControllers && cachedControllers.indexOf(controller) > -1 ? data.view(controller) : {tag: "placeholder"};
 		if (data.subtree === "retain") return data;
-		if (key === key) (data.attrs = data.attrs || {}).key = key;
+		(data.attrs = data.attrs || {}).key = key;
 		updateLists(views, controllers, view, controller);
 		return data;
 	}
@@ -29793,15 +29791,19 @@ System.register('flarum/helpers/listItems', ['flarum/components/Separator', 'fla
       var className = item.props ? item.props.itemClassName : item.itemClassName;
 
       if (isListItem) {
-        item.props.key = item.itemName;
+        item.attrs = item.attrs || {};
+        item.attrs.key = item.attrs.key || item.itemName;
       }
+
+      var space = new String(' ');
+      space.attrs = { key: '_space_' + item.itemName };
 
       return [isListItem ? item : m(
         'li',
         { className: classList([item.itemName ? 'item-' + item.itemName : '', className, active ? 'active' : '']),
           key: item.itemName },
         item
-      ), ' '];
+      ), space];
     });
   }
 

js/lib/helpers/listItems.js
@@ -35,8 +35,12 @@ export default function listItems(items) {
     const className = item.props ? item.props.itemClassName : item.itemClassName;
 
     if (isListItem) {
-      item.props.key = item.itemName;
+      item.attrs = item.attrs || {};
+      item.attrs.key = item.attrs.key || item.itemName;
     }
+
+    const space = new String(' ');
+    space.attrs = {key: '_space_'+item.itemName};
 
     return [
       isListItem
@@ -49,7 +53,7 @@ export default function listItems(items) {
             key={item.itemName}>
             {item}
           </li>,
-      ' '
+      space
     ];
   });
 }