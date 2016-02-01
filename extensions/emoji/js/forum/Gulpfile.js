bd7a72911bb4445569360d496e612e8dd9f548ba
Sajjad Hasehmian <wolaws@gmail.com>
2016-02-01 23:17:56 +0330

    Add Emoji auto suggestion menu fixes flarum/core#397
    Clean Up

--- extensions/emoji/js/forum/Gulpfile.js
+++ extensions/emoji/js/forum/Gulpfile.js
@@ -3,5 +3,8 @@ var gulp = require('flarum-gulp');
 gulp({
   modules: {
     'flarum/emoji': 'src/**/*.js'
-  }
+  },
+  files: [
+    'bower_components/textarea-caret-position/index.js'
+  ]
 });
--- extensions/emoji/js/forum/src/main.js
+++ extensions/emoji/js/forum/src/main.js
@@ -1,7 +1,11 @@
-/*global twemoji, s9e*/
-
 import { override } from 'flarum/extend';
 import app from 'flarum/app';
 import Post from 'flarum/models/Post';
 
-app.initializers.add('flarum-emoji', () => {});
+import addComposerAutocomplete from 'flarum/emoji/addComposerAutocomplete';
+
+app.initializers.add('flarum-emoji', () => {
+  // After typing ':' in the composer, show a dropdown suggesting a bunch of
+  // emoji that the user could use.
+  addComposerAutocomplete();
+});
