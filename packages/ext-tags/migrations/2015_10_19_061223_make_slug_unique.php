9c3a2885a2932d6471f5df748132244e271e7623
Franz Liedke <franz@develophp.org>
2016-02-24 23:31:04 +0900

    Add another missing copyright header

--- packages/ext-tags/migrations/2015_10_19_061223_make_slug_unique.php
+++ packages/ext-tags/migrations/2015_10_19_061223_make_slug_unique.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
