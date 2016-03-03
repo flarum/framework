af76eff9493410908393fddfaac48a7bbf6c53ff
Franz Liedke <franz@develophp.org>
2016-03-04 01:11:39 +0900

    Use new shortcuts for migrations

--- extensions/likes/migrations/2015_05_11_000000_create_posts_likes_table.php
+++ extensions/likes/migrations/2015_05_11_000000_create_posts_likes_table.php
@@ -8,19 +8,14 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
 
-return [
-    'up' => function (Builder $schema) {
-        $schema->create('posts_likes', function (Blueprint $table) {
-            $table->integer('post_id')->unsigned();
-            $table->integer('user_id')->unsigned();
-            $table->primary(['post_id', 'user_id']);
-        });
-    },
-
-    'down' => function (Builder $schema) {
-        $schema->drop('posts_likes');
+return Migration::createTable(
+    'posts_likes',
+    function (Blueprint $table) {
+        $table->integer('post_id')->unsigned();
+        $table->integer('user_id')->unsigned();
+        $table->primary(['post_id', 'user_id']);
     }
-];
+);
