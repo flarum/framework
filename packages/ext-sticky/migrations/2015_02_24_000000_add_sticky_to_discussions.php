9d88c3f56c2b5e4fe6d76e76b3798ef20d74a880
Franz Liedke <franz@develophp.org>
2016-03-04 01:12:30 +0900

    Use new shortcuts for migrations

--- packages/ext-sticky/migrations/2015_02_24_000000_add_sticky_to_discussions.php
+++ packages/ext-sticky/migrations/2015_02_24_000000_add_sticky_to_discussions.php
@@ -8,19 +8,8 @@
  * file that was distributed with this source code.
  */
 
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
+use Flarum\Database\Migration;
 
-return [
-    'up' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) {
-            $table->boolean('is_sticky')->default(0);
-        });
-    },
-
-    'down' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) {
-            $table->dropColumn('is_sticky');
-        });
-    }
-];
+return Migration::addColumns('discussions', [
+    'is_sticky' => ['boolean', 'default' => 0]
+]);
