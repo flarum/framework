5a7b57df96ed5d25f6227264391d597a908d4925
Daniel Klabbers <daniel+hyn@klabbers.email>
2016-03-03 11:00:11 +0100

    adding new tests to cover api handlers, part 1 of #245 and #74

---
+++ tests/Flarum/Api/Handler/TokenMismatchExceptionHandlerTest.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Flarum\Api\Handler;
+
+use Exception;
+use Flarum\Api\Handler\TokenMismatchExceptionHandler;
+use Flarum\Http\Exception\TokenMismatchException;
+use Tests\Test\TestCase;
+
+class TokenMismatchExceptionHandlerTest extends TestCase
+{
+    private $handler;
+
+    public function init()
+    {
+        $this->handler = new TokenMismatchExceptionHandler;
+    }
+
+    public function test_it_handles_recognisable_exceptions()
+    {
+        $this->assertFalse($this->handler->manages(new Exception));
+        $this->assertTrue($this->handler->manages(new TokenMismatchException()));
+    }
+
+    public function test_managing_exceptions()
+    {
+        $response = $this->handler->handle(new TokenMismatchException);
+
+        $this->assertEquals(400, $response->getStatus());
+        $this->assertEquals([
+            [
+                'status' => '400',
+                'code' => 'csrf_token_mismatch'
+            ]
+        ], $response->getErrors());
+    }
+}
