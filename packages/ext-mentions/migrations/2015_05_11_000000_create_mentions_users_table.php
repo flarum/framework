46f854b8ef04d39f9553e223195c7660bca260f2
Franz Liedke <franz@develophp.org>
2016-03-04 01:12:03 +0900

    Use new shortcuts for migrations

--- packages/ext-mentions/migrations/2015_05_11_000000_create_mentions_users_table.php
+++ packages/ext-mentions/migrations/2015_05_11_000000_create_mentions_users_table.php
@@ -8,19 +8,14 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Schema\Builder;
 
-return [
-    'up' => function (Builder $schema) {
-        $schema->create('mentions_users', function (Blueprint $table) {
-            $table->integer('post_id')->unsigned();
-            $table->integer('mentions_id')->unsigned();
-            $table->primary(['post_id', 'mentions_id']);
-        });
-    },
-
-    'down' => function (Builder $schema) {
-        $schema->drop('mentions_users');
+return Migration::createTable(
+    'mentions_users',
+    function (Blueprint $table) {
+        $table->integer('post_id')->unsigned();
+        $table->integer('mentions_id')->unsigned();
+        $table->primary(['post_id', 'mentions_id']);
     }
-];
+);
