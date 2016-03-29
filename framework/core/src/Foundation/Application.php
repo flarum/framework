f925bd127491434cca91cd9011184dd10282a362
Toby Zerner <toby.zerner@gmail.com>
2016-03-29 18:02:12 +1030

    v0.1.0-beta.5

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -24,7 +24,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '0.1.0-beta.4';
+    const VERSION = '0.1.0-beta.5';
 
     /**
      * The base path for the Flarum installation.
