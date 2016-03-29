f6e2a7a7e567e2c859f90d5b5ab2bc83ff32be8c
Franz Liedke <franz@develophp.org>
2016-03-29 22:39:26 +0900

    Use GroupBadge component since we already have that

--- framework/core/js/admin/src/components/EditGroupModal.js
+++ framework/core/js/admin/src/components/EditGroupModal.js
@@ -1,6 +1,6 @@
 import Modal from 'flarum/components/Modal';
 import Button from 'flarum/components/Button';
-import Badge from 'flarum/components/Badge';
+import GroupBadge from 'flarum/components/GroupBadge';
 import Group from 'flarum/models/Group';
 
 /**
@@ -23,9 +23,10 @@ export default class EditGroupModal extends Modal {
 
   title() {
     return [
-      this.color() || this.icon() ? Badge.component({
-        icon: this.icon(),
-        style: {backgroundColor: this.color()}
+      this.color() || this.icon() ? GroupBadge.component({
+        group: this.group,
+        className: 'Group-icon',
+        label: null
       }) : '',
       ' ',
       this.namePlural() || app.translator.trans('core.admin.edit_group.title')
