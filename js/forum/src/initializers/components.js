cd16adfa69b91f0d725b0d90f27a5306cdd76df7
Franz Liedke <franz@develophp.org>
2016-04-14 23:22:33 +0900

    Remove unused imports

--- js/forum/src/initializers/components.js
+++ js/forum/src/initializers/components.js
@@ -1,7 +1,5 @@
 import CommentPost from 'flarum/components/CommentPost';
 import DiscussionRenamedPost from 'flarum/components/DiscussionRenamedPost';
-import PostedActivity from 'flarum/components/PostedActivity';
-import JoinedActivity from 'flarum/components/JoinedActivity';
 import DiscussionRenamedNotification from 'flarum/components/DiscussionRenamedNotification';
 
 /**
