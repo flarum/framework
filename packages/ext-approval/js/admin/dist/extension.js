4cdb94ef0675de7c9c2911133a825b52af36e0e4
Toby Zerner <toby.zerner@gmail.com>
2016-06-03 10:58:39 +0930

    Recompile dist JS using latest version of babel

--- packages/ext-approval/js/admin/dist/extension.js
+++ packages/ext-approval/js/admin/dist/extension.js
@@ -1,6 +1,8 @@
 'use strict';
 
 System.register('flarum/approval/main', ['flarum/extend', 'flarum/app', 'flarum/components/PermissionGrid'], function (_export, _context) {
+  "use strict";
+
   var extend, app, PermissionGrid;
   return {
     setters: [function (_flarumExtend) {
