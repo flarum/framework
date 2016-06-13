bf422ae76779593703456f96afc756db61686d2e
Davis (produes.co) <connor0095@gmail.com>
2016-06-13 17:12:20 -0400

    Add Web App Compatible Meta Tags

--- views/app.blade.php
+++ views/app.blade.php
@@ -5,7 +5,10 @@
     <title>{{ $title }}</title>
     <meta name="description" content="{{ $description }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="theme-color" content="{{ array_get($forum, 'attributes.themePrimaryColor') }}">
+    <meta name="apple-mobile-web-app-status-bar-style" content="default">
 
     @foreach ($cssUrls as $url)
       <link rel="stylesheet" href="{{ $url }}">
