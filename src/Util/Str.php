888b7a3db2a177bfdfc905c14387af68d2c1c87e
Juno_okyo <J2TeaM@users.noreply.github.com>
2016-09-24 14:36:58 +0700

    Update slug function

--- src/Util/Str.php
+++ src/Util/Str.php
@@ -23,6 +23,13 @@ class Str
     public static function slug($str)
     {
         $str = strtolower($str);
+        $str = preg_replace('/á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/i', 'a', $str);
+        $str = preg_replace('/đ/i', 'd', $str);
+        $str = preg_replace('/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/i', 'e', $str);
+        $str = preg_replace('/í|ì|ỉ|ĩ|ị/i', 'i', $str);
+        $str = preg_replace('/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/i', 'o', $str);
+        $str = preg_replace('/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/i', 'u', $str);
+        $str = preg_replace('/ý|ỳ|ỷ|ỹ|ỵ/i', 'y', $str);
         $str = preg_replace('/[^a-z0-9]/i', '-', $str);
         $str = preg_replace('/-+/', '-', $str);
         $str = preg_replace('/-$|^-/', '', $str);
