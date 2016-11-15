974b81646c2cf9ad21c4baf90d739cca6f63166a
liji <25699440@qq.com>
2016-11-15 10:05:28 +0800

    add AuthorIdGambit to support search discussion by user id

---
+++ src/Core/Search/Discussion/Gambit/AuthorIdGambit.php
@@ -0,0 +1,39 @@
+<?php
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Core\Search\Discussion\Gambit;
+
+use Flarum\Core\Repository\UserRepository;
+use Flarum\Core\Search\AbstractRegexGambit;
+use Flarum\Core\Search\AbstractSearch;
+use Flarum\Core\Search\Discussion\DiscussionSearch;
+use LogicException;
+
+class AuthorIdGambit extends AbstractRegexGambit
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $pattern = 'authorid:(([1-9]\d*,?)*)';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function conditions(AbstractSearch $search, array $matches, $negate)
+    {
+        if (! $search instanceof DiscussionSearch) {
+            throw new LogicException('This gambit can only be applied on a DiscussionSearch');
+        }
+
+        $ids = explode(',', $matches[1]);
+
+        $search->getQuery()->whereIn('start_user_id', $ids, 'and', $negate);
+    }
+}
