2bbb877f8726d67bdd1bf20c7f5701d33768f9fc
Zeokat <zeokat@gmail.com>
2017-04-25 03:48:56 +0200

    Update index.blade.php

--- views/index.blade.php
+++ views/index.blade.php
@@ -2,7 +2,7 @@
 $url = app('Flarum\Forum\UrlGenerator');
 ?>
 <div class="container">
-    <h2>{{ $translator->trans('core.views.index.all_discussions_heading') }}</h2>
+    <h3>{{ $translator->trans('core.views.index.all_discussions_heading') }}</h3>
 
     <ul>
         @foreach ($document->data as $discussion)
