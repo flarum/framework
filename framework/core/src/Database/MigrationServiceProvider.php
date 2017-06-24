925ef227a3adc6a5b6d0207d0fa8f96e524d16ea
Franz Liedke <franz@develophp.org>
2017-06-24 11:34:29 +0200

    Restructure Flarum\Database namespace

---
+++ framework/core/src/Database/MigrationServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Database;
+
+use Flarum\Foundation\AbstractServiceProvider;
+use Flarum\Foundation\Application;
+
+class MigrationServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        $this->app->singleton('Flarum\Database\MigrationRepositoryInterface', function ($app) {
+            return new DatabaseMigrationRepository($app['db'], 'migrations');
+        });
+
+        $this->app->bind(MigrationCreator::class, function (Application $app) {
+            return new MigrationCreator($app->make('Illuminate\Filesystem\Filesystem'), $app->basePath());
+        });
+    }
+}
