6268c3010f8d35489f9369db5ec5b26cb56eb69b
Franz Liedke <franz@develophp.org>
2017-06-24 11:40:45 +0200

    Fix serializer class names

--- src/Api/Serializer/BasicDiscussionSerializer.php
+++ src/Api/Serializer/BasicDiscussionSerializer.php
@@ -46,7 +46,7 @@ class BasicDiscussionSerializer extends AbstractSerializer
      */
     protected function startUser($discussion)
     {
-        return $this->hasOne($discussion, 'Flarum\Api\Serializer\UserBasicSerializer');
+        return $this->hasOne($discussion, 'Flarum\Api\Serializer\BasicUserSerializer');
     }
 
     /**
@@ -54,7 +54,7 @@ class BasicDiscussionSerializer extends AbstractSerializer
      */
     protected function startPost($discussion)
     {
-        return $this->hasOne($discussion, 'Flarum\Api\Serializer\PostBasicSerializer');
+        return $this->hasOne($discussion, 'Flarum\Api\Serializer\BasicPostSerializer');
     }
 
     /**
@@ -62,7 +62,7 @@ class BasicDiscussionSerializer extends AbstractSerializer
      */
     protected function lastUser($discussion)
     {
-        return $this->hasOne($discussion, 'Flarum\Api\Serializer\UserBasicSerializer');
+        return $this->hasOne($discussion, 'Flarum\Api\Serializer\BasicUserSerializer');
     }
 
     /**
@@ -70,7 +70,7 @@ class BasicDiscussionSerializer extends AbstractSerializer
      */
     protected function lastPost($discussion)
     {
-        return $this->hasOne($discussion, 'Flarum\Api\Serializer\PostBasicSerializer');
+        return $this->hasOne($discussion, 'Flarum\Api\Serializer\BasicPostSerializer');
     }
 
     /**
@@ -86,6 +86,6 @@ class BasicDiscussionSerializer extends AbstractSerializer
      */
     protected function relevantPosts($discussion)
     {
-        return $this->hasMany($discussion, 'Flarum\Api\Serializer\PostBasicSerializer');
+        return $this->hasMany($discussion, 'Flarum\Api\Serializer\BasicPostSerializer');
     }
 }
