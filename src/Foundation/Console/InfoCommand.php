5ae2e9d232471449facbdc6bd3c287e3e2509adb
Franz Liedke <franz@develophp.org>
2017-06-24 11:36:24 +0200

    Get rid of Flarum\Debug namespace

--- src/Foundation/Console/InfoCommand.php
+++ src/Foundation/Console/InfoCommand.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace Flarum\Debug\Console;
+namespace Flarum\Foundation\Console;
 
 use Flarum\Console\AbstractCommand;
 use Flarum\Extension\ExtensionManager;
