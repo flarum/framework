86496f3e06824d16cc5bc9c52040f1d1cd21cf8a
Clark Winkelmann <clark.winkelmann@gmail.com>
2017-06-29 00:56:15 +0200

    Keep images and links when quoting (#25)
    * Keep images and links when quoting
    
    * Fix bower dependencies

--- extensions/mentions/js/forum/src/utils/selectedText.js
+++ extensions/mentions/js/forum/src/utils/selectedText.js
@@ -5,9 +5,22 @@ export default function selectedText(body) {
     const parent = range.commonAncestorContainer;
     if (body[0] === parent || $.contains(body[0], parent)) {
       const clone = $("<div>").append(range.cloneContents());
+
+      // Replace emoji images with their shortcode (found in alt attribute)
       clone.find('img.emoji').replaceWith(function() {
         return this.alt;
       });
+
+      // Replace all other images with a Markdown image
+      clone.find('img').replaceWith(function() {
+        return '![](' + this.src + ')';
+      });
+
+      // Replace all links with a Markdown link
+      clone.find('a').replaceWith(function() {
+        return '[' + this.innerText + '](' + this.href + ')';
+      });
+
       return clone.text();
     }
   }
