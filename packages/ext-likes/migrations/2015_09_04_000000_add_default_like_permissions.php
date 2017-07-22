21f68fa0b6047278c40d05b46209e3ae409a37a7
Toby Zerner <toby.zerner@gmail.com>
2017-07-22 12:19:29 +0930

    Use new migration helper

--- packages/ext-likes/migrations/2015_09_04_000000_add_default_like_permissions.php
+++ packages/ext-likes/migrations/2015_09_04_000000_add_default_like_permissions.php
@@ -9,23 +9,9 @@
  * file that was distributed with this source code.
  */
 
-use Illuminate\Database\ConnectionInterface;
+use Flarum\Core\Group;
+use Flarum\Database\Migration;
 
-$permissionAttributes = [
-    'group_id' => 3, // Default group ID of members
-    'permission' => 'discussion.likePosts',
-];
-
-return [
-    'up' => function (ConnectionInterface $db) use ($permissionAttributes) {
-        $instance = $db->table('permissions')->where($permissionAttributes)->first();
-
-        if (is_null($instance)) {
-            $db->table('permissions')->insert($permissionAttributes);
-        }
-    },
-
-    'down' => function (ConnectionInterface $db) use ($permissionAttributes) {
-        $db->table('permissions')->where($permissionAttributes)->delete();
-    }
-];
+return Migration::addPermissions([
+    'discussion.likePosts' => Group::MEMBER_ID
+]);
