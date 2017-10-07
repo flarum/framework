ed381cfcbafe166cf8c77d27376b788259856a30
Toby Zerner <toby.zerner@gmail.com>
2017-10-08 08:59:54 +1030

    Fix extractText breaking in some cases

--- packages/core/js/lib/utils/extractText.js
+++ packages/core/js/lib/utils/extractText.js
@@ -7,7 +7,7 @@
 export default function extractText(vdom) {
   if (vdom instanceof Array) {
     return vdom.map(element => extractText(element)).join('');
-  } else if (typeof vdom === 'object') {
+  } else if (typeof vdom === 'object' && vdom !== null) {
     return extractText(vdom.children);
   } else {
     return vdom;
