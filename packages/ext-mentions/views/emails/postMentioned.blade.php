1c86e2148e57649e37cd78667cc18cfcb28fe21d
Toby Zerner <toby.zerner@gmail.com>
2017-11-05 15:29:46 +1030

    Don't sanitize values in plain-text emails

--- packages/ext-mentions/views/emails/postMentioned.blade.php
+++ packages/ext-mentions/views/emails/postMentioned.blade.php
@@ -1,9 +1,9 @@
-Hey {{ $user->username }}!
+Hey {!! $user->username !!}!
 
-{{ $blueprint->reply->user->username }} replied to your post (#{{ $blueprint->post->number }}) in {{ $blueprint->post->discussion->title }}.
+{!! $blueprint->reply->user->username !!} replied to your post (#{!! $blueprint->post->number !!}) in {!! $blueprint->post->discussion->title !!}.
 
-{{ app()->url() }}/d/{{ $blueprint->reply->discussion_id }}/{{ $blueprint->reply->number }}
+{!! app()->url() !!}/d/{!! $blueprint->reply->discussion_id !!}/{!! $blueprint->reply->number !!}
 
 ---
 
-{{ strip_tags($blueprint->reply->contentHtml) }}
+{!! strip_tags($blueprint->reply->contentHtml) !!}
