5b46ec801d246afcbc4d1f32895e613fbdf308bf
Toby Zerner <toby.zerner@gmail.com>
2017-11-29 22:20:06 +1030

    Generate URL in the controller instead of the view

--- views/log-out.blade.php
+++ views/log-out.blade.php
@@ -1,13 +1,12 @@
 @extends('flarum.forum::layouts.basic')
-@inject('url', 'Flarum\Forum\UrlGenerator')
 
 @section('title', $translator->trans('core.views.log_out.title'))
 
 @section('content')
-  <p>{{ $translator->trans('core.views.log_out.log_out_confirmation', ['{forum}' => $forumTitle]) }}</p>
+  <p>{{ $translator->trans('core.views.log_out.log_out_confirmation', ['{forum}' => $settings->get('forum_title')]) }}</p>
 
   <p>
-    <a href="{{ $url->toRoute('logout') }}?token={{ $csrfToken }}@if ($return)&return={{ urlencode($return) }}@endif" class="button">
+    <a href="{{ $url }}" class="button">
       {{ $translator->trans('core.views.log_out.log_out_button') }}
     </a>
   </p>
