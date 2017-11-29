d592322a61fce840074ceafca77c96818a695e88
Toby Zerner <toby.zerner@gmail.com>
2017-11-29 17:19:20 +1030

    Basic implementation of dashboard widgets, statistics
    Currently not user-customizable. Just needed to display statistics for a
    client, so figured I'd make a start at this. Nothing too fancy for now,
    but I'm sure some people will be happy to have this information at their
    fingertips.

--- framework/core/js/admin/src/components/DashboardPage.js
+++ framework/core/js/admin/src/components/DashboardPage.js
@@ -1,20 +1,14 @@
 import Page from 'flarum/components/Page';
+import StatusWidget from 'flarum/components/StatusWidget';
+import StatisticsWidget from 'flarum/components/StatisticsWidget';
 
 export default class DashboardPage extends Page {
   view() {
     return (
       <div className="DashboardPage">
         <div className="container">
-          <h2>{app.translator.trans('core.admin.dashboard.welcome_text')}</h2>
-          <p>{app.translator.trans('core.admin.dashboard.version_text', {version: <strong>{app.forum.attribute('version')}</strong>})}</p>
-          <p>{app.translator.trans('core.admin.dashboard.beta_warning_text', {strong: <strong/>})}</p>
-          <ul>
-            <li>{app.translator.trans('core.admin.dashboard.contributing_text', {a: <a href="http://flarum.org/docs/contributing" target="_blank"/>})}</li>
-            <li>{app.translator.trans('core.admin.dashboard.troubleshooting_text', {a: <a href="http://flarum.org/docs/troubleshooting" target="_blank"/>})}</li>
-            <li>{app.translator.trans('core.admin.dashboard.support_text', {a: <a href="http://discuss.flarum.org/t/support" target="_blank"/>})}</li>
-            <li>{app.translator.trans('core.admin.dashboard.features_text', {a: <a href="http://discuss.flarum.org/t/features" target="_blank"/>})}</li>
-            <li>{app.translator.trans('core.admin.dashboard.extension_text', {a: <a href="http://flarum.org/docs/extend" target="_blank"/>})}</li>
-          </ul>
+          <StatusWidget/>
+          <StatisticsWidget/>
         </div>
       </div>
     );
