0c1e90719cbfe3aef36c5ff47fe66594edf088b6
Toby Zerner <toby.zerner@gmail.com>
2017-12-10 21:03:48 +1030

    Extract admin dashboard statistics from core into an extension

--- framework/core/js/admin/src/components/DashboardPage.js
+++ framework/core/js/admin/src/components/DashboardPage.js
@@ -1,16 +1,18 @@
 import Page from 'flarum/components/Page';
 import StatusWidget from 'flarum/components/StatusWidget';
-import StatisticsWidget from 'flarum/components/StatisticsWidget';
 
 export default class DashboardPage extends Page {
   view() {
     return (
       <div className="DashboardPage">
         <div className="container">
-          <StatusWidget/>
-          <StatisticsWidget/>
+          {this.availableWidgets()}
         </div>
       </div>
     );
+  }
+
+  availableWidgets() {
+    return [<StatusWidget/>];
   }
 }
