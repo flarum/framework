74a9c62136477f4583a6daefba8cf9a1054585c1
Daniel Klabbers <daniel@productip.com>
2017-12-15 08:15:08 +0100

    and another..

--- packages/core/src/User/Listener/SelfDemotionGuard.php
+++ packages/core/src/User/Listener/SelfDemotionGuard.php
@@ -11,9 +11,9 @@
 
 namespace Flarum\User\Listener;
 
+use Flarum\Group\Group;
 use Flarum\User\Event\Saving;
 use Flarum\User\Exception\PermissionDeniedException;
-use Flarum\Group\Group;
 use Illuminate\Contracts\Events\Dispatcher;
 
 class SelfDemotionGuard
