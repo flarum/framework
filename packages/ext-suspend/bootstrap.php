398c4998a9f42619758b1c8a171a69e9f3ae9fbf
Franz Liedke <franz@develophp.org>
2018-01-16 23:20:02 +0100

    Adapt new bootstrap format, use Asset extender

--- packages/ext-suspend/bootstrap.php
+++ packages/ext-suspend/bootstrap.php
@@ -9,15 +9,23 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Extend;
 use Flarum\Suspend\Access;
 use Flarum\Suspend\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddUserSuspendAttributes::class);
-    $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
-    $events->subscribe(Listener\SaveSuspensionToDatabase::class);
+return [
+    (new Extend\Assets('forum'))
+        ->defaultAssets(__DIR__)
+        ->bootstrapper('flarum/suspend/main'),
+    (new Extend\Assets('admin'))
+        ->defaultAssets(__DIR__)
+        ->bootstrapper('flarum/suspend/main'),
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddUserSuspendAttributes::class);
+        $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
+        $events->subscribe(Listener\SaveSuspensionToDatabase::class);
 
-    $events->subscribe(Access\UserPolicy::class);
-};
+        $events->subscribe(Access\UserPolicy::class);
+    }
+];
