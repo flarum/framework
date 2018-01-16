918ca2201df9419d6af632f9f0090bc75207064d
Franz Liedke <franz@develophp.org>
2018-01-16 23:18:41 +0100

    Adapt new bootstrap format, use Asset extender

--- extensions/lock/bootstrap.php
+++ extensions/lock/bootstrap.php
@@ -9,17 +9,25 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Extend;
 use Flarum\Lock\Access;
 use Flarum\Lock\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddDiscussionLockedAttributes::class);
-    $events->subscribe(Listener\AddLockedGambit::class);
-    $events->subscribe(Listener\CreatePostWhenDiscussionIsLocked::class);
-    $events->subscribe(Listener\SaveLockedToDatabase::class);
+return [
+    (new Extend\Assets('forum'))
+        ->defaultAssets(__DIR__)
+        ->bootstrapper('flarum/lock/main'),
+    (new Extend\Assets('admin'))
+        ->asset(__DIR__.'/js/admin/dist/extension.js')
+        ->bootstrapper('flarum/lock/main'),
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddDiscussionLockedAttributes::class);
+        $events->subscribe(Listener\AddLockedGambit::class);
+        $events->subscribe(Listener\CreatePostWhenDiscussionIsLocked::class);
+        $events->subscribe(Listener\SaveLockedToDatabase::class);
 
-    $events->subscribe(Access\DiscussionPolicy::class);
-    $events->subscribe(Access\PostPolicy::class);
-};
+        $events->subscribe(Access\DiscussionPolicy::class);
+        $events->subscribe(Access\PostPolicy::class);
+    },
+];
