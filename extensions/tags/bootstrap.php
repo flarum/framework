dd9907a26b881a6cbe907a0f1c4a5bd6c8b5ac6a
Franz Liedke <franz@develophp.org>
2018-01-16 23:20:25 +0100

    Adapt new bootstrap format, use Asset extender

--- extensions/tags/bootstrap.php
+++ extensions/tags/bootstrap.php
@@ -9,23 +9,32 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Extend;
 use Flarum\Tags\Access;
 use Flarum\Tags\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddDiscussionTagsRelationship::class);
-    $events->subscribe(Listener\AddForumTagsRelationship::class);
-    $events->subscribe(Listener\AddTagsApi::class);
-    $events->subscribe(Listener\CreatePostWhenTagsAreChanged::class);
-    $events->subscribe(Listener\FilterDiscussionListByTags::class);
-    $events->subscribe(Listener\FilterPostsQueryByTag::class);
-    $events->subscribe(Listener\SaveTagsToDatabase::class);
-    $events->subscribe(Listener\UpdateTagMetadata::class);
+return [
+    (new Extend\Assets('forum'))
+        ->defaultAssets(__DIR__)
+        ->bootstrapper('flarum/tags/main'),
+    (new Extend\Assets('admin'))
+        ->defaultAssets(__DIR__)
+        ->bootstrapper('flarum/tags/main'),
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddClientAssets::class);
+        $events->subscribe(Listener\AddDiscussionTagsRelationship::class);
+        $events->subscribe(Listener\AddForumTagsRelationship::class);
+        $events->subscribe(Listener\AddTagsApi::class);
+        $events->subscribe(Listener\CreatePostWhenTagsAreChanged::class);
+        $events->subscribe(Listener\FilterDiscussionListByTags::class);
+        $events->subscribe(Listener\FilterPostsQueryByTag::class);
+        $events->subscribe(Listener\SaveTagsToDatabase::class);
+        $events->subscribe(Listener\UpdateTagMetadata::class);
 
-    $events->subscribe(Access\GlobalPolicy::class);
-    $events->subscribe(Access\DiscussionPolicy::class);
-    $events->subscribe(Access\TagPolicy::class);
-    $events->subscribe(Access\FlagPolicy::class);
-};
+        $events->subscribe(Access\GlobalPolicy::class);
+        $events->subscribe(Access\DiscussionPolicy::class);
+        $events->subscribe(Access\TagPolicy::class);
+        $events->subscribe(Access\FlagPolicy::class);
+    },
+];
