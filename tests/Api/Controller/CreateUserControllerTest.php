96e67e469b0d201727902cfdb1db412e15e14ddc
Daniel Klabbers <daniel@productip.com>
2018-01-17 16:43:11 +0100

    now asserting properties of newly created user and whether an admin can create an activated user

--- tests/Api/Controller/CreateUserControllerTest.php
+++ tests/Api/Controller/CreateUserControllerTest.php
@@ -3,10 +3,18 @@
 namespace Flarum\Tests\Api\Controller;
 
 use Flarum\Api\Controller\CreateUserController;
+use Flarum\User\User;
+use Illuminate\Support\Arr;
 
 class CreateUserControllerTest extends AbstractTestController
 {
     protected $controller = CreateUserController::class;
+
+    protected $data = [
+        'username' => 'test',
+        'password' => 'too-obscure',
+        'email' => 'test@machine.local'
+    ];
 
     /**
      * @test
@@ -23,12 +31,42 @@ class CreateUserControllerTest extends AbstractTestController
      */
     public function can_create_user()
     {
-        $response = $this->callWith([
-            'username' => 'test',
-            'password' => 'too-obscure',
-            'email' => 'test@machine.local'
-        ]);
+        $response = $this->callWith($this->data);
 
         $this->assertEquals(201, $response->getStatusCode());
+
+        /** @var User $user */
+        $user = User::where('username', 'test')->firstOrFail();
+
+        $this->assertEquals(0, $user->is_activated);
+
+        foreach (Arr::except($this->data, 'password') as $property => $value) {
+            $this->assertEquals($value, $user->{$property});
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function admins_can_create_activated_users()
+    {
+        $this->actor = User::find(1);
+
+        $response = $this->callWith(array_merge($this->data, [
+            'isActivated' => 1
+        ]));
+
+        $this->assertEquals(201, $response->getStatusCode());
+
+        /** @var User $user */
+        $user = User::where('username', 'test')->firstOrFail();
+
+        $this->assertEquals(1, $user->is_activated);
+
+    }
+
+    public function tearDown()
+    {
+        User::where('username', $this->data['username'])->delete();
     }
 }
