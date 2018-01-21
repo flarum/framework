bb92a80585bac41bad1732e402a53457527b9025
Franz Liedke <franz@develophp.org>
2018-01-21 21:10:05 +0100

    Extender: List all assets explicitly

--- extensions/pusher/bootstrap.php
+++ extensions/pusher/bootstrap.php
@@ -15,7 +15,8 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Assets('forum'))
-        ->defaultAssets(__DIR__)
+        ->asset(__DIR__.'/js/forum/dist/extension.js')
+        ->asset(__DIR__.'/less/forum/extension.less')
         ->bootstrapper('flarum/pusher/main'),
     (new Extend\Assets('admin'))
         ->asset(__DIR__.'/js/admin/dist/extension.js')
