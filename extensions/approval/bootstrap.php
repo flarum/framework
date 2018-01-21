324140d2e626f23c4dde5bc546da8ed455218407
Franz Liedke <franz@develophp.org>
2018-01-21 21:07:58 +0100

    Extender: List all assets explicitly

--- extensions/approval/bootstrap.php
+++ extensions/approval/bootstrap.php
@@ -16,7 +16,8 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Assets('forum'))
-        ->defaultAssets(__DIR__)
+        ->asset(__DIR__.'/js/forum/dist/extension.js')
+        ->asset(__DIR__.'/less/forum/extension.less')
         ->bootstrapper('flarum/approval/main'),
     (new Extend\Assets('admin'))
         ->asset(__DIR__.'/js/admin/dist/extension.js')
