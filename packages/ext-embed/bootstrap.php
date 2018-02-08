3b1a4b59e243ea56b800322952104e4e4e380e26
Franz Liedke <franz@develophp.org>
2018-02-08 18:10:17 +0100

    Use new Routes extender

--- packages/ext-embed/bootstrap.php
+++ packages/ext-embed/bootstrap.php
@@ -9,13 +9,18 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Embed\EmbeddedDiscussionController;
 use Flarum\Embed\Listener;
+use Flarum\Extend;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\View\Factory;
 
-return function (Dispatcher $events, Factory $view) {
-    $events->subscribe(Listener\AddEmbedRoute::class);
-    $events->subscribe(Listener\FlushEmbedAssetsWhenSettingsAreChanged::class);
+return [
+    (new Extend\Routes('forum'))
+        ->get('/embed/{id:\d+(?:-[^/]*)?}[/{near:[^/]*}]', 'embed.discussion', EmbeddedDiscussionController::class),
+    function (Dispatcher $events, Factory $view) {
+        $events->subscribe(Listener\FlushEmbedAssetsWhenSettingsAreChanged::class);
 
-    $view->addNamespace('flarum-embed', __DIR__.'/views');
-};
+        $view->addNamespace('flarum-embed', __DIR__.'/views');
+    }
+];
