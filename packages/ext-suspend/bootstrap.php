0643389b0bbe1a1832a231e1b5138165f3886030
David Sevilla Martín <dsevilla192@icloud.com>
2018-02-12 08:34:36 -0500

    fix merge conflicts

--- packages/ext-suspend/bootstrap.php
+++ packages/ext-suspend/bootstrap.php
@@ -9,16 +9,26 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Extend;
 use Flarum\Suspend\Access;
 use Flarum\Suspend\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
-return function (Dispatcher $events) {
-    $events->subscribe(Listener\AddClientAssets::class);
-    $events->subscribe(Listener\AddUserSuspendAttributes::class);
-    $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
-    $events->subscribe(Listener\SaveSuspensionToDatabase::class);
-    $events->subscribe(Listener\SendNotificationWhenUserIsSuspended::class);
+return [
+    (new Extend\Assets('forum'))
+        ->asset(__DIR__.'/js/forum/dist/extension.js')
+        ->asset(__DIR__.'/less/forum/extension.less')
+        ->bootstrapper('flarum/suspend/main'),
+    (new Extend\Assets('admin'))
+        ->asset(__DIR__.'/js/admin/dist/extension.js')
+        ->asset(__DIR__.'/less/admin/extension.less')
+        ->bootstrapper('flarum/suspend/main'),
+    function (Dispatcher $events) {
+        $events->subscribe(Listener\AddUserSuspendAttributes::class);
+        $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
+        $events->subscribe(Listener\SaveSuspensionToDatabase::class);
+        $events->subscribe(Listener\SendNotificationWhenUserIsSuspended::class);
 
-    $events->subscribe(Access\UserPolicy::class);
-};
+        $events->subscribe(Access\UserPolicy::class);
+    }
+];
