a5de179d23a0e15c08e7c52d6a97569f329fdcf2
Toby Zerner <toby.zerner@gmail.com>
2018-02-24 16:36:20 +1030

    Update flarum-webpack-config

--- js/admin/webpack.config.js
+++ js/admin/webpack.config.js
@@ -2,7 +2,7 @@ const config = require('flarum-webpack-config');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 
-module.exports = merge(config({ compat: true }), {
+module.exports = merge(config(), {
   output: {
     library: 'flarum'
   },
