bf0a24a3fc73c7caf7915667f61572af5f45b71c
Toby Zerner <toby.zerner@gmail.com>
2018-02-24 15:45:01 +1030

    Extract common webpack config into a package

--- js/forum/webpack.config.js
+++ js/forum/webpack.config.js
@@ -1,37 +1,12 @@
-const path = require('path');
+const config = require('flarum-webpack-config');
 const webpack = require('webpack');
+const merge = require('webpack-merge');
 
-module.exports = {
+module.exports = merge(config({ compat: true }), {
   output: {
     library: 'flarum'
-  },
-  resolve: {
-    modules: [
-      path.resolve(__dirname, 'src'),
-      path.resolve(__dirname, '../lib'),
-      path.resolve(__dirname, 'node_modules')
-    ]
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              '@babel/plugin-transform-runtime',
-              ['@babel/plugin-transform-react-jsx', {pragma: 'm'}],
-              '@babel/plugin-proposal-class-properties'
-            ]
-          }
-        }
-      }
-    ]
   },
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
   ]
-};
+});
