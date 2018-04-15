fe07d4064be08b8e001f184f4b74692daf487da6
Franz Liedke <franz@develophp.org>
2018-04-16 01:31:00 +0200

    Split up Site into several classes
    Depending on the state of the Flarum installation (installed, not
    installed, currently upgrading, maintenance mode), we should enable
    different sets of service providers.
    
    For example, during installation we should not resolve a setting
    repository from the container. This new architecture lets us do so,
    but we can easily (and cleanly) register a different implementation
    during installation.
    
    This should prevent problems such as #1370 in the future.

---
+++ framework/core/src/Foundation/InstalledApp.php
@@ -0,0 +1,132 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Foundation;
+
+use Flarum\Database\Console\GenerateMigrationCommand;
+use Flarum\Database\Console\MigrateCommand;
+use Flarum\Database\Console\ResetCommand;
+use Flarum\Foundation\Console\CacheClearCommand;
+use Flarum\Foundation\Console\InfoCommand;
+use Flarum\Http\Middleware\DispatchRoute;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Stratigility\MiddlewarePipe;
+use function Zend\Stratigility\middleware;
+use function Zend\Stratigility\path;
+
+class InstalledApp implements AppInterface
+{
+    /**
+     * @var Application
+     */
+    protected $laravel;
+
+    /**
+     * @var array
+     */
+    protected $config;
+
+    public function __construct(Application $laravel, array $config)
+    {
+        $this->laravel = $laravel;
+        $this->config = $config;
+    }
+
+    /**
+     * @return \Psr\Http\Server\RequestHandlerInterface
+     */
+    public function getRequestHandler()
+    {
+        if ($this->inMaintenanceMode()) {
+            return $this->getMaintenanceMiddleware();
+        } elseif ($this->needsUpdate()) {
+            return $this->getUpdaterMiddleware();
+        }
+
+        $pipe = new MiddlewarePipe;
+
+        $pipe->pipe($this->subPath('api', 'flarum.api.middleware'));
+        $pipe->pipe($this->subPath('admin', 'flarum.admin.middleware'));
+        $pipe->pipe($this->subPath('', 'flarum.forum.middleware'));
+
+        return $pipe;
+    }
+
+    private function inMaintenanceMode(): bool
+    {
+        return $this->config['offline'] ?? false;
+    }
+
+    /**
+     * @return \Psr\Http\Server\RequestHandlerInterface
+     */
+    private function getMaintenanceMiddleware()
+    {
+        $pipe = new MiddlewarePipe;
+
+        $pipe->pipe(middleware(function () {
+            // FIXME: Fix path to 503.html
+            // TODO: FOR API render JSON-API error document for HTTP 503
+            return new HtmlResponse(
+                file_get_contents(__DIR__.'/../../503.html'), 503
+            );
+        }));
+
+        return $pipe;
+    }
+
+    private function needsUpdate(): bool
+    {
+        $settings = $this->laravel->make(SettingsRepositoryInterface::class);
+        $version = $settings->get('version');
+
+        return $version !== Application::VERSION;
+    }
+
+    /**
+     * @return \Psr\Http\Server\RequestHandlerInterface
+     */
+    public function getUpdaterMiddleware()
+    {
+        $pipe = new MiddlewarePipe;
+        $pipe->pipe(
+            $this->laravel->make(
+                DispatchRoute::class,
+                ['routes' => $this->laravel->make('flarum.update.routes')]
+            )
+        );
+
+        return $pipe;
+    }
+
+    private function subPath($pathName, $middlewareStack)
+    {
+        return path(
+            parse_url($this->laravel->url($pathName), PHP_URL_PATH) ?: '/',
+            $this->laravel->make($middlewareStack)
+        );
+    }
+
+    /**
+     * @return \Symfony\Component\Console\Command\Command[]
+     */
+    public function getConsoleCommands()
+    {
+        return [
+            $this->laravel->make(GenerateMigrationCommand::class),
+            $this->laravel->make(InfoCommand::class, ['config' => $this->config]),
+            $this->laravel->make(MigrateCommand::class),
+            $this->laravel->make(ResetCommand::class),
+            $this->laravel->make(CacheClearCommand::class),
+        ];
+    }
+}
