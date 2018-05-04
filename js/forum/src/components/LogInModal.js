393641757510b0b214fc0ab66e74163a51caffa6
Clark Winkelmann <clark.winkelmann@gmail.com>
2018-05-04 20:54:26 +0200

    Use ItemList for signup and login inputs

--- js/forum/src/components/LogInModal.js
+++ js/forum/src/components/LogInModal.js
@@ -4,6 +4,7 @@ import SignUpModal from 'flarum/components/SignUpModal';
 import Button from 'flarum/components/Button';
 import LogInButtons from 'flarum/components/LogInButtons';
 import extractText from 'flarum/utils/extractText';
+import ItemList from 'flarum/utils/ItemList';
 
 /**
  * The `LogInModal` component displays a modal dialog with a login form.
@@ -63,37 +64,45 @@ export default class LogInModal extends Modal {
       <LogInButtons/>,
 
       <div className="Form Form--centered">
-        <div className="Form-group">
-          <input className="FormControl" name="identification" type="text" placeholder={extractText(app.translator.trans('core.forum.log_in.username_or_email_placeholder'))}
-            bidi={this.identification}
-            disabled={this.loading} />
-        </div>
-
-        <div className="Form-group">
-          <input className="FormControl" name="password" type="password" placeholder={extractText(app.translator.trans('core.forum.log_in.password_placeholder'))}
-            bidi={this.password}
-            disabled={this.loading} />
-        </div>
-
-        <div className="Form-group">
-          <div>
-            <label className="checkbox">
-              <input type="checkbox" bidi={this.remember} disabled={this.loading} />
-              {app.translator.trans('core.forum.log_in.remember_me_label')}
-            </label>
-          </div>
-        </div>
-
-        <div className="Form-group">
-          {Button.component({
-            className: 'Button Button--primary Button--block',
-            type: 'submit',
-            loading: this.loading,
-            children: app.translator.trans('core.forum.log_in.submit_button')
-          })}
-        </div>
+        {this.fields().toArray()}
       </div>
     ];
+  }
+
+  fields() {
+    const items = new ItemList();
+
+    items.add('identification', <div className="Form-group">
+      <input className="FormControl" name="identification" type="text" placeholder={extractText(app.translator.trans('core.forum.log_in.username_or_email_placeholder'))}
+        bidi={this.identification}
+        disabled={this.loading} />
+    </div>, 30);
+
+    items.add('password', <div className="Form-group">
+      <input className="FormControl" name="password" type="password" placeholder={extractText(app.translator.trans('core.forum.log_in.password_placeholder'))}
+        bidi={this.password}
+        disabled={this.loading} />
+    </div>, 20);
+
+    items.add('remember', <div className="Form-group">
+      <div>
+        <label className="checkbox">
+          <input type="checkbox" bidi={this.remember} disabled={this.loading} />
+          {app.translator.trans('core.forum.log_in.remember_me_label')}
+        </label>
+      </div>
+    </div>, 10);
+
+    items.add('submit', <div className="Form-group">
+      {Button.component({
+        className: 'Button Button--primary Button--block',
+        type: 'submit',
+        loading: this.loading,
+        children: app.translator.trans('core.forum.log_in.submit_button')
+      })}
+    </div>, -10);
+
+    return items;
   }
 
   footer() {
