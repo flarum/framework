1b06769463c38b38b04133ccc8226b5ac1aa758f
Franz Liedke <franz@develophp.org>
2018-05-30 00:12:06 +0200

    Replace ControllerInterface with PSR-15 interface
    The custom interface already had the same signature as the
    one from the standard (except for the return type hint), so
    why not use that one now? :)

--- packages/core/src/Forum/Controller/LogOutController.php
+++ packages/core/src/Forum/Controller/LogOutController.php
@@ -12,7 +12,6 @@
 namespace Flarum\Forum\Controller;
 
 use Flarum\Foundation\Application;
-use Flarum\Http\Controller\ControllerInterface;
 use Flarum\Http\Exception\TokenMismatchException;
 use Flarum\Http\Rememberer;
 use Flarum\Http\SessionAuthenticator;
@@ -21,11 +20,13 @@ use Flarum\User\AssertPermissionTrait;
 use Flarum\User\Event\LoggedOut;
 use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Contracts\View\Factory;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\RedirectResponse;
 
-class LogOutController implements ControllerInterface
+class LogOutController implements RequestHandlerInterface
 {
     use AssertPermissionTrait;
 
@@ -85,10 +86,10 @@ class LogOutController implements ControllerInterface
 
     /**
      * @param Request $request
-     * @return \Psr\Http\Message\ResponseInterface
+     * @return ResponseInterface
      * @throws TokenMismatchException
      */
-    public function handle(Request $request)
+    public function handle(Request $request): ResponseInterface
     {
         $session = $request->getAttribute('session');
         $actor = $request->getAttribute('actor');
