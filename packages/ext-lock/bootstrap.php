6fdfe6cb6e9fe9eedc5cd24db8e4b011ec641aff
Toby Zerner <toby.zerner@gmail.com>
2018-06-20 13:35:19 +0930

    Webpack (#14)
    See https://github.com/flarum/core/pull/1367
    
    * Replace gulp with webpack and npm scripts for JS compilation
    * Set up Travis CI to commit compiled JS
    * Restructure `js` directory; only one instance of npm, forum/admin are "submodules"
    * Restructure `less` directory

--- packages/ext-lock/bootstrap.php
+++ packages/ext-lock/bootstrap.php
@@ -16,12 +16,12 @@ use Illuminate\Contracts\Events\Dispatcher;
 
 return [
     (new Extend\Assets('forum'))
-        ->asset(__DIR__.'/js/forum/dist/extension.js')
-        ->asset(__DIR__.'/less/forum/extension.less')
-        ->bootstrapper('flarum/lock/main'),
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->asset(__DIR__.'/less/forum.less'),
+
     (new Extend\Assets('admin'))
-        ->asset(__DIR__.'/js/admin/dist/extension.js')
-        ->bootstrapper('flarum/lock/main'),
+        ->js(__DIR__.'/js/dist/admin.js'),
+
     function (Dispatcher $events) {
         $events->subscribe(Listener\AddDiscussionLockedAttributes::class);
         $events->subscribe(Listener\AddLockedGambit::class);
