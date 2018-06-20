3ff07832f40feaacab0e95697813f2dcd3f9a638
Toby Zerner <toby.zerner@gmail.com>
2018-06-20 13:34:30 +0930

    Webpack (#9)
    See https://github.com/flarum/core/pull/1367
    
    * Replace gulp with webpack and npm scripts for JS compilation
    * Set up Travis CI to commit compiled JS
    * Restructure `js` directory; only one instance of npm, forum/admin are "submodules"
    * Restructure `less` directory

--- extensions/akismet/js/src/admin/index.js
+++ extensions/akismet/js/src/admin/index.js
@@ -1,6 +1,6 @@
 import app from 'flarum/app';
 
-import AkismetSettingsModal from 'flarum/akismet/components/AkismetSettingsModal';
+import AkismetSettingsModal from './components/AkismetSettingsModal';
 
 app.initializers.add('flarum-akismet', () => {
   app.extensionSettings['flarum-akismet'] = () => app.modal.show(new AkismetSettingsModal());
