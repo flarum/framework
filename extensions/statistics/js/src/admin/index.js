6eae43688eff9d54b831c8b981b0dd66aab3fd51
Toby Zerner <toby.zerner@gmail.com>
2018-06-20 13:35:48 +0930

    Webpack (#4)
    See https://github.com/flarum/core/pull/1367
    
    * Replace gulp with webpack and npm scripts for JS compilation
    * Set up Travis CI to commit compiled JS
    * Restructure `js` directory; only one instance of npm, forum/admin are "submodules"
    * Restructure `less` directory

--- extensions/statistics/js/src/admin/index.js
+++ extensions/statistics/js/src/admin/index.js
@@ -3,7 +3,7 @@ import { extend } from 'flarum/extend';
 
 import DashboardPage from 'flarum/components/DashboardPage';
 
-import StatisticsWidget from 'flarum/statistics/components/StatisticsWidget';
+import StatisticsWidget from './components/StatisticsWidget';
 
 app.initializers.add('flarum-statistics', () => {
   extend(DashboardPage.prototype, 'availableWidgets', widgets => {
