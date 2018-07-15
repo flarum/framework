fffab39c4b45268f0e4e290704eef33542c02745
Franz Liedke <franz@develophp.org>
2018-07-16 01:43:06 +0200

    Discussion HTML: Add a canonical URL
    Refs #358 and #1140.

--- framework/core/src/Forum/Content/Discussion.php
+++ framework/core/src/Forum/Content/Discussion.php
@@ -90,6 +90,7 @@ class Discussion implements ContentInterface
         }
 
         $document->title = $apiDocument->data->attributes->title;
+        $document->canonicalUrl = $url([]);
         $document->content = $this->view->make('flarum.forum::frontend.content.discussion', compact('apiDocument', 'page', 'getResource', 'posts', 'url'));
         $document->payload['apiDocument'] = $apiDocument;
 
