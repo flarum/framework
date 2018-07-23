7844ec105e7b07577260bb6edf073ef1729a91ed
Franz Liedke <franz@develophp.org>
2018-07-23 16:30:06 +0200

    Use new Frontend extender
    See flarum/core#851.

--- extensions/pusher/bootstrap.php
+++ extensions/pusher/bootstrap.php
@@ -15,11 +15,11 @@ use Flarum\Pusher\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
-    (new Extend\Assets('forum'))
+    (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->asset(__DIR__.'/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less'),
 
-    (new Extend\Assets('admin'))
+    (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
     (new Extend\Routes('api'))
