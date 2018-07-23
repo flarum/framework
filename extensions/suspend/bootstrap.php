3f37d683146dad020cf24526fd6684215ecf676c
Franz Liedke <franz@develophp.org>
2018-07-23 16:33:50 +0200

    Use new Frontend extender
    See flarum/core#851.

--- extensions/suspend/bootstrap.php
+++ extensions/suspend/bootstrap.php
@@ -15,13 +15,13 @@ use Flarum\Suspend\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
-    (new Extend\Assets('forum'))
+    (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
-        ->asset(__DIR__.'/less/forum.less'),
+        ->css(__DIR__.'/less/forum.less'),
 
-    (new Extend\Assets('admin'))
+    (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
-        ->asset(__DIR__.'/less/admin.less'),
+        ->css(__DIR__.'/less/admin.less'),
 
     function (Dispatcher $events) {
         $events->subscribe(Listener\AddUserSuspendAttributes::class);
