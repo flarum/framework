c30c3b4406f8d477a35722c2a18c04711a4620a9
Toby Zerner <toby.zerner@gmail.com>
2018-08-24 21:47:57 +0930

    Fix up Notification model

--- packages/core/js/src/common/models/Notification.js
+++ packages/core/js/src/common/models/Notification.js
@@ -1,17 +1,13 @@
 import Model from '../Model';
-import computed from '../utils/computed';
 
 export default class Notification extends Model {}
 
 Object.assign(Notification.prototype, {
   contentType: Model.attribute('contentType'),
-  subjectId: Model.attribute('subjectId'),
   content: Model.attribute('content'),
-  time: Model.attribute('time', Model.date),
+  createdAt: Model.attribute('createdAt', Model.transformDate),
 
   isRead: Model.attribute('isRead'),
-  unreadCount: Model.attribute('unreadCount'),
-  additionalUnreadCount: computed('unreadCount', unreadCount => Math.max(0, unreadCount - 1)),
 
   user: Model.hasOne('user'),
   fromUser: Model.hasOne('fromUser'),
