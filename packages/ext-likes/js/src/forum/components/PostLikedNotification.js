7ba0bd5f884642a13338ad04a78dce04a06d068b
Toby Zerner <toby.zerner@gmail.com>
2018-09-17 04:19:51 +0930

    Database changes (#16)
    * Add foreign keys
    
    * Rename table
    
    * Use whereColumn
    
    * Update core attribute names

--- packages/ext-likes/js/src/forum/components/PostLikedNotification.js
+++ packages/ext-likes/js/src/forum/components/PostLikedNotification.js
@@ -13,7 +13,7 @@ export default class PostLikedNotification extends Notification {
 
   content() {
     const notification = this.props.notification;
-    const user = notification.sender();
+    const user = notification.fromUser();
     const auc = notification.additionalUnreadCount();
 
     return app.translator.transChoice('flarum-likes.forum.notifications.post_liked_text', auc + 1, {
