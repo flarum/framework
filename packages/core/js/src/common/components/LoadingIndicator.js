899924ed4c44e665767297184716f29553248380
Toby Zerner <toby.zerner@gmail.com>
2018-09-21 11:19:32 +0930

    Add "tiny" preset for LoadingIndicator
    This is used in various places but had not been added since presets were
    removed from Spin.js in a recent version upgrade.

--- packages/core/js/src/common/components/LoadingIndicator.js
+++ packages/core/js/src/common/components/LoadingIndicator.js
@@ -29,6 +29,10 @@ export default class LoadingIndicator extends Component {
         Object.assign(options, { lines: 10, length: 8, width: 4, radius: 8 });
         break;
 
+      case 'tiny':
+        Object.assign(options, { lines: 8, length: 2, width: 2, radius: 3 });
+        break;
+
       default:
         Object.assign(options, { lines: 8, length: 4, width: 3, radius: 5 });
     }
