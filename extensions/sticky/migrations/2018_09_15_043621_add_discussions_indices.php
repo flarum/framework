aa386b28789d76070d597508fddb0c3ce57150be
Toby Zerner <toby.zerner@gmail.com>
2018-09-25 07:19:55 +0930

    Add database indices. ref flarum/core#127 (#12)

---
+++ extensions/sticky/migrations/2018_09_15_043621_add_discussions_indices.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->index(['is_sticky', 'created_at']);
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->dropIndex(['is_sticky', 'created_at']);
+        });
+    }
+];
