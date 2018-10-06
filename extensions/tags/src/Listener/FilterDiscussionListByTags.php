f9744b5f9805c251f507a897cd48857a21d89546
David Sevilla Martín <dsevilla192@icloud.com>
2018-10-06 08:37:28 -0400

    Remove unused import

--- extensions/tags/src/Listener/FilterDiscussionListByTags.php
+++ extensions/tags/src/Listener/FilterDiscussionListByTags.php
@@ -13,7 +13,6 @@ namespace Flarum\Tags\Listener;
 
 use Flarum\Discussion\Event\Searching;
 use Flarum\Event\ConfigureDiscussionGambits;
-use Flarum\Subscriptions\Gambit\SubscriptionGambit;
 use Flarum\Tags\Gambit\TagGambit;
 use Flarum\Tags\Tag;
 use Illuminate\Contracts\Events\Dispatcher;
