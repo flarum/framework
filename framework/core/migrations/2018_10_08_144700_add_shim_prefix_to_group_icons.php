5f785c947345c41d2e15df232cc9ff002d836785
David Sevilla Martín <datitisev@users.noreply.github.com>
2018-10-09 18:39:19 -0400

    Add migration to add 'fa fa-' to group icons (#1597)
    * Add migration to add 'fa fa-' (FA v4 shim) to group icons
    
    * StyleCI
    
    * Change prefix to `fas fa-`

---
+++ framework/core/migrations/2018_10_08_144700_add_shim_prefix_to_group_icons.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $groups = $schema->getConnection()->table('groups')
+            ->where('icon', '<>', '')
+            ->where('icon', 'NOT LIKE', '%fa-%')
+            ->select('id', 'icon')
+            ->cursor();
+
+        foreach ($groups as $group) {
+            $schema->getConnection()->table('groups')
+                ->where('id', $group->id)
+                ->update([
+                    'icon' => 'fas fa-'.$group->icon
+                ]);
+        }
+    },
+
+    'down' => function (Builder $schema) {
+        //
+    }
+];
