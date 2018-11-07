bb0fc165afa6f41feb03d1a509b83a3bdebe1663
Daniël Klabbers <luceos@users.noreply.github.com>
2018-11-07 22:34:09 +0100

    [b8] master token fix (#1622)
    * fixed not being able to use master token because id column no longer holds key
    * added flexibility of user_id column
    * added tests to confirm the api keys actually work as intended

--- src/Api/ApiKey.php
+++ src/Api/ApiKey.php
@@ -11,7 +11,9 @@
 
 namespace Flarum\Api;
 
+use Carbon\Carbon;
 use Flarum\Database\AbstractModel;
+use Flarum\User\User;
 
 /**
  * @property int $id
@@ -19,11 +21,14 @@ use Flarum\Database\AbstractModel;
  * @property string|null $allowed_ips
  * @property string|null $scopes
  * @property int|null $user_id
+ * @property \Flarum\User\User|null $user
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon|null $last_activity_at
  */
 class ApiKey extends AbstractModel
 {
+    protected $dates = ['last_activity_at'];
+
     /**
      * Generate an API key.
      *
@@ -36,5 +41,17 @@ class ApiKey extends AbstractModel
         $key->key = str_random(40);
 
         return $key;
+    }
+
+    public function touch()
+    {
+        $this->last_activity_at = Carbon::now();
+
+        return $this->save();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
