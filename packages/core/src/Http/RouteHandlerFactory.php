67cdaffacb73cf8f03eb223d1c1f09f0fd53c2b3
Toby Zerner <toby.zerner@gmail.com>
2018-11-16 13:55:10 +1030

    Consolidate ControllerRouteHandler into RouteHandlerFactory
    Also allow closure to be passed for frontend content when creating routes

--- packages/core/src/Http/RouteHandlerFactory.php
+++ packages/core/src/Http/RouteHandlerFactory.php
@@ -11,8 +11,12 @@
 
 namespace Flarum\Http;
 
+use Closure;
 use Flarum\Frontend\Controller as FrontendController;
 use Illuminate\Contracts\Container\Container;
+use InvalidArgumentException;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Server\RequestHandlerInterface as Handler;
 
 class RouteHandlerFactory
 {
@@ -21,56 +25,61 @@ class RouteHandlerFactory
      */
     protected $container;
 
-    /**
-     * @param Container $container
-     */
     public function __construct(Container $container)
     {
         $this->container = $container;
     }
 
-    /**
-     * @param string|callable $controller
-     * @return ControllerRouteHandler
-     */
-    public function toController($controller)
+    public function toController($controller): Closure
     {
-        return new ControllerRouteHandler($this->container, $controller);
+        return function (Request $request, array $routeParams) use ($controller) {
+            $controller = $this->resolveController($controller);
+
+            $request = $request->withQueryParams(array_merge($request->getQueryParams(), $routeParams));
+
+            return $controller->handle($request);
+        };
     }
 
     /**
      * @param string $frontend
-     * @param string|null $content
-     * @return ControllerRouteHandler
+     * @param string|callable|null $content
      */
-    public function toFrontend(string $frontend, string $content = null)
+    public function toFrontend(string $frontend, $content = null): Closure
     {
         return $this->toController(function (Container $container) use ($frontend, $content) {
             $frontend = $container->make("flarum.frontend.$frontend");
 
             if ($content) {
-                $frontend->add($container->make($content));
+                $frontend->content(is_callable($content) ? $content : $container->make($content));
             }
 
             return new FrontendController($frontend);
         });
     }
 
-    /**
-     * @param string|null $content
-     * @return ControllerRouteHandler
-     */
-    public function toForum(string $content = null)
+    public function toForum(string $content = null): Closure
     {
         return $this->toFrontend('forum', $content);
     }
 
-    /**
-     * @param string|null $content
-     * @return ControllerRouteHandler
-     */
-    public function toAdmin(string $content = null)
+    public function toAdmin(string $content = null): Closure
     {
         return $this->toFrontend('admin', $content);
+    }
+
+    private function resolveController($controller): Handler
+    {
+        if (is_callable($controller)) {
+            $controller = $this->container->call($controller);
+        } else {
+            $controller = $this->container->make($controller);
+        }
+
+        if (! $controller instanceof Handler) {
+            throw new InvalidArgumentException('Controller must be an instance of '.Handler::class);
+        }
+
+        return $controller;
     }
 }
