41688c73f8227e132902356726f839f740351654
Toby Zerner <toby.zerner@gmail.com>
2018-11-27 12:19:13 +1030

    Fix index names in migrations
    This can be reverted when we upgrade to Laravel 5.7.

--- framework/core/migrations/2018_01_11_095000_change_api_keys_columns.php
+++ framework/core/migrations/2018_01_11_095000_change_api_keys_columns.php
@@ -9,18 +9,21 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->table('api_keys', function (Blueprint $table) {
+        $schema->table('api_keys', function (Blueprint $table) use ($schema) {
             $table->dropPrimary(['id']);
             $table->renameColumn('id', 'key');
             $table->unique('key');
+
+            Migration::fixIndexNames($schema, $table);
         });
 
-        $schema->table('api_keys', function (Blueprint $table) {
+        $schema->table('api_keys', function (Blueprint $table) use ($schema) {
             $table->increments('id');
             $table->string('allowed_ips')->nullable();
             $table->string('scopes')->nullable();
@@ -29,19 +32,25 @@ return [
             $table->dateTime('last_activity_at')->nullable();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            Migration::fixIndexNames($schema, $table);
         });
     },
 
     'down' => function (Builder $schema) {
-        $schema->table('api_keys', function (Blueprint $table) {
+        $schema->table('api_keys', function (Blueprint $table) use ($schema) {
             $table->dropForeign(['user_id']);
             $table->dropColumn('id', 'allowed_ips', 'user_id', 'scopes', 'created_at');
+
+            Migration::fixIndexNames($schema, $table);
         });
 
-        $schema->table('api_keys', function (Blueprint $table) {
+        $schema->table('api_keys', function (Blueprint $table) use ($schema) {
             $table->dropUnique(['key']);
             $table->renameColumn('key', 'id');
             $table->primary('id');
+
+            Migration::fixIndexNames($schema, $table);
         });
     }
 ];
