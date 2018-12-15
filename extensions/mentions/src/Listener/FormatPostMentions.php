3656072ff45a353ad0699da577804405f59d2d79
Franz Liedke <franz@develophp.org>
2018-12-15 12:06:08 +0100

    Use Formatter extender to avoid resolving the UrlGenerator too early
    Refs flarum/core#1578.
    Fixes flarum/core#1703.

--- extensions/mentions/src/Listener/FormatPostMentions.php
+++ extensions/mentions/src/Listener/FormatPostMentions.php
@@ -11,69 +11,12 @@
 
 namespace Flarum\Mentions\Listener;
 
-use Flarum\Formatter\Event\Configuring;
 use Flarum\Formatter\Event\Rendering;
-use Flarum\Http\UrlGenerator;
-use Flarum\Post\CommentPost;
-use Illuminate\Contracts\Events\Dispatcher;
 use s9e\TextFormatter\Utils;
 
 class FormatPostMentions
 {
-    /**
-     * @var UrlGenerator
-     */
-    protected $url;
-
-    /**
-     * @param UrlGenerator $url
-     */
-    public function __construct(UrlGenerator $url)
-    {
-        $this->url = $url;
-    }
-
-    /**
-     * @param Dispatcher $events
-     */
-    public function subscribe(Dispatcher $events)
-    {
-        $events->listen(Configuring::class, [$this, 'configure']);
-        $events->listen(Rendering::class, [$this, 'render']);
-    }
-
-    /**
-     * @param Configuring $event
-     */
-    public function configure(Configuring $event)
-    {
-        $configurator = $event->configurator;
-
-        $configurator->rendering->parameters['DISCUSSION_URL'] = $this->url->to('forum')->route('discussion', ['id' => '']);
-
-        $tagName = 'POSTMENTION';
-
-        $tag = $configurator->tags->add($tagName);
-
-        $tag->attributes->add('username');
-        $tag->attributes->add('displayname');
-        $tag->attributes->add('number')->filterChain->append('#uint');
-        $tag->attributes->add('discussionid')->filterChain->append('#uint');
-        $tag->attributes->add('id')->filterChain->append('#uint');
-
-        $tag->template = '<a href="{$DISCUSSION_URL}{@discussionid}/{@number}" class="PostMention" data-id="{@id}"><xsl:value-of select="@displayname"/></a>';
-
-        $tag->filterChain
-            ->prepend([static::class, 'addId'])
-            ->setJS('function(tag) { return flarum.extensions["flarum-mentions"].filterPostMentions(tag); }');
-
-        $configurator->Preg->match('/\B@(?<username>[a-z0-9_-]+)#(?<id>\d+)/i', $tagName);
-    }
-
-    /**
-     * @param Rendering $event
-     */
-    public function render(Rendering $event)
+    public function handle(Rendering $event)
     {
         $post = $event->context;
 
@@ -85,22 +28,5 @@ class FormatPostMentions
 
             return $attributes;
         });
-    }
-
-    /**
-     * @param $tag
-     * @return bool
-     */
-    public static function addId($tag)
-    {
-        $post = CommentPost::find($tag->getAttribute('id'));
-
-        if ($post) {
-            $tag->setAttribute('discussionid', (int) $post->discussion_id);
-            $tag->setAttribute('number', (int) $post->number);
-            $tag->setAttribute('displayname', $post->user->display_name);
-
-            return true;
-        }
     }
 }
--- extensions/mentions/src/Listener/FormatUserMentions.php
+++ extensions/mentions/src/Listener/FormatUserMentions.php
@@ -11,64 +11,12 @@
 
 namespace Flarum\Mentions\Listener;
 
-use Flarum\Formatter\Event\Configuring;
 use Flarum\Formatter\Event\Rendering;
-use Flarum\Http\UrlGenerator;
-use Flarum\User\User;
-use Illuminate\Contracts\Events\Dispatcher;
 use s9e\TextFormatter\Utils;
 
 class FormatUserMentions
 {
-    /**
-     * @var UrlGenerator
-     */
-    protected $url;
-
-    /**
-     * @param UrlGenerator $url
-     */
-    public function __construct(UrlGenerator $url)
-    {
-        $this->url = $url;
-    }
-
-    /**
-     * @param Dispatcher $events
-     */
-    public function subscribe(Dispatcher $events)
-    {
-        $events->listen(Configuring::class, [$this, 'configure']);
-        $events->listen(Rendering::class, [$this, 'render']);
-    }
-
-    /**
-     * @param Configuring $event
-     */
-    public function configure(Configuring $event)
-    {
-        $configurator = $event->configurator;
-
-        $configurator->rendering->parameters['PROFILE_URL'] = $this->url->to('forum')->route('user', ['username' => '']);
-
-        $tagName = 'USERMENTION';
-
-        $tag = $configurator->tags->add($tagName);
-        $tag->attributes->add('username');
-        $tag->attributes->add('displayname');
-        $tag->attributes->add('id')->filterChain->append('#uint');
-
-        $tag->template = '<a href="{$PROFILE_URL}{@username}" class="UserMention">@<xsl:value-of select="@displayname"/></a>';
-        $tag->filterChain->prepend([static::class, 'addId'])
-            ->setJS('function(tag) { return flarum.extensions["flarum-mentions"].filterUserMentions(tag); }');
-
-        $configurator->Preg->match('/\B@(?<username>[a-z0-9_-]+)(?!#)/i', $tagName);
-    }
-
-    /**
-     * @param Rendering $event
-     */
-    public function render(Rendering $event)
+    public function handle(Rendering $event)
     {
         $post = $event->context;
 
@@ -81,20 +29,5 @@ class FormatUserMentions
 
             return $attributes;
         });
-    }
-
-    /**
-     * @param $tag
-     *
-     * @return bool
-     */
-    public static function addId($tag)
-    {
-        if ($user = User::where('username', 'like', $tag->getAttribute('username'))->first()) {
-            $tag->setAttribute('id', $user->id);
-            $tag->setAttribute('displayname', $user->display_name);
-
-            return true;
-        }
     }
 }
