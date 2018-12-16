2c4706dd3a172843d0f2d2d846774f5eb0f7c1ac
Franz Liedke <franz@develophp.org>
2018-12-16 15:05:58 +0100

    Get rid of event subscribers that resolve services too early
    Refs flarum/core#1578.

--- packages/ext-suspend/extend.php
+++ packages/ext-suspend/extend.php
@@ -9,9 +9,16 @@
  * file that was distributed with this source code.
  */
 
+use Flarum\Api\Serializer\BasicUserSerializer;
+use Flarum\Event\ConfigureNotificationTypes;
 use Flarum\Extend;
 use Flarum\Suspend\Access;
+use Flarum\Suspend\Event\Suspended;
+use Flarum\Suspend\Event\Unsuspended;
 use Flarum\Suspend\Listener;
+use Flarum\Suspend\Notification\UserSuspendedBlueprint;
+use Flarum\Suspend\Notification\UserUnsuspendedBlueprint;
+use Flarum\User\Event\Saving;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
@@ -26,8 +33,15 @@ return [
     function (Dispatcher $events) {
         $events->subscribe(Listener\AddUserSuspendAttributes::class);
         $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
-        $events->subscribe(Listener\SaveSuspensionToDatabase::class);
-        $events->subscribe(Listener\SendNotificationWhenUserIsSuspended::class);
+
+        $events->listen(Saving::class, Listener\SaveSuspensionToDatabase::class);
+
+        $events->listen(ConfigureNotificationTypes::class, function (ConfigureNotificationTypes $event) {
+            $event->add(UserSuspendedBlueprint::class, BasicUserSerializer::class, ['alert', 'email']);
+            $event->add(UserUnsuspendedBlueprint::class, BasicUserSerializer::class, ['alert', 'email']);
+        });
+        $events->listen(Suspended::class, Listener\SendNotificationWhenUserIsSuspended::class);
+        $events->listen(Unsuspended::class, Listener\SendNotificationWhenUserIsUnsuspended::class);
 
         $events->subscribe(Access\UserPolicy::class);
     }
