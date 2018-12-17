4b65844e421630357be47f26f17ab7d8db280db0
Franz Liedke <franz@develophp.org>
2018-12-18 00:11:36 +0100

    Get rid of event subscribers that resolve services too early
    Refs flarum/core#1578.

---
+++ packages/ext-akismet/src/Listener/SubmitHam.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Akismet\Listener;
+
+use Flarum\Approval\Event\PostWasApproved;
+use TijsVerkoyen\Akismet\Akismet;
+
+class SubmitHam
+{
+    /**
+     * @var Akismet
+     */
+    protected $akismet;
+
+    public function __construct(Akismet $akismet)
+    {
+        $this->akismet = $akismet;
+    }
+
+    public function handle(PostWasApproved $event)
+    {
+        $post = $event->post;
+
+        if ($post->is_spam) {
+            $this->akismet->submitHam(
+                $post->ip_address,
+                null,
+                $post->content,
+                $post->user->username,
+                $post->user->email
+            );
+        }
+    }
+}
---
+++ packages/ext-akismet/src/Listener/ValidatePost.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Akismet\Listener;
+
+use Flarum\Flags\Flag;
+use Flarum\Post\Event\Saving;
+use TijsVerkoyen\Akismet\Akismet;
+
+class ValidatePost
+{
+    /**
+     * @var Akismet
+     */
+    protected $akismet;
+
+    public function __construct(Akismet $akismet)
+    {
+        $this->akismet = $akismet;
+    }
+
+    public function handle(Saving $event)
+    {
+        $post = $event->post;
+
+        if ($post->exists || $post->user->groups()->count()) {
+            return;
+        }
+
+        $isSpam = $this->akismet->isSpam(
+            $post->content,
+            $post->user->username,
+            $post->user->email,
+            null,
+            'comment'
+        );
+
+        if ($isSpam) {
+            $post->is_approved = false;
+            $post->is_spam = true;
+
+            $post->afterSave(function ($post) {
+                if ($post->number == 1) {
+                    $post->discussion->is_approved = false;
+                    $post->discussion->save();
+                }
+
+                $flag = new Flag;
+
+                $flag->post_id = $post->id;
+                $flag->type = 'akismet';
+                $flag->created_at = time();
+
+                $flag->save();
+            });
+        }
+    }
+}
