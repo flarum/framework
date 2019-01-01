cd0d8594801760fdb82e9d045df8558d16529dda
Daniël Klabbers <luceos@users.noreply.github.com>
2019-01-01 21:02:18 +0100

    Increasing test coverage (#1711)
    * added a few more tests, renamed singular to plural to match controller
    
    * increase error reporting
    
    * removed debugging and wait for tests

---
+++ packages/core/tests/Api/Controller/ListGroupsControllerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Tests\Api\Controller;
+
+use Flarum\Api\Controller\ListGroupsController;
+use Flarum\Group\Group;
+
+class ListGroupsControllerTest extends ApiControllerTestCase
+{
+    protected $controller = ListGroupsController::class;
+
+    /**
+     * @test
+     */
+    public function shows_index_for_guest()
+    {
+        $response = $this->callWith();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $data = json_decode($response->getBody()->getContents(), true);
+
+        $this->assertEquals(Group::count(), count($data['data']));
+    }
+}
