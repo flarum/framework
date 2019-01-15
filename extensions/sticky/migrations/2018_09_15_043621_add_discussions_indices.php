bb6ed4a764c26184788783de33bea76124329b4d
Daniël Klabbers <daniel@klabbers.email>
2019-01-15 21:01:44 +0100

    revert the fix index names logic, preparing for illuminate 5.7

--- extensions/sticky/migrations/2018_09_15_043621_add_discussions_indices.php
+++ extensions/sticky/migrations/2018_09_15_043621_add_discussions_indices.php
@@ -9,24 +9,19 @@
  * file that was distributed with this source code.
  */
 
-use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) use ($schema) {
+        $schema->table('discussions', function (Blueprint $table) {
             $table->index(['is_sticky', 'created_at']);
-
-            Migration::fixIndexNames($schema, $table);
         });
     },
 
     'down' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) use ($schema) {
+        $schema->table('discussions', function (Blueprint $table) {
             $table->dropIndex(['is_sticky', 'created_at']);
-
-            Migration::fixIndexNames($schema, $table);
         });
     }
 ];
