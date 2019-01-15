c18731953d266eccb782c5b51c4c1e789db51a28
Daniël Klabbers <daniel@klabbers.email>
2019-01-15 21:00:38 +0100

    revert the fix index names logic, preparing for illuminate 5.7

--- extensions/tags/migrations/2018_06_27_085300_change_tags_add_foreign_keys.php
+++ extensions/tags/migrations/2018_06_27_085300_change_tags_add_foreign_keys.php
@@ -9,7 +9,6 @@
  * file that was distributed with this source code.
  */
 
-use Flarum\Database\Migration;
 use Illuminate\Database\Query\Expression;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
@@ -31,22 +30,18 @@ return [
             'last_posted_discussion_id' => $select('id', 'discussions', 'last_posted_discussion_id'),
         ]);
 
-        $schema->table('tags', function (Blueprint $table) use ($schema) {
+        $schema->table('tags', function (Blueprint $table) {
             $table->foreign('parent_id')->references('id')->on('tags')->onDelete('set null');
             $table->foreign('last_posted_user_id')->references('id')->on('users')->onDelete('set null');
             $table->foreign('last_posted_discussion_id')->references('id')->on('discussions')->onDelete('set null');
-
-            Migration::fixIndexNames($schema, $table);
         });
     },
 
     'down' => function (Builder $schema) {
-        $schema->table('tags', function (Blueprint $table) use ($schema) {
+        $schema->table('tags', function (Blueprint $table) {
             $table->dropForeign(['parent_id']);
             $table->dropForeign(['last_posted_discussion_id']);
             $table->dropForeign(['last_posted_user_id']);
-
-            Migration::fixIndexNames($schema, $table);
         });
     }
 ];
--- extensions/tags/migrations/2018_06_27_100200_change_tag_user_add_foreign_keys.php
+++ extensions/tags/migrations/2018_06_27_100200_change_tag_user_add_foreign_keys.php
@@ -9,7 +9,6 @@
  * file that was distributed with this source code.
  */
 
-use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
@@ -27,20 +26,16 @@ return [
             })
             ->delete();
 
-        $schema->table('tag_user', function (Blueprint $table) use ($schema) {
+        $schema->table('tag_user', function (Blueprint $table) {
             $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-            Migration::fixIndexNames($schema, $table);
         });
     },
 
     'down' => function (Builder $schema) {
-        $schema->table('tag_user', function (Blueprint $table) use ($schema) {
+        $schema->table('tag_user', function (Blueprint $table) {
             $table->dropForeign(['tag_id']);
             $table->dropForeign(['user_id']);
-
-            Migration::fixIndexNames($schema, $table);
         });
     }
 ];
