b82fef3a1fd3964ca8839c736bc8eff07bbd4a41
Franz Liedke <franz@develophp.org>
2019-03-16 12:56:44 +0100

    Add drivers for Mailgun, Mandrill, SES (#1169)

---
+++ packages/core/src/Mail/SesDriver.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Mail;
+
+use Aws\Ses\SesClient;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Mail\Transport\SesTransport;
+use Swift_Transport;
+
+class SesDriver implements DriverInterface
+{
+    public function buildTransport(SettingsRepositoryInterface $settings): Swift_Transport
+    {
+        $config = [
+            'version' => 'latest',
+            'service' => 'email',
+            'region' => $settings->get('mail_ses_region'),
+            'credentials' => [
+                'key' => $settings->get('mail_ses_key'),
+                'secret' => $settings->get('mail_ses_secret'),
+            ],
+        ];
+
+        return new SesTransport(new SesClient($config));
+    }
+}
