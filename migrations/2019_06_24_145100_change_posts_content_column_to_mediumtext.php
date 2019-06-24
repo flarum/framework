590b3115708bf94a9c7f169d98c6126380c7056e
Daniël Klabbers <daniel@klabbers.email>
2019-06-24 14:53:56 +0200

    fixes #1801, increasing the size of posts.content to mediumText correctly

--- migrations/2019_06_24_145100_change_posts_content_column_to_mediumtext.php
+++ migrations/2019_06_24_145100_change_posts_content_column_to_mediumtext.php
@@ -12,16 +12,17 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Schema\Builder;
 
+// https://github.com/doctrine/dbal/issues/2566#issuecomment-480217999
 return [
     'up' => function (Builder $schema) {
         $schema->table('posts', function (Blueprint $table) {
-            $table->mediumText('content')->change();
+            $table->mediumText('content')->comment(' ')->change();
         });
     },
 
     'down' => function (Builder $schema) {
         $schema->table('posts', function (Blueprint $table) {
-            $table->text('content')->change();
+            $table->text('content')->comment('')->change();
         });
     }
 ];
