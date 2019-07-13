c5c86e504be2436b236f544aaf459b2be0e55fd4
David Sevilla Martín <dsevilla192@icloud.com>
2019-07-13 18:50:30 -0400

    Replace jQuery with Zepto

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -5,4 +5,19 @@ module.exports = merge(config(), {
   output: {
     library: 'flarum.core'
   },
+
+  // use zepto instead of jquery
+  module: {
+    rules: [
+      {
+        test: require.resolve("zepto"),
+        use: "imports-loader?this=>window"
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      jquery: "zepto"
+    }
+  }
 });
