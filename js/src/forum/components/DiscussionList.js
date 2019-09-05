febe60553d90dc58467065c0955783d274bbff04
SychO <sychocouldy@gmail.com>
2019-09-05 14:52:34 +0100

    restore the addDiscussion() method

--- js/src/forum/components/DiscussionList.js
+++ js/src/forum/components/DiscussionList.js
@@ -205,4 +205,14 @@ export default class DiscussionList extends Component {
       this.discussions.splice(index, 1);
     }
   }
+
+  /**
+   * Add a discussion to the top of the list.
+   *
+   * @param {Discussion} discussion
+   * @public
+   */
+  addDiscussion(discussion) {
+    this.discussions.unshift(discussion);
+  }
 }
