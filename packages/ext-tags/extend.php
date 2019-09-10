abc499759cd04cd4aca7f98e4726bb3ed5231adf
David Sevilla Martín <6401250+datitisev@users.noreply.github.com>
2019-09-10 14:34:07 -0400

    Add view & frontend documents for tags & tag discussion list  (#64)
    Refs flarum/core#1820.
    Fixes flarum/core#1134.

--- packages/ext-tags/extend.php
+++ packages/ext-tags/extend.php
@@ -14,15 +14,17 @@ use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
 use Flarum\Tags\Access;
 use Flarum\Tags\Api\Controller;
+use Flarum\Tags\Content;
 use Flarum\Tags\Listener;
 use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\View\Factory;
 
 return [
     (new Extend\Frontend('forum'))
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less')
-        ->route('/t/{slug}', 'tag')
-        ->route('/tags', 'tags'),
+        ->route('/t/{slug}', 'tag', Content\Tag::class)
+        ->route('/tags', 'tags', Content\Tags::class),
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
@@ -35,7 +37,7 @@ return [
         ->patch('/tags/{id}', 'tags.update', Controller\UpdateTagController::class)
         ->delete('/tags/{id}', 'tags.delete', Controller\DeleteTagController::class),
 
-    function (Dispatcher $events) {
+    function (Dispatcher $events, Factory $view) {
         $events->subscribe(Listener\AddDiscussionTagsRelationship::class);
 
         $events->subscribe(Listener\AddForumTagsRelationship::class);
@@ -51,5 +53,7 @@ return [
         $events->subscribe(Access\DiscussionPolicy::class);
         $events->subscribe(Access\TagPolicy::class);
         $events->subscribe(Access\FlagPolicy::class);
+
+        $view->addNamespace('tags', __DIR__.'/views');
     },
 ];
---
+++ packages/ext-tags/src/Content/Tag.php
@@ -0,0 +1,108 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * (c) Toby Zerner <toby.zerner@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Tags\Content;
+
+use Flarum\Api\Client;
+use Flarum\Api\Controller\ListDiscussionsController;
+use Flarum\Frontend\Document;
+use Flarum\Tags\TagRepository;
+use Flarum\User\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Support\Arr;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class Tag
+{
+    /**
+     * @var Client
+     */
+    protected $api;
+
+    /**
+     * @var Factory
+     */
+    protected $view;
+
+    /**
+     * @var TagRepository
+     */
+    protected $tags;
+
+    /**
+     * @param Client $api
+     * @param Factory $view
+     */
+    public function __construct(Client $api, Factory $view, TagRepository $tags)
+    {
+        $this->api = $api;
+        $this->view = $view;
+        $this->tags = $tags;
+    }
+
+    public function __invoke(Document $document, Request $request)
+    {
+        $queryParams = $request->getQueryParams();
+        $actor = $request->getAttribute('actor');
+
+        $slug = Arr::pull($queryParams, 'slug');
+        $sort = Arr::pull($queryParams, 'sort');
+        $q = Arr::pull($queryParams, 'q', '');
+        $page = Arr::pull($queryParams, 'page', 1);
+
+        $sortMap = $this->getSortMap();
+
+        $tagId = $this->tags->getIdForSlug($slug);
+        $tag = $this->tags->findOrFail($tagId, $actor);
+
+        $params = [
+            'sort' => $sort && isset($sortMap[$sort]) ? $sortMap[$sort] : '',
+            'filter' => [
+                'q' => "$q tag:$slug"
+            ],
+            'page' => ['offset' => ($page - 1) * 20, 'limit' => 20]
+        ];
+
+        $apiDocument = $this->getApiDocument($actor, $params);
+
+        $document->content = $this->view->make('tags::frontend.content.tag', compact('apiDocument', 'page', 'tag'));
+        $document->payload['apiDocument'] = $apiDocument;
+
+        return $document;
+    }
+
+    /**
+     * Get a map of sort query param values and their API sort params.
+     *
+     * @return array
+     */
+    private function getSortMap()
+    {
+        return [
+            'latest' => '-lastPostedAt',
+            'top' => '-commentCount',
+            'newest' => '-createdAt',
+            'oldest' => 'createdAt'
+        ];
+    }
+
+    /**
+     * Get the result of an API request to list discussions.
+     *
+     * @param User $actor
+     * @param array $params
+     * @return object
+     */
+    private function getApiDocument(User $actor, array $params)
+    {
+        return json_decode($this->api->send(ListDiscussionsController::class, $actor, $params)->getBody());
+    }
+}
