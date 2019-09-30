209cf8a25db44bc4c5bbc726023dba053eceac4a
David Sevilla Martín <6401250+datitisev@users.noreply.github.com>
2019-09-30 16:57:47 -0400

    Update Application version string to beta 11

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -23,7 +23,7 @@ class Application extends Container implements ApplicationContract
      *
      * @var string
      */
-    const VERSION = '0.1.0-beta.10';
+    const VERSION = '0.1.0-beta.11';
 
     /**
      * The base path for the Flarum installation.
