479266b36835379e43321805c2c21a45d76be5a8
luceos <daniel+github@klabbers.email>
2019-10-02 12:36:29 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- framework/core/src/Console/Event/Configuring.php
+++ framework/core/src/Console/Event/Configuring.php
@@ -14,7 +14,6 @@ namespace Flarum\Console\Event;
 use Flarum\Foundation\Application;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Application as ConsoleApplication;
-use Symfony\Component\EventDispatcher\EventDispatcher;
 
 /**
  * Configure the console application.
