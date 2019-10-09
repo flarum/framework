cf37cc58948a8f8b5068c264a540474a3884ef90
SILVIO Laurie <l.silvio@travaux.com>
2019-10-09 22:14:28 +0200

    Add search form to 404 page

--- views/error/not_found.blade.php
+++ views/error/not_found.blade.php
@@ -5,6 +5,12 @@
     {{ $message }}
   </p>
   <p>
+    <form class="form" method="GET" action="/">
+        <input type="text" name="q" placeholder="{{ $translator->trans('core.forum.header.search_placeholder') }}"/>
+        <button type="submit" class="button">Go</button>
+    </form>
+  </p>
+  <p>
     <a href="{{ $app->url() }}">
       {{ $translator->trans('core.views.error.not_found_return_link', ['{forum}' => $settings->get('forum_title')]) }}
     </a>
