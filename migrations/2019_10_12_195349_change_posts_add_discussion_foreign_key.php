8b9f03e99827bea689f81a00cb11007fbe8ada09
David Sevilla Martin <me@datitisev.me>
2019-10-12 15:57:35 -0400

    Add discussion_id foreign key to posts table

---
+++ migrations/2019_10_12_195349_change_posts_add_discussion_foreign_key.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        // Delete rows with non-existent entities so that we will be able to create
+        // foreign keys without any issues.
+        $connection = $schema->getConnection();
+        $connection->table('posts')
+            ->orWhereNotExists(function ($query) {
+                $query->selectRaw(1)->from('discussions')->whereColumn('id', 'discussion_id');
+            })
+            ->delete();
+
+
+        $schema->table('posts', function (Blueprint $table) {
+            $table->foreign('discussion_id')->references('id')->on('discussions')->onDelete('cascade');
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->table('posts', function (Blueprint $table) {
+            $table->dropForeign(['discussion_id']);
+        });
+    }
+];
