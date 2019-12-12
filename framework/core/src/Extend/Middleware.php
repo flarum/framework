441d74f1af82a3bc91ef4c0df54ade6178aebad7
Matt Kilgore <tankerkiller125@gmail.com>
2019-12-12 03:22:04 -0500

    Middleware extender (#1952)

---
+++ framework/core/src/Extend/Middleware.php
@@ -0,0 +1,41 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Extend;
+
+use Flarum\Extension\Extension;
+use Illuminate\Contracts\Container\Container;
+use Zend\Stratigility\MiddlewarePipe;
+
+class Middleware implements ExtenderInterface
+{
+    protected $middlewares = [];
+    protected $frontend;
+
+    public function __construct(string $frontend)
+    {
+        $this->frontend = $frontend;
+    }
+
+    public function add($middleware)
+    {
+        $this->middlewares[] = $middleware;
+
+        return $this;
+    }
+
+    public function extend(Container $container, Extension $extension = null)
+    {
+        $container->resolving("flarum.{$this->frontend}.middleware", function (MiddlewarePipe $pipe) use ($container) {
+            foreach ($this->middlewares as $middleware) {
+                $pipe->pipe($container->make($middleware));
+            }
+        });
+    }
+}
