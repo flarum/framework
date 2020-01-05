ca0c52d60aa1fd721a695a43388b0b2eaf53b69c
luceos <daniel+github@klabbers.email>
2020-01-05 21:28:46 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- views/install/problems.php
+++ views/install/problems.php
@@ -3,12 +3,12 @@
 <p>These problems must be resolved before you can continue the installation. If you're having trouble, get help on the <a href="https://flarum.org/docs/install.html" target="_blank">Flarum website</a>.</p>
 
 <div class="Problems">
-  <?php foreach ($problems as $problem): ?>
+  <?php foreach ($problems as $problem) { ?>
     <div class="Problem">
       <h3 class="Problem-message"><?php echo $problem['message']; ?></h3>
-      <?php if (! empty($problem['detail'])): ?>
+      <?php if (! empty($problem['detail'])) { ?>
         <p class="Problem-detail"><?php echo $problem['detail']; ?></p>
-      <?php endif; ?>
+      <?php } ?>
     </div>
-  <?php endforeach; ?>
+  <?php } ?>
 </div>
