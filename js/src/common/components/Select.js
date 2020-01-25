d4b6026099635a28ac4ffe7c0df69bb013f59277
Clark Winkelmann <clark.winkelmann@gmail.com>
2020-01-25 22:05:47 +0100

    Add disabled prop to the Select component

--- js/src/common/components/Select.js
+++ js/src/common/components/Select.js
@@ -8,14 +8,15 @@ import icon from '../helpers/icon';
  * - `options` A map of option values to labels.
  * - `onchange` A callback to run when the selected value is changed.
  * - `value` The value of the selected option.
+ * - `disabled` Disabled state for the input.
  */
 export default class Select extends Component {
   view() {
-    const {options, onchange, value} = this.props;
+    const {options, onchange, value, disabled} = this.props;
 
     return (
       <span className="Select">
-        <select className="Select-input FormControl" onchange={onchange ? m.withAttr('value', onchange.bind(this)) : undefined} value={value}>
+        <select className="Select-input FormControl" onchange={onchange ? m.withAttr('value', onchange.bind(this)) : undefined} value={value} disabled={disabled}>
           {Object.keys(options).map(key => <option value={key}>{options[key]}</option>)}
         </select>
         {icon('fas fa-sort', {className: 'Select-caret'})}
