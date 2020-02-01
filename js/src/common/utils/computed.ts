393d78af5ebaeba48d3e70c7107c66f90ea7c04a
David Sevilla Martin <me@datitisev.me>
2020-02-01 09:45:12 -0500

    common: fix computed util when only passing one key as a string

--- js/src/common/utils/computed.ts
+++ js/src/common/utils/computed.ts
@@ -2,12 +2,12 @@
  * The `computed` utility creates a function that will cache its output until
  * any of the dependent values are dirty.
  *
- * @param {...String} dependentKeys The keys of the dependent values.
- * @param {function} compute The function which computes the value using the
+ * @param dependentKeys The keys of the dependent values.
+ * @param compute The function which computes the value using the
  *     dependent values.
  */
 export default function computed(dependentKeys: string | string[], compute: Function): () => any {
-    const keys = Array.from(dependentKeys);
+    const keys = [].concat(dependentKeys);
 
     const dependentValues = {};
     let computedValue;
