69ff24fc505e8de9cef6f766c8d3d71b3c34f98a
Matthew Kilgore <tankerkiller125@gmail.com>
2020-02-21 17:49:44 -0500

    First Middleware Extender Test

---
+++ tests/integration/extenders/MiddlewareTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Flarum\Tests\integration\extenders;
+
+use Flarum\Extend;
+use Flarum\Tests\integration\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class MiddlewareTest extends TestCase
+{
+
+    // This adds the first custom middleware for test that require a middleware to already exist
+    private function add_first_middleware() {
+        $this->extend(
+            (new Extend\Middleware('forum'))->add(FirstTestMiddleware::class)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function can_add_middleware() {
+        $this->extend(
+            (new Extend\Middleware('forum'))->add(FirstTestMiddleware::class)
+        );
+
+        $response = $this->send($this->request('GET', '/'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayHasKey('X-First-Test-Middleware', $response->getHeaders());
+    }
+
+    /**
+     * @test
+     */
+    public function can_replace_middleware() {
+        $this->add_first_middleware();
+        $this->extend(
+            (new Extend\Middleware('forum'))->replace(FirstTestMiddleware::class, SecondTestMiddleware::class)
+        );
+
+        $response = $this->send($this->request('GET', '/'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayNotHasKey('X-First-Test-Middleware', $response->getHeaders());
+        $this->assertArrayHasKey('X-Second-Test-Middleware', $response->getHeaders());
+    }
+
+    /**
+     * @test
+     */
+    public function can_remove_middleware() {
+        $this->add_first_middleware();
+        $this->extend(
+            (new Extend\Middleware('forum'))->remove(FirstTestMiddleware::class)
+        );
+
+        $response = $this->send($this->request('GET', '/'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertArrayNotHasKey('X-First-Test-Middleware', $response->getHeaders());
+    }
+}
+
+class FirstTestMiddleware implements MiddlewareInterface {
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+        $response = $handler->handle($request);
+        return $response->withAddedHeader('X-First-Test-Middleware', 'This is a test!');
+    }
+}
+
+class SecondTestMiddleware implements MiddlewareInterface {
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $response = $handler->handle($request);
+        return $response->withAddedHeader('X-Second-Test-Middleware', 'This is another test!');
+    }
+}
