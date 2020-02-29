0ff882fb61ea6d2d0183b612d22b65528ac78df6
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-02-28 20:53:14 -0500

    Fixed unnecessary, undefined bvariable

--- src/Forum/ForumServiceProvider.php
+++ src/Forum/ForumServiceProvider.php
@@ -63,7 +63,7 @@ class ForumServiceProvider extends AbstractServiceProvider
             return [];
         });
 
-        $this->$app->singleton('flarum.forum.middleware.csrf', function ($app) {
+        $this->$app->singleton('flarum.forum.middleware.csrf', function () {
             return new HttpMiddleware\CheckCsrfToken($this->app->make('flarum.forum.csrfExemptRoutes'));
         });
 
