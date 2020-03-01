94c4bc878548c067edfa54f70f4a25dcd0c84bd4
Matthew Kilgore <tankerkiller125@gmail.com>
2020-03-01 09:52:30 -0500

    Hide mentions in inline spoilers
    Fixes #2033

--- packages/ext-markdown/extend.php
+++ packages/ext-markdown/extend.php
@@ -12,10 +12,12 @@ use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
 
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->Litedown;
+            $config->tags['ispoiler']->template = '<span class="spoiler" data-s9e-livepreview-ignore-attrs="style" onclick="removeAttribute(\'style\'); removeAttribute(\'class\')" style="background:#444;color:transparent"><xsl:apply-templates/></span>';
         })
 ];
