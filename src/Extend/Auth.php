615e4f5f52e2abdb27353e4dcbaca4cc6a11983a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-03-11 03:10:14 -0400

    Add abstract auth lifecycle handler to auth extender

--- src/Extend/Auth.php
+++ src/Extend/Auth.php
@@ -11,13 +11,16 @@ namespace Flarum\Extend;
 
 use Flarum\Extension\Extension;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\Events\Dispatcher;
 
 class Auth implements ExtenderInterface
 {
     protected $addAuthDrivers = [];
 
+    protected $addAuthLifecycleHandlers = [];
+
     /**
-     * @param string $identifier: URL-friendly, lowercase identifier (ex. 'github', 'saml', 'google', 'facebook', 'wechat')
+     * @param string $identifier URL-friendly, lowercase identifier (ex. 'github', 'saml', 'google', 'facebook', 'wechat')
      * @param $driver Class attribute of driver that implements Flarum\Forum\Auth\ExternalAuthDriverInterface
      */
     public function addAuthDriver($identifier, $driver)
@@ -27,10 +30,24 @@ class Auth implements ExtenderInterface
         return $this;
     }
 
+    /**
+     * @param $handler class attribute of Auth Lifecycle Handler that extends Flarum\User\AbstractAuthLifecycleHandler
+     */
+    public function addAuthLifecycleHandler($handler)
+    {
+        $this->addAuthLifecycleHandlers[] = $handler;
+    }
+
     public function extend(Container $container, Extension $extension = null)
     {
         $container->extend("flarum.auth.supported_drivers", function ($existingMiddleware) {
             return array_merge($existingMiddleware, $this->addAuthDrivers);
         });
+
+        $events = $container->make(Dispatcher::class);
+
+        foreach ($this->addAuthLifecycleHandlers as $handler) {
+            $events->subscribe($container->make($handler));
+        }
     }
 }
