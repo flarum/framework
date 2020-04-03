1a9e6bf218860e869c71710d8552db6dfa0e692c
Matthew Kilgore <tankerkiller125@gmail.com>
2020-04-02 20:16:52 -0400

    Split permissions

--- js/src/admin/components/PermissionGrid.js
+++ js/src/admin/components/PermissionGrid.js
@@ -312,15 +312,23 @@ export default class PermissionGrid extends Component {
       60
     );
 
-    items.add(
-      'userEdit',
-      {
-        icon: 'fas fa-user-cog',
-        label: app.translator.trans('core.admin.permissions.edit_users_label'),
-        permission: 'user.edit',
-      },
-      60
-    );
+    items.add('editCredentials', {
+      icon: 'fas fa-user-cog',
+      label: app.translator.trans('core.admin.permissions.edit_credentials_label'),
+      permission: 'edit.credentials'
+    }, 50);
+
+    items.add('editUsername', {
+      icon: 'fas fa-address-card',
+      label: app.translator.trans('core.admin.permissions.edit_username_label'),
+      permission: 'edit.username'
+    }, 50);
+
+    items.add('editGroups', {
+      icon: 'fas fa-users-cog',
+      label: app.translator.trans('core.admin.permissions.edit_groups_label'),
+      permission: 'edit.groups'
+    }, 50);
 
     return items;
   }
