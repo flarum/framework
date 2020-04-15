72737821e0f74a32170cbf77d24b4bfda3db1193
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-04-15 18:12:18 -0400

    Fixed review

--- js/src/common/components/Select.tsx
+++ js/src/common/components/Select.tsx
@@ -1,9 +1,9 @@
-import Component, { ComponentProps } from "../Component";
-import icon from "../helpers/icon";
+import Component, { ComponentProps } from '../Component';
+import icon from '../helpers/icon';
 
 export interface SelectProps extends ComponentProps {
-    /**u
-     * Whether or not the select is disabled.
+    /**
+     * Disabled state for the input.
      */
     disabled: boolean;
 
@@ -13,44 +13,37 @@ export interface SelectProps extends ComponentProps {
     onchange?: Function;
 
     /**
-     * An object of options
+     * A map of option values to labels.
      */
     options: {
-        [index: string]: string;
+        [key: string]: string;
     };
 
     /**
-     * The current value of the select.
+     * The value of the selected option.
      */
     value: boolean;
 }
 
 /**
  * The `Select` component displays a <select> input, surrounded with some extra
- * elements for styling. It accepts the following props:
- *
- * - `options` A map of option values to labels.
- * - `onchange` A callback to run when the selected value is changed.
- * - `value` The value of the selected option.
- * - `disabled` Disabled state for the input.
+ * elements for styling.
  */
-export default class Select<
-    T extends SelectProps = SelectProps
-> extends Component<SelectProps> {
+export default class Select<T extends SelectProps = SelectProps> extends Component<SelectProps> {
     view() {
         return (
             <span className="Select">
                 <select
                     className="Select-input FormControl"
                     disabled={this.props.disabled}
-                    onchange={m.withAttr("value", this.onchange.bind(this))}
+                    onchange={m.withAttr('value', this.onchange.bind(this))}
                     value={this.props.value}
                 >
                     {Object.keys(this.props.options).map((key: string) => (
                         <option value={key}>{this.props.options[key]}</option>
                     ))}
                 </select>
-                {icon("fas fa-sort", { className: "Select-caret" })}
+                {icon('fas fa-sort', { className: 'Select-caret' })}
             </span>
         );
     }
