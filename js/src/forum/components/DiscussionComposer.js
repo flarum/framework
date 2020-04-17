yiyinyiyang/core

Fix `TypeError: Cannot read property 'addDiscussion' of undefined`
Sentry > DEMOS-Q

TypeError: Cannot read property 'addDiscussion' of undefined
  at onSuccess(webpack://@flarum/core/./src/forum/components/DiscussionComposer.js:95:34)
  at notThennable(webpack://@flarum/core/./node_modules/mithril/mithril.js:2033:1)
  at thennable(webpack://@flarum/core/./node_modules/mithril/mithril.js:2004:1)
  at fire(webpack://@flarum/core/./node_modules/mithril/mithril.js:2024:1)
  at resolve(webpack://@flarum/core/./node_modules/mithril/mithril.js:1941:1)
  at ? (webpack://@flarum/core/./node_modules/mithril/mithril.js:1976:1)

js/src/forum/components/DiscussionComposer.js
@@ -92,7 +92,9 @@ export default class DiscussionComposer extends ComposerBody {
     app.store.createRecord('discussions').save(data).then(
       discussion => {
         app.composer.hide();
-        app.cache.discussionList.addDiscussion(discussion);
+
+        if (app.cache.discussionList) app.cache.discussionList.addDiscussion(discussion);
+
         m.route(app.route.discussion(discussion));
       },
       this.loaded.bind(this)

flarum/framework

js/src/forum/components/DiscussionComposer.js
@@ -98,7 +98,9 @@ export default class DiscussionComposer extends ComposerBody {
       .save(data)
       .then((discussion) => {
         app.composer.hide();
-        app.cache.discussionList.refresh();
+
+        if (app.cache.discussionList) app.cache.discussionList.refresh(discussion);
+
         m.route(app.route.discussion(discussion));
       }, this.loaded.bind(this));
   }