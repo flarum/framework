38bc36dc1cc0000ff720531e7bcdbd3746c95c81
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-04-21 12:32:16 -0400

    Added doc blocks for model extender

--- src/Extend/Model.php
+++ src/Extend/Model.php
@@ -19,18 +19,36 @@ class Model implements ExtenderInterface
     private $defaultAttributeCallbacks = [];
     private $relationships = [];
 
+    /**
+     * @param string $modelClass The ::class attribute of the model you are modifying.
+     *                           This model should extend from \Flarum\Database\AbstractModel.
+     */
     public function __construct(string $modelClass)
     {
         $this->modelClass = $modelClass;
     }
 
-    public function configureDates(callable $callback)
+    /**
+     * Modify which attributes of this model are treated as dates.
+     *
+     * @param callable $callable: A callback, or class attribute of invokable class.
+     *                            It should take as input, and return, an array of
+     *                            attributes that should be treated as dates.
+     */
+    public function configureDates(callable $callable)
     {
-        $this->dateCallbacks[] = $callback;
+        $this->dateCallbacks[] = $callable;
 
         return $this;
     }
 
+    /**
+     * Modify default attribute values for this model.
+     *
+     * @param callable $callable: A callback, or class attribute of invokable class.
+     *                            It should take as input, and return, an
+     *                            associative array of attributes => default values.
+     */
     public function configureDefaultAttributes(callable $callable)
     {
         $this->defaultAttributeCallbacks[] = $callable;
@@ -38,6 +56,15 @@ class Model implements ExtenderInterface
         return $this;
     }
 
+    /**
+     * Add a relationship from this model to another model.
+     *
+     * @param string $name: the name of the relation. This doesn't have to match anything,
+     *                      but has to be unique from other relation names for this model.
+     * @param callable $callable: A callable that takes as input an instance of this model, and
+     *                            returns a Laravel Relationship object. See relevant methods of
+     *                            models like \Flarum\User\User for examples of how relationships should be returned.
+     */
     public function relationship(string $name, callable $callable)
     {
         $this->relationships[$name] = $callable;
