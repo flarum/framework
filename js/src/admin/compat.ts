7c9d5931da9f0837cca8a3d04e6609c41c2d6f24
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-04-29 18:38:05 -0400

    Mithril/typing redo for upload image button and settings modal

--- js/src/admin/compat.ts
+++ js/src/admin/compat.ts
@@ -1,7 +1,11 @@
 import compat from '../common/compat';
 
 import Admin from './Admin';
+import SettingsModal from './components/SettingsModal';
+import UploadImageButton from './components/UploadImageButton';
 
 export default Object.assign(compat, {
     Admin: Admin,
+    SettingsModal: SettingsModal,
+    UploadImageButton: UploadImageButton,
 }) as any;
