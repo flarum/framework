9ce41d76057d0dc28b306780090ad6d1c6d11127
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-04-29 17:16:32 -0400

    Removed license docblock that we don't use anywhere else in the frontend

--- js/src/admin/components/StatusWidget.tsx
+++ js/src/admin/components/StatusWidget.tsx
@@ -1,12 +1,3 @@
-/*
- * This file is part of Flarum.
- *
- * (c) Toby Zerner <toby.zerner@gmail.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 import DashboardWidget from './DashboardWidget';
 import listItems from '../../common/helpers/listItems';
 import ItemList from '../../common/utils/ItemList';
