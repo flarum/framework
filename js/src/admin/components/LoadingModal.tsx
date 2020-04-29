6ed23ecf7bbb7518e6a349908c3333638b0e8d9a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-04-29 17:42:55 -0400

    LoadingModal typing fix

--- js/src/admin/components/LoadingModal.tsx
+++ js/src/admin/components/LoadingModal.tsx
@@ -1,6 +1,7 @@
+import { ComponentProps } from '../../common/Component';
 import Modal from '../../common/components/Modal';
 
-export default class LoadingModal extends Modal {
+export default class LoadingModal extends Modal<ComponentProps> {
     isDismissible() {
         return false;
     }
@@ -10,7 +11,7 @@ export default class LoadingModal extends Modal {
     }
 
     title() {
-        return app.translator.trans('core.admin.loading.title');
+        return app.translator.transText('core.admin.loading.title');
     }
 
     content() {
