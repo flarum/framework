74a1ebe8130c63c3aeabd9b0ec12bb29b01b36da
Golden <35162802+Littlegolden@users.noreply.github.com>
2020-05-01 17:35:46 +0800

    Use different CDN for loading Pusher JS library (#20)
    Use jsDelivr to increase the loading speed (when compared to MaxCDN) in China.

--- extensions/pusher/js/src/forum/index.js
+++ extensions/pusher/js/src/forum/index.js
@@ -10,7 +10,7 @@ import Button from 'flarum/components/Button';
 app.initializers.add('flarum-pusher', () => {
   const loadPusher = m.deferred();
 
-  $.getScript('//js.pusher.com/3.0/pusher.min.js', () => {
+  $.getScript('//cdn.jsdelivr.net/npm/pusher-js@3.0.0/dist/pusher.min.js', () => {
     const socket = new Pusher(app.forum.attribute('pusherKey'), {
       authEndpoint: app.forum.attribute('apiUrl') + '/pusher/auth',
       cluster: app.forum.attribute('pusherCluster'),
