80c09f8e3f8da6a324f0ac385df5ef389c3e59b0
Franz Liedke <franz@develophp.org>
2020-05-08 12:17:15 +0200

    Drop useless app_path() helper
    This was probably taken straight from Laravel. There is no equivalent
    concept in Flarum, so this should be safe to remove.

--- src/helpers.php
+++ src/helpers.php
@@ -27,19 +27,6 @@ if (! function_exists('app')) {
     }
 }
 
-if (! function_exists('app_path')) {
-    /**
-     * Get the path to the application folder.
-     *
-     * @param  string  $path
-     * @return string
-     */
-    function app_path($path = '')
-    {
-        return app('path').($path ? DIRECTORY_SEPARATOR.$path : $path);
-    }
-}
-
 if (! function_exists('base_path')) {
     /**
      * Get the path to the base of the install.
