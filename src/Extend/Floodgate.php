4e8c20aba600c564cea7593306a75a4b893b8e4c
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-17 18:03:41 -0400

    Add doc blocks to floodgate extender

--- src/Extend/Floodgate.php
+++ src/Extend/Floodgate.php
@@ -17,6 +17,22 @@ class Floodgate implements ExtenderInterface
     private $setFloodgates = [];
     private $removeFloodgates = [];
 
+    /**
+     * Register a flood gate checker.
+     *
+     * @param string $name: The name of the flood gate checker. These must be unique:
+     *                      if you use the name of an existing checker it will be overriden.
+     * @param string[] $paths: An array of paths (e.g. /api/discussions) on which to apply this checker.
+     *                         Wildcards are supported.
+     * @param string[] $methods: An array of methods on which to apply this checker.
+     *                           Methods should be uppercase, wildcards are NOT supported.
+     * @param string[] $callback: A closure or invokable class that contains the logic of the flood gate.
+     *                            It should return one of:
+     *                              - `false`: This marks the request as NOT flooding. It overrides all other outputs
+     *                              - `true`: This marks the request as flooding.
+     *                            All other outputs will be ignored.
+     * @return self
+     */
     public function set(string $name, array $paths, array $methods, callable $callback)
     {
         $this->setFloodgates[$name] = [
@@ -29,6 +45,13 @@ class Floodgate implements ExtenderInterface
     }
 
 
+    /**
+     * Remove a flood gate checker registered with this name.
+     *
+     * @param string $name: The name of the flood gate checker to remove.
+     *
+     * @return self
+     */
     public function remove(string $name)
     {
         $this->removeFloodgates[] = $name;
