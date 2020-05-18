4e34797ef80880ee027ea1ac2301f1bcbfaeffa4
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-17 21:36:27 -0400

    Add in missing formattedError parameter

--- js/src/common/Application.js
+++ js/src/common/Application.js
@@ -399,7 +399,7 @@ export default class Application {
    * @param {string[]} [formattedError]
    * @private
    */
-  showDebug(error) {
+  showDebug(error, formattedError) {
     this.alerts.dismiss(this.requestError.alertKey);
 
     this.modal.show(new RequestErrorModal({ error, formattedError }));
