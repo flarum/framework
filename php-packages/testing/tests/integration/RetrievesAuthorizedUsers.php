b1a18971cab4db5336645639b0564b27bf6af293
Franz Liedke <franz@develophp.org>
2020-05-20 00:18:00 +0200

    Tests: Rely on admin user, groups, permissions from test setup script

--- php-packages/testing/tests/integration/RetrievesAuthorizedUsers.php
+++ php-packages/testing/tests/integration/RetrievesAuthorizedUsers.php
@@ -11,50 +11,6 @@ namespace Flarum\Tests\integration;
 
 trait RetrievesAuthorizedUsers
 {
-    protected function adminGroup(): array
-    {
-        return [
-            'id' => 1,
-            'name_singular' => 'Admin',
-            'name_plural' => 'Admins',
-            'color' => '#B72A2A',
-            'icon' => 'fas fa-wrench',
-        ];
-    }
-
-    protected function guestGroup(): array
-    {
-        return [
-            'id' => 2,
-            'name_singular' => 'Guest',
-            'name_plural' => 'Guests',
-            'color' => null,
-            'icon' => null,
-        ];
-    }
-
-    protected function memberGroup(): array
-    {
-        return [
-            'id' => 3,
-            'name_singular' => 'Member',
-            'name_plural' => 'Members',
-            'color' => null,
-            'icon' => null,
-        ];
-    }
-
-    protected function adminUser(): array
-    {
-        return [
-            'id' => 1,
-            'username' => 'admin',
-            'password' => '$2y$10$HMOAe.XaQjOimA778VmFue1OCt7tj5j0wk5vfoL/CMSJq2BQlfBV2', // BCrypt hash for "password"
-            'email' => 'admin@machine.local',
-            'is_email_confirmed' => 1,
-        ];
-    }
-
     protected function normalUser(): array
     {
         return [
