e5d19a88be3a23c666df8ed2e3d0c7660630af41
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-05-30 22:17:48 -0400

    Undid accidential formating changes

--- views/frontend/content/index.blade.php
+++ views/frontend/content/index.blade.php
@@ -14,10 +14,10 @@
     </ul>
 
     @if (isset($apiDocument->links->prev))
-    <a href="{{ $url->to('forum')->route('index') }}?page={{ $page - 1 }}">&laquo; {{ $translator->trans('core.views.index.previous_page_button') }}</a>
+        <a href="{{ $url->to('forum')->route('index') }}?page={{ $page - 1 }}">&laquo; {{ $translator->trans('core.views.index.previous_page_button') }}</a>
     @endif
 
     @if (isset($apiDocument->links->next))
-    <a href="{{ $url->to('forum')->route('index') }}?page={{ $page + 1 }}">{{ $translator->trans('core.views.index.next_page_button') }} &raquo;</a>
+        <a href="{{ $url->to('forum')->route('index') }}?page={{ $page + 1 }}">{{ $translator->trans('core.views.index.next_page_button') }} &raquo;</a>
     @endif
 </div>
