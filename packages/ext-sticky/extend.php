7f146bc3ebb9ecd846a69ee6a3975e311adcc09b
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-06-19 22:16:32 -0400

    Add empty locale extender to clear cache properly on enable/disable.
    See https://github.com/flarum/core/pull/2020#issuecomment-616149583

--- packages/ext-sticky/extend.php
+++ packages/ext-sticky/extend.php
@@ -19,6 +19,8 @@ return [
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
+    new Extend\Locales(__DIR__.'/locale'),
+
     function (Dispatcher $events) {
         $events->subscribe(Listener\AddApiAttributes::class);
         $events->subscribe(Listener\CreatePostWhenDiscussionIsStickied::class);
