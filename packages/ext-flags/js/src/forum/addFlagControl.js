2a640a1bbf2a29fbc08d6cbf9fa90fc8e51a91ea
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-01 19:58:59 -0400

    Fix extension to work with latest state changes

--- packages/ext-flags/js/src/forum/addFlagControl.js
+++ packages/ext-flags/js/src/forum/addFlagControl.js
@@ -10,7 +10,7 @@ export default function() {
     if (post.isHidden() || post.contentType() !== 'comment' || !post.canFlag()) return;
 
     items.add('flag',
-      <Button icon="fas fa-flag" onclick={() => app.modal.show(new FlagPostModal({post}))}>{app.translator.trans('flarum-flags.forum.post_controls.flag_button')}</Button>
+      <Button icon="fas fa-flag" onclick={() => app.modal.show(FlagPostModal, {post})}>{app.translator.trans('flarum-flags.forum.post_controls.flag_button')}</Button>
     );
   });
 }
