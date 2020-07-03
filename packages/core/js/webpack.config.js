7e84d6c789a769b93de512263896246f51c7a2a2
David Sevilla Martín <6401250+datitisev@users.noreply.github.com>
2020-07-03 08:47:44 -0400

    Initial TypeScript infrastructure (#2206)
    This allows us to get started with converting all Flarum JavaScript code to TypeScript.
    In addition, we will have time to experiment to find the best Webpack configuration before integrating into flarum-webpack-config.
    
    See flarum/flarum-webpack-config#3.

--- packages/core/js/webpack.config.js
+++ packages/core/js/webpack.config.js
@@ -5,4 +5,12 @@ module.exports = merge(config(), {
   output: {
     library: 'flarum.core'
   },
+
+  // temporary TS configuration
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
 });
+
+module.exports['module'].rules[0].test = /\.(tsx?|js)$/;
+module.exports['module'].rules[0].use.options.presets.push('@babel/preset-typescript');
