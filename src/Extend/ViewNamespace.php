35f579caee4e10b7556ec8279bcd1b1a27b0fb97
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-10 14:04:20 -0400

    Expanded documentation for ViewNamespace extender

--- src/Extend/ViewNamespace.php
+++ src/Extend/ViewNamespace.php
@@ -18,10 +18,19 @@ class ViewNamespace implements ExtenderInterface
     private $adds = [];
 
     /**
-     * Register a new namespace of laravel views.
+     * Register a new namespace of Laravel views.
      *
-     * @param  string  $namespace
-     * @param  string|array  $hints
+     * Views are php files that use the Laravel Blade syntax for creation of server-side generated html.
+     * Flarum core uses them for error pages, the installer, HTML emails, and the skeletons for the forum and admin sites.
+     * To create and use views in your extension, you will need to put them in a folder, and register that folder as a namespace.
+     *
+     * Views can then be used in your extension by injecting an instance of `Illuminate\Contracts\View\Factory`,
+     * and calling its `make` method. The `make` method takes the view parameter in the format NAMESPACE::VIEW_NAME.
+     * You can also pass variables into a view: for more information, see https://laravel.com/api/6.x/Illuminate/View/Factory.html#method_make
+     *
+     * @param  string  $namespace: The name of the namespace.
+     * @param  string|array  $hints: This is a path (or an array of paths) to the folder(s)
+     *                               where view files are stored, relative to the extend.php file.
      * @return $this
      */
     public function add($namespace, $hints)
