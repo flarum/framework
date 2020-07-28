f879182ef70e7285840ad9dadfa81b5b6c47f8d7
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-28 18:08:49 -0400

    Add a formatter for rendering errors via the frontend

---
+++ src/Foundation/ErrorHandling/FrontendFormatter.php
@@ -0,0 +1,60 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Foundation\ErrorHandling;
+
+use Flarum\Frontend\Controller;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Container\Container;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Symfony\Component\Translation\TranslatorInterface;
+
+/**
+ * A formatter for turning caught exceptions into "pretty" HTML error pages.
+ *
+ * For certain known error types, we display pages with dedicated information
+ * relevant to this class of error, e.g. a page with a search form for HTTP 404
+ * "Not Found" errors. We look for templates in the `views/error` directory.
+ *
+ * If no specific template exists, a generic "Something went wrong" page will be
+ * displayed, optionally enriched with a more specific error message if found in
+ * the translation files.
+ */
+class FrontendFormatter implements HttpFormatter
+{
+    /**
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
+
+    public function __construct(Container $container, TranslatorInterface $translator, SettingsRepositoryInterface $settings)
+    {
+        $this->container = $container;
+        $this->translator = $translator;
+        $this->settings = $settings;
+    }
+
+    public function format(HandledError $error, Request $request): Response
+    {
+        $frontend = $this->container->make("flarum.frontend.forum");
+
+        return (new Controller($frontend))->handle($request)->withStatus($error->getStatusCode());
+    }
+}
