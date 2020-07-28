a7aa952215970be83057217dc47a83c3b2d61877
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-07-27 20:48:10 -0400

    Fix route collection getting wrong path when optional parameters present, add unit tests

--- src/Http/RouteCollection.php
+++ src/Http/RouteCollection.php
@@ -88,7 +88,23 @@ class RouteCollection
     public function getPath($name, array $parameters = [])
     {
         if (isset($this->reverse[$name])) {
+            $curr_max_count = 0;
             $parts = $this->reverse[$name][0];
+
+            // For a given route name, we want to choose the option that best matches the given parameters.
+            // Each routing option is an array of parts. Each part is either a constant string
+            // (which we don't care about here), or an array where the first element is the parameter name
+            // and the second element is a regex into which the parameter value is inserted, if the parameter matches.
+            foreach ($this->reverse[$name] as $parts_option) {
+                for ($i = 0; $i < count($parts_option); $i++) {
+                    $part = $parts_option[$i];
+                    if (is_array($part) && in_array($part[0], array_keys($parameters)) && $i > $curr_max_count) {
+                        $curr_max_count = $i;
+                        $parts = $parts_option;
+                    }
+                }
+            }
+
             array_walk($parts, [$this, 'fixPathPart'], $parameters);
 
             return '/'.ltrim(implode('', $parts), '/');
