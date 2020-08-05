64d4eb8c4cdceabe0c848c9e84a305fab67b0453
Matthew Kilgore <tankerkiller125@gmail.com>
2020-08-04 20:07:32 -0400

    update: forum/components/HeaderSecondary (no search, session, notifs)

--- js/src/forum/components/HeaderSecondary.js
+++ js/src/forum/components/HeaderSecondary.js
@@ -19,13 +19,6 @@ export default class HeaderSecondary extends Component {
     return <ul className="Header-controls">{listItems(this.items().toArray())}</ul>;
   }
 
-  config(isInitialized, context) {
-    // Since this component is 'above' the content of the page (that is, it is a
-    // part of the global UI that persists between routes), we will flag the DOM
-    // to be retained across route changes.
-    context.retain = true;
-  }
-
   /**
    * Build an item list for the controls.
    *
@@ -34,62 +27,70 @@ export default class HeaderSecondary extends Component {
   items() {
     const items = new ItemList();
 
-    items.add('search', Search.component({ state: app.search }), 30);
+    // items.add('search', Search.component({ state: app.search }), 30);
 
     if (app.forum.attribute('showLanguageSelector') && Object.keys(app.data.locales).length > 1) {
       const locales = [];
 
       for (const locale in app.data.locales) {
         locales.push(
-          Button.component({
-            active: app.data.locale === locale,
-            children: app.data.locales[locale],
-            icon: app.data.locale === locale ? 'fas fa-check' : true,
-            onclick: () => {
-              if (app.session.user) {
-                app.session.user.savePreferences({ locale }).then(() => window.location.reload());
-              } else {
-                document.cookie = `locale=${locale}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT`;
-                window.location.reload();
-              }
+          Button.component(
+            {
+              active: app.data.locale === locale,
+              icon: app.data.locale === locale ? 'fas fa-check' : true,
+              onclick: () => {
+                if (app.session.user) {
+                  app.session.user.savePreferences({ locale }).then(() => window.location.reload());
+                } else {
+                  document.cookie = `locale=${locale}; path=/; expires=Tue, 19 Jan 2038 03:14:07 GMT`;
+                  window.location.reload();
+                }
+              },
             },
-          })
+            app.data.locales[locale]
+          )
         );
       }
 
       items.add(
         'locale',
-        SelectDropdown.component({
-          children: locales,
-          buttonClassName: 'Button Button--link',
-        }),
+        SelectDropdown.component(
+          {
+            buttonClassName: 'Button Button--link',
+          },
+          locales
+        ),
         20
       );
     }
 
     if (app.session.user) {
-      items.add('notifications', NotificationsDropdown.component({ state: app.notifications }), 10);
-      items.add('session', SessionDropdown.component(), 0);
+      // items.add('notifications', NotificationsDropdown.component({ state: app.notifications }), 10);
+      // items.add('session', SessionDropdown.component(), 0);
     } else {
       if (app.forum.attribute('allowSignUp')) {
         items.add(
           'signUp',
-          Button.component({
-            children: app.translator.trans('core.forum.header.sign_up_link'),
-            className: 'Button Button--link',
-            onclick: () => app.modal.show(SignUpModal),
-          }),
+          Button.component(
+            {
+              className: 'Button Button--link',
+              onclick: () => app.modal.show(SignUpModal),
+            },
+            app.translator.trans('core.forum.header.sign_up_link')
+          ),
           10
         );
       }
 
       items.add(
         'logIn',
-        Button.component({
-          children: app.translator.trans('core.forum.header.log_in_link'),
-          className: 'Button Button--link',
-          onclick: () => app.modal.show(LogInModal),
-        }),
+        Button.component(
+          {
+            className: 'Button Button--link',
+            onclick: () => app.modal.show(LogInModal),
+          },
+          app.translator.trans('core.forum.header.log_in_link')
+        ),
         0
       );
     }
