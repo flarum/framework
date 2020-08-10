4d45aaa9ae9e047545afaf712eeb6879df8f6186
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-08-10 00:51:47 -0400

    update: forum/components/LoginButton

--- js/src/forum/components/LogInButton.js
+++ js/src/forum/components/LogInButton.js
@@ -4,21 +4,21 @@ import Button from '../../common/components/Button';
  * The `LogInButton` component displays a social login button which will open
  * a popup window containing the specified path.
  *
- * ### Props
+ * ### Attrs
  *
  * - `path`
  */
 export default class LogInButton extends Button {
-  static initProps(props) {
-    props.className = (props.className || '') + ' LogInButton';
+  initAttrs(attrs) {
+    attrs.className = (attrs.className || '') + ' LogInButton';
 
-    props.onclick = function () {
+    attrs.onclick = function () {
       const width = 580;
       const height = 400;
       const $window = $(window);
 
       window.open(
-        app.forum.attribute('baseUrl') + props.path,
+        app.forum.attribute('baseUrl') + attrs.path,
         'logInPopup',
         `width=${width},` +
           `height=${height},` +
@@ -28,6 +28,6 @@ export default class LogInButton extends Button {
       );
     };
 
-    super.initProps(props);
+    super.initAttrs(attrs);
   }
 }
