5f2c9da2f5e3f42bb0c46254651bf4ee841b7f38
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-08-10 19:26:52 -0400

    update: admin/components/SessionDropdown

--- js/src/admin/components/SessionDropdown.js
+++ js/src/admin/components/SessionDropdown.js
@@ -9,18 +9,18 @@ import ItemList from '../../common/utils/ItemList';
  * avatar/name, with a dropdown of session controls.
  */
 export default class SessionDropdown extends Dropdown {
-  static initProps(props) {
-    super.initProps(props);
+  initAttrs(attrs) {
+    super.initAttrs(attrs);
 
-    props.className = 'SessionDropdown';
-    props.buttonClassName = 'Button Button--user Button--flat';
-    props.menuClassName = 'Dropdown-menu--right';
+    attrs.className = 'SessionDropdown';
+    attrs.buttonClassName = 'Button Button--user Button--flat';
+    attrs.menuClassName = 'Dropdown-menu--right';
   }
 
-  view() {
-    this.props.children = this.items().toArray();
+  view(vnode) {
+    vnode.children = this.items().toArray();
 
-    return super.view();
+    return super.view(vnode);
   }
 
   getButtonContent() {
@@ -39,11 +39,13 @@ export default class SessionDropdown extends Dropdown {
 
     items.add(
       'logOut',
-      Button.component({
-        icon: 'fas fa-sign-out-alt',
-        children: app.translator.trans('core.admin.header.log_out_button'),
-        onclick: app.session.logout.bind(app.session),
-      }),
+      Button.component(
+        {
+          icon: 'fas fa-sign-out-alt',
+          onclick: app.session.logout.bind(app.session),
+        },
+        app.translator.trans('core.admin.header.log_out_button')
+      ),
       -100
     );
 
