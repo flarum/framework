e8e4b64d7de9c6a5dd1eec8872a8159a198061a0
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-08-10 00:48:15 -0400

    mapRoutes: remove obsolete setting of routeName attr on components

--- js/src/common/utils/mapRoutes.js
+++ js/src/common/utils/mapRoutes.js
@@ -13,8 +13,6 @@ export default function mapRoutes(routes, basePath = '') {
   for (const key in routes) {
     const route = routes[key];
 
-    // if (route.component) route.component.props.routeName = key;
-
     map[basePath + route.path] = route.component;
   }
 
