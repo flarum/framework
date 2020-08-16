b8abd2522eed83fde7225a40ae9985a8141eff70
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-08-15 20:03:03 -0400

    Add documentation for withAttr util

--- js/src/common/utils/withAttr.ts
+++ js/src/common/utils/withAttr.ts
@@ -1,3 +1,10 @@
+/**
+ * An event handler factory that makes it simpler to implement data
+ * binding for component event listeners.
+ *
+ * Replaces m.withAttr for mithril 2.0
+ * @see https://mithril.js.org/archive/v0.2.5/mithril.withAttr.html
+ */
 export default (key: string, cb: Function) =>
   function (this: Element) {
     cb(this.getAttribute(key) || this[key]);
