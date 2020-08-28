aa6d59a2d27ded09966e6dca54ad3cc43545fbd9
Franz Liedke <franz@develophp.org>
2020-08-28 18:30:52 +0200

    Use dayjs, not moment.js
    Refs flarum/core#2219.

--- packages/ext-suspend/js/src/forum/components/UserSuspendedNotification.js
+++ packages/ext-suspend/js/src/forum/components/UserSuspendedNotification.js
@@ -12,7 +12,7 @@ export default class UserSuspendedNotification extends Notification {
   content() {
     const notification = this.props.notification;
     const suspendedUntil = notification.content();
-    const timeReadable = moment(suspendedUntil.date).from(notification.createdAt(), true);
+    const timeReadable = dayjs(suspendedUntil.date).from(notification.createdAt(), true);
 
     return app.translator.trans('flarum-suspend.forum.notifications.user_suspended_text', {
       user: notification.fromUser(),
