f3665fc61bac150bfb1e6bd24c8c5af42b76ed04
Johannes Nilsson <nlssn@users.noreply.github.com>
2020-09-01 05:52:37 +0200

    Remove unwanted semicolon in assets files (#2280)

--- packages/core/src/Frontend/Compiler/JsCompiler.php
+++ packages/core/src/Frontend/Compiler/JsCompiler.php
@@ -65,7 +65,7 @@ class JsCompiler extends RevisionCompiler
      */
     protected function format(string $string): string
     {
-        return preg_replace('~//# sourceMappingURL.*$~m', '', $string).";\n";
+        return preg_replace('~//# sourceMappingURL.*$~m', '', $string)."\n";
     }
 
     /**
