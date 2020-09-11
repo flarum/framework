abb896d430dacc0453f95430aef1db7a76a0d3d2
Franz Liedke <franz@develophp.org>
2020-09-11 15:49:12 +0200

    Extend documentation of withAttr

--- js/src/common/utils/withAttr.ts
+++ js/src/common/utils/withAttr.ts
@@ -1,8 +1,12 @@
 /**
- * An event handler factory that makes it simpler to implement data
- * binding for component event listeners.
+ * An event handler factory that makes it simpler to implement data binding
+ * for component event listeners.
  *
- * Replaces m.withAttr for mithril 2.0
+ * The handler created by this factory passes the DOM element's attribute
+ * identified by the first argument to the callback (usually a bidirectional
+ * Mithril stream: https://mithril.js.org/stream.html#bidirectional-bindings).
+ *
+ * Replaces m.withAttr for Mithril 2.0.
  * @see https://mithril.js.org/archive/v0.2.5/mithril.withAttr.html
  */
 export default (key: string, cb: Function) =>
