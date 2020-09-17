bdd30ceecc87ebda587a3b0ec6a0aeb6d90f2a36
Franz Liedke <franz@develophp.org>
2020-09-17 02:19:47 +0200

    Add documentation, keep lifecycle methods on top

--- js/src/forum/components/AffixedSidebar.js
+++ js/src/forum/components/AffixedSidebar.js
@@ -1,8 +1,31 @@
 import Component from '../../common/Component';
 
+/**
+ * The `AffixedSidebar` component uses Bootstrap's "affix" plugin to keep a
+ * sidebar navigation at the top of the viewport when scrolling.
+ *
+ * ### Children
+ *
+ * The component must wrap an element that itself wraps an <ul> element, which
+ * will be "affixed".
+ *
+ * @see https://getbootstrap.com/docs/3.4/javascript/#affix
+ */
 export default class AffixedSidebar extends Component {
   view(vnode) {
     return vnode.children[0];
+  }
+
+  oncreate(vnode) {
+    super.oncreate(vnode);
+
+    // Register the affix plugin to execute on every window resize (and trigger)
+    this.boundOnresize = this.onresize.bind(this);
+    $(window).on('resize', this.boundOnresize).resize();
+  }
+
+  onremove() {
+    $(window).off('resize', this.boundOnresize);
   }
 
   onresize() {
@@ -24,17 +47,5 @@ export default class AffixedSidebar extends Component {
         bottom: () => (this.bottom = $footer.outerHeight(true)),
       },
     });
-  }
-
-  oncreate(vnode) {
-    super.oncreate(vnode);
-
-    // Register the affix plugin to execute on every window resize (and trigger)
-    this.boundOnresize = this.onresize.bind(this);
-    $(window).on('resize', this.boundOnresize).resize();
-  }
-
-  onremove() {
-    $(window).off('resize', this.boundOnresize);
   }
 }
