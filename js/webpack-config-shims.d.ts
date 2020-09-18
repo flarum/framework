27017b718160009ccf2861e4a91c2b354bd0770e
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-17 23:17:42 -0400

    Register jQuery with shims

--- js/webpack-config-shims.d.ts
+++ js/webpack-config-shims.d.ts
@@ -2,12 +2,14 @@ import * as Mithril from 'mithril';
 import Stream from 'mithril/stream';
 
 import * as _dayjs from 'dayjs';
+import * as _$ from 'jquery';
 
 interface m extends Mithril.Static {
     prop: typeof Stream;
 }
 
 declare global {
+    const $: typeof _$;
     const m: m;
     const dayjs: typeof _dayjs;
 }
