e10ebf0489c351d633789f471e98bdb2cb8e5a8b
Franz Liedke <franz@develophp.org>
2020-09-18 14:17:20 +0200

    TS config / shims: Fix indentation / whitespace

--- js/shims.d.ts
+++ js/shims.d.ts
@@ -3,5 +3,5 @@ export * from './webpack-config-shims';
 import Application from './src/common/Application';
 
 declare global {
-    const app: Application;
+  const app: Application;
 }
--- js/webpack-config-shims.d.ts
+++ js/webpack-config-shims.d.ts
@@ -5,13 +5,13 @@ import * as _dayjs from 'dayjs';
 import * as _$ from 'jquery';
 
 interface m extends Mithril.Static {
-    prop: typeof Stream;
+  prop: typeof Stream;
 }
 
 declare global {
-    const $: typeof _$;
-    const m: m;
-    const dayjs: typeof _dayjs;
+  const $: typeof _$;
+  const m: m;
+  const dayjs: typeof _dayjs;
 }
 
 export as namespace Mithril;
