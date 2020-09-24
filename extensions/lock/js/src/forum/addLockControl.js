63198e67c6b63a473e1c68edcadbacab82339cf8
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2020-09-23 22:49:53 -0400

    Mithril 2 Update (#23)
    Update for Mithril 2

--- extensions/lock/js/src/forum/addLockControl.js
+++ extensions/lock/js/src/forum/addLockControl.js
@@ -7,10 +7,9 @@ export default function addLockControl() {
   extend(DiscussionControls, 'moderationControls', function(items, discussion) {
     if (discussion.canLock()) {
       items.add('lock', Button.component({
-        children: app.translator.trans(discussion.isLocked() ? 'flarum-lock.forum.discussion_controls.unlock_button' : 'flarum-lock.forum.discussion_controls.lock_button'),
         icon: 'fas fa-lock',
         onclick: this.lockAction.bind(discussion)
-      }));
+      }, app.translator.trans(discussion.isLocked() ? 'flarum-lock.forum.discussion_controls.unlock_button' : 'flarum-lock.forum.discussion_controls.lock_button')));
     }
   });
 
