a4ba00ecd620b552b067dc524a175877e5e55f45
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-09-24 18:39:23 -0400

    Make attrs in AlertAttrs optional

--- js/src/common/components/Alert.tsx
+++ js/src/common/components/Alert.tsx
@@ -6,20 +6,20 @@ import Mithril from 'mithril';
 
 export interface AlertAttrs extends ComponentAttrs {
   /** The type of alert this is. Will be used to give the alert a class name of `Alert--{type}`. */
-  type: string;
+  type?: string;
   /** An array of controls to show in the alert. */
-  controls: Mithril.Children;
+  controls?: Mithril.Children;
   /** Whether or not the alert can be dismissed. */
-  dismissible: boolean;
+  dismissible?: boolean;
   /** A callback to run when the alert is dismissed */
-  ondismiss: Function;
+  ondismiss?: Function;
 }
 
 /**
  * The `Alert` component represents an alert box, which contains a message,
  * some controls, and may be dismissible.
  */
-export default class Alert<T extends AlertAttrs> extends Component<T> {
+export default class Alert<T extends AlertAttrs = AlertAttrs> extends Component<T> {
   view(vnode: Mithril.Vnode) {
     const attrs = Object.assign({}, this.attrs);
 
