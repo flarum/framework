2b676dcac5dc4509252102024bf078d391f7a92e
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2020-09-30 15:51:00 -0400

    Translate notification emails (#30)

--- packages/ext-subscriptions/views/emails/newPost.blade.php
+++ packages/ext-subscriptions/views/emails/newPost.blade.php
@@ -1,14 +1,7 @@
-Hey {!! $user->display_name !!}!
-
-{!! $blueprint->post->user->display_name !!} made a post in a discussion you're following: {!! $blueprint->post->discussion->title !!}
-
-To view the new activity, check out the following link:
-{!! app()->url() !!}/d/{!! $blueprint->post->discussion_id !!}/{!! $blueprint->post->number !!}
-
----
-
-{!! $blueprint->post->content !!}
-
----
-
-You won't receive any more notifications about this discussion until you're up-to-date.
+{!! $translator->trans('flarum-subscriptions.email.new_post.body', [
+'{recipient_display_name}' => $user->display_name,
+'{poster_display_name}' => $blueprint->post->user->display_name,
+'{title}' => $blueprint->post->discussion->title,
+'{url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->post->discussion_id, 'near' => $blueprint->post->number]),
+'{content}' => $blueprint->post->content
+]) !!}
