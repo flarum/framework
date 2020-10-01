9ef207fa85183de606c37c4f17da8f94803bf2c9
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2020-10-01 11:16:20 -0400

    Translate notification emails (#50)

--- extensions/mentions/src/Notification/PostMentionedBlueprint.php
+++ extensions/mentions/src/Notification/PostMentionedBlueprint.php
@@ -12,6 +12,7 @@ namespace Flarum\Mentions\Notification;
 use Flarum\Notification\Blueprint\BlueprintInterface;
 use Flarum\Notification\MailableInterface;
 use Flarum\Post\Post;
+use Symfony\Component\Translation\TranslatorInterface;
 
 class PostMentionedBlueprint implements BlueprintInterface, MailableInterface
 {
@@ -70,9 +71,12 @@ class PostMentionedBlueprint implements BlueprintInterface, MailableInterface
     /**
      * {@inheritdoc}
      */
-    public function getEmailSubject()
+    public function getEmailSubject(TranslatorInterface $translator)
     {
-        return "{$this->reply->user->display_name} replied to your post in {$this->post->discussion->title}";
+        return $translator->trans('flarum-mentions.email.post_mentioned.subject', [
+            '{replier_display_name}' => $this->post->user->display_name,
+            '{title}' => $this->post->discussion->title
+        ]);
     }
 
     /**
--- extensions/mentions/views/emails/postMentioned.blade.php
+++ extensions/mentions/views/emails/postMentioned.blade.php
@@ -1,9 +1,8 @@
-Hey {!! $user->display_name !!}!
-
-{!! $blueprint->reply->user->display_name !!} replied to your post (#{!! $blueprint->post->number !!}) in {!! $blueprint->post->discussion->title !!}.
-
-{!! app()->url() !!}/d/{!! $blueprint->reply->discussion_id !!}/{!! $blueprint->reply->number !!}
-
----
-
-{!! $blueprint->reply->content !!}
+{!! $translator->trans('flarum-mentions.email.post_mentioned.body', [
+'{recipient_display_name}' => $user->display_name,
+'{replier_display_name}' => $blueprint->post->user->display_name,
+'{post_number}' => $blueprint->post->number,
+'{title}' => $blueprint->post->discussion->title,
+'{url}' => $url->to('forum')->route('discussion', ['id' => $blueprint->reply->discussion_id, 'near' => $blueprint->reply->number]),
+'{content}' => $blueprint->reply->content
+]) !!}
