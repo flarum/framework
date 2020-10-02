2e75a564af1c8f8b03305f1f223c108a1eafc266
Wadim Kalmykov <w@wadim-k.com>
2020-10-02 19:23:03 +0700

    add more documentation

--- js/src/common/utils/ActiveRequests.ts
+++ js/src/common/utils/ActiveRequests.ts
@@ -2,6 +2,10 @@
  * The `ActiveRequests` class keeps track of pending XHR requests.
  */
 export default class ActiveRequests extends Map {
+  /**
+   * Aborts a request. Aborted request promises neither get rejected
+   * nor resolved, they remain pending.
+   */
   abort(requestId: number) {
     const xhr: XMLHttpRequest = this.get(requestId);
     if (!xhr) return;
