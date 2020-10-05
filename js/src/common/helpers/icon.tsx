ff1b208a55e02cfcfde042b42f51369ee7b35563
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-10-05 01:31:47 -0300

    Convert icon helper to Typescript

--- js/src/common/helpers/icon.tsx
+++ js/src/common/helpers/icon.tsx
@@ -1,11 +1,13 @@
+import { Attributes, Vnode } from 'mithril';
+
 /**
  * The `icon` helper displays an icon.
  *
  * @param {String} fontClass The full icon class, prefix and the icon’s name.
- * @param {Object} attrs Any other attributes to apply.
- * @return {Object}
+ * @param {Attributes} attrs Any other attributes to apply.
+ * @return {Vnode}
  */
-export default function icon(fontClass, attrs = {}) {
+export default function icon(fontClass: string, attrs: Attributes = {}): Vnode {
   attrs.className = 'icon ' + fontClass + ' ' + (attrs.className || '');
 
   return <i {...attrs} />;
