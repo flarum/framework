2d807e9561717dd396c78d61805472fb1d09a8af
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2020-10-24 19:19:06 -0300

    Fix typo

--- js/src/common/helpers/fullTime.tsx
+++ js/src/common/helpers/fullTime.tsx
@@ -1,11 +1,11 @@
 import dayjs from 'dayjs';
-import * as Mithrill from 'mithril';
+import * as Mithril from 'mithril';
 
 /**
  * The `fullTime` helper displays a formatted time string wrapped in a <time>
  * tag.
  */
-export default function fullTime(time: Date): Mithrill.Vnode {
+export default function fullTime(time: Date): Mithril.Vnode {
   const d = dayjs(time);
 
   const datetime = d.format();
