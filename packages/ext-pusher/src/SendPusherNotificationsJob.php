4eeb301f7eeeb88b86a71a66306e34b3dd623d23
Sami Mazouz <sychocouldy@gmail.com>
2020-11-05 18:09:13 +0100

    Use a notification driver with the new channel extender (#28)

---
+++ packages/ext-pusher/src/SendPusherNotificationsJob.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Pusher;
+
+use Flarum\Notification\Blueprint\BlueprintInterface;
+use Flarum\Queue\AbstractJob;
+use Flarum\User\User;
+use Pusher;
+
+class SendPusherNotificationsJob extends AbstractJob
+{
+    /**
+     * @var BlueprintInterface
+     */
+    private $blueprint;
+
+    /**
+     * @var User[]
+     */
+    private $recipients;
+
+    public function __construct(BlueprintInterface $blueprint, array $recipients)
+    {
+        $this->blueprint = $blueprint;
+        $this->recipients = $recipients;
+    }
+
+    public function handle(Pusher $pusher)
+    {
+        foreach ($this->recipients as $user) {
+            if ($user->shouldAlert($this->blueprint::getType())) {
+                $pusher->trigger('private-user'.$user->id, 'notification', null);
+            }
+        }
+    }
+}
