9b18a303c1dd24b1dab409e18baddb54d6cd6507
SychO9 <sychocouldy@gmail.com>
2020-11-05 13:29:29 +0100

    Test the order in which providers resolve

--- tests/integration/extenders/ServiceProviderTest.php
+++ tests/integration/extenders/ServiceProviderTest.php
@@ -12,33 +12,37 @@ namespace Flarum\Tests\integration\extenders;
 use Flarum\Extend;
 use Flarum\Foundation\AbstractServiceProvider;
 use Flarum\Tests\integration\TestCase;
-use Illuminate\Contracts\Container\BindingResolutionException;
 
 class ServiceProviderTest extends TestCase
 {
     /**
      * @test
      */
-    public function provider_doesnt_work_by_default()
+    public function providers_dont_work_by_default()
     {
         $this->app();
 
-        $this->expectException(BindingResolutionException::class);
-
-        $this->app->getContainer()->make('flarum.tests.service_provider_test');
+        $this->assertIsArray(
+            $this->app->getContainer()->make('flarum.forum.middleware')
+        );
     }
 
     /**
      * @test
      */
-    public function provider_works_if_added()
+    public function providers_order_is_correct()
     {
-        $this->extend((new Extend\ServiceProvider())->register(CustomServiceProvider::class));
+        $this->extend(
+            (new Extend\ServiceProvider())
+                ->register(CustomServiceProvider::class)
+                ->register(SecondCustomServiceProvider::class)
+        );
 
         $this->app();
 
-        $this->assertTrue(
-            $this->app->getContainer()->make('flarum.tests.service_provider_test')
+        $this->assertEquals(
+            'overriden_by_provider_test__final',
+            $this->app->getContainer()->make('flarum.forum.middleware')
         );
     }
 }
@@ -47,8 +51,37 @@ class CustomServiceProvider extends AbstractServiceProvider
 {
     public function register()
     {
-        $this->app->singleton('flarum.tests.service_provider_test', function () {
-            return true;
+        // First we override the singleton here.
+        $this->app->extend('flarum.forum.middleware', function () {
+            return 'overriden_by_provider_test';
         });
     }
+
+    public function boot()
+    {
+        // Third we override one last time here, to make sure this is the final result.
+        $this->app->extend('flarum.forum.middleware', function ($forumRoutes) {
+            return 'overriden_by_provider_test__final';
+        });
+    }
+}
+
+class SecondCustomServiceProvider extends AbstractServiceProvider
+{
+    public function register()
+    {
+        // Second we check that the singleton was overriden here.
+        $this->app->extend('flarum.forum.middleware', function ($forumRoutes) {
+            if ($forumRoutes !== 'overriden_by_provider_test') {
+                throw new ProviderTestException('CustomServiceProvider did not override the singleton.');
+            }
+
+            return $forumRoutes;
+        });
+    }
+}
+
+class ProviderTestException extends \Exception
+{
+    // ...
 }
