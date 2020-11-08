6b63efb3001d150afc03024f6705645fb16d1a2f
SychO9 <sychocouldy@gmail.com>
2020-11-08 15:27:44 +0100

    Add documentation

--- src/Extend/ApiSerializer.php
+++ src/Extend/ApiSerializer.php
@@ -21,25 +21,43 @@ class ApiSerializer implements ExtenderInterface
     private $settings = [];
     private $relationships = [];
 
+    /**
+     * @param string $serializerClass The ::class attribute of the serializer you are modifying.
+     *                                This serializer should extend from \Flarum\Api\Serializer\AbstractSerializer.
+     */
     public function __construct(string $serializerClass)
     {
         $this->serializerClass = $serializerClass;
     }
 
     /**
-     * @param callable|string $handler
+     * Add to or modify the attributes array of this serializer.
+     *
+     * @param callable|string $callback
+     *
+     * The callback can be a closure or an invokable class, and should accept:
+     * - $attributes: An array of existing attributes.
+     * - $model: An instance of the model being serialized.
+     * - $serializer: An instance of this serializer.
+     *
+     * The callable should return:
+     * - An array of additional attributes to merge with the existing array.
+     *   Or a modified $attributes array.
+     *
      * @return self
      */
-    public function attributes($handler)
+    public function attributes($callback)
     {
-        $this->attributeHandlers[] = $handler;
+        $this->attributeHandlers[] = $callback;
 
         return $this;
     }
 
     /**
-     * @param string $key
-     * @param mixed $default
+     * Add a setting value to the attributes of this serializer.
+     *
+     * @param string $key: The key of the setting. It should be prefixed with the extension ID. (ex: extension-id.setting_key).
+     * @param mixed $default: The default value of the setting.
      * @return self
      */
     public function setting(string $key, $default = null)
@@ -50,8 +68,13 @@ class ApiSerializer implements ExtenderInterface
     }
 
     /**
-     * @param string $name
-     * @param string $serializerClass
+     * Establish a simple hasOne relationship from this serializer to another serializer.
+     * This represents a one-to-one relationship.
+     *
+     * @param string $name: The name of the relation. Has to be unique from other relation names.
+     *                      The relation has to exist in the model handled by this serializer.
+     * @param string $serializerClass: The ::class attribute the serializer that handles this relation.
+     *                                 This serializer should extend from \Flarum\Api\Serializer\AbstractSerializer.
      * @return self
      */
     public function hasOneRelationship(string $name, string $serializerClass)
@@ -62,8 +85,13 @@ class ApiSerializer implements ExtenderInterface
     }
 
     /**
-     * @param string $name
-     * @param string $serializerClass
+     * Establish a simple hasMany relationship from this serializer to another serializer.
+     * This represents a one-to-many relationship.
+     *
+     * @param string $name: The name of the relation. Has to be unique from other relation names.
+     *                      The relation has to exist in the model handled by this serializer.
+     * @param string $serializerClass: The ::class attribute the serializer that handles this relation.
+     *                                 This serializer should extend from \Flarum\Api\Serializer\AbstractSerializer.
      * @return self
      */
     public function hasManyRelationship(string $name, string $serializerClass)
@@ -74,8 +102,19 @@ class ApiSerializer implements ExtenderInterface
     }
 
     /**
-     * @param string $name
+     * Add a relationship from this serializer to another serializer.
+     *
+     * @param string $name: The name of the relation. Has to be unique from other relation names.
+     *                      The relation has to exist in the model handled by this serializer.
      * @param callable|string $callback
+     *
+     * The callable can be a closure or an invokable class, and should accept:
+     * - $serializer: An instance of this serializer.
+     * - $model: An instance of the model being serialized.
+     *
+     * The callable should return:
+     * - $relationship: An instance of \Tobscure\JsonApi\Relationship.
+     *
      * @return self
      */
     public function relationship(string $name, $callback)
