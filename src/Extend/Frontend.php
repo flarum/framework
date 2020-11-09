f5cfec15e36b42d58679be65268e7b0dc7780e09
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-08 21:49:11 -0500

    Add missing import

--- src/Extend/Frontend.php
+++ src/Extend/Frontend.php
@@ -12,6 +12,7 @@ namespace Flarum\Extend;
 use Flarum\Extension\Event\Disabled;
 use Flarum\Extension\Event\Enabled;
 use Flarum\Extension\Extension;
+use Flarum\Foundation\ContainerUtil;
 use Flarum\Foundation\Event\ClearingCache;
 use Flarum\Frontend\Assets;
 use Flarum\Frontend\Compiler\Source\SourceCollector;
