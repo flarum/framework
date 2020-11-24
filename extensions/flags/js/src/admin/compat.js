9e2d794cd499b75735780716fe2e31763c76868d
Charlie <13856015+KyrneDev@users.noreply.github.com>
2020-11-24 13:58:40 -0800

    AdminUX Changes (#33)

--- extensions/flags/js/src/admin/compat.js
+++
@@ -1,5 +0,0 @@
-import FlagsSettingsModal from './components/FlagsSettingsModal';
-
-export default {
-  'flags/components/FlagsSettingsModal': FlagsSettingsModal,
-};
--- extensions/flags/js/src/admin/index.js
+++ extensions/flags/js/src/admin/index.js
@@ -1,31 +1,26 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
-import PermissionGrid from 'flarum/components/PermissionGrid';
-
-import FlagsSettingsModal from './components/FlagsSettingsModal';
 
 app.initializers.add('flarum-flags', () => {
-  app.extensionSettings['flarum-flags'] = () => app.modal.show(FlagsSettingsModal);
+  app.extensionData.for('flarum-flags')
+    .registerSetting({
+      setting: 'flarum-flags.guidelines_url',
+      type: 'text',
+      label: app.translator.trans('flarum-flags.admin.settings.guidelines_url_label')
+    }, 15)
+    .registerSetting({
+      setting: 'flarum-flags.can_flag_own',
+      type: 'boolean',
+      label: app.translator.trans('flarum-flags.admin.settings.flag_own_posts_label')
+    })
+    .registerPermission({
+        icon: 'fas fa-flag',
+        label: app.translator.trans('flarum-flags.admin.permissions.view_flags_label'),
+        permission: 'discussion.viewFlags'
+      }, 'moderate', 65)
 
-  extend(PermissionGrid.prototype, 'moderateItems', items => {
-    items.add('viewFlags', {
-      icon: 'fas fa-flag',
-      label: app.translator.trans('flarum-flags.admin.permissions.view_flags_label'),
-      permission: 'discussion.viewFlags'
-    }, 65);
-  });
-
-  extend(PermissionGrid.prototype, 'replyItems', items => {
-    items.add('flagPosts', {
-      icon: 'fas fa-flag',
-      label: app.translator.trans('flarum-flags.admin.permissions.flag_posts_label'),
-      permission: 'discussion.flagPosts'
-    }, 70);
-  });
+    .registerPermission({
+        icon: 'fas fa-flag',
+        label: app.translator.trans('flarum-flags.admin.permissions.flag_posts_label'),
+        permission: 'discussion.flagPosts'
+      }, 'reply', 65);
 });
-
-// Expose compat API
-import flagsCompat from './compat';
-import { compat } from '@flarum/core/admin';
-
-Object.assign(compat, flagsCompat);
