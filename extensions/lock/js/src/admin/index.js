e5913ba7f675cb03d29a1424066eea7d03bebe47
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 01:20:17 -0500

    Admin js changes for admin ux revamp

--- extensions/lock/js/src/admin/index.js
+++ extensions/lock/js/src/admin/index.js
@@ -1,13 +1,10 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
-import PermissionGrid from 'flarum/components/PermissionGrid';
 
 app.initializers.add('lock', () => {
-  extend(PermissionGrid.prototype, 'moderateItems', items => {
-    items.add('lock', {
+  app.extensionData.for('flarum-lock')
+    .registerPermission({
       icon: 'fas fa-lock',
       label: app.translator.trans('flarum-lock.admin.permissions.lock_discussions_label'),
       permission: 'discussion.lock'
-    }, 95);
-  });
+    }, 'moderate', 95);
 });
