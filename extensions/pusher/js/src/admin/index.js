f5c8dade5599b8862528da1a170f4874c2a6fdd3
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 01:26:36 -0500

    Update admin js for admin us redesign

--- extensions/pusher/js/src/admin/index.js
+++ extensions/pusher/js/src/admin/index.js
@@ -1,8 +1,38 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
 
-import PusherSettingsModal from './components/PusherSettingsModal';
-
 app.initializers.add('flarum-pusher', app => {
-  app.extensionSettings['flarum-pusher'] = () => app.modal.show(PusherSettingsModal);
+  app.extensionData
+    .for('flarum-pusher')
+    .registerSetting(
+      {
+        setting: 'flarum-pusher.app_id',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_id_label'),
+        type: 'text',
+      },
+      30
+    )
+    .registerSetting(
+      {
+        setting: 'flarum-pusher.app_key',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_key_label'),
+        type: 'text',
+      },
+      20
+    )
+    .registerSetting(
+      {
+        setting: 'flarum-pusher.app_secret',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_secret_label'),
+        type: 'text',
+      },
+      10
+    )
+    .registerSetting(
+      {
+        setting: 'flarum-pusher.app_cluster',
+        label: app.translator.trans('flarum-pusher.admin.pusher_settings.app_cluster_label'),
+        type: 'text',
+      },
+      0
+    );
 });
