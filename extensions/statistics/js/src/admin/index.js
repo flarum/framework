3b86682f84ecc2f7ce275ab73b78e167895e5668
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 01:28:33 -0500

    Update admin JS for admin ux redesign

--- extensions/statistics/js/src/admin/index.js
+++ extensions/statistics/js/src/admin/index.js
@@ -7,6 +7,6 @@ import StatisticsWidget from './components/StatisticsWidget';
 
 app.initializers.add('flarum-statistics', () => {
   extend(DashboardPage.prototype, 'availableWidgets', widgets => {
-    widgets.push(<StatisticsWidget/>);
+    widgets.add('statistics', <StatisticsWidget/>, 20);
   });
 });
