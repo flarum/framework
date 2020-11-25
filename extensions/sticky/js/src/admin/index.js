27fa6456ec924cf1aaf70c68f5d22526a09f8ba7
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 01:31:19 -0500

    Update admin js for admin ux redesign

--- extensions/sticky/js/src/admin/index.js
+++ extensions/sticky/js/src/admin/index.js
@@ -1,13 +1,11 @@
-import { extend } from 'flarum/extend';
 import app from 'flarum/app';
-import PermissionGrid from 'flarum/components/PermissionGrid';
 
 app.initializers.add('flarum-sticky', () => {
-  extend(PermissionGrid.prototype, 'moderateItems', items => {
-    items.add('sticky', {
+  app.extensionData
+    .for('flarum-sticky')
+    .registerPermission({
       icon: 'fas fa-thumbtack',
       label: app.translator.trans('flarum-sticky.admin.permissions.sticky_discussions_label'),
       permission: 'discussion.sticky'
-    }, 95);
-  });
+    }, 'moderate', 95);
 });
