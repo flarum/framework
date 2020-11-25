18922cdf1299f6a397dbb50e6083722f98b6fa6a
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-11-25 01:03:14 -0500

    Update admin JS for new UX

--- packages/ext-akismet/js/src/admin/index.js
+++ packages/ext-akismet/js/src/admin/index.js
@@ -1,7 +1,11 @@
 import app from 'flarum/app';
 
-import AkismetSettingsModal from './components/AkismetSettingsModal';
-
 app.initializers.add('flarum-akismet', () => {
-  app.extensionSettings['flarum-akismet'] = () => app.modal.show(AkismetSettingsModal);
+  app.extensionData
+    .for('flarum-akismet')
+    .registerSetting({
+      setting: 'flarum-akismet.api_key',
+      type: 'text',
+      label: app.translator.trans('flarum-akismet.admin.akismet_settings.api_key_label')
+    });
 });
