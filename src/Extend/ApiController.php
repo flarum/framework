32c1fbd2d5fa1638229fb8ba7579cc9782c989bd
SychO9 <sychocouldy@gmail.com>
2020-11-27 18:37:16 +0100

    Add more extender methods

--- src/Extend/ApiController.php
+++ src/Extend/ApiController.php
@@ -11,6 +11,7 @@ namespace Flarum\Extend;
 
 use Flarum\Api\Controller\AbstractSerializeController;
 use Flarum\Extension\Extension;
+use Flarum\Foundation\ContainerUtil;
 use Illuminate\Contracts\Container\Container;
 
 class ApiController implements ExtenderInterface
@@ -18,6 +19,16 @@ class ApiController implements ExtenderInterface
     private $controllerClass;
     private $beforeDataCallbacks = [];
     private $beforeSerializationCallbacks = [];
+    private $serializer;
+    private $addIncludes = [];
+    private $removeIncludes = [];
+    private $addOptionalIncludes = [];
+    private $removeOptionalIncludes = [];
+    private $limit;
+    private $maxLimit;
+    private $addSortFields = [];
+    private $removeSortFields = [];
+    private $sort;
 
     /**
      * @param string $controllerClass The ::class attribute of the controller you are modifying.
@@ -44,7 +55,7 @@ class ApiController implements ExtenderInterface
     }
 
     /**
-     * @param $callback
+     * @param callable|string $callback
      *
      * The callback can be a closure or an invokable class, and should accept:
      * - $controller: An instance of this controller.
@@ -65,8 +76,202 @@ class ApiController implements ExtenderInterface
         return $this;
     }
 
+    /**
+     * Set the serializer that will serialize data for the endpoint.
+     *
+     * @param string $serializerClass
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function setSerializer(string $serializerClass, $callback = null)
+    {
+        $this->serializer = [$serializerClass, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Include the given relationship by default.
+     *
+     * @param string|array $name
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function addInclude($name, $callback = null)
+    {
+        $this->addIncludes[] = [$name, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Don't include the given relationship by default.
+     *
+     * @param string|array $name
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function removeInclude($name, $callback = null)
+    {
+        $this->removeIncludes[] = [$name, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Make the given relationship available for inclusion.
+     *
+     * @param string|array $name
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function addOptionalInclude($name, $callback = null)
+    {
+        $this->addOptionalIncludes[] = [$name, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Don't allow the given relationship to be included.
+     *
+     * @param string|array $name
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function removeOptionalInclude($name, $callback = null)
+    {
+        $this->removeOptionalIncludes = [$name, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Set the default number of results.
+     *
+     * @param int $limit
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function setLimit(int $limit, $callback = null)
+    {
+        $this->limit = [$limit, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Set the maximum number of results.
+     *
+     * @param int $max
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function setMaxLimit(int $max, $callback = null)
+    {
+        $this->maxLimit = [$max, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Allow sorting results by the given field.
+     *
+     * @param string|array $field
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function addSortField($field, $callback = null)
+    {
+        $this->addSortFields[] = [$field, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Disallow sorting results by the given field.
+     *
+     * @param string|array $field
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function removeSortField($field, $callback = null)
+    {
+        $this->removeSortFields[] = [$field, $callback];
+
+        return $this;
+    }
+
+    /**
+     * Set the default sort order for the results.
+     *
+     * @param array $sort
+     * @param callable|string|null $callback
+     * @return self
+     */
+    public function setSort(array $sort, $callback = null)
+    {
+        $this->sort = [$sort, $callback];
+
+        return $this;
+    }
+
     public function extend(Container $container, Extension $extension = null)
     {
+        $this->beforeDataCallbacks[] = function (AbstractSerializeController $controller) use ($container) {
+            if (isset($this->serializer) && $this->isApplicable($this->serializer[1], $controller, $container)) {
+                $controller->setSerializer($this->serializer[0]);
+            }
+
+            foreach ($this->addIncludes as $addingInclude) {
+                if ($this->isApplicable($addingInclude[1], $controller, $container)) {
+                    $controller->addInclude($addingInclude[0]);
+                }
+            }
+
+            foreach ($this->removeIncludes as $removingInclude) {
+                if ($this->isApplicable($removingInclude[1], $controller, $container)) {
+                    $controller->removeInclude($removingInclude[0]);
+                }
+            }
+
+            foreach ($this->addOptionalIncludes as $addingOptionalInclude) {
+                if ($this->isApplicable($addingOptionalInclude[1], $controller, $container)) {
+                    $controller->addOptionalInclude($addingOptionalInclude[0]);
+                }
+            }
+
+            foreach ($this->removeOptionalIncludes as $removingOptionalInclude) {
+                if ($this->isApplicable($removingOptionalInclude[1], $controller, $container)) {
+                    $controller->removeOptionalInclude($removingOptionalInclude[0]);
+                }
+            }
+
+            foreach ($this->addSortFields as $addingSortField) {
+                if ($this->isApplicable($addingSortField[1], $controller, $container)) {
+                    $controller->addSortField($addingSortField[0]);
+                }
+            }
+
+            foreach ($this->removeSortFields as $removingSortField) {
+                if ($this->isApplicable($removingSortField[1], $controller, $container)) {
+                    $controller->removeSortField($removingSortField[0]);
+                }
+            }
+
+            if (isset($this->limit) && $this->isApplicable($this->limit[1], $controller, $container)) {
+                $controller->setLimit($this->limit[0]);
+            }
+
+            if (isset($this->maxLimit) && $this->isApplicable($this->maxLimit[1], $controller, $container)) {
+                $controller->setMaxLimit($this->maxLimit[0]);
+            }
+
+            if (isset($this->sort) && $this->isApplicable($this->sort[1], $controller, $container)) {
+                $controller->setSort($this->sort[0]);
+            }
+        };
+
         foreach ($this->beforeDataCallbacks as $beforeDataCallback) {
             AbstractSerializeController::addDataPreparationCallback($this->controllerClass, $beforeDataCallback);
         }
@@ -74,5 +279,22 @@ class ApiController implements ExtenderInterface
         foreach ($this->beforeSerializationCallbacks as $beforeSerializationCallback) {
             AbstractSerializeController::addSerializationPreparationCallback($this->controllerClass, $beforeSerializationCallback);
         }
+    }
+
+    /**
+     * @param callable|string|null $callback
+     * @param AbstractSerializeController $controller
+     * @param Container $container
+     * @return bool
+     */
+    private function isApplicable($callback, AbstractSerializeController $controller, Container $container)
+    {
+        if (! isset($callback)) {
+            return true;
+        }
+
+        $callback = ContainerUtil::wrapCallback($callback, $container);
+
+        return (bool) $callback($controller);
     }
 }
