d06812a6bf3f90b8a718d79eef3fcd39302ee6a1
Sam Rechsteiner <samrech@umich.edu>
2020-11-28 21:59:43 -0500

    change updateCommentsCount to accept a user parameter

--- src/User/UserMetadataUpdater.php
+++ src/User/UserMetadataUpdater.php
@@ -15,6 +15,7 @@ use Flarum\Discussion\Event\Started;
 use Flarum\Post\Event\Deleted as PostDeleted;
 use Flarum\Post\Event\Posted;
 use Illuminate\Contracts\Events\Dispatcher;
+use Flarum\User\User;
 
 class UserMetadataUpdater
 {
@@ -34,7 +35,7 @@ class UserMetadataUpdater
      */
     public function whenPostWasPosted(Posted $event)
     {
-        $this->updateCommentsCount($event->post);
+        $this->updateCommentsCount($event->post->user);
     }
 
     /**
@@ -42,7 +43,7 @@ class UserMetadataUpdater
      */
     public function whenPostWasDeleted(PostDeleted $event)
     {
-        $this->updateCommentsCount($event->post);
+        $this->updateCommentsCount($event->post->user);
     }
 
     /**
@@ -59,16 +60,14 @@ class UserMetadataUpdater
     public function whenDiscussionWasDeleted(DiscussionDeleted $event)
     {
         $this->updateDiscussionsCount($event->discussion);
-        $this->updateCommentsCount($event->discussion);
+        $this->updateCommentsCount($event->discussion->user);
     }
 
     /**
-     * @param \Flarum\Discussion\Discussion|\Flarum\Post\Post $post
+     * @param \Flarum\User\User $user
      */
-    private function updateCommentsCount($post)
+    private function updateCommentsCount(User $user)
     {
-        $user = $post->user;
-
         if ($user && $user->exists) {
             $user->refreshCommentCount()->save();
         }
