d8103864cf4ef2b41da4b3250692df2081ddcc58
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-02 15:29:08 -0500

    If unique nicknames are enabled, don't allow using someone else's username as a nickname

--- packages/ext-nicknames/src/SaveNicknameToDatabase.php
+++ packages/ext-nicknames/src/SaveNicknameToDatabase.php
@@ -21,7 +21,18 @@ class SaveNicknameToDatabase {
             } else {
                 $actor->assertCan('edit', $user);
             }
-            $user->nickname = $attributes['nickname'];
+
+            $nickname = $attributes['nickname'];
+
+            // If unique validation is enabled, the nickname will be checked
+            // against ALL nicknames and usernames, including the username
+            // of the current user. So, to allow users to reset their nickname
+            // back to their username, in this case we'd set it to null.
+            if ($user->username === $nickname) {
+                $user->nickname = null;
+            } else {
+                $user->nickname = $nickname;
+            }
         }
     }
 }
