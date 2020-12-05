5353ccbd8c54874d74947c1142cc24a153115a43
Sami Mazouz <sychocouldy@gmail.com>
2020-12-05 23:27:05 +0100

    Make sure usernames cannot take the value of nicknames (#1)

--- packages/ext-nicknames/src/AddNicknameValidation.php
+++ packages/ext-nicknames/src/AddNicknameValidation.php
@@ -21,7 +21,10 @@ class AddNicknameValidation
 
     public function __invoke($flarumValidator, Validator $validator)
     {
-        $nicknameRules = [
+        $idSuffix = $flarumValidator->getUser() ? ','.$flarumValidator->getUser()->id : '';
+        $rules = $validator->getRules();
+
+        $rules['nickname'] = [
             function ($attribute, $value, $fail) {
                 $regex = $this->settings->get('flarum-nicknames.regex');
                 if ($regex && !preg_match_all("/$regex/", $value)) {
@@ -34,12 +37,11 @@ class AddNicknameValidation
         ];
 
         if ($this->settings->get('flarum-nicknames.unique')) {
-            $nicknameRules[] = 'unique:users,username';
-            $nicknameRules[] = 'unique:users,nickname';
+            $rules['nickname'][] = 'unique:users,username'.$idSuffix;
+            $rules['nickname'][] = 'unique:users,nickname'.$idSuffix;
+            $rules['username'][] = 'unique:users,nickname'.$idSuffix;
         }
 
-        $validator->setRules([
-                'nickname' => $nicknameRules,
-            ] + $validator->getRules());
+        $validator->setRules($rules);
     }
 }
