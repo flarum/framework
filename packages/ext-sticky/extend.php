ceac153330da2a2b30f5b00f06367481f20c4ac5
Sami Mazouz <sychocouldy@gmail.com>
2020-12-08 17:53:05 +0100

    Use new extenders (#22)

--- packages/ext-sticky/extend.php
+++ packages/ext-sticky/extend.php
@@ -7,8 +7,11 @@
  * LICENSE file that was distributed with this source code.
  */
 
+use Flarum\Api\Controller\ListDiscussionsController;
+use Flarum\Api\Serializer\DiscussionSerializer;
 use Flarum\Extend;
 use Flarum\Sticky\Listener;
+use Flarum\Sticky\Post\DiscussionStickiedPost;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return [
@@ -16,13 +19,26 @@ return [
         ->js(__DIR__.'/js/dist/forum.js')
         ->css(__DIR__.'/less/forum.less'),
 
+    (new Extend\Post())
+        ->type(DiscussionStickiedPost::class),
+
+    (new Extend\ApiSerializer(DiscussionSerializer::class))
+        ->attribute('isSticky', function (DiscussionSerializer $serializer, $discussion) {
+            return (bool) $discussion->is_sticky;
+        })
+        ->attribute('canSticky', function (DiscussionSerializer $serializer, $discussion) {
+            return (bool) $serializer->getActor()->can('sticky', $discussion);
+        }),
+
+    (new Extend\ApiController(ListDiscussionsController::class))
+        ->addInclude('firstPost'),
+
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js'),
 
     new Extend\Locales(__DIR__.'/locale'),
 
     function (Dispatcher $events) {
-        $events->subscribe(Listener\AddApiAttributes::class);
         $events->subscribe(Listener\CreatePostWhenDiscussionIsStickied::class);
         $events->subscribe(Listener\PinStickiedDiscussionsToTop::class);
         $events->subscribe(Listener\SaveStickyToDatabase::class);
--- packages/ext-sticky/src/Listener/CreatePostWhenDiscussionIsStickied.php
+++ packages/ext-sticky/src/Listener/CreatePostWhenDiscussionIsStickied.php
@@ -10,7 +10,6 @@
 namespace Flarum\Sticky\Listener;
 
 use Flarum\Discussion\Discussion;
-use Flarum\Event\ConfigurePostTypes;
 use Flarum\Sticky\Event\DiscussionWasStickied;
 use Flarum\Sticky\Event\DiscussionWasUnstickied;
 use Flarum\Sticky\Post\DiscussionStickiedPost;
@@ -24,17 +23,8 @@ class CreatePostWhenDiscussionIsStickied
      */
     public function subscribe(Dispatcher $events)
     {
-        $events->listen(ConfigurePostTypes::class, [$this, 'configurePostTypes']);
         $events->listen(DiscussionWasStickied::class, [$this, 'whenDiscussionWasStickied']);
         $events->listen(DiscussionWasUnstickied::class, [$this, 'whenDiscussionWasUnstickied']);
-    }
-
-    /**
-     * @param ConfigurePostTypes $event
-     */
-    public function configurePostTypes(ConfigurePostTypes $event)
-    {
-        $event->add(DiscussionStickiedPost::class);
     }
 
     /**
