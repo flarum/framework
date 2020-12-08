d1968d7cac8a59f1b74b0d395a11bab6c2d5ec08
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-08 15:08:18 -0500

    Refactor for new extenders

--- packages/ext-approval/src/Access/ScopePrivatePostVisibility.php
+++ packages/ext-approval/src/Access/ScopePrivatePostVisibility.php
@@ -10,45 +10,23 @@
 namespace Flarum\Approval\Access;
 
 use Flarum\Discussion\Discussion;
-use Flarum\Event\ScopeModelVisibility;
-use Flarum\Post\Post;
-use Flarum\User\AbstractPolicy;
 use Flarum\User\User;
-use Illuminate\Contracts\Events\Dispatcher;
 use Illuminate\Database\Eloquent\Builder;
 
-class PostPolicy extends AbstractPolicy
+class ScopePrivatePostVisibility
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected $model = Post::class;
-
-    /**
-     * @var Dispatcher
-     */
-    protected $events;
-
-    /**
-     * @param Dispatcher $events
-     */
-    public function __construct(Dispatcher $events)
-    {
-        $this->events = $events;
-    }
-
     /**
      * @param Builder $query
      * @param User $actor
      */
-    public function findPrivate(User $actor, Builder $query)
+    public function __invoke(User $actor, Builder $query)
     {
         // Show private posts if they require approval and they are
         // authored by the current user, or the current user has permission to
         // approve posts.
         $query->where('posts.is_approved', 0);
 
-        if (! $actor->hasPermission('discussion.approvePosts')) {
+        if (!$actor->hasPermission('discussion.approvePosts')) {
             $query->where(function (Builder $query) use ($actor) {
                 $query->where('posts.user_id', $actor->id)
                     ->orWhereExists($this->discussionWhereCanApprovePosts($actor));
@@ -62,16 +40,9 @@ class PostPolicy extends AbstractPolicy
             $query->selectRaw('1')
                 ->from('discussions')
                 ->whereColumn('discussions.id', 'posts.discussion_id')
-                ->where($this->canApprovePosts($actor));
-        };
-    }
-
-    private function canApprovePosts(User $actor)
-    {
-        return function ($query) use ($actor) {
-            $this->events->dispatch(
-                new ScopeModelVisibility(Discussion::query()->setQuery($query), $actor, 'approvePosts')
-            );
+                ->where(function ($query) use ($actor) {
+                    Discussion::query()->setQuery($query)->whereVisibleTo($actor, 'approvePosts');
+                });
         };
     }
 }
