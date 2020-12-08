6bed0b07ad9cb655ea34c4fadf15bb0afead05c7
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2020-12-08 14:02:06 -0500

    Use new Policy and User Permission Groups permissions

--- packages/ext-suspend/extend.php
+++ packages/ext-suspend/extend.php
@@ -11,13 +11,14 @@ use Flarum\Api\Serializer\BasicUserSerializer;
 use Flarum\Api\Serializer\UserSerializer;
 use Flarum\Event\ConfigureUserGambits;
 use Flarum\Extend;
-use Flarum\Suspend\Access;
+use Flarum\Suspend\Access\UserPolicy;
 use Flarum\Suspend\AddUserSuspendAttributes;
 use Flarum\Suspend\Event\Suspended;
 use Flarum\Suspend\Event\Unsuspended;
 use Flarum\Suspend\Listener;
 use Flarum\Suspend\Notification\UserSuspendedBlueprint;
 use Flarum\Suspend\Notification\UserUnsuspendedBlueprint;
+use Flarum\Suspend\RevokeAccessFromSuspendedUsers;
 use Flarum\Suspend\Search\Gambit\SuspendedGambit;
 use Flarum\User\Event\Saving;
 use Flarum\User\User;
@@ -44,16 +45,19 @@ return [
         ->type(UserSuspendedBlueprint::class, BasicUserSerializer::class, ['alert', 'email'])
         ->type(UserUnsuspendedBlueprint::class, BasicUserSerializer::class, ['alert', 'email']),
 
+    (new Extend\Event())
+        ->listen(Saving::class, Listener\SaveSuspensionToDatabase::class)
+        ->listen(Suspended::class, Listener\SendNotificationWhenUserIsSuspended::class)
+        ->listen(Unsuspended::class, Listener\SendNotificationWhenUserIsUnsuspended::class),
+
+    (new Extend\Policy())
+        ->modelPolicy(User::class, UserPolicy::class),
+
+    (new Extend\User())
+        ->permissionGroups(RevokeAccessFromSuspendedUsers::class),
+
+
     function (Dispatcher $events) {
-        $events->subscribe(Listener\RevokeAccessFromSuspendedUsers::class);
-
-        $events->listen(Saving::class, Listener\SaveSuspensionToDatabase::class);
-
-        $events->listen(Suspended::class, Listener\SendNotificationWhenUserIsSuspended::class);
-        $events->listen(Unsuspended::class, Listener\SendNotificationWhenUserIsUnsuspended::class);
-
-        $events->subscribe(Access\UserPolicy::class);
-
         $events->listen(ConfigureUserGambits::class, function (ConfigureUserGambits $event) {
             $event->gambits->add(SuspendedGambit::class);
         });
--- packages/ext-suspend/src/Access/UserPolicy.php
+++ packages/ext-suspend/src/Access/UserPolicy.php
@@ -9,16 +9,11 @@
 
 namespace Flarum\Suspend\Access;
 
-use Flarum\User\AbstractPolicy;
+use Flarum\User\Access\AbstractPolicy;
 use Flarum\User\User;
 
 class UserPolicy extends AbstractPolicy
 {
-    /**
-     * {@inheritdoc}
-     */
-    protected $model = User::class;
-
     /**
      * @param User $actor
      * @param User $user
@@ -27,7 +22,7 @@ class UserPolicy extends AbstractPolicy
     public function suspend(User $actor, User $user)
     {
         if ($user->isAdmin() || $user->id === $actor->id) {
-            return false;
+            return $this->deny();
         }
     }
 }
