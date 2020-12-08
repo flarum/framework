955a1dffaae290c8b36117527459f225857836f3
Sami Mazouz <sychocouldy@gmail.com>
2020-12-08 20:20:50 +0100

    Use new extenders (#102)

--- packages/ext-tags/extend.php
+++ packages/ext-tags/extend.php
@@ -7,14 +7,18 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use Flarum\Api\Event\Serializing;
+use Flarum\Api\Controller as FlarumController;
+use Flarum\Api\Serializer\DiscussionSerializer;
+use Flarum\Api\Serializer\ForumSerializer;
 use Flarum\Discussion\Discussion;
 use Flarum\Discussion\Event\Saving;
 use Flarum\Extend;
 use Flarum\Tags\Access;
 use Flarum\Tags\Api\Controller;
+use Flarum\Tags\Api\Serializer\TagSerializer;
 use Flarum\Tags\Content;
 use Flarum\Tags\Listener;
+use Flarum\Tags\LoadForumTagsRelationship;
 use Flarum\Tags\Tag;
 use Illuminate\Contracts\Events\Dispatcher;
 
@@ -39,17 +43,40 @@ return [
     (new Extend\Model(Discussion::class))
         ->belongsToMany('tags', Tag::class, 'discussion_tag'),
 
+    (new Extend\ApiSerializer(ForumSerializer::class))
+        ->hasMany('tags', TagSerializer::class),
+
+    (new Extend\ApiSerializer(DiscussionSerializer::class))
+        ->hasMany('tags', TagSerializer::class)
+        ->attribute('canTag', function (DiscussionSerializer $serializer, $model) {
+            return $serializer->getActor()->can('tag', $model);
+        }),
+
+    (new Extend\ApiController(FlarumController\ListDiscussionsController::class))
+        ->addInclude(['tags', 'tags.state']),
+
+    (new Extend\ApiController(FlarumController\ShowDiscussionController::class))
+        ->addInclude(['tags', 'tags.state']),
+
+    (new Extend\ApiController(FlarumController\CreateDiscussionController::class))
+        ->addInclude(['tags', 'tags.state']),
+
+    (new Extend\ApiController(FlarumController\ShowForumController::class))
+        ->addInclude(['tags', 'tags.lastPostedDiscussion', 'tags.parent'])
+        ->prepareDataForSerialization(LoadForumTagsRelationship::class),
+
+    (new Extend\Settings())
+        ->serializeToForum('minPrimaryTags', 'flarum-tags.min_primary_tags')
+        ->serializeToForum('maxPrimaryTags', 'flarum-tags.max_primary_tags')
+        ->serializeToForum('minSecondaryTags', 'flarum-tags.min_secondary_tags')
+        ->serializeToForum('maxSecondaryTags', 'flarum-tags.max_secondary_tags'),
+
     new Extend\Locales(__DIR__.'/locale'),
 
     (new Extend\View)
         ->namespace('tags', __DIR__.'/views'),
 
     function (Dispatcher $events) {
-        $events->subscribe(Listener\AddDiscussionTagsRelationship::class);
-
-        $events->subscribe(Listener\AddForumTagsRelationship::class);
-        $events->listen(Serializing::class, Listener\PrepareForumTagsApiAttributes::class);
-
         $events->subscribe(Listener\CreatePostWhenTagsAreChanged::class);
         $events->subscribe(Listener\FilterDiscussionListByTags::class);
         $events->subscribe(Listener\FilterPostsQueryByTag::class);
