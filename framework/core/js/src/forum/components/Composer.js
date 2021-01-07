f6478096ee74d218454454cf2c97092b2adc20f6
Sami Mazouz <sychocouldy@gmail.com>
2021-01-07 16:25:12 +0100

    Fix composer header hidden by mobile browser (#2279)

--- framework/core/js/src/forum/components/Composer.js
+++ framework/core/js/src/forum/components/Composer.js
@@ -265,7 +265,7 @@ export default class Composer extends Component {
     this.animateHeightChange().then(() => this.focus());
 
     if (app.screen() === 'phone') {
-      this.$().css('top', $(window).scrollTop());
+      this.$().css('top', 0);
       this.showBackdrop();
     }
   }
