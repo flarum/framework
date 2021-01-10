d39b9ab450e9731867f6e145d52ee798a4017af6
Daniel Leśniak <dlesniak781@gmail.com>
2021-01-10 10:29:13 +0000

    Revert back arguments' descriptions but remove types from JSDoc

--- js/src/common/helpers/highlight.ts
+++ js/src/common/helpers/highlight.ts
@@ -3,6 +3,11 @@ import { truncate } from '../utils/string';
 /**
  * The `highlight` helper searches for a word phrase in a string, and wraps
  * matches with the <mark> tag.
+ *
+ * @param string The string to highlight.
+ * @param phrase The word or words to highlight.
+ * @param [length] The number of characters to truncate the string to.
+ *     The string will be truncated surrounding the first match.
  */
 export default function highlight(string: string, phrase: string | RegExp, length: number): Object | string {
   if (!phrase && !length) return string;
