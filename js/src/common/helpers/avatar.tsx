5478885dc6d65e475332072255eba40195da3cd8
Daniel Leśniak <dlesniak781@gmail.com>
2021-01-13 18:50:46 +0000

    Convert common helpers to typescript

--- js/src/common/helpers/avatar.tsx
+++ js/src/common/helpers/avatar.tsx
@@ -1,26 +1,29 @@
+import * as Mithril from "mithril";
+import User from "../models/User";
+
 /**
  * The `avatar` helper displays a user's avatar.
  *
- * @param {User} user
- * @param {Object} attrs Attributes to apply to the avatar element
- * @return {Object}
+ * @param user
+ * @param attrs Attributes to apply to the avatar element
  */
-export default function avatar(user, attrs = {}) {
+
+export default function avatar(user: User, attrs: Object = {}): Mithril.Vnode {
   attrs.className = 'Avatar ' + (attrs.className || '');
-  let content = '';
+  let content: string = '';
 
   // If the `title` attribute is set to null or false, we don't want to give the
   // avatar a title. On the other hand, if it hasn't been given at all, we can
   // safely default it to the user's username.
-  const hasTitle = attrs.title === 'undefined' || attrs.title;
+  const hasTitle: boolean | string = attrs.title === 'undefined' || attrs.title;
   if (!hasTitle) delete attrs.title;
 
   // If a user has been passed, then we will set up an avatar using their
   // uploaded image, or the first letter of their username if they haven't
   // uploaded one.
   if (user) {
-    const username = user.displayName() || '?';
-    const avatarUrl = user.avatarUrl();
+    const username: string = user.displayName() || '?';
+    const avatarUrl: string = user.avatarUrl();
 
     if (hasTitle) attrs.title = attrs.title || username;
 
