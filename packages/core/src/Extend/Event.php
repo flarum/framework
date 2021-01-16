2a122b5546b82a79f8ba1d5988c75ef7bfc7d1cc
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2021-01-15 20:33:29 -0500

    Add `subscribe` method to event extender (#2535)
    Historically, extensions using subscribers has caused problems because subscribers were constructed/applied at extension boot. This caused some classes (e.g. UrlGenerator) to be resolved early, breaking parts of Flarum. For this reason, subscriber support wasn't included in the initial version of the Event extender.
    
    However, updating extensions has shown that there is a legitimate use case for subscribers in organizing clean code; for instance, core's own `DiscussionMetadataUpdater`.
    
    This commit introduces support for subscribers, but only applies them after the app has booted, which avoids the early resolution issues. Since event listeners/subscribers are only intended to be used with domain events, which would never be dispatched during app boot, the late activation of subscribers should not cause issue.

--- packages/core/src/Extend/Event.php
+++ packages/core/src/Extend/Event.php
@@ -16,13 +16,16 @@ use Illuminate\Contracts\Events\Dispatcher;
 class Event implements ExtenderInterface
 {
     private $listeners = [];
+    private $subscribers = [];
 
     /**
      * Add a listener to a domain event dispatched by flarum or a flarum extension.
      *
      * The listener can either be:
-     *  - a callback function or
+     *  - a callback function
      *  - the class attribute of a class with a public `handle` method, which accepts an instance of the event as a parameter
+     *  - an array, where the first argument is an object or class name, and the second argument is the method on the
+     *    first argument that should be executed as the listener
      *
      * @param string $event
      * @param callable|string $listener
@@ -34,6 +37,22 @@ class Event implements ExtenderInterface
         return $this;
     }
 
+    /**
+     * Add a subscriber for a set of domain events dispatched by flarum or a flarum extension.
+     * Event subscribers are classes that may subscribe to multiple events from within the subscriber class itself,
+     * allowing you to define several event handlers within a single class.
+     *
+     * @see https://laravel.com/docs/6.x/events#writing-event-subscribers
+     *
+     * @param string $subscriber: The class attribute of the subscriber class
+     */
+    public function subscribe(string $subscriber)
+    {
+        $this->subscribers[] = $subscriber;
+
+        return $this;
+    }
+
     public function extend(Container $container, Extension $extension = null)
     {
         $events = $container->make(Dispatcher::class);
@@ -41,5 +60,13 @@ class Event implements ExtenderInterface
         foreach ($this->listeners as $listener) {
             $events->listen($listener[0], $listener[1]);
         }
+
+        $app = $container->make('flarum');
+
+        $app->booted(function () use ($events) {
+            foreach ($this->subscribers as $subscriber) {
+                $events->subscribe($subscriber);
+            }
+        });
     }
 }
