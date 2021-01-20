a10da427ff259a37d47bb1cd283e2faafdab974d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-19 19:09:06 -0500

    Remove deprecated CSRF wildcard path match

--- tests/integration/extenders/CsrfTest.php
+++ tests/integration/extenders/CsrfTest.php
@@ -41,38 +41,8 @@ class CsrfTest extends TestCase
 
     /**
      * @test
-     * @deprecated
      */
     public function create_user_post_doesnt_need_csrf_token_if_whitelisted()
-    {
-        $this->extend(
-            (new Extend\Csrf)
-                ->exemptPath('/api/users')
-        );
-
-        $response = $this->send(
-            $this->request('POST', '/api/users', [
-                'json' => [
-                    'data' => [
-                        'attributes' => $this->testUser
-                    ]
-                ],
-            ])
-        );
-
-        $this->assertEquals(201, $response->getStatusCode());
-
-        $user = User::where('username', $this->testUser['username'])->firstOrFail();
-
-        $this->assertEquals(0, $user->is_email_confirmed);
-        $this->assertEquals($this->testUser['username'], $user->username);
-        $this->assertEquals($this->testUser['email'], $user->email);
-    }
-
-    /**
-     * @test
-     */
-    public function create_user_post_doesnt_need_csrf_token_if_whitelisted_via_routename()
     {
         $this->extend(
             (new Extend\Csrf)
@@ -96,23 +66,5 @@ class CsrfTest extends TestCase
         $this->assertEquals(0, $user->is_email_confirmed);
         $this->assertEquals($this->testUser['username'], $user->username);
         $this->assertEquals($this->testUser['email'], $user->email);
-    }
-
-    /**
-     * @test
-     * @deprecated
-     */
-    public function csrf_matches_wildcards_properly()
-    {
-        $this->extend(
-            (new Extend\Csrf)
-                ->exemptPath('/api/fake/*/up')
-        );
-
-        $response = $this->send(
-            $this->request('POST', '/api/fake/route/i/made/up')
-        );
-
-        $this->assertEquals(404, $response->getStatusCode());
     }
 }
