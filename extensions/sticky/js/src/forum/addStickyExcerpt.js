4dbc7369a258146fe88ab94ab214eea6f620d032
Ian Morland <ian@morland.me>
2021-01-20 03:22:20 +0000

    Restrict post excerps to IndexPage and DiscussionPage only #2066 (#25)

--- extensions/sticky/js/src/forum/addStickyExcerpt.js
+++ extensions/sticky/js/src/forum/addStickyExcerpt.js
@@ -1,11 +1,15 @@
 import { extend } from 'flarum/extend';
 import DiscussionListState from 'flarum/states/DiscussionListState';
 import DiscussionListItem from 'flarum/components/DiscussionListItem';
+import DiscussionPage from 'flarum/components/DiscussionPage';
+import IndexPage from 'flarum/components/IndexPage';
 import { truncate } from 'flarum/utils/string';
 
 export default function addStickyControl() {
   extend(DiscussionListState.prototype, 'requestParams', function(params) {
-    params.include.push('firstPost');
+    if (app.current.matches(IndexPage) || app.current.matches(DiscussionPage)) {
+      params.include.push('firstPost');
+    }
   });
 
   extend(DiscussionListItem.prototype, 'infoItems', function(items) {
