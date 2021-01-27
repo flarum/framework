31f6db6b6dadda94ba0ead6882adc97f16f11902
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-01-27 18:47:07 -0500

    Go back to using prepDb since we need to insert discussions outside of a transaction

--- tests/integration/extenders/SimpleFlarumSearchTest.php
+++ tests/integration/extenders/SimpleFlarumSearchTest.php
@@ -20,25 +20,20 @@ use Flarum\Tests\integration\RetrievesAuthorizedUsers;
 use Flarum\Tests\integration\TestCase;
 use Flarum\User\User;
 
-class FlarumSearchTest extends TestCase
+class SimpleFlarumSearchTest extends TestCase
 {
     use RetrievesAuthorizedUsers;
 
-    /**
-     * @inheritDoc
-     */
-    public function setUp()
+    public function prepDb()
     {
-        parent::setUp();
-
         $this->database()->rollBack();
 
         // We need to insert these outside of a transaction, because FULLTEXT indexing,
         // which is needed for search, doesn't happen in transactions.
         // We clean it up explcitly at the end.
         $this->database()->table('discussions')->insert([
-            ['id' => 1, 'title' => 'lightsail in title', 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'comment_count' => 1],
-            ['id' => 2, 'title' => 'not in title', 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'comment_count' => 1],
+            ['id' => 1, 'title' => 'DISCUSSION 1', 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'comment_count' => 1],
+            ['id' => 2, 'title' => 'DISCUSSION 2', 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'comment_count' => 1],
         ]);
 
         $this->database()->table('posts')->insert([
@@ -65,6 +60,8 @@ class FlarumSearchTest extends TestCase
 
     public function searchDiscussions($query, $limit = null)
     {
+        $this->app();
+
         $actor = User::find(1);
 
         $criteria = new SearchCriteria($actor, $query);
@@ -77,11 +74,13 @@ class FlarumSearchTest extends TestCase
      */
     public function works_as_expected_with_no_modifications()
     {
+        $this->prepDb();
+
         $searchForAll = json_encode($this->searchDiscussions('in text', 5));
         $this->assertContains('DISCUSSION 1', $searchForAll);
         $this->assertContains('DISCUSSION 2', $searchForAll);
 
-        $searchForSecond = json_encode($this->searchDiscussions('lightsail in text', 5));
+        $searchForSecond = json_encode($this->searchDiscussions('lightsail', 5));
         $this->assertNotContains('DISCUSSION 1', $searchForSecond);
         $this->assertContains('DISCUSSION 2', $searchForSecond);
     }
@@ -103,6 +102,8 @@ class FlarumSearchTest extends TestCase
     {
         $this->extend((new Extend\SimpleFlarumSearch(DiscussionSearcher::class))->setFullTextGambit(NoResultFilterGambit::class));
 
+        $this->prepDb();
+
         $withResultSearch = json_encode($this->searchDiscussions('noResult:0', 5));
         $this->assertContains('DISCUSSION 1', $withResultSearch);
         $this->assertContains('DISCUSSION 2', $withResultSearch);
@@ -118,6 +119,8 @@ class FlarumSearchTest extends TestCase
             $search->getquery()->whereRaw('1=0');
         }));
 
+        $this->prepDb();
+
         $this->assertEquals('[]', json_encode($this->searchDiscussions('in text', 5)));
     }
 
@@ -127,6 +130,8 @@ class FlarumSearchTest extends TestCase
     public function search_mutator_has_effect_if_added_with_invokable_class()
     {
         $this->extend((new Extend\SimpleFlarumSearch(DiscussionSearcher::class))->addSearchMutator(CustomSearchMutator::class));
+
+        $this->prepDb();
 
         $this->assertEquals('[]', json_encode($this->searchDiscussions('in text', 5)));
     }
