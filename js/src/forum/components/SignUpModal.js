f00dc7eeeeca4ae8160589ac0c8fbeb2ef8dc694
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-02-03 14:20:04 -0500

    Don't return promise where reload happens

--- js/src/forum/components/SignUpModal.js
+++ js/src/forum/components/SignUpModal.js
@@ -161,7 +161,7 @@ export default class SignUpModal extends Modal {
 
     const body = this.submitData();
 
-    return app
+    app
       .request({
         url: app.forum.attribute('baseUrl') + '/register',
         method: 'POST',
