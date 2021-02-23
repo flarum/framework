862d706a418ff23d8f02bf8839e30b87ca615e58
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-02-24 00:37:30 +0100

    Completely redact boot error unless debug mode or display_errors is enabled. Attempt to use Flarum log file when possible. Fixes #2290

--- src/Http/Server.php
+++ src/Http/Server.php
@@ -9,6 +9,7 @@
 
 namespace Flarum\Http;
 
+use Flarum\Foundation\ErrorHandling\LogReporter;
 use Flarum\Foundation\SiteInterface;
 use Laminas\Diactoros\Response;
 use Laminas\Diactoros\ServerRequest;
@@ -16,6 +17,7 @@ use Laminas\Diactoros\ServerRequestFactory;
 use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use Laminas\HttpHandlerRunner\RequestHandlerRunner;
 use Laminas\Stratigility\Middleware\ErrorResponseGenerator;
+use Psr\Log\LoggerInterface;
 use Throwable;
 
 class Server
@@ -55,7 +57,28 @@ class Server
         try {
             return $this->site->bootApp()->getRequestHandler();
         } catch (Throwable $e) {
-            exit($this->formatBootException($e));
+            if (app()->has('flarum.config') && app('flarum.config')->inDebugMode()) {
+                // If the application booted far enough for the config to be available, we will check for debug mode
+                // Since the config is loaded very early, it is very likely to be available from the container
+                echo $this->formatBootException($e);
+            } else if (app()->has(LoggerInterface::class)) {
+                // If the application booted far enough for the logger to be available, we will log the error there
+                // Considering most boot errors are related to database or extensions, the logger will be available
+                // We check for LoggerInterface binding because it's a constructor dependency of LogReporter,
+                // then instantiate LogReporter through the container for automatic dependency injection
+                app(LogReporter::class)->report($e);
+
+                echo 'Flarum encountered a boot error. Details have been logged to the Flarum log file.';
+            } else {
+                echo 'Flarum encountered a boot error. Details have been logged to the system PHP log file.<br />';
+
+                // Throwing the exception ensures it will be visible with PHP display_errors=On
+                // but invisible if that feature is turned off
+                // PHP will also automatically choose a valid place to log it based on the system settings
+                throw $e;
+            }
+
+            exit(1);
         }
     }
 
