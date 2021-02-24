dev2lead/core

Add filter versions of filtering code from ListPostsController

src/Filter/FilterServiceProvider.php
@@ -10,14 +10,12 @@
 namespace Flarum\Filter;
 
 use Flarum\Discussion\Discussion;
-use Flarum\Discussion\Filter\AuthorFilter;
-use Flarum\Discussion\Filter\CreatedFilter;
-use Flarum\Discussion\Filter\HiddenFilter;
-use Flarum\Discussion\Filter\UnreadFilter;
+use Flarum\Discussion\Filter as DiscussionFilter;
 use Flarum\Foundation\AbstractServiceProvider;
-use Flarum\User\Filter\EmailFilter;
-use Flarum\User\Filter\GroupFilter;
+use Flarum\Post\Post;
+use Flarum\Post\Filter as PostFilter;
 use Flarum\User\User;
+use Flarum\User\Filter as UserFilter;
 
 class FilterServiceProvider extends AbstractServiceProvider
 {
@@ -31,14 +29,21 @@ class FilterServiceProvider extends AbstractServiceProvider
         $this->app->singleton('flarum.filter.filters', function () {
             return [
                 Discussion::class => [
-                    AuthorFilter::class,
-                    CreatedFilter::class,
-                    HiddenFilter::class,
-                    UnreadFilter::class,
+                    DiscussionFilter\AuthorFilter::class,
+                    DiscussionFilter\CreatedFilter::class,
+                    DiscussionFilter\HiddenFilter::class,
+                    DiscussionFilter\UnreadFilter::class,
+                ],
+                Post::class => [
+                    PostFilter\AuthorFilter::class,
+                    PostFilter\DiscussionFilter::class,
+                    PostFilter\IdFilter::class,
+                    PostFilter\NumberFilter::class,
+                    PostFilter\TypeFilter::class,
                 ],
                 User::class => [
-                    EmailFilter::class,
-                    GroupFilter::class,
+                    UserFilter\EmailFilter::class,
+                    UserFilter\GroupFilter::class,
                 ]
             ];
         });

flarum/framework

src/Filter/FilterServiceProvider.php
@@ -9,17 +9,13 @@
 
 namespace Flarum\Filter;
 
-use Flarum\Discussion\Filter\AuthorFilterGambit;
-use Flarum\Discussion\Filter\CreatedFilterGambit;
-use Flarum\Discussion\Filter\DiscussionFilterer;
-use Flarum\Discussion\Filter\HiddenFilterGambit;
-use Flarum\Discussion\Filter\UnreadFilterGambit;
+use Flarum\Discussion\Discussion;
+use Flarum\Discussion\Filter as DiscussionFilter;
 use Flarum\Foundation\AbstractServiceProvider;
-use Flarum\Foundation\ContainerUtil;
-use Flarum\User\Filter\EmailFilterGambit;
-use Flarum\User\Filter\GroupFilterGambit;
-use Flarum\User\Filter\UserFilterer;
-use Illuminate\Support\Arr;
+use Flarum\Post\Post;
+use Flarum\Post\Filter as PostFilter;
+use Flarum\User\User;
+use Flarum\User\Filter as UserFilter;
 
 class FilterServiceProvider extends AbstractServiceProvider
 {
@@ -33,15 +29,22 @@ class FilterServiceProvider extends AbstractServiceProvider
         $this->app->singleton('flarum.filter.filters', function () {
             return [
                 DiscussionFilterer::class => [
-                    AuthorFilterGambit::class,
-                    CreatedFilterGambit::class,
-                    HiddenFilterGambit::class,
-                    UnreadFilterGambit::class,
+                    DiscussionFilter\AuthorFilterGambit::class,
+                    DiscussionFilter\CreatedFilterGambit::class,
+                    DiscussionFilter\HiddenFilterGambit::class,
+                    DiscussionFilter\UnreadFilterGambit::class,
                 ],
                 UserFilterer::class => [
-                    EmailFilterGambit::class,
-                    GroupFilterGambit::class,
-                ]
+                    UserFilter\EmailFilterGambit::class,
+                    UserFilter\GroupFilterGambit::class,
+                ],
+                Post::class => [
+                    PostFilter\AuthorFilter::class,
+                    PostFilter\DiscussionFilter::class,
+                    PostFilter\IdFilter::class,
+                    PostFilter\NumberFilter::class,
+                    PostFilter\TypeFilter::class,
+                ],
             ];
         });
 