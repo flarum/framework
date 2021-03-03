719b3ad28e954c2425980ed3fa14330577661468
daniellesniak <wyimaginowany12@gmail.com>
2021-03-04 00:50:54 +0100

    Convert common helpers to Typescript (#2541)

--- framework/core/js/src/common/helpers/userOnline.tsx
+++ framework/core/js/src/common/helpers/userOnline.tsx
@@ -1,12 +1,11 @@
+import * as Mithril from 'mithril';
+import User from '../models/User';
 import icon from './icon';
 
 /**
  * The `useronline` helper displays a green circle if the user is online
- *
- * @param {User} user
- * @return {Object}
  */
-export default function userOnline(user) {
+export default function userOnline(user: User): Mithril.Vnode {
   if (user.lastSeenAt() && user.isOnline()) {
     return <span className="UserOnline">{icon('fas fa-circle')}</span>;
   }
