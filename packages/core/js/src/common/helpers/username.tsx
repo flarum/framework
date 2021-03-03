c6cd2d33409322972488b22ca5eab6cb75160fbd
daniellesniak <wyimaginowany12@gmail.com>
2021-03-04 00:50:54 +0100

    Convert common helpers to Typescript (#2541)

--- packages/core/js/src/common/helpers/username.tsx
+++ packages/core/js/src/common/helpers/username.tsx
@@ -1,11 +1,11 @@
+import * as Mithril from 'mithril';
+import User from '../models/User';
+
 /**
  * The `username` helper displays a user's username in a <span class="username">
  * tag. If the user doesn't exist, the username will be displayed as [deleted].
- *
- * @param {User} user
- * @return {Object}
  */
-export default function username(user) {
+export default function username(user: User): Mithril.Vnode {
   const name = (user && user.displayName()) || app.translator.trans('core.lib.username.deleted_text');
 
   return <span className="username">{name}</span>;
