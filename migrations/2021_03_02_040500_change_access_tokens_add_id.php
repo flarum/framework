83580278ccb295a78e27e6de98ffaeb64374b01c
Clark Winkelmann <clark.winkelmann@gmail.com>
2021-03-04 22:13:49 +0100

    Let MySQL handle setting the new increment ID values

--- migrations/2021_03_02_040500_change_access_tokens_add_id.php
+++ migrations/2021_03_02_040500_change_access_tokens_add_id.php
@@ -13,31 +13,15 @@ use Illuminate\Database\Schema\Builder;
 return [
     'up' => function (Builder $schema) {
         $schema->table('access_tokens', function (Blueprint $table) {
-            $table->unsignedInteger('id')->first();
-        });
-
-        $tokens = $schema->getConnection()->table('access_tokens')
-            ->cursor();
-
-        // Insert initial value for our new primary key
-        // This migration runs after the "add type" migration, this ensures we have a minimal number of tokens remaining
-        foreach ($tokens as $i => $token) {
-            $schema->getConnection()->table('access_tokens')
-                ->where('token', $token->token)
-                ->update([
-                    'id' => $i + 1
-                ]);
-        }
-
-        $schema->table('access_tokens', function (Blueprint $table) {
+            // Replace primary key with unique index so we can create a new primary
             $table->dropPrimary('token');
             $table->unique('token');
-            $table->primary('id');
         });
 
         // This needs to be done in a second statement because of the order Laravel runs operations in
         $schema->table('access_tokens', function (Blueprint $table) {
-            $table->increments('id')->change();
+            // Introduce new increment-based ID
+            $table->increments('id')->first();
         });
     },
 
