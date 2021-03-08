6f09af5ad9a78acb1715e3400b92224e06571139
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-08 16:21:36 -0500

    Use new `author` key for filtering posts
    Fixes https://github.com/flarum/core/issues/2671

--- framework/core/js/src/forum/components/PostsUserPage.js
+++ framework/core/js/src/forum/components/PostsUserPage.js
@@ -121,7 +121,7 @@ export default class PostsUserPage extends UserPage {
   loadResults(offset) {
     return app.store.find('posts', {
       filter: {
-        user: this.user.id(),
+        author: this.user.id(),
         type: 'comment',
       },
       page: { offset, limit: this.loadLimit },
