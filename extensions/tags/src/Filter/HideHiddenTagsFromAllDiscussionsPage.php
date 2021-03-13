932c31db6d3bd4f54ffea70b77f8f5f23749710e
Alexander Skvortsov <askvortsov1@users.noreply.github.com>
2021-03-13 22:00:39 +0000

    Apply fixes from StyleCI

--- extensions/tags/src/Filter/HideHiddenTagsFromAllDiscussionsPage.php
+++ extensions/tags/src/Filter/HideHiddenTagsFromAllDiscussionsPage.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Flarum\Tags\Filter;
 
 use Flarum\Filter\FilterState;
