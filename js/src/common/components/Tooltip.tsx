faffdebf7ebb784e12a869b6027eec5eba461ddb
David Wheatley <hi@davwheat.dev>
2021-03-15 21:46:41 +0000

    Create Tooltip helper component

---
+++ js/src/common/components/Tooltip.tsx
@@ -0,0 +1,56 @@
+import Component, { ComponentAttrs } from '../Component';
+import type Mithril from 'mithril';
+
+export interface TooltipAttrs extends ComponentAttrs {
+  /** Tooltip text */
+  text: string;
+  /** If inline, uses a `<span>` container, else uses a `<div>` */
+  inline: boolean;
+}
+
+/**
+ * The `Tooltip` component is used to create a tooltip for an element which
+ * already has pseudo-elements as part of its styles, or text vnodes.
+ *
+ * If your element does not use pseudo-elements on its first direct child,
+ * instead use the attributes `data-tooltip` and `aria-label="tooltip text"`
+ * wherever possible.
+ *
+ * @example <caption>Correct use of Tooltip component</caption>
+ * // div has ::before or ::after
+ * <Tooltip text="Hello flarumites!">
+ *   <div />
+ * </Tooltip>
+ *
+ * @example <caption>INCORRECT use of Tooltip component</caption>
+ * // Do not use this!
+ * <Tooltip text="This is wrong!">
+ *   <span>0 replies</span>
+ * </Tooltip>
+ *
+ * // Use this instead
+ * <span data-tooltip aria-label="This is correct!" />
+ */
+export default class Tooltip extends Component<TooltipAttrs> {
+  view(vnode: Mithril.Vnode<TooltipAttrs, this>) {
+    const { children } = vnode;
+
+    const { inline, text } = this.attrs;
+
+    const attrs = {
+      ...this.attrs,
+      'data-tooltip': true,
+      'aria-label': text,
+    };
+
+    if (inline) {
+      return <span {...attrs}>{children}</span>;
+    }
+
+    return (
+      <div {...attrs}>
+        {children}
+      </div>
+    );
+  }
+}
