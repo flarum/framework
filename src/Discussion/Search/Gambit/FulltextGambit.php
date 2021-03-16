75ba560e6f987604c8ec9045b08ac0eca5eab50e
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-16 19:29:01 -0400

    Fix searching titles in discussions

--- src/Discussion/Search/Gambit/FulltextGambit.php
+++ src/Discussion/Search/Gambit/FulltextGambit.php
@@ -9,6 +9,7 @@
 
 namespace Flarum\Discussion\Search\Gambit;
 
+use Flarum\Discussion\Discussion;
 use Flarum\Post\Post;
 use Flarum\Search\GambitInterface;
 use Flarum\Search\SearchState;
@@ -29,6 +30,12 @@ class FulltextGambit implements GambitInterface
         $query = $search->getQuery();
         $grammar = $query->getGrammar();
 
+
+        $discussionSubquery = Discussion::select('id')
+            ->selectRaw('NULL as score')
+            ->selectRaw('first_post_id as most_relevant_post_id')
+            ->whereRaw('MATCH('.$grammar->wrap('discussions.title').') AGAINST (? IN BOOLEAN MODE)', [$bit]);
+
         // Construct a subquery to fetch discussions which contain relevant
         // posts. Retrieve the collective relevance of each discussion's posts,
         // which we will use later in the order by clause, and also retrieve
@@ -39,7 +46,8 @@ class FulltextGambit implements GambitInterface
             ->selectRaw('SUBSTRING_INDEX(GROUP_CONCAT('.$grammar->wrap('posts.id').' ORDER BY MATCH('.$grammar->wrap('posts.content').') AGAINST (?) DESC, '.$grammar->wrap('posts.number').'), \',\', 1) as most_relevant_post_id', [$bit])
             ->where('posts.type', 'comment')
             ->whereRaw('MATCH('.$grammar->wrap('posts.content').') AGAINST (? IN BOOLEAN MODE)', [$bit])
-            ->groupBy('posts.discussion_id');
+            ->groupBy('posts.discussion_id')
+            ->union($discussionSubquery);
 
         // Join the subquery into the main search query and scope results to
         // discussions that have a relevant title or that contain relevant posts.
@@ -51,11 +59,7 @@ class FulltextGambit implements GambitInterface
                 '=',
                 'discussions.id'
             )
-            ->addBinding($subquery->getBindings(), 'join')
-            ->where(function ($query) use ($grammar, $bit) {
-                $query->whereRaw('MATCH('.$grammar->wrap('discussions.title').') AGAINST (? IN BOOLEAN MODE)', [$bit])
-                      ->orWhereNotNull('posts_ft.score');
-            });
+            ->addBinding($subquery->getBindings(), 'join');
 
         $search->setDefaultSort(function ($query) use ($grammar, $bit) {
             $query->orderByRaw('MATCH('.$grammar->wrap('discussions.title').') AGAINST (?) desc', [$bit]);
