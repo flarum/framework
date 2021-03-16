1b339267f86c104154f84f2edbe440153a6f00a5
Ian Morland <ian@morland.me>
2021-03-16 01:44:22 +0000

    Canonical URL: Use UrlGenerator in place of extracting the url from request (#122)

--- extensions/tags/src/Content/Tags.php
+++ extensions/tags/src/Content/Tags.php
@@ -94,7 +94,7 @@ class Tags
         $document->title = $this->translator->trans('flarum-tags.forum.all_tags.meta_title_text');
         $document->meta['description'] = $this->translator->trans('flarum-tags.forum.all_tags.meta_description_text');
         $document->content = $this->view->make('tags::frontend.content.tags', compact('primaryTags', 'secondaryTags', 'children'));
-        $document->canonicalUrl = $defaultRoute === '/tags' ? $this->url->to('forum')->base() : $request->getUri()->withQuery('');
+        $document->canonicalUrl = $this->url->to('forum')->base().($defaultRoute === '/tags' ? '' : $request->getUri()->getPath());
 
         return $document;
     }
