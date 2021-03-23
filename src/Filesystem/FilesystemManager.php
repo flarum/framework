602d0f84c1c5af50c51b6a4832e2c4d5780d6f9d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-03-23 15:31:34 -0400

    Override FilesystemManager
    This gives Flarum much more flexibility in configuring filesystem disks and drivers, enabling the upcoming FIlesystem extender.

---
+++ src/Filesystem/FilesystemManager.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Filesystem;
+
+use Flarum\Foundation\Config;
+use Flarum\Foundation\Paths;
+use Flarum\Http\UrlGenerator;
+use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Filesystem\FilesystemAdapter;
+use Illuminate\Filesystem\FilesystemManager as LaravelFilesystemManager;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+
+class FilesystemManager extends LaravelFilesystemManager
+{
+    protected $diskLocalConfig = [];
+    protected $drivers = [];
+
+    public function __construct($app, $diskLocalConfig, $drivers)
+    {
+        parent::__construct($app);
+
+        $this->diskLocalConfig = $diskLocalConfig;
+        $this->drivers = $drivers;
+    }
+
+   /**
+    * @inheritDoc
+    */
+    protected function resolve($name)
+    {
+        $driver = $this->getDriver($name);
+
+        $localConfig = $this->getLocalConfig($name);
+
+        if (empty($localConfig)) {
+            throw new InvalidArgumentException("Disk [{$name}] has not been declared. Use the Filesystem extender to do this.");
+        }
+
+        if ($driver === 'local') {
+            return $this->createLocalDriver($localConfig);
+        }
+
+        $settings = $this->app->make(SettingsRepositoryInterface::class);
+        $config = $this->app->make(Config::class);
+        return new FilesystemAdapter($driver->build($name, $settings, $config, $localConfig));
+    }
+
+    protected function getDriver($name)
+    {
+        $settings = $this->app->make(SettingsRepositoryInterface::class);
+        $configuredDriver = $settings->get("disk_driver.$name", 'local');
+        return Arr::get($this->drivers, $configuredDriver, 'local');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function getLocalConfig($name)
+    {
+        if (!array_key_exists($name, $this->diskLocalConfig)) return [];
+
+        return $this->diskLocalConfig[$name](
+            $this->app->make(Paths::class),
+            $this->app->make(UrlGenerator::class)
+        );
+    }
+}
