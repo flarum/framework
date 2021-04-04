108e90503b6655816512d43cc9db91ac19c714ff
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-04-04 15:52:02 -0400

    Remove excess methods, only support getting/setting actor for now.

--- src/Http/RequestUtil.php
+++ src/Http/RequestUtil.php
@@ -10,7 +10,6 @@
 namespace Flarum\Http;
 
 use Flarum\User\User;
-use Illuminate\Contracts\Session\Session;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 class RequestUtil
@@ -20,38 +19,8 @@ class RequestUtil
         return $request->getAttribute('actor');
     }
 
-    public function withActor(Request $request, User $actor): Request
+    public static function withActor(Request $request, User $actor): Request
     {
         return $request->withAttribute('actor', $actor);
-    }
-
-    public function getSession(Request $request): Session
-    {
-        return $request->getAttribute('session');
-    }
-
-    public function withSession(Request $request, Session $session): Request
-    {
-        return $request->withAttribute('session', $session);
-    }
-
-    public function getLocale(Request $request): string
-    {
-        return $request->getAttribute('bypassCsrfToken');
-    }
-
-    public function withLocale(Request $request, string $locale): Request
-    {
-        return $request->withAttribute('locale', $locale);
-    }
-
-    public function getRouteName(Request $request): string
-    {
-        return $request->getAttribute('routeName');
-    }
-
-    public function withRouteName(Request $request, string $routeName): Request
-    {
-        return $request->withAttribute('routeName', $routeName);
     }
 }
