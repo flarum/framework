34c6e3e9f70bfb280a1b3ae7b00b98f6fc9e9819
David Wheatley <hi@davwheat.dev>
2021-04-04 01:36:30 +0100

    Update JSDoc comments and types

--- js/src/common/components/Tooltip.tsx
+++ js/src/common/components/Tooltip.tsx
@@ -18,9 +18,17 @@ export interface TooltipAttrs extends ComponentAttrs {
  * The `Tooltip` component is used to create a tooltip for an element which
  * already has pseudo-elements as part of its styles, or text vnodes.
  *
- * If your element does not use pseudo-elements on its first direct child,
+ * **If your element does not use pseudo-elements on its first direct child,
  * instead use the attributes `data-tooltip` and `aria-label="tooltip text"`
- * wherever possible.
+ * wherever possible.**
+ *
+ * @example <caption>Correct use of Tooltip component</caption>
+ * // div has ::before or ::after
+ * <Tooltip text="You wish!">
+ *   <Button>
+ *     Click for free money!
+ *   </Button>
+ * </Tooltip>
  *
  * @example <caption>Correct use of Tooltip component</caption>
  * // div has ::before or ::after
@@ -31,11 +39,11 @@ export interface TooltipAttrs extends ComponentAttrs {
  * @example <caption>INCORRECT use of Tooltip component</caption>
  * // Do not use this!
  * <Tooltip text="This is wrong!">
- *   <span>0 replies</span>
+ *   <div>0 replies</div>
  * </Tooltip>
  *
  * // Use this instead
- * <span data-tooltip aria-label="This is correct!" />
+ * <div data-tooltip aria-label="This is correct!" />
  */
 export default class Tooltip extends Component<TooltipAttrs> {
   view(vnode: Mithril.Vnode<TooltipAttrs, this>) {
--- js/src/common/jqueryTooltipCompatLayer.ts
+++ js/src/common/jqueryTooltipCompatLayer.ts
@@ -1,8 +1,27 @@
 type BootstrapTooltipPlacement = 'top' | 'bottom' | 'left' | 'right';
 
-// This is a backwards compatibility layer to provide support for $.tooltip()
-export default function jqueryTooltipCompatLayer(element, options) {
-  const tooltipText = this.attr('title');
+interface TooltipOptions {
+  placement: BootstrapTooltipPlacement;
+}
+
+/**
+ * This is a backwards compatibility layer to provide support for `$.tooltip()`, since it
+ * is used extensively across multiple core and extension files.
+ *
+ * **Do not use this in new code. Instead add `data-tooltip` and `aria-label="text"`
+ * attributes to create a tooltip, or use the `<Tooltip>` component from core if the
+ * element already has styles on its `::before` or `::after`.**
+ *
+ * @deprecated Backwards compatibility layer for Bootstrap tooltips.
+ * @param options Tooltip options
+ */
+export default function jqueryTooltipCompatLayer(this: JQuery, options: TooltipOptions): JQuery {
+  const tooltipText = this.attr('title') || this.attr('aria-label');
+
+  if (typeof tooltipText === 'undefined' || tooltipText === '') {
+    console.warn(`Called $.tooltip() on element without a title or aria-label attribute.`, this.get(0));
+    return this;
+  }
 
   this.attr('data-tooltip', '');
   this.attr('aria-label', tooltipText);
@@ -14,4 +33,6 @@ export default function jqueryTooltipCompatLayer(element, options) {
     // Placement manually specified
     this.attr('data-tooltip-position', placement);
   }
+
+  return this;
 }
