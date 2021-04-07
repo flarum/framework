9ed29cd35ee9a4d8fc6300ffd7f63634fb35de7b
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-04-07 15:01:55 -0400

    Add automated tests for `flarum/testing`

---
+++ php-packages/testing/tests/extend.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of flarum/testing-tests.
+ *
+ * Copyright (c) 2021 .
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Flarum\Testing;
+
+use Flarum\Extend;
+
+return [
+    
+    
+    
+];
