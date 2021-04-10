7d8b26d1e2b38d91a50077400a95ace5d9ea5610
David Wheatley <hi@davwheat.dev>
2021-04-10 15:00:48 +0100

    Add option to build with Webpack Bundle Analyzer (#2708)
    * Add option to build with webpack bundle analyzer
    
    * Bump npm to v7 as recommended in actions/setup-node#213
    
    * Workaround for npm/cli#558
    
    * Add missing dep

--- packages/core/js/webpack.config.js
+++ packages/core/js/webpack.config.js
@@ -1,5 +1,14 @@
 const config = require('flarum-webpack-config');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const merge = require('webpack-merge');
+
+const useBundleAnalyzer = process.env.ANALYZER === 'true';
+
+const plugins = [];
+
+if (useBundleAnalyzer) {
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 module.exports = merge(config(), {
   output: {
@@ -10,6 +19,8 @@ module.exports = merge(config(), {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
+
+  plugins,
 });
 
 module.exports['module'].rules[0].test = /\.(tsx?|js)$/;
