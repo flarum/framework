5e082515c569e9f7bb631c4a6e286b7705e39deb
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-04-12 17:28:04 -0400

    Fully rely on messageformat in frontend

--- js/src/common/Translator.js
+++ js/src/common/Translator.js
@@ -2,8 +2,17 @@ import { MessageFormatter, pluralTypeHandler, selectTypeHandler } from '@ultraq/
 import username from './helpers/username';
 import extract from './utils/extract';
 
+const fillTags = (tag, tags, children) => {
+  if (tag in tags) {
+    tags[tag].children = m.fragment(children).children;
+    return tags[tag];
+  }
+
+  return children;
+};
+
 export default class Translator {
-  constructor(locale) {
+  constructor() {
     /**
      * A map of translation keys to their translated values.
      *
@@ -12,7 +21,7 @@ export default class Translator {
      */
     this.translations = {};
 
-    this.formatter = new MessageFormatter(null, this.formatterTypeHandlers());
+    this.formatter = new MessageFormatter(null, this.formatterTypeHandlers(), fillTags);
   }
 
   formatterTypeHandlers() {
@@ -20,6 +29,14 @@ export default class Translator {
       plural: pluralTypeHandler,
       select: selectTypeHandler,
     };
+  }
+
+  setLocale(locale) {
+    this.formatter.locale = locale;
+  }
+
+  addTranslations(translations) {
+    Object.assign(this.translations, translations);
   }
 
   preprocessParameters(parameters) {
@@ -36,24 +53,12 @@ export default class Translator {
     return parameters;
   }
 
-  setLocale(locale) {
-    this.formatter.locale = locale;
-  }
-
-  addTranslations(translations) {
-    Object.assign(this.translations, translations);
-  }
-
   trans(id, parameters) {
     const translation = this.translations[id];
 
     if (translation) {
       parameters = this.preprocessParameters(parameters || {});
-
-      console.log(translation, parameters);
-      console.log(this.formatter.format(translation, parameters));
-
-      return this.apply(translation, parameters);
+      return this.formatter.rich(translation, parameters);
     }
 
     return id;
@@ -64,50 +69,5 @@ export default class Translator {
    */
   transChoice(id, number, parameters) {
     return this.trans(id, parameters);
-  }
-
-  apply(translation, input) {
-    translation = translation.split(new RegExp('({[a-z0-9_]+}|</?[a-z0-9_]+>)', 'gi'));
-
-    const hydrated = [];
-    const open = [hydrated];
-
-    translation.forEach((part) => {
-      const match = part.match(new RegExp('{([a-z0-9_]+)}|<(/?)([a-z0-9_]+)>', 'i'));
-
-      if (match) {
-        // Either an opening or closing tag.
-        if (match[1]) {
-          open[0].push(input[match[1]]);
-        } else if (match[3]) {
-          if (match[2]) {
-            // Closing tag. We start by removing all raw children (generally in the form of strings) from the temporary
-            // holding array, then run them through m.fragment to convert them to vnodes. Usually this will just give us a
-            // text vnode, but using m.fragment as opposed to an explicit conversion should be more flexible. This is necessary because
-            // otherwise, our generated vnode will have raw strings as its children, and mithril expects vnodes.
-            // Finally, we add the now-processed vnodes back onto the holding array (which is the same object in memory as the
-            // children array of the vnode we are currently processing), and remove the reference to the holding array so that
-            // further text will be added to the full set of returned elements.
-            const rawChildren = open[0].splice(0, open[0].length);
-            open[0].push(...m.fragment(rawChildren).children);
-            open.shift();
-          } else {
-            // If a vnode with a matching tag was provided in the translator input, we use that. Otherwise, we create a new vnode
-            // with this tag, and an empty children array (since we're expecting to insert children, as that's the point of having this in translator)
-            let tag = input[match[3]] || { tag: match[3], children: [] };
-            open[0].push(tag);
-            // Insert the tag's children array as the first element of open, so that text in between the opening
-            // and closing tags will be added to the tag's children, not to the full set of returned elements.
-            open.unshift(tag.children || tag);
-          }
-        }
-      } else {
-        // Not an html tag, we add it to open[0], which is either the full set of returned elements (vnodes and text),
-        // or if an html tag is currently being processed, the children attribute of that html tag's vnode.
-        open[0].push(part);
-      }
-    });
-
-    return hydrated.filter((part) => part);
   }
 }
