tdtrung17693/core

Add middleware in admin and forum to some errors via the frontend

src/Http/HttpServiceProvider.php
@@ -10,6 +10,10 @@
 namespace Flarum\Http;
 
 use Flarum\Foundation\AbstractServiceProvider;
+use Flarum\Foundation\ErrorHandling\Registry;
+use Flarum\Foundation\ErrorHandling\Reporter;
+use Flarum\Foundation\ErrorHandling\FrontendFormatter;
+use Flarum\Foundation\ErrorHandling\WhoopsFormatter;
 
 class HttpServiceProvider extends AbstractServiceProvider
 {
@@ -24,6 +28,24 @@ class HttpServiceProvider extends AbstractServiceProvider
 
         $this->app->bind(Middleware\CheckCsrfToken::class, function ($app) {
             return new Middleware\CheckCsrfToken($app->make('flarum.http.csrfExemptPaths'));
+        });
+
+        $this->app->singleton('flarum.http.frontend_exceptions', function () {
+            return [
+                NotAuthenticatedException::class, // 401
+                PermissionDeniedException::class, // 403
+                ModelNotFoundException::class, // 404
+                RouteNotFoundException::class, // 404
+            ];
+        });
+
+        $this->app->singleton('flarum.http.frontend_handler', function () {
+            return new Middleware\HandleErrors(
+                $this->app->make(Registry::class),
+                $this->app['flarum']->inDebugMode() ? $this->app->make(WhoopsFormatter::class) : $this->app->make(FrontendFormatter::class),
+                $this->app->tagged(Reporter::class),
+                $this->app->make('flarum.http.frontend_exceptions')
+            );
         });
     }
 }

flarum/framework

src/Http/HttpServiceProvider.php
@@ -16,6 +16,10 @@ use Flarum\Settings\SettingsRepositoryInterface;
 use Flarum\User\User;
 use Flarum\User\UsernameSlugDriver;
 use Illuminate\Support\Arr;
+use Flarum\Foundation\ErrorHandling\Registry;
+use Flarum\Foundation\ErrorHandling\Reporter;
+use Flarum\Foundation\ErrorHandling\FrontendFormatter;
+use Flarum\Foundation\ErrorHandling\WhoopsFormatter;
 
 class HttpServiceProvider extends AbstractServiceProvider
 {
@@ -58,8 +62,27 @@ class HttpServiceProvider extends AbstractServiceProvider
 
             return $compiledDrivers;
         });
+
         $this->container->bind(SlugManager::class, function () {
             return new SlugManager($this->container->make('flarum.http.selectedSlugDrivers'));
+        });
+
+        $this->container->singleton('flarum.http.frontend_exceptions', function () {
+            return [
+                NotAuthenticatedException::class, // 401
+                PermissionDeniedException::class, // 403
+                ModelNotFoundException::class, // 404
+                RouteNotFoundException::class, // 404
+            ];
+        });
+
+        $this->container->singleton('flarum.http.frontend_handler', function () {
+            return new Middleware\HandleErrors(
+                $this->container->make(Registry::class),
+                $this->container['flarum']->inDebugMode() ? $this->container->make(WhoopsFormatter::class) : $this->container->make(FrontendFormatter::class),
+                $this->container->tagged(Reporter::class),
+                $this->container->make('flarum.http.frontend_exceptions')
+            );
         });
     }
 