3105d0a15c42902a80592af0c16101160dbee3d7
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2021-04-19 15:11:03 -0400

    Use Laravel filesystem interface for assets and avatars (#2729)
    * WIP: Use Laravel filesystem interface where possible
    * Drop vendorFilesystem
    * Support getting URL of cloud-based logo and favicon
    * FilesystemAdapter should always be cloud
    * Get base avatar URL from filesystem adapter
    * Restore deleted getAsset method
    
    Co-authored-by: Alexander Skvortsov <askvortsov1@users.noreply.github.com>

--- packages/core/src/Api/Controller/DeleteLogoController.php
+++ packages/core/src/Api/Controller/DeleteLogoController.php
@@ -11,8 +11,9 @@ namespace Flarum\Api\Controller;
 
 use Flarum\Http\RequestUtil;
 use Flarum\Settings\SettingsRepositoryInterface;
+use Illuminate\Contracts\Filesystem\Factory;
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Laminas\Diactoros\Response\EmptyResponse;
-use League\Flysystem\FilesystemInterface;
 use Psr\Http\Message\ServerRequestInterface;
 
 class DeleteLogoController extends AbstractDeleteController
@@ -23,18 +24,18 @@ class DeleteLogoController extends AbstractDeleteController
     protected $settings;
 
     /**
-     * @var FilesystemInterface
+     * @var Filesystem
      */
     protected $uploadDir;
 
     /**
      * @param SettingsRepositoryInterface $settings
-     * @param FilesystemInterface $uploadDir
+     * @param Factory $filesystemFactory
      */
-    public function __construct(SettingsRepositoryInterface $settings, FilesystemInterface $uploadDir)
+    public function __construct(SettingsRepositoryInterface $settings, Factory $filesystemFactory)
     {
         $this->settings = $settings;
-        $this->uploadDir = $uploadDir;
+        $this->uploadDir = $filesystemFactory->disk('flarum-assets');
     }
 
     /**
@@ -48,7 +49,7 @@ class DeleteLogoController extends AbstractDeleteController
 
         $this->settings->set('logo_path', null);
 
-        if ($this->uploadDir->has($path)) {
+        if ($this->uploadDir->exists($path)) {
             $this->uploadDir->delete($path);
         }
 
