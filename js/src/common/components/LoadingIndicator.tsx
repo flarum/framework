e4b8f7c8e9b7dea0ebbfbf6eb8eed4337c4727e6
David Wheatley <hi@davwheat.dev>
2021-04-20 17:48:02 +0100

    Use classes for different sizes

--- js/src/common/components/LoadingIndicator.tsx
+++ js/src/common/components/LoadingIndicator.tsx
@@ -30,12 +30,13 @@ export interface LoadingIndicatorAttrs extends ComponentAttrs {
  * To set a custom color, use the CSS `color` property.
  *
  * To increase spacing around the spinner, use the CSS `height` property on the
- * spinner's **container**. Applying the `block` attribute handles this for you.
+ * spinner's **container**. Setting the `display` attribute to `block` will set
+ * a height of `100px` by default.
  *
  * To apply a custom size to the loading indicator, set the `--size` and
- * `--thickness` custom properties on the loading indicator container.
+ * `--thickness` CSS custom properties on the loading indicator container.
  *
- * If you really want to change how this looks as part of your custom theme,
+ * If you *really* want to change how this looks as part of your custom theme,
  * you can override the `border-radius` and `border` then set either a
  * background image, or use `content: "\<glyph>"` (e.g. `content: "\f1ce"`)
  * and `font-family: 'Font Awesome 5 Free'` to set an FA icon if you'd rather.
@@ -44,9 +45,9 @@ export interface LoadingIndicatorAttrs extends ComponentAttrs {
  *
  * - `containerClassName` Class name(s) to apply to the indicator's parent
  * - `className` Class name(s) to apply to the indicator itself
+ * - `display` Determines how the spinner should be displayed (`inline` or `block`)
  * - `size` Size of the loading indicator (`small`, `medium` or `large`)
  * - `containerAttrs` Optional attrs to be applied to the container DOM element
- * - `display` Determines how the spinner should be displayed (`inline` or `block`)
  *
  * All other attrs will be assigned as attributes on the DOM element.
  */
@@ -57,8 +58,8 @@ export default class LoadingIndicator extends Component<LoadingIndicatorAttrs> {
     const completeClassName = classList('LoadingIndicator', className);
     const completeContainerClassName = classList(
       'LoadingIndicator-container',
-      display === 'block' && 'LoadingIndicator--block',
-      display === 'inline' && 'LoadingIndicator--inline',
+      display && `LoadingIndicator-container--${display}`,
+      size && `LoadingIndicator-container--${size}`,
       containerClassName
     );
 
