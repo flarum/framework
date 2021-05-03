882f742cba1efa8ec6c69c57a16691e469fe5714
David Wheatley <hi@davwheat.dev>
2021-05-03 02:35:41 +0100

    Save a few bytes in `escapeRegExp`

--- js/src/common/utils/escapeRegExp.ts
+++ js/src/common/utils/escapeRegExp.ts
@@ -1,13 +1,10 @@
-const specialChars = /[\\^$.*+?()[\]{}|]/g;
+const specialChars = /[.*+?^${}()|[\]\\]/g;
 
 /**
- * Escapes the `RegExp` special characters "^", "$", "\", ".", "*", "+", "?", "(", ")", "[", "]", "{", "}", and "|" in `input`.
+ * Escapes the `RegExp` special characters in `input`.
  *
- * The difference between this function and Lodash is that we blindly trust that the `input` is a string, not a number, Symbol,
- * `null` or anything else.
- *
- * Based on Lodash's `escapeRegExp`: https://github.com/lodash/lodash/blob/4.1.2-npm-packages/lodash.escaperegexp/index.js
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
  */
-export default function escapeRegExp(input: string) {
-  return specialChars.test(input) ? input.replace(specialChars, '\\$&') : input;
+export default function escapeRegExp(input: string): string {
+  return input.replace(specialChars, '\\$&');
 }
