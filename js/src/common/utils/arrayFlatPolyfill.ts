87760b3d6d1c997590c42265f41ac7604cc968f0
David Wheatley <hi@davwheat.dev>
2021-05-06 00:11:41 +0100

    Use smaller Array.flat polyfill from MDN

--- js/src/common/utils/arrayFlatPolyfill.ts
+++ js/src/common/utils/arrayFlatPolyfill.ts
@@ -1,25 +1,12 @@
-// Based off of the CC-0 polyfill at https://github.com/jonathantneal/array-flat-polyfill
+// Based off of the polyfill on MDN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#reduce_concat_isarray_recursivity
 //
 // Needed to provide support for Safari on iOS < 12
 
 if (!Array.prototype['flat']) {
-  Array.prototype['flat'] = function flat(this: any, depth?: number) {
-    const realDepth = isNaN(depth as any) ? 1 : depth;
-
-    return realDepth
-      ? Array.prototype.reduce.call(
-          this,
-          function (acc, cur) {
-            if (Array.isArray(cur)) {
-              (acc as Array<any>).push.apply(acc, flat.call(cur, realDepth - 1));
-            } else {
-              (acc as Array<any>).push(cur);
-            }
-
-            return acc;
-          },
-          [] as Array<any>
-        )
+  Array.prototype['flat'] = function flat(this: any[], depth: number = 1): any[] {
+    return depth > 0
+      ? Array.prototype.reduce.call(this, (acc, val): any[] => acc.concat(Array.isArray(val) ? flat.call(val, depth - 1) : val), [])
       : // If no depth is provided, or depth is 0, just return a copy of
         // the array. Spread is supported in all major browsers (iOS 8+)
         [...this];
