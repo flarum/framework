fdb40e5779c39633831989c813ec362ec69d973e
David Wheatley <hi@davwheat.dev>
2021-05-08 23:58:59 +0100

    Add `delay` attr, update docblocks

--- js/src/common/components/Tooltip.tsx
+++ js/src/common/components/Tooltip.tsx
@@ -1,6 +1,7 @@
 import Component, { ComponentAttrs } from '../Component';
 import type Mithril from 'mithril';
 import classList from '../utils/classList';
+import { TooltipOptions } from '../../../@types/tooltips';
 
 export interface TooltipAttrs extends ComponentAttrs {
   /**
@@ -39,6 +40,16 @@ export interface TooltipAttrs extends ComponentAttrs {
    * @deprecated
    */
   html?: boolean;
+  /**
+   * Sets the delay between a trigger state occurring and the tooltip appearing
+   * on-screen.
+   *
+   * **Warning:** this option will be removed when we switch to CSS-only
+   * tooltips.
+   *
+   * @deprecated
+   */
+  delay?: number;
 }
 
 /**
@@ -50,15 +61,15 @@ export interface TooltipAttrs extends ComponentAttrs {
  * compatibility when we switch to pure CSS tooltips instead of Bootstrap
  * tooltips.
  *
- * @example <caption>Correct use of Tooltip component</caption>
+ * @example <caption>Basic usage</caption>
  *          <Tooltip text="You wish!">
  *            <Button>
  *              Click for free money!
  *            </Button>
  *          </Tooltip>
  *
- * @example <caption>Correct use of Tooltip component</caption>
- *          <Tooltip text="Replies from xxx, yyy, zzz and more.">
+ * @example <caption>Use of `position` and `showOnFocus` attrs</caption>
+ *          <Tooltip text="Woah! That's cool!" position="bottom" showOnFocus>
  *            <div>3 replies</div>
  *          </Tooltip>
  */
@@ -67,7 +78,7 @@ export default class Tooltip extends Component<TooltipAttrs> {
     const { children } = vnode;
 
     // We remove these to get the remaining attrs to pass to the DOM element
-    const { text, inline, tooltipVisible, showOnFocus, position, html, ...attrs } = this.attrs;
+    const { text, inline, tooltipVisible, showOnFocus, position, html, delay, ...attrs } = this.attrs;
 
     if (inline) {
       return <span {...attrs}>{children}</span>;
@@ -93,6 +104,7 @@ export default class Tooltip extends Component<TooltipAttrs> {
       tooltipVisible,
       showOnFocus = true,
       position = 'top',
+      delay,
       // This will have no effect when switching to CSS tooltips
       html = false,
       ...attrs
@@ -101,12 +113,13 @@ export default class Tooltip extends Component<TooltipAttrs> {
     this.attrs['aria-label'] = text;
 
     // https://getbootstrap.com/docs/3.3/javascript/#tooltips-options
-    this.$.tooltip({
+    this.$().tooltip({
       html,
+      delay,
       placement: position,
       title: text,
       // Fancy "hack" to assemble the trigger string
-      trigger: classList('hover', [showOnFocus && 'focus']),
+      trigger: classList('hover', [showOnFocus && 'focus']) as TooltipOptions['trigger'],
     });
   }
 }
