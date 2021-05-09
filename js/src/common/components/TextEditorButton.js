14ee976ac94d1aae12d51d694882fd50cc6cca86
David Wheatley <hi@davwheat.dev>
2021-05-09 00:48:39 +0000

    Use new Tooltip component in TextEditorButton

--- js/src/common/components/TextEditorButton.js
+++ js/src/common/components/TextEditorButton.js
@@ -1,19 +1,30 @@
 import Button from './Button';
+import Tooltip from './Tooltip';
 
 /**
  * The `TextEditorButton` component displays a button suitable for the text
  * editor toolbar.
  */
 export default class TextEditorButton extends Button {
+  view(vnode) {
+    const originalView = super.view(vnode);
+
+    console.log(originalView);
+
+    // Steal tooltip label from the Button superclass
+    const tooltipText = originalView.attrs.title;
+    delete originalView.attrs.title;
+
+    return (
+      <Tooltip inline text={tooltipText}>
+        {originalView}
+      </Tooltip>
+    );
+  }
+
   static initAttrs(attrs) {
     super.initAttrs(attrs);
 
     attrs.className = attrs.className || 'Button Button--icon Button--link';
-  }
-
-  oncreate(vnode) {
-    super.oncreate(vnode);
-
-    this.$().tooltip();
   }
 }
