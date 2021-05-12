c1aafff0b0a1f29077c73d7687415a68178fc75f
David Wheatley <hi@davwheat.dev>
2021-05-12 23:58:03 +0100

    Remove bundle analyzer from core

--- js/webpack.config.js
+++ js/webpack.config.js
@@ -1,20 +1,8 @@
 const config = require('flarum-webpack-config');
-
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const merge = require('webpack-merge');
-
-const useBundleAnalyzer = process.env.ANALYZER === 'true';
-
-const plugins = [];
-
-if (useBundleAnalyzer) {
-  plugins.push(new BundleAnalyzerPlugin());
-}
 
 module.exports = merge(config(), {
   output: {
     library: 'flarum.core',
   },
-
-  plugins,
 });
