41101a85f55f8f40ce4ad1a137a0c4119df50f6b
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-12 19:48:17 -0400

    Use RequestUtil to access actor

--- packages/ext-tags/src/Api/Controller/ShowTagController.php
+++ packages/ext-tags/src/Api/Controller/ShowTagController.php
@@ -10,6 +10,7 @@
 namespace Flarum\Tags\Api\Controller;
 
 use Flarum\Api\Controller\AbstractShowController;
+use Flarum\Http\RequestUtil;
 use Flarum\Tags\Api\Serializer\TagSerializer;
 use Flarum\Tags\Tag;
 use Illuminate\Support\Arr;
@@ -46,7 +47,7 @@ class ShowTagController extends AbstractShowController
     protected function data(ServerRequestInterface $request, Document $document)
     {
         $slug = Arr::get($request->getQueryParams(), 'slug');
-        $actor = $request->getAttribute('actor');
+        $actor = RequestUtil::getActor($request);
         $include = $this->extractInclude($request);
 
         return $this->tags
