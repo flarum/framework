f076329636819d45a4e2533c873f589124f8d154
David Wheatley <hi@davwheat.dev>
2021-05-17 14:43:35 +0100

    Fix tooltip deprecation warning

--- extensions/markdown/js/src/forum/components/MarkdownButton.js
+++ extensions/markdown/js/src/forum/components/MarkdownButton.js
@@ -1,20 +1,29 @@
 import Component from 'flarum/common/Component';
 import icon from 'flarum/common/helpers/icon';
+import Tooltip from 'flarum/common/components/Tooltip';
 
 export default class MarkdownButton extends Component {
   oncreate(vnode) {
     super.oncreate(vnode);
-
-    this.$().tooltip();
   }
 
   view() {
-    return (
-      <button className="Button Button--icon Button--link" title={this.attrs.title} data-hotkey={this.attrs.hotkey}
-        onkeydown={this.keydown.bind(this)} onclick={this.attrs.onclick}>
+    const button = (
+      <button
+        className="Button Button--icon Button--link"
+        data-hotkey={this.attrs.hotkey}
+        onkeydown={this.keydown.bind(this)}
+        onclick={this.attrs.onclick}
+      >
         {icon(this.attrs.icon)}
       </button>
     );
+
+    if (this.attrs.title) {
+      return <Tooltip text={this.attrs.title}>{button}</Tooltip>;
+    }
+
+    return button;
   }
 
   keydown(event) {
