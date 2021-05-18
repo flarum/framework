188dc98c4ad92d4814aa9caca74822ef23c45c9d
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-05-18 03:08:32 -0400

    Fix onOneServer, withoutOverlapping console scheduling options
    Flarum doesn't fully use Laravel's cache system, but rather
    creates and binds a single cache store.
    See \Flarum\Foundation\InstalledSite::registerCache
    Since certain config options (e.g. withoutOverlapping, onOneServer)
    need the cache, we must override the cache factory we give to the scheduling
    mutexes so it returns our single custom cache.

---
+++ src/Console/Cache/Factory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Flarum\Console\Cache;
+
+use Illuminate\Contracts\Cache\Factory as FactoryContract;
+use Illuminate\Contracts\Cache\Repository;
+use Illuminate\Contracts\Container\Container;
+
+class Factory implements FactoryContract
+{
+    /**
+     * @var Container
+     */
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * Get a cache store instance by name.
+     *
+     * @param  string|null $name
+     * @return Repository
+     */
+    public function store($name = null)
+    {
+        return $this->container['cache.store'];
+    }
+}
