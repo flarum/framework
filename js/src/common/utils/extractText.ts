99c56d8809572011e016f7159393800d64e9f532
David Sevilla Martin <me@datitisev.me>
2021-06-20 11:43:52 -0400

    Convert common/utils/extractText

--- js/src/common/utils/extractText.ts
+++ js/src/common/utils/extractText.ts
@@ -1,15 +1,14 @@
+import Mithril from 'mithril';
+
 /**
  * Extract the text nodes from a virtual element.
- *
- * @param {VirtualElement} vdom
- * @return {String}
  */
-export default function extractText(vdom) {
+export default function extractText(vdom: Mithril.Children): string {
   if (vdom instanceof Array) {
     return vdom.map((element) => extractText(element)).join('');
   } else if (typeof vdom === 'object' && vdom !== null) {
-    return vdom.children ? extractText(vdom.children) : vdom.text;
+    return vdom.children ? extractText(vdom.children) : (vdom.text as string);
   } else {
-    return vdom;
+    return vdom as string;
   }
 }
