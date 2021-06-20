e59c2172c430684253b15c5c9e3f6e00de97f7b9
David Sevilla Martin <me@datitisev.me>
2021-06-20 09:54:48 -0400

    Allow using file extension in core compat imports
    Necessary for extend imports to have proper typings as we also have an unrelated extend/index.js file

--- js/src/common/utils/proxifyCompat.ts
+++ js/src/common/utils/proxifyCompat.ts
@@ -1,10 +1,17 @@
 export default (compat: { [key: string]: any }, namespace: string) => {
   // regex to replace common/ and NAMESPACE/ for core & core extensions
+  // and remove .js, .ts and .tsx extensions
   // e.g. admin/utils/extract --> utils/extract
   // e.g. tags/common/utils/sortTags --> tags/utils/sortTags
-  const regex = new RegExp(`(\\w+\\/)?(${namespace}|common)\\/`);
+  const regex = new RegExp(`^(?:\w+\/)?(?:${namespace}|common)\/(.+?)(?:\.(?:js|tsx?))?$`);
 
   return new Proxy(compat, {
-    get: (obj, prop: string) => obj[prop] || obj[prop.replace(regex, '$1')],
+    get: (obj, prop: string) => {
+      if (obj[prop]) return obj[prop];
+
+      const out = regex.exec(prop);
+
+      return out && obj[out[1]];
+    },
   });
 };
