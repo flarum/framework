eda97325d6eaaa258a7312fdf05a88b59a291245
SychO9 <sychocouldy@gmail.com>
2021-06-27 14:47:45 +0100

    Use laravel validator to replace validation error params
    When we switched to intl icu message format, the translation parameters 
    have become required to be in the format `{param}`, so we can no longer 
    use the translator here.
    
    This PR switches to consistently rely on the laravel validator to make 
    the replacements, just like with all other validation errors in the rest 
    of the application.

--- src/User/AvatarValidator.php
+++ src/User/AvatarValidator.php
@@ -17,12 +17,19 @@ use Symfony\Component\Mime\MimeTypes;
 class AvatarValidator extends AbstractValidator
 {
     /**
+     * @var \Illuminate\Validation\Validator
+     */
+    protected $laravelValidator;
+
+    /**
      * Throw an exception if a model is not valid.
      *
      * @param array $attributes
      */
     public function assertValid(array $attributes)
     {
+        $this->laravelValidator = $this->makeValidator($attributes);
+
         $this->assertFileRequired($attributes['avatar']);
         $this->assertFileMimes($attributes['avatar']);
         $this->assertFileSize($attributes['avatar']);
@@ -59,15 +66,14 @@ class AvatarValidator extends AbstractValidator
         $maxSize = $this->getMaxSize();
 
         if ($file->getSize() / 1024 > $maxSize) {
-            $this->raise('max.file', [':max' => $maxSize]);
+            $this->raise('max.file', [':max' => $maxSize], 'max');
         }
     }
 
-    protected function raise($error, array $parameters = [])
+    protected function raise($error, array $parameters = [], $rule = null)
     {
-        $message = $this->translator->trans(
-            "validation.$error",
-            $parameters + [':attribute' => 'avatar']
+        $message = $this->laravelValidator->makeReplacements(
+            $this->translator->trans("validation.$error"), 'avatar', $rule ?? $error, array_values($parameters)
         );
 
         throw new ValidationException(['avatar' => $message]);
