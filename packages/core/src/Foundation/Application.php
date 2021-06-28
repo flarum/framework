aee94be63d71a7481d05404c4fe3a1a7cccbbdb6
Daniel Klabbers <daniel+git@klabbers.email>
2021-06-28 12:22:48 +0200

    changelog for v1.0.4

--- packages/core/src/Foundation/Application.php
+++ packages/core/src/Foundation/Application.php
@@ -21,7 +21,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.0.4-dev';
+    const VERSION = '1.0.4';
 
     /**
      * The IoC container for the Flarum application.
