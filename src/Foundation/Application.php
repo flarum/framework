a077ae9ca36a8ad0adc25042118ba2407feaa5fe
Daniel Klabbers <daniel+git@klabbers.email>
2021-06-28 12:26:15 +0200

    set version constant for 1.0.5-dev

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -21,7 +21,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.0.4';
+    const VERSION = '1.0.5-dev';
 
     /**
      * The IoC container for the Flarum application.
