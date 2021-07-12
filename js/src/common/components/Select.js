2910555420fa1ba0076263634ad51061552eefb0
David Wheatley <hi@davwheat.dev>
2021-07-12 19:33:34 +0100

    Allow direct passing attrs to the Select wrapper

--- js/src/common/components/Select.js
+++ js/src/common/components/Select.js
@@ -11,16 +11,30 @@ import classList from '../utils/classList';
  * - `onchange` A callback to run when the selected value is changed.
  * - `value` The value of the selected option.
  * - `disabled` Disabled state for the input.
- * - `outerClassName` class given to the containing Select element
+ * - `wrapperAttrs` A map of attrs to be passed to the DOM element wrapping the `<select>`
  *
  * Other attributes are passed directly to the `<select>` element rendered to the DOM.
  */
 export default class Select extends Component {
   view() {
-    const { options, onchange, value, disabled, outerClassName, ...domAttrs } = this.attrs;
+    const {
+      options,
+      onchange,
+      value,
+      disabled,
+
+      // Destructure the `wrapperAttrs` object to extract the `className` for passing to `classList()`
+      // `= {}` prevents errors when `wrapperAttrs` is undefined
+      wrapperAttrs: {
+        className: wrapperClassName,
+        ...wrapperAttrs
+      } = {},
+
+      ...domAttrs
+    } = this.attrs;
 
     return (
-      <span className={classList('Select', outerClassName)}>
+      <span className={classList('Select', wrapperClassName)} {...wrapperAttrs}>
         <select
           className="Select-input FormControl"
           onchange={onchange ? withAttr('value', onchange.bind(this)) : undefined}
