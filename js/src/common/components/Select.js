43813ef933f4d968045562a0783b64afe59e6aed
David Wheatley <hi@davwheat.dev>
2021-07-12 18:47:07 +0100

    Allow extra attrs provided to `<Select>` to be passed through to the DOM element

--- js/src/common/components/Select.js
+++ js/src/common/components/Select.js
@@ -1,6 +1,7 @@
 import Component from '../Component';
 import icon from '../helpers/icon';
 import withAttr from '../utils/withAttr';
+import classList from '../utils/classList';
 
 /**
  * The `Select` component displays a <select> input, surrounded with some extra
@@ -10,18 +11,22 @@ import withAttr from '../utils/withAttr';
  * - `onchange` A callback to run when the selected value is changed.
  * - `value` The value of the selected option.
  * - `disabled` Disabled state for the input.
+ * - `outerClassName` class given to the containing Select element
+ *
+ * Other attributes are passed directly to the `<select>` element rendered to the DOM.
  */
 export default class Select extends Component {
   view() {
-    const { options, onchange, value, disabled } = this.attrs;
+    const { options, onchange, value, disabled, outerClassName, ...domAttrs } = this.attrs;
 
     return (
-      <span className="Select">
+      <span className={classList('Select', outerClassName)}>
         <select
           className="Select-input FormControl"
           onchange={onchange ? withAttr('value', onchange.bind(this)) : undefined}
           value={value}
           disabled={disabled}
+          {...domAttrs}
         >
           {Object.keys(options).map((key) => (
             <option value={key}>{options[key]}</option>
