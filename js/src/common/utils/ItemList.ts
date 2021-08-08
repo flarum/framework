5cf2ba1fce3a2ed7248f5dda494c0873f8c6a3a8
David Wheatley <hi@davwheat.dev>
2021-08-08 20:03:33 +0200

    Improve typings for ItemList

--- js/src/common/utils/ItemList.ts
+++ js/src/common/utils/ItemList.ts
@@ -1,9 +1,20 @@
-class Item {
-  content: any;
+class Item<T> {
+  content:
+    | T
+    | (T & {
+        /**
+         * Set when calling `.toArray()`
+         */
+        itemName: string;
+      });
   priority: number;
+
+  /**
+   * Set when calling `.toArray()`
+   */
   key?: number;
 
-  constructor(content: any, priority?: number) {
+  constructor(content: any, priority: number = 0) {
     this.content = content;
     this.priority = priority;
   }
@@ -13,23 +24,17 @@ class Item {
  * The `ItemList` class collects items and then arranges them into an array
  * by priority.
  */
-export default class ItemList {
+export default class ItemList<T> {
   /**
    * The items in the list
    */
-  items: { [key: string]: Item } = {};
+  items: { [key: string]: Item<T> } = {};
 
   /**
    * Check whether the list is empty.
    */
   isEmpty(): boolean {
-    for (const i in this.items) {
-      if (this.items.hasOwnProperty(i)) {
-        return false;
-      }
-    }
-
-    return true;
+    return !!Object.keys(this.items).length;
   }
 
   /**
@@ -42,7 +47,7 @@ export default class ItemList {
   /**
    * Get the content of an item.
    */
-  get(key: string): any {
+  get(key: string): T {
     return this.items[key].content;
   }
 
@@ -54,7 +59,7 @@ export default class ItemList {
    * @param [priority] The priority of the item. Items with a higher
    *     priority will be positioned before items with a lower priority.
    */
-  add(key: string, content: any, priority: number = 0): this {
+  add(key: string, content: T, priority: number = 0): this {
     this.items[key] = new Item(content, priority);
 
     return this;
@@ -62,8 +67,10 @@ export default class ItemList {
 
   /**
    * Replace an item in the list, only if it is already present.
+   *
+   * If `content` or `priority` are `null`, these values will not be replaced.
    */
-  replace(key: string, content: any = null, priority: number = null): this {
+  replace(key: string, content: T | null = null, priority: number | null = null): this {
     if (this.items[key]) {
       if (content !== null) {
         this.items[key].content = content;
@@ -88,13 +95,18 @@ export default class ItemList {
 
   /**
    * Merge another list's items into this one.
+   *
+   * The list passed to this function will overwrite items whichalready exist
+   * with the same key.
    */
-  merge(items: this): this {
-    for (const i in items.items) {
-      if (items.items.hasOwnProperty(i) && items.items[i] instanceof Item) {
-        this.items[i] = items.items[i];
+  merge<K>(otherList: ItemList<K>): ItemList<T | K> {
+    Object.keys(otherList.items).forEach((key) => {
+      const val = otherList.items[key];
+
+      if (val instanceof Item) {
+        (this as ItemList<T | K>).items[key] = otherList.items[key];
       }
-    }
+    });
 
     return this;
   }
@@ -104,27 +116,29 @@ export default class ItemList {
    * item's content will be assigned an `itemName` property equal to the item's
    * unique key.
    */
-  toArray(): any[] {
-    const items: Item[] = [];
+  toArray(): T[] {
+    const items: Item<T>[] = [];
 
-    for (const i in this.items) {
-      if (this.items.hasOwnProperty(i) && this.items[i] instanceof Item) {
-        this.items[i].content = Object(this.items[i].content);
+    Object.keys(this.items).forEach((key) => {
+      const val = this.items[key];
 
-        this.items[i].content.itemName = i;
-        items.push(this.items[i]);
-        this.items[i].key = items.length;
+      if (val instanceof Item) {
+        val.content = Object(val.content);
+
+        (val.content as T & { itemName: string }).itemName = key;
+        val.key = items.length;
+
+        items.push(val);
       }
-    }
+    });
 
     return items
       .sort((a, b) => {
         if (a.priority === b.priority) {
-          return a.key - b.key;
-        } else if (a.priority > b.priority) {
-          return -1;
+          return a.key! - b.key!;
         }
-        return 1;
+
+        return b.priority - a.priority;
       })
       .map((item) => item.content);
   }
