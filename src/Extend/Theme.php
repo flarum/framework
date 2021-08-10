5b5749ffeebe2720b58a926da16cff67984ecd9e
SychO9 <sychocouldy@gmail.com>
2021-08-10 18:04:45 +0100

    docs: Add Php docblocks

--- src/Extend/Theme.php
+++ src/Extend/Theme.php
@@ -17,6 +17,16 @@ class Theme implements ExtenderInterface
     private $lessImportOverrides = [];
     private $fileSourceOverrides = [];
 
+    /**
+     * This can be used to override LESS files that are imported within the code.
+     * For example, core's `forum.less` file imports a `forum/DiscussionListItem.less` file.
+     * The contents of this file can be overriden with this method.
+     *
+     * @param string $file: Relative path of the file to override, for example: `forum/Hero.less`
+     * @param string $newFilePath: Absolute path of the new file.
+     * @param string $extensionId: If overriding an extension file, specify its ID, for example: `flarum-tags`.
+     * @return self
+     */
     public function overrideLessImport(string $file, string $newFilePath, string $extensionId = null): self
     {
         $this->lessImportOverrides[] = compact('file', 'newFilePath', 'extensionId');
@@ -24,6 +34,16 @@ class Theme implements ExtenderInterface
         return $this;
     }
 
+    /**
+     * This method allows overriding LESS file sources.
+     * For example `forum.less`, `admin.less`, `mixins.less` and `variables.less` are file sources,
+     * and can therefore be overriden using this method.
+     *
+     * @param string $file: Name of the file to override, for example: `admin.less`
+     * @param string $newFilePath: Absolute path of the new file.
+     * @param string $extensionId: If overriding an extension file, specify its ID, for example: `flarum-tags`.
+     * @return self
+     */
     public function overrideFileSource(string $file, string $newFilePath, string $extensionId = null): self
     {
         $this->fileSourceOverrides[] = compact('file', 'newFilePath', 'extensionId');
