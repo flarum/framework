53c88732d0dbbc96a0d954210fc5273a54027bd7
David Wheatley <hi@davwheat.dev>
2021-08-20 23:59:54 +0200

    Revert "Move `HTMLInputTypes` type to global declarations"
    This reverts commit c900cb3f6d50c7e315aeb9a97ec966c5fe2134d1.

--- js/src/admin/components/AdminPage.tsx
+++ js/src/admin/components/AdminPage.tsx
@@ -23,6 +23,30 @@ export interface AdminHeaderOptions {
   className: string;
 }
 
+type HTMLInputTypes =
+  | 'button'
+  | 'checkbox'
+  | 'color'
+  | 'date'
+  | 'datetime-local'
+  | 'email'
+  | 'file'
+  | 'hidden'
+  | 'image'
+  | 'month'
+  | 'number'
+  | 'password'
+  | 'radio'
+  | 'range'
+  | 'reset'
+  | 'search'
+  | 'submit'
+  | 'tel'
+  | 'text'
+  | 'time'
+  | 'url'
+  | 'week';
+
 interface CommonSettingsItemOptions extends Mithril.Attributes {
   setting: string;
   label: Mithril.Children;
