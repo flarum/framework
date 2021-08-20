9c1fb860cd1e61a383c2b169b31da95cdace048f
David Wheatley <hi@davwheat.dev>
2021-08-20 21:53:11 +0200

    Update regex strings

--- js/src/common/utils/proxifyCompat.ts
+++ js/src/common/utils/proxifyCompat.ts
@@ -3,8 +3,8 @@ export default function proxifyCompat(compat: Record<string, unknown>, namespace
   // and remove .js, .ts and .tsx extensions
   // e.g. admin/utils/extract --> utils/extract
   // e.g. tags/common/utils/sortTags --> tags/utils/sortTags
-  const regex = new RegExp(`^(.+\/)?(${namespace}|common)\/`);
-  const fileExt = /\.(\.js|\.tsx?)$/;
+  const regex = new RegExp(String.raw`(\w+\/)?(${namespace}|common)\/`);
+  const fileExt = /(\.js|\.tsx?)$/;
 
   return new Proxy(compat, {
     get: (obj, prop: string) => obj[prop] || obj[prop.replace(regex, '$1').replace(fileExt, '')],
