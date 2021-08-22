3cf19dd2ea154104658737af593a55756fda3cee
David Wheatley <hi@davwheat.dev>
2021-08-22 20:38:01 +0100

    Rewrite Button to Typescript (#2984)
    * Rename Button file
    
    * Convert to TS
    
    * Add debug warning helper
    
    Fires `console.warn`, but only when the forum is in debug mode. Can help to inform extension developers of possible issues with their JS code.
    
    * Simplify button content template
    
    * Rewrite Button component
    
    - Prefer `aria-label` over `title`
    - Don't duplicate button content to `title` attribute
    - Warn in debug mode if button has no accessible content
    - Use modern JS/TS syntax (`||=`, spread, etc)
    
    * Update to work with new Button component
    
    * Update warning
    
    Co-authored-by: Matt Kilgore <tankerkiller125@gmail.com>
    
    * Fire warning in `oncreate`
    
    * Format
    
    * Make Button have extensible Attributes type via generics
    
    * Update args type
    
    * Update js/src/common/components/Button.tsx
    
    Co-authored-by: Matt Kilgore <tankerkiller125@gmail.com>
    Co-authored-by: David Sevilla Martin <me@datitisev.me>
    Co-authored-by: Alexander Skvortsov <sasha.skvortsov109@gmail.com>

--- js/src/common/components/TextEditorButton.js
+++ js/src/common/components/TextEditorButton.js
@@ -1,24 +1,27 @@
+import extractText from '../utils/extractText';
 import Button from './Button';
 import Tooltip from './Tooltip';
 
 /**
  * The `TextEditorButton` component displays a button suitable for the text
  * editor toolbar.
+ *
+ * Automatically creates tooltips using the Tooltip component and provided text.
+ *
+ * ## Attrs
+ * - `title` - Tooltip for the button
  */
 export default class TextEditorButton extends Button {
   view(vnode) {
     const originalView = super.view(vnode);
 
-    // Steal tooltip label from the Button superclass
-    const tooltipText = originalView.attrs.title;
-    delete originalView.attrs.title;
-
-    return <Tooltip text={tooltipText}>{originalView}</Tooltip>;
+    return <Tooltip text={this.attrs.tooltipText || extractText(vnode.children)}>{originalView}</Tooltip>;
   }
 
   static initAttrs(attrs) {
     super.initAttrs(attrs);
 
     attrs.className = attrs.className || 'Button Button--icon Button--link';
+    attrs.tooltipText = attrs.title;
   }
 }
