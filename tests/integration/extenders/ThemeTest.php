76d0d3e6878cb74d1c4665acb4a032e7baf7067f
SychO9 <sychocouldy@gmail.com>
2021-08-27 11:27:00 +0100

    test: Improve tests by checking contents

--- tests/integration/extenders/ThemeTest.php
+++ tests/integration/extenders/ThemeTest.php
@@ -17,6 +17,19 @@ class ThemeTest extends TestCase
     /**
      * @test
      */
+    public function theme_extender_override_import_doesnt_work_by_default()
+    {
+        $response = $this->send($this->request('GET', '/'));
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $cssFilePath = $this->app()->getContainer()->make('filesystem')->disk('flarum-assets')->path('forum.css');
+        $this->assertStringNotContainsString('.dummy_test_case{color:red}', file_get_contents($cssFilePath));
+    }
+
+    /**
+     * @test
+     */
     public function theme_extender_override_import_works()
     {
         $this->extend(
@@ -27,12 +40,16 @@ class ThemeTest extends TestCase
         $response = $this->send($this->request('GET', '/'));
 
         $this->assertEquals(200, $response->getStatusCode());
+
+        $cssFilePath = $this->app()->getContainer()->make('filesystem')->disk('flarum-assets')->path('forum.css');
+
+        $this->assertStringContainsString('.dummy_test_case{color:red}', file_get_contents($cssFilePath));
     }
 
     /**
      * @test
      */
-    public function theme_extender_override_import_works_by_failing_when_necessary()
+    public function theme_extender_override_import_works_with_external_sources()
     {
         $this->extend(
             (new Extend\Frontend('forum'))
@@ -43,7 +60,14 @@ class ThemeTest extends TestCase
 
         $response = $this->send($this->request('GET', '/'));
 
-        $this->assertEquals(500, $response->getStatusCode());
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $cssFilePath = $this->app()->getContainer()->make('filesystem')->disk('flarum-assets')->path('forum.css');
+        $contents = file_get_contents($cssFilePath);
+
+        $this->assertStringNotContainsString('.Imported', $contents);
+        $this->assertStringContainsString('.dummy_test_case{color:red}', $contents);
+        $this->assertStringContainsString('.dummy{color:yellow}', $contents);
     }
 
     /**
@@ -53,12 +77,16 @@ class ThemeTest extends TestCase
     {
         $this->extend(
             (new Extend\Theme)
-                ->overrideFileSource('forum.less', __DIR__.'/../../fixtures/less/forum.less')
+                ->overrideFileSource('forum.less', __DIR__.'/../../fixtures/less/override_filesource.less')
         );
 
         $response = $this->send($this->request('GET', '/'));
 
         $this->assertEquals(200, $response->getStatusCode());
+
+        $cssFilePath = $this->app()->getContainer()->make('filesystem')->disk('flarum-assets')->path('forum.css');
+
+        $this->assertEquals('body{color:orange}', file_get_contents($cssFilePath));
     }
 
     /**
