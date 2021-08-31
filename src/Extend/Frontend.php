99aeb1247efe555b1654676fcf3bfbaebc6938bb
luceos <luceos@users.noreply.github.com>
2021-08-31 13:42:16 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Extend/Frontend.php
+++ src/Extend/Frontend.php
@@ -173,7 +173,7 @@ class Frontend implements ExtenderInterface
             return;
         }
 
-        $abstract = 'flarum.assets.' . $this->frontend;
+        $abstract = 'flarum.assets.'.$this->frontend;
 
         $container->resolving($abstract, function (Assets $assets) use ($moduleName) {
             if ($this->js) {
@@ -197,7 +197,7 @@ class Frontend implements ExtenderInterface
             }
         });
 
-        if (!$container->bound($abstract)) {
+        if (! $container->bound($abstract)) {
             $container->bind($abstract, function (Container $container) {
                 return $container->make('flarum.assets.factory')($this->frontend);
             });
