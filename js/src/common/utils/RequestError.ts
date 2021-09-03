624e55b3a5da0284d197df3affe1df4564ed80a0
David Wheatley <hi@davwheat.dev>
2021-09-03 20:42:57 +0100

    Fix incorrect type

--- js/src/common/utils/RequestError.ts
+++ js/src/common/utils/RequestError.ts
@@ -1,21 +1,21 @@
 export default class RequestError {
-  status: string;
-  options: object;
+  status: number;
+  options: Record<string, unknown>;
   xhr: XMLHttpRequest;
 
   responseText: string | null;
-  response: object | null;
+  response: Record<string, unknown> | null;
 
   alert: any;
 
-  constructor(status: string, responseText: string | null, options: object, xhr: XMLHttpRequest) {
+  constructor(status: number, responseText: string | null, options: Record<string, unknown>, xhr: XMLHttpRequest) {
     this.status = status;
     this.responseText = responseText;
     this.options = options;
     this.xhr = xhr;
 
     try {
-      this.response = JSON.parse(responseText);
+      this.response = JSON.parse(responseText ?? 'null');
     } catch (e) {
       this.response = null;
     }
