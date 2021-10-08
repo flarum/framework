17b7154b6dbffdd4ac31e30a6ab53e6520b9ebe3
Braunson Yager <braunson@gmail.com>
2021-10-08 10:10:57 -0400

    Made locale an optional parameter, default to user locale

--- js/src/common/utils/formatNumber.ts
+++ js/src/common/utils/formatNumber.ts
@@ -1,12 +1,16 @@
 /**
  * The `formatNumber` utility localizes a number into a string with the
- * appropriate punctuation based on the provided locale.
+ * appropriate punctuation based on the provided locale otherwise will default to the users locale.
  *
  * @example
  * formatNumber(1234, 'en-US');
  * // 1,234
  */
-export default function formatNumber(number: number, locale: string = 'en-US'): string {
+export default function formatNumber(number: number, locale?: string): string {
+  if (typeof locale === 'undefined') {
+    const locale: string = app.data.locale;
+  }
+
   return new Intl
     .NumberFormat(locale)
     .format(number);
