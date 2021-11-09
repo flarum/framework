3bd2dd4813b9e0fe5d1834963e2c2f453c835036
SychO9 <sychocouldy@gmail.com>
2021-11-09 20:32:09 +0100

    Move exception cause guessing out of handler

--- extensions/package-manager/src/Exception/ComposerRequireFailedException.php
+++ extensions/package-manager/src/Exception/ComposerRequireFailedException.php
@@ -11,5 +11,20 @@ namespace Flarum\PackageManager\Exception;
 
 class ComposerRequireFailedException extends ComposerCommandFailedException
 {
-    // ...
+    protected const INCOMPATIBLE_REGEX = '/(?:(?: +- {PACKAGE_NAME}(?: v[0-9A-z.-]+ requires|\[[^\[\]]+\] require) flarum\/core)|(?:Could not find a version of package {PACKAGE_NAME} matching your minim)|(?: +- Root composer.json requires {PACKAGE_NAME} [^,]+, found {PACKAGE_NAME}\[[^\[\]]+\]+ but it does not match your minimum-stability))/m';
+
+    public function guessCause(): ?string
+    {
+        $hasMatches = preg_match(
+            str_replace('{PACKAGE_NAME}', preg_quote($this->getRawPackageName(), '/'), self::INCOMPATIBLE_REGEX),
+            $this->getMessage(),
+            $matches
+        );
+
+        if ($hasMatches) {
+            return 'extension_incompatible_with_instance';
+        }
+
+        return null;
+    }
 }
