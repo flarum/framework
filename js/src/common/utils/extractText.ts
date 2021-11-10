4bba10a1d7cb05a6f6d5d65ab077850d78577735
David Sevilla Martin <me@datitisev.me>
2021-11-10 17:49:07 -0500

    extractText: cast vdom and vdom.text to String instead of typing

--- js/src/common/utils/extractText.ts
+++ js/src/common/utils/extractText.ts
@@ -7,8 +7,8 @@ export default function extractText(vdom: Mithril.Children): string {
   if (vdom instanceof Array) {
     return vdom.map((element) => extractText(element)).join('');
   } else if (typeof vdom === 'object' && vdom !== null) {
-    return vdom.children ? extractText(vdom.children) : (vdom.text as string);
+    return vdom.children ? extractText(vdom.children) : String(vdom.text);
   } else {
-    return vdom as string;
+    return String(vdom);
   }
 }
