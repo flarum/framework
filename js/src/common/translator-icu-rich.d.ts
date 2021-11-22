c44e4b455820b2889e736354ba1db496fd5ae939
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-11-21 21:51:36 -0500

    format

--- js/src/common/translator-icu-rich.d.ts
+++ js/src/common/translator-icu-rich.d.ts
@@ -1,7 +1,13 @@
 declare module '@askvortsov/rich-icu-message-formatter' {
   type IValues = Record<string, any>;
 
-  type ITypeHandler = (value: string, matches: string, locale: string, values: IValues, format: (message: string, values: IValues) => string) => string
+  type ITypeHandler = (
+    value: string,
+    matches: string,
+    locale: string,
+    values: IValues,
+    format: (message: string, values: IValues) => string
+  ) => string;
   type IRichHandler = (tag: any, values: IValues, contents: string) => any;
 
   type ValueOrArray<T> = T | ValueOrArray<T>[];
@@ -10,12 +16,11 @@ declare module '@askvortsov/rich-icu-message-formatter' {
   export class RichMessageFormatter {
     locale: string | null;
     constructor(locale: string | null, typeHandlers: Record<string, ITypeHandler>, richHandler: IRichHandler);
-  
+
     format(message: string, values: IValues): string;
     process(message: string, values: IValues): NestedStringArray;
     rich(message: string, values: IValues): NestedStringArray;
   }
 
-  export function mithrilRichHandler(tag: any, values: IValues, contents: string): any
-
+  export function mithrilRichHandler(tag: any, values: IValues, contents: string): any;
 }
