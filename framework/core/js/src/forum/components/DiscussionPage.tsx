11a72f8450e724284cb8881d1f5cceac1851ad22
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2021-11-21 19:54:37 -0500

    Remove unnecessary nonnull assertions

--- framework/core/js/src/forum/components/DiscussionPage.tsx
+++ framework/core/js/src/forum/components/DiscussionPage.tsx
@@ -222,7 +222,7 @@ export default class DiscussionPage<CustomAttrs extends IDiscussionPageAttrs = I
             record.relationships.discussion.data.id === discussionId
         )
         .map((record) => app.store.getById<Post>('posts', record.id))
-        .sort((a?: Post, b?: Post) => a?.createdAt()?.getTime()! - b?.createdAt()?.getTime()!)
+        .sort((a?: Post, b?: Post) => (a?.createdAt()?.getTime() ?? 0) - (b?.createdAt()?.getTime() ?? 0))
         .slice(0, 20);
     }
 
