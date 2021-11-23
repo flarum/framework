8e39edb200580d10f14966ee49ab678f176c9802
SychO9 <sychocouldy@gmail.com>
2021-11-23 23:06:30 +0100

    chore: remove test code

--- extensions/package-manager/src/Command/MajorUpdateHandler.php
+++ extensions/package-manager/src/Command/MajorUpdateHandler.php
@@ -68,28 +68,6 @@ class MajorUpdateHandler
     {
         $command->actor->assertAdmin();
 
-        // @todo remove testing code
-        throw new MajorUpdateFailedException(
-            '*',
-            'Loading composer repositories with package information
-Updating dependencies
-Your requirements could not be resolved to an installable set of packages.
-
-  Problem 1
-    - Root composer.json requires flarum/tags * -> satisfiable by flarum/tags[1.0.0].
-    - flarum/tags 1.0.0 requires flarum/core >=0.1.0-beta.15 <0.1.0-beta.16 -> found flarum/core[v0.1.0-beta.15] but it conflicts with your root composer.json require (^1.1.1).
-  Problem 2
-    - Root composer.json requires sycho/flarum-profile-cover * -> satisfiable by sycho/flarum-profile-cover[1.0.0].
-    - sycho/flarum-profile-cover 1.0.0 requires flarum/core >=0.1.0-beta.15 <=0.1.0-beta.16 -> found flarum/core[v0.1.0-beta.15, v0.1.0-beta.16] but it conflicts with your root composer.json require (^1.1.1).
-  Problem 3
-    - Root composer.json requires askvortsov/flarum-auto-moderator * -> satisfiable by askvortsov/flarum-auto-moderator[1.0.0].
-    - askvortsov/flarum-auto-moderator 1.0.0 requires flarum/core 0.1.0-beta.15 -> found flarum/core[v0.1.0-beta.15] but it conflicts with your root composer.json require (^1.1.1).
-
-<warning>Running update with --no-dev does not mean require-dev is ignored, it just means the packages will not be installed. If dev requirements are blocking the update you have to resolve those problems.</warning>
-  ',
-            '2.0',
-        );
-
         $majorVersion = $this->lastUpdateCheck->getNewMajorVersion();
 
         if (! $majorVersion) {
