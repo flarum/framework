aa0b68bc8de96dc2a69c61fbe3ec90821ce9b552
David Sevilla Martin <6401250+datitisev@users.noreply.github.com>
2021-11-23 17:51:04 -0500

    Convert some common classes/utils to TS (#2929)
    * Convert common/Session
    
    * Update common/states/AlertManagerState
    
    * Convert common/utils/extractText
    
    Co-authored-by: David Wheatley <hi@davwheat.dev>

--- js/src/common/utils/extractText.ts
+++ js/src/common/utils/extractText.ts
@@ -1,15 +1,14 @@
+import type Mithril from 'mithril';
+
 /**
  * Extract the text nodes from a virtual element.
- *
- * @param {VirtualElement} vdom
- * @return {String}
  */
-export default function extractText(vdom) {
+export default function extractText(vdom: Mithril.Children): string {
   if (vdom instanceof Array) {
     return vdom.map((element) => extractText(element)).join('');
   } else if (typeof vdom === 'object' && vdom !== null) {
-    return vdom.children ? extractText(vdom.children) : vdom.text;
+    return vdom.children ? extractText(vdom.children) : String(vdom.text);
   } else {
-    return vdom;
+    return String(vdom);
   }
 }
