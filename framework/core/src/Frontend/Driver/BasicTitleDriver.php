60c3e2fe77a707b09adfac15be2c9c4ece69aca6
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2021-12-03 13:31:50 -0500

    Centralize pagination/canonical meta URL generation in Document (#3077)
    * Centralize pagination/canonical meta URL generation in Document
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * Use translations for title template
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * Dont add translator to title driver interface
    
    It's an implementation detail, and can be made available on specific implementations as needed.
    
    Co-authored-by: Alexander Skvortsov <askvortsov1@users.noreply.github.com>

--- framework/core/src/Frontend/Driver/BasicTitleDriver.php
+++ framework/core/src/Frontend/Driver/BasicTitleDriver.php
@@ -12,13 +12,32 @@ namespace Flarum\Frontend\Driver;
 use Flarum\Frontend\Document;
 use Illuminate\Support\Arr;
 use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class BasicTitleDriver implements TitleDriverInterface
 {
+    /**
+     * @var TranslatorInterface
+     */
+    protected $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function makeTitle(Document $document, ServerRequestInterface $request, array $forumApiDocument): string
     {
         $onHomePage = rtrim($request->getUri()->getPath(), '/') === '';
 
-        return ($document->title && ! $onHomePage ? $document->title.' - ' : '').Arr::get($forumApiDocument, 'data.attributes.title');
+        $params = [
+            'pageTitle' => $document->title ?? '',
+            'forumName' => Arr::get($forumApiDocument, 'data.attributes.title'),
+            'pageNumber' => $document->page ?? 1,
+        ];
+
+        return $onHomePage || ! $document->title
+            ? $this->translator->trans('core.views.meta_titles.without_page_title', $params)
+            : $this->translator->trans('core.views.meta_titles.with_page_title', $params);
     }
 }
