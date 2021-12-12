0103b9ad098637eddcde171df46118f36e049f59
Rafał Całka <calka.rafal@gmail.com>
2021-12-12 08:41:25 +0100

    Fixed broken discussion list pagination when clicking on the update button (#33)

--- packages/ext-pusher/js/src/forum/index.js
+++ packages/ext-pusher/js/src/forum/index.js
@@ -78,7 +78,7 @@ app.initializers.add('flarum-pusher', () => {
           Button.component({
             className: 'Button Button--block DiscussionList-update',
             onclick: () => {
-              this.attrs.state.refresh(false).then(() => {
+              this.attrs.state.refresh().then(() => {
                 this.loadingUpdated = false;
                 app.pushedUpdates = [];
                 app.setTitleCount(0);
