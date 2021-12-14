56b476984793c20938805779e8a52bd5ef29d87a
Ian Morland <16573496+imorland@users.noreply.github.com>
2021-12-14 20:38:50 +0000

    feat: Add events for Notification read / read all (#3203)

--- packages/core/src/Notification/Command/ReadNotificationHandler.php
+++ packages/core/src/Notification/Command/ReadNotificationHandler.php
@@ -10,10 +10,25 @@
 namespace Flarum\Notification\Command;
 
 use Carbon\Carbon;
+use Flarum\Notification\Event\Read;
 use Flarum\Notification\Notification;
+use Illuminate\Contracts\Events\Dispatcher;
 
 class ReadNotificationHandler
 {
+    /**
+     * @var Dispatcher
+     */
+    protected $events;
+
+    /**
+     * @param Dispatcher $events
+     */
+    public function __construct(Dispatcher $events)
+    {
+        $this->events = $events;
+    }
+
     /**
      * @param ReadNotification $command
      * @return \Flarum\Notification\Notification
@@ -35,6 +50,8 @@ class ReadNotificationHandler
             ->update(['read_at' => Carbon::now()]);
 
         $notification->read_at = Carbon::now();
+
+        $this->events->dispatch(new Read($actor, $notification, Carbon::now()));
 
         return $notification;
     }
---
+++ packages/core/src/Notification/Event/Read.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Notification\Event;
+
+use DateTime;
+use Flarum\Notification\Notification;
+use Flarum\User\User;
+
+class Read
+{
+    /**
+     * @var User
+     */
+    public $actor;
+
+    /**
+     * @var Notification
+     */
+    public $notification;
+
+    /**
+     * @var DateTime
+     */
+    public $timestamp;
+
+    public function __construct(User $user, Notification $notification, DateTime $timestamp)
+    {
+        $this->user = $user;
+        $this->notification = $notification;
+        $this->timestamp = $timestamp;
+    }
+}
