6136ce8d8cb96f4397b5bd578692793e41524fa3
Ian Morland <16573496+imorland@users.noreply.github.com>
2021-12-14 20:38:50 +0000

    feat: Add events for Notification read / read all (#3203)

---
+++ src/Notification/Event/ReadAll.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Notification\Event;
+
+use DateTime;
+use Flarum\User\User;
+
+class ReadAll
+{
+    /**
+     * @var User
+     */
+    public $actor;
+
+    /**
+     * @var DateTime
+     */
+    public $timestamp;
+
+    public function __construct(User $user, DateTime $timestamp)
+    {
+        $this->user = $user;
+        $this->timestamp = $timestamp;
+    }
+}
