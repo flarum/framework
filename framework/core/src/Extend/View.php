fbaf936e7ebbf4fb5051ac74e1760c6d6664ca8d
David Wheatley <hi@davwheat.dev>
2021-12-20 09:56:48 +0100

    feat: allow replacing of blade template namespaces via extender (#3167)
    * feat: allow replacing of blade template namespaces
    
    * wip: add `prependNamespace` support
    
    * test: add replace namespace test
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * fix: add missing property
    
    * test: add prepend test
    
    * fix: add view namespaces before resolving
    
    Allows `replaceNamespace()` extender to actually remove old routes.
    
    * test: make replace test ensure that replaced view does not exist
    
    * docs: update docblock
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * fix: missing `\` before class
    
    * fix: change test view namespace
    
    * chore: simplify test
    
    * Remove replace namespace code
    
    We only really need prepend.
    
    * chore: rename extender
    
    * ci: add override test
    
    * Apply fixes from StyleCI
    
    [ci skip] [skip ci]
    
    * fix(tests): add `trim` call
    
    * revert: 3d46ead14b7a4b349992c8dbd3212cbf51eb1679
    
    Co-authored-by: luceos <luceos@users.noreply.github.com>

--- framework/core/src/Extend/View.php
+++ framework/core/src/Extend/View.php
@@ -14,20 +14,24 @@ use Flarum\Foundation\Paths;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\View\Factory;
 
+/**
+ * Views are PHP files that use the Laravel Blade syntax for creation of server-side generated HTML.
+ *
+ * Flarum's core uses them for error pages, the installer, HTML emails, and the skeletons for the forum and admin sites.
+ */
 class View implements ExtenderInterface, LifecycleInterface
 {
     private $namespaces = [];
+    private $prependNamespaces = [];
 
     /**
      * Register a new namespace of Laravel views.
      *
-     * Views are php files that use the Laravel Blade syntax for creation of server-side generated html.
-     * Flarum core uses them for error pages, the installer, HTML emails, and the skeletons for the forum and admin sites.
      * To create and use views in your extension, you will need to put them in a folder, and register that folder as a namespace.
      *
      * Views can then be used in your extension by injecting an instance of `Illuminate\Contracts\View\Factory`,
      * and calling its `make` method. The `make` method takes the view parameter in the format NAMESPACE::VIEW_NAME.
-     * You can also pass variables into a view: for more information, see https://laravel.com/api/8.x/Illuminate/View/Factory.html#method_make
+     * You can also pass variables into a view. For more information, see: https://laravel.com/api/8.x/Illuminate/View/Factory.html#method_make
      *
      * @param  string  $namespace: The name of the namespace.
      * @param  string|string[]  $hints: This is a path (or an array of paths) to the folder(s)
@@ -41,11 +45,32 @@ class View implements ExtenderInterface, LifecycleInterface
         return $this;
     }
 
+    /**
+     * Extend an existing namespace of Laravel views.
+     *
+     * To extend an existing namespace, you will need to put views in a folder in your extension,
+     * and register that folder under the existing namespace with this extender.
+     *
+     * @param  string  $namespace: The name of the namespace.
+     * @param  string|string[]  $hints: This is a path (or an array of paths) to the folder(s)
+     *                               where view files are stored, relative to the extend.php file.
+     * @return self
+     */
+    public function extendNamespace(string $namespace, $hints): self
+    {
+        $this->prependNamespaces[$namespace] = $hints;
+
+        return $this;
+    }
+
     public function extend(Container $container, Extension $extension = null)
     {
         $container->resolving(Factory::class, function (Factory $view) {
             foreach ($this->namespaces as $namespace => $hints) {
                 $view->addNamespace($namespace, $hints);
+            }
+            foreach ($this->prependNamespaces as $namespace => $hints) {
+                $view->prependNamespace($namespace, $hints);
             }
         });
     }
