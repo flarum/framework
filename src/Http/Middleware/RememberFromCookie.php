eca870636ced1f5bf5dd91c6b197ba33c999250a
David Wheatley <hi@davwheat.dev>
2021-12-28 01:05:39 +0100

    perf: include request info in first accesstoken touch

--- src/Http/Middleware/RememberFromCookie.php
+++ src/Http/Middleware/RememberFromCookie.php
@@ -41,7 +41,7 @@ class RememberFromCookie implements Middleware
             $token = AccessToken::findValid($id);
 
             if ($token && $token instanceof RememberAccessToken) {
-                $token->touch();
+                $token->touch($request);
 
                 /** @var \Illuminate\Contracts\Session\Session $session */
                 $session = $request->getAttribute('session');
