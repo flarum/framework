975a158d84a3f5bb1f0e30845ff78b28d7ae8f81
David Wheatley <hi@davwheat.dev>
2021-12-29 13:17:02 +0100

    feat: add prettier formatting

--- packages/ext-markdown/js/src/forum/components/MarkdownToolbar.js
+++ packages/ext-markdown/js/src/forum/components/MarkdownToolbar.js
@@ -2,8 +2,6 @@ import Component from 'flarum/common/Component';
 
 export default class MarkdownToolbar extends Component {
   view(vnode) {
-    return <div class="MarkdownToolbar">
-      {vnode.children}
-    </div>;
+    return <div class="MarkdownToolbar">{vnode.children}</div>;
   }
 }
