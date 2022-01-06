dfbca172e0568a0c604a5896b786e6d82322a493
Ian Morland <ian@morland.me>
2022-01-06 21:07:51 +0000

    Add extender

---
+++ src/Extend/Sortmap.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Extend;
+
+use Flarum\Extension\Extension;
+use Illuminate\Contracts\Container\Container;
+
+class Sortmap implements ExtenderInterface
+{
+    private $sortFields = [];
+
+    public function addSort(string $key, string $value):self
+    {
+        $this->sortFields[$key] = $value;
+
+        return $this;
+    }
+
+    public function extend(Container $container, Extension $extension = null)
+    {
+        $container->extend('flarum.forum.sortmap', function (array $sortFields) {
+            return array_merge($sortFields, $this->sortFields);
+        });
+    }
+}
