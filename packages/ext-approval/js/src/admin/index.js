5b04b38e6a989d472637f07183d836ae6d90acb7
Ian Morland <16573496+imorland@users.noreply.github.com>
2022-01-11 10:43:15 +0000

    chore: repo maintenance (#28)
    * Bump deps
    
    * Update imports
    
    * format

--- packages/ext-approval/js/src/admin/index.js
+++ packages/ext-approval/js/src/admin/index.js
@@ -1,5 +1,5 @@
-import { extend } from 'flarum/extend';
-import app from 'flarum/app';
+import { extend } from 'flarum/common/extend';
+import app from 'flarum/admin/app';
 
 app.initializers.add('flarum-approval', () => {
   extend(app, 'getRequiredPermissions', function (required, permission) {
@@ -13,19 +13,31 @@ app.initializers.add('flarum-approval', () => {
 
   app.extensionData
     .for('flarum-approval')
-    .registerPermission({
-      icon: 'fas fa-check',
-      label: app.translator.trans('flarum-approval.admin.permissions.start_discussions_without_approval_label'),
-      permission: 'discussion.startWithoutApproval'
-    }, 'start', 95)
-    .registerPermission({
-      icon: 'fas fa-check',
-      label: app.translator.trans('flarum-approval.admin.permissions.reply_without_approval_label'),
-      permission: 'discussion.replyWithoutApproval'
-    }, 'reply', 95)
-    .registerPermission({
-      icon: 'fas fa-check',
-      label: app.translator.trans('flarum-approval.admin.permissions.approve_posts_label'),
-      permission: 'discussion.approvePosts'
-    }, 'moderate', 65);
+    .registerPermission(
+      {
+        icon: 'fas fa-check',
+        label: app.translator.trans('flarum-approval.admin.permissions.start_discussions_without_approval_label'),
+        permission: 'discussion.startWithoutApproval',
+      },
+      'start',
+      95
+    )
+    .registerPermission(
+      {
+        icon: 'fas fa-check',
+        label: app.translator.trans('flarum-approval.admin.permissions.reply_without_approval_label'),
+        permission: 'discussion.replyWithoutApproval',
+      },
+      'reply',
+      95
+    )
+    .registerPermission(
+      {
+        icon: 'fas fa-check',
+        label: app.translator.trans('flarum-approval.admin.permissions.approve_posts_label'),
+        permission: 'discussion.approvePosts',
+      },
+      'moderate',
+      65
+    );
 });
