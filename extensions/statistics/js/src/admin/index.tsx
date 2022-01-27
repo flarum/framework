f3d0bdce47cc27f96e69b9c44197dd0acccd2220
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-01-27 15:57:07 -0500

    chore: convert to TypeScript

--- extensions/statistics/js/src/admin/index.tsx
+++ extensions/statistics/js/src/admin/index.tsx
@@ -5,9 +5,10 @@ import DashboardPage from 'flarum/admin/components/DashboardPage';
 
 import StatisticsWidget from './components/StatisticsWidget';
 import ItemList from 'flarum/common/utils/ItemList';
+import type Mithril from 'mithril';
 
 app.initializers.add('flarum-statistics', () => {
-  extend(DashboardPage.prototype, 'availableWidgets', function (widgets: ItemList) {
+  extend(DashboardPage.prototype, 'availableWidgets', function (widgets: ItemList<Mithril.Children>) {
     widgets.add('statistics', <StatisticsWidget />, 20);
   });
 });
