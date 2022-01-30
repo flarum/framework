9fa48d088dcbc90cad234a7076a1e221f6ae36db
Ngô Quốc Đạt <56961917+datlechin@users.noreply.github.com>
2022-01-30 05:13:54 -0800

    PHP 8.1: Type declarations for `Foundation/Config.php`

--- src/Foundation/Config.php
+++ src/Foundation/Config.php
@@ -53,22 +53,22 @@ class Config implements ArrayAccess
         }
     }
 
-    public function offsetGet($offset)
+    public function offsetGet($offset): ?string
     {
         return Arr::get($this->data, $offset);
     }
 
-    public function offsetExists($offset)
+    public function offsetExists($offset): bool
     {
         return Arr::has($this->data, $offset);
     }
 
-    public function offsetSet($offset, $value)
+    public function offsetSet($offset, $value): void
     {
         throw new RuntimeException('The Config is immutable');
     }
 
-    public function offsetUnset($offset)
+    public function offsetUnset($offset): void
     {
         throw new RuntimeException('The Config is immutable');
     }
