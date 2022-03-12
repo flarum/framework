762a46977b32165988fc369f013545da78c88e3b
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-03-12 02:27:51 -0500

    fix(akismet): fix some typing issues

--- extensions/akismet/js/src/forum/index.ts
+++ extensions/akismet/js/src/forum/index.ts
@@ -5,14 +5,18 @@ import PostControls from 'flarum/forum/utils/PostControls';
 import CommentPost from 'flarum/forum/components/CommentPost';
 import ItemList from 'flarum/common/utils/ItemList';
 import Post from 'flarum/common/models/Post';
+import Mithril from 'mithril';
 
 app.initializers.add('flarum-akismet', () => {
-  extend(PostControls, 'destructiveControls', function (items: ItemList, post: Post) {
+  extend(PostControls, 'destructiveControls', function (items: ItemList<Mithril.Children>, post: Post) {
     if (items.has('approve')) {
       const flags = post.flags();
 
       if (flags && flags.some((flag) => flag.type() === 'akismet')) {
-        items.get('approve').children = app.translator.trans('flarum-akismet.forum.post.not_spam_button');
+        const approveItem = items.get('approve');
+        if (approveItem && typeof approveItem === 'object' && 'children' in approveItem) {
+          approveItem.children = app.translator.trans('flarum-akismet.forum.post.not_spam_button');
+        }
       }
     }
   });
