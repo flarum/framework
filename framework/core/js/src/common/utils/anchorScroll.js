7c6c16c20dd7b1d75e25be85b6840e105f5f7c92
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-03-13 03:27:49 -0400

    fix: anchorScroll should accept string selectors

--- framework/core/js/src/common/utils/anchorScroll.js
+++ framework/core/js/src/common/utils/anchorScroll.js
@@ -8,7 +8,7 @@
  * position can be anchor to an element that is in or below the viewport, so
  * the content in the viewport will stay the same.
  *
- * @param {HTMLElement | SVGElement | Element} element The element to anchor the scroll position to.
+ * @param {string | HTMLElement | SVGElement | Element} element The element to anchor the scroll position to.
  * @param {() => void} callback The callback to run that will change page content.
  */
 export default function anchorScroll(element, callback) {
