1b62e30977357fd3601b2ca81f4a2e5eef0bb5da
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-03-14 14:23:57 -0400

    feat: start tags TypeScript conversion

--- extensions/tags/js/src/forum/index.ts
+++ extensions/tags/js/src/forum/index.ts
@@ -15,11 +15,11 @@ import addTagLabels from './addTagLabels';
 import addTagControl from './addTagControl';
 import addTagComposer from './addTagComposer';
 
-app.initializers.add('flarum-tags', function(app) {
+app.initializers.add('flarum-tags', function() {
   app.routes.tags = {path: '/tags', component: TagsPage };
   app.routes.tag = {path: '/t/:tags', component: IndexPage };
 
-  app.route.tag = tag => app.route('tag', {tags: tag.slug()});
+  app.route.tag = (tag: Tag) => app.route('tag', {tags: tag.slug()});
 
   app.postComponents.discussionTagged = DiscussionTaggedPost;
 
@@ -27,8 +27,8 @@ app.initializers.add('flarum-tags', function(app) {
 
   app.tagList = new TagListState();
 
-  Discussion.prototype.tags = Model.hasMany('tags');
-  Discussion.prototype.canTag = Model.attribute('canTag');
+  Discussion.prototype.tags = Model.hasMany<Tag>('tags');
+  Discussion.prototype.canTag = Model.attribute<boolean>('canTag');
 
   addTagList();
   addTagFilter();
