392b90710c85d50ddb748f2110d836adb29869b1
Alexander Skvortsov <38059171+askvortsov1@users.noreply.github.com>
2022-03-24 21:26:30 -0400

    Update framework/core/js/src/common/components/EditUserModal.tsx
    Co-authored-by: David Wheatley <hi@davwheat.dev>

--- framework/core/js/src/common/components/EditUserModal.tsx
+++ framework/core/js/src/common/components/EditUserModal.tsx
@@ -6,7 +6,7 @@ import Group from '../models/Group';
 import extractText from '../utils/extractText';
 import ItemList from '../utils/ItemList';
 import Stream from '../utils/Stream';
-import Mithril from 'mithril';
+import type Mithril from 'mithril';
 import type User from '../models/User';
 import type { SaveAttributes, SaveRelationships } from '../Model';
 
