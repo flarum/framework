75f34f48853a9041ce45164a63e384cae0482aad
SychO9 <sychocouldy@gmail.com>
2022-04-09 21:07:49 +0100

    test: searching other types of languages

--- framework/core/tests/integration/api/discussions/ListWithFulltextSearchTest.php
+++ framework/core/tests/integration/api/discussions/ListWithFulltextSearchTest.php
@@ -12,6 +12,7 @@ namespace Flarum\Tests\integration\api\discussions;
 use Carbon\Carbon;
 use Flarum\Testing\integration\RetrievesAuthorizedUsers;
 use Flarum\Testing\integration\TestCase;
+use Illuminate\Support\Arr;
 
 class ListWithFulltextSearchTest extends TestCase
 {
@@ -42,6 +43,8 @@ class ListWithFulltextSearchTest extends TestCase
             ['id' => 3, 'discussion_id' => 2, 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>another lightsail for discussion 2!</p></t>'],
             ['id' => 4, 'discussion_id' => 3, 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>just one lightsail for discussion 3.</p></t>'],
             ['id' => 5, 'discussion_id' => 4, 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>not in title or text</p></t>'],
+            ['id' => 6, 'discussion_id' => 4, 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>తెలుగు</p></t>'],
+            ['id' => 7, 'discussion_id' => 2, 'created_at' => Carbon::now()->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>支持中文吗</p></t>'],
         ]);
 
         // We need to call these again, since we rolled back the transaction started by `::app()`.
@@ -79,7 +82,7 @@ class ListWithFulltextSearchTest extends TestCase
             return $row['id'];
         }, $data['data']);
 
-        $this->assertEquals(['2', '1', '3'], $ids, 'IDs do not match');
+        $this->assertEqualsCanonicalizing(['2', '1', '3'], $ids, 'IDs do not match');
     }
 
     /**
@@ -100,7 +103,51 @@ class ListWithFulltextSearchTest extends TestCase
             return $row['id'];
         }, $data['data']);
 
-        $this->assertEquals(['2', '1', '3'], $ids, 'IDs do not match');
+        $this->assertEqualsCanonicalizing(['2', '1', '3'], $ids, 'IDs do not match');
+    }
+
+    /**
+     * @test
+     */
+    public function can_search_telugu_like_languages()
+    {
+        $response = $this->send(
+            $this->request('GET', '/api/discussions')
+                ->withQueryParams([
+                    'filter' => ['q' => 'తెలుగు'],
+                    'include' => 'mostRelevantPost',
+                ])
+        );
+
+        $data = json_decode($response->getBody()->getContents(), true);
+        $ids = array_map(function ($row) {
+            return $row['id'];
+        }, $data['data']);
+
+        $this->assertEqualsCanonicalizing(['4'], $ids, 'IDs do not match');
+        $this->assertEqualsCanonicalizing(['6'], Arr::pluck($data['included'], 'id'));
+    }
+
+    /**
+     * @test
+     */
+    public function can_search_cjk_languages()
+    {
+        $response = $this->send(
+            $this->request('GET', '/api/discussions')
+                ->withQueryParams([
+                    'filter' => ['q' => '支持中文吗'],
+                    'include' => 'mostRelevantPost',
+                ])
+        );
+
+        $data = json_decode($response->getBody()->getContents(), true);
+        $ids = array_map(function ($row) {
+            return $row['id'];
+        }, $data['data']);
+
+        $this->assertEqualsCanonicalizing(['2'], $ids, 'IDs do not match');
+        $this->assertEqualsCanonicalizing(['7'], Arr::pluck($data['included'], 'id'));
     }
 
     /**
