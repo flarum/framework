c32ef4a9b7e432c16e1aece57da591eb83fd8244
Alexander Skvortsov <sasha.skvortsov109@gmail.com>
2022-04-21 17:22:40 -0400

    fix(core): don't pass null to functions where that's deprecated

--- framework/core/src/User/User.php
+++ framework/core/src/User/User.php
@@ -488,7 +488,7 @@ class User extends AbstractModel
             return $value['default'];
         }, static::$preferences);
 
-        $user = Arr::only((array) json_decode($value, true), array_keys(static::$preferences));
+        $user = $value !== null ? Arr::only((array) json_decode($value, true), array_keys(static::$preferences)) : [];
 
         return array_merge($defaults, $user);
     }
