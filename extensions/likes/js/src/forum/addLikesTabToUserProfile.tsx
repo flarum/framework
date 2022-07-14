2ac6d5bd4829ade7250d41ad5be7397ac653e27f
Ian Morland <16573496+imorland@users.noreply.github.com>
2022-07-14 13:40:50 +0100

    Update extensions/likes/js/src/forum/addLikesTabToUserProfile.tsx
    Co-authored-by: David Wheatley <hi@davwheat.dev>

--- extensions/likes/js/src/forum/addLikesTabToUserProfile.tsx
+++ extensions/likes/js/src/forum/addLikesTabToUserProfile.tsx
@@ -12,7 +12,7 @@ export default function addLikesTabToUserProfile() {
     const user = this.user;
     items.add(
       'likes',
-      <LinkButton href={app.route('user.likes', { username: user.slug() })} name="likes" icon="far fa-thumbs-up">
+      <LinkButton href={app.route('user.likes', { username: user.slug() })} icon="far fa-thumbs-up">
         {app.translator.trans('flarum-likes.forum.user.likes_link')}
       </LinkButton>,
       88
