9b75c2f3fefaec71cf4bb2a8ee47b1769676e9e7
David Wheatley <hi@davwheat.dev>
2022-07-14 10:34:59 +0100

    perf: add posts.type index
    `type` is heavily queried against in flarum/statistics, and is a common filtering point, such as for the `comments` relation on Discussion.

---
+++ framework/core/migrations/2022_07_14_000000_add_type_index_to_posts.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('posts', function (Blueprint $table) {
+            $table->index('type');
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->table('posts', function (Blueprint $table) {
+            $table->dropIndex(['type']);
+        });
+    }
+];
