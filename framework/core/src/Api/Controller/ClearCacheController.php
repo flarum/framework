e952b57ab85fed972f207d05c4641f4b9c49f554
David Wheatley <hi@davwheat.dev>
2022-07-25 23:56:20 +0100

    feat: publish assets on cache clear from admin dashboard

--- framework/core/src/Api/Controller/ClearCacheController.php
+++ framework/core/src/Api/Controller/ClearCacheController.php
@@ -9,6 +9,7 @@
 
 namespace Flarum\Api\Controller;
 
+use Flarum\Foundation\Console\AssetsPublishCommand;
 use Flarum\Foundation\Console\CacheClearCommand;
 use Flarum\Http\RequestUtil;
 use Laminas\Diactoros\Response\EmptyResponse;
@@ -24,11 +25,17 @@ class ClearCacheController extends AbstractDeleteController
     protected $command;
 
     /**
+     * @var AssetsPublishCommand
+     */
+    protected $assetsPublishCommand;
+
+    /**
      * @param CacheClearCommand $command
      */
-    public function __construct(CacheClearCommand $command)
+    public function __construct(CacheClearCommand $command, AssetsPublishCommand $assetsPublishCommand)
     {
         $this->command = $command;
+        $this->assetsPublishCommand = $assetsPublishCommand;
     }
 
     /**
@@ -43,10 +50,19 @@ class ClearCacheController extends AbstractDeleteController
             new NullOutput()
         );
 
-        if ($exitCode === 0) {
-            return new EmptyResponse(204);
+        if ($exitCode !== 0) {
+            throw new \Exception('Clearing cache failed. Try running `php flarum cache:clear` from the command line to see more info.');
         }
 
-        throw new \Exception('Clear cache command failed.');
+        $exitCode = $this->assetsPublishCommand->run(
+            new ArrayInput([]),
+            new NullOutput()
+        );
+
+        if ($exitCode !== 0) {
+            throw new \Exception('Asset publishing failed. Try running `php flarum assets:publish` from the command line to see more info.');
+        }
+
+        return new EmptyResponse(204);
     }
 }
