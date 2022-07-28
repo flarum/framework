d13bf4b395fc27988b1f122af3c2973f388b2a95
Sami Mazouz <ilyasmazouz@gmail.com>
2022-07-28 14:04:45 +0100

    test: email tokens are deleted after password reset
    Signed-off-by: Sami Mazouz <ilyasmazouz@gmail.com>

--- framework/core/tests/integration/api/users/PasswordEmailTokensTest.php
+++ framework/core/tests/integration/api/users/PasswordEmailTokensTest.php
@@ -130,4 +130,43 @@ class PasswordEmailTokensTest extends TestCase
         $this->assertEquals(302, $response->getStatusCode());
         $this->assertEquals(0, EmailToken::query()->where('user_id', 2)->count());
     }
+
+    /** @test */
+    public function email_tokens_are_deleted_after_password_reset()
+    {
+        $this->app();
+
+        // Request password change to generate a token.
+        $response = $this->send(
+            $this->request('POST', '/api/forgot', [
+                'authenticatedAs' => 2,
+                'json' => [
+                    'email' => 'normal@machine.local'
+                ]
+            ])
+        );
+
+        // Additional Tokens
+        EmailToken::generate('new-normal@machine.local', 2)->save();
+        EmailToken::generate('new-normal@machine.local', 2)->save();
+
+        $this->assertEquals(204, $response->getStatusCode());
+        $this->assertEquals(2, EmailToken::query()->where('user_id', 2)->count());
+
+        // Use a token to reset password
+        $response = $this->send(
+            $request = $this->requestWithCsrfToken(
+                $this->request('POST', '/reset', [
+                    'authenticatedAs' => 2,
+                ])->withParsedBody([
+                    'passwordToken' => PasswordToken::query()->latest()->first()->token,
+                    'password' => 'new-password',
+                    'password_confirmation' => 'new-password',
+                ])
+            )
+        );
+
+        $this->assertEquals(302, $response->getStatusCode());
+        $this->assertEquals(0, EmailToken::query()->where('user_id', 2)->count());
+    }
 }
