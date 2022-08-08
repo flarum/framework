f69210b6d184336f8a57c288f2dca3d7db2733eb
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2022-08-08 14:29:14 +0200

    feat(modals): support stacking modals, remove bootstrap modals dependency (#3456)
    * Feature: Stackable modals
    
    * Processed feedback
    
    * fix: use position in modal stack rather than key for modal number
    
    * fix: use correct base z-index
    
    * chore: simplify `className` to `class`
    
    * chore: add `key` attribute to ModalManager element
    
    * fix: backdrop flashing as modals are stacked/unstacked
    
    * chore: simplify modal close process
    
    * docs: add TS overload to indicate deprecated modal opening syntax
    
    Require explicit values for `attrs` and `stackModal` from Flarum 2.0, beginning deprecation from now.
    
    * feat: use stackable modal for forgot password modal above sign in
    
    * chore: explicitly check if modal is open before trying to focus trap
    
    * fix(a11y): add missing  `aria-hidden` on main content when modal open
    
    * fix(a11y): add missing `aria-modal` on modal
    
    * chore: remove test code
    
    * chore: remove dead CSS
    
    * chore: remove overload
    
    * fix: lock page scrolling when modal is open
    
    * fix: strange scrolling behaviour
    
    * chore: convert to JSX
    
    * fix: event listener memory leak
    
    * chore: remove unneeded optional chaining
    
    * fix: incorrect return types
    
    * chore: rewrite backdrop system - use one backdrop for all modals
    
    * docs: typos in comment block
    
    * fix: show backdrop directly below top-most modal
    
    Co-authored-by: Sami Mazouz <sychocouldy@gmail.com>
    
    * chore: format
    
    * fix: use an invisible backdrop for each modal to exit
    
    Signed-off-by: Sami Mazouz <ilyasmazouz@gmail.com>
    
    * chore: remove debugging code
    
    Signed-off-by: Sami Mazouz <ilyasmazouz@gmail.com>
    
    * chore: remove forgotten debug code
    
    Co-authored-by: David Wheatley <david@davwheat.dev>
    Co-authored-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/js/src/common/components/ModalManager.tsx
+++ framework/core/js/src/common/components/ModalManager.tsx
@@ -2,6 +2,8 @@ import Component from '../Component';
 
 import { createFocusTrap, FocusTrap } from '../utils/focusTrap';
 
+import { disableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+
 import type ModalManagerState from '../states/ModalManagerState';
 import type Mithril from 'mithril';
 
@@ -10,46 +12,81 @@ interface IModalManagerAttrs {
 }
 
 /**
- * The `ModalManager` component manages a modal dialog. Only one modal dialog
- * can be shown at once; loading a new component into the ModalManager will
- * overwrite the previous one.
+ * The `ModalManager` component manages one or more modal dialogs. Stacking modals
+ * is supported. Multiple dialogs can be shown at once; loading a new component
+ * into the ModalManager will overwrite the previous one.
  */
 export default class ModalManager extends Component<IModalManagerAttrs> {
+  // Current focus trap
   protected focusTrap: FocusTrap | undefined;
 
-  /**
-   * Whether a modal is currently shown by this modal manager.
-   */
-  protected modalShown: boolean = false;
+  // Keep track of the last set focus trap
+  protected lastSetFocusTrap: number | undefined;
+
+  // Keep track if there's an modal closing
+  protected modalClosing: boolean = false;
+
+  protected keyUpListener: null | ((e: KeyboardEvent) => void) = null;
 
   view(vnode: Mithril.VnodeDOM<IModalManagerAttrs, this>): Mithril.Children {
-    const modal = this.attrs.state.modal;
-    const Tag = modal?.componentClass;
-
     return (
-      <div className="ModalManager modal fade">
-        {!!Tag && (
-          <Tag
-            key={modal?.key}
-            {...modal.attrs}
-            animateShow={this.animateShow.bind(this)}
-            animateHide={this.animateHide.bind(this)}
-            state={this.attrs.state}
+      <>
+        {this.attrs.state.modalList.map((modal, i) => {
+          const Tag = modal?.componentClass;
+
+          return (
+            <div
+              key={modal.key}
+              class="ModalManager modal"
+              data-modal-key={modal.key}
+              data-modal-number={i}
+              role="dialog"
+              aria-modal="true"
+              style={{ '--modal-number': i }}
+              aria-hidden={this.attrs.state.modal !== modal && 'true'}
+            >
+              {!!Tag && [
+                <Tag
+                  key={modal.key}
+                  {...modal.attrs}
+                  animateShow={this.animateShow.bind(this)}
+                  animateHide={this.animateHide.bind(this)}
+                  state={this.attrs.state}
+                />,
+                /* This backdrop is invisible and used for outside clicks to close the modal. */
+                <div
+                  key={modal.key}
+                  className="ModalManager-invisibleBackdrop"
+                  onclick={this.handlePossibleBackdropClick.bind(this)} />
+              ]}
+            </div>
+          );
+        })}
+
+        {this.attrs.state.backdropShown && (
+          <div
+            class="Modal-backdrop backdrop"
+            ontransitionend={this.onBackdropTransitionEnd.bind(this)}
+            data-showing={!!this.attrs.state.modalList.length}
+            style={{ '--modal-count': this.attrs.state.modalList.length }}
           />
         )}
-      </div>
+      </>
     );
   }
 
   oncreate(vnode: Mithril.VnodeDOM<IModalManagerAttrs, this>): void {
     super.oncreate(vnode);
 
-    // Ensure the modal state is notified about a closed modal, even when the
-    // DOM-based Bootstrap JavaScript code triggered the closing of the modal,
-    // e.g. via ESC key or a click on the modal backdrop.
-    this.$().on('hidden.bs.modal', this.attrs.state.close.bind(this.attrs.state));
+    this.keyUpListener = this.handleEscPress.bind(this);
+    document.body.addEventListener('keyup', this.keyUpListener);
+  }
 
-    this.focusTrap = createFocusTrap(this.element as HTMLElement, { allowOutsideClick: true });
+  onbeforeremove(vnode: Mithril.VnodeDOM<IModalManagerAttrs, this>): void {
+    super.onbeforeremove(vnode);
+
+    this.keyUpListener && document.body.removeEventListener('keyup', this.keyUpListener);
+    this.keyUpListener = null;
   }
 
   onupdate(vnode: Mithril.VnodeDOM<IModalManagerAttrs, this>): void {
@@ -57,43 +94,123 @@ export default class ModalManager extends Component<IModalManagerAttrs> {
 
     requestAnimationFrame(() => {
       try {
-        if (this.modalShown) this.focusTrap!.activate?.();
-        else this.focusTrap!.deactivate?.();
+        // Main content should gain or lose `aria-hidden` when modals are shown/removed
+        // See: http://web-accessibility.carnegiemuseums.org/code/dialogs/
+
+        if (!this.attrs.state.isModalOpen()) {
+          document.getElementById('app')?.setAttribute('aria-hidden', 'false');
+          this.focusTrap!.deactivate?.();
+          clearAllBodyScrollLocks();
+
+          return;
+        }
+
+        document.getElementById('app')?.setAttribute('aria-hidden', 'true');
+
+        // Get current dialog key
+        const dialogKey = this.attrs.state.modal!.key;
+
+        // Deactivate focus trap if there's a new dialog/closed
+        if (this.focusTrap && this.lastSetFocusTrap !== dialogKey) {
+          this.focusTrap!.deactivate?.();
+
+          clearAllBodyScrollLocks();
+        }
+
+        // Activate focus trap if there's a new dialog which is not trapped yet
+        if (this.activeDialogElement && this.lastSetFocusTrap !== dialogKey) {
+          this.focusTrap = createFocusTrap(this.activeDialogElement as HTMLElement, { allowOutsideClick: true });
+          this.focusTrap!.activate?.();
+
+          disableBodyScroll(this.activeDialogManagerElement!, { reserveScrollBarGap: true });
+        }
+
+        // Update key of current opened modal
+        this.lastSetFocusTrap = dialogKey;
       } catch {
         // We can expect errors to occur here due to the nature of mithril rendering
       }
     });
   }
 
-  animateShow(readyCallback: () => void): void {
-    if (!this.attrs.state.modal) return;
-
-    const dismissible = !!this.attrs.state.modal.componentClass.isDismissible;
-
-    this.modalShown = true;
-
-    // If we are opening this modal while another modal is already open,
-    // the shown event will not run, because the modal is already open.
-    // So, we need to manually trigger the readyCallback.
-    if (this.$().hasClass('in')) {
-      readyCallback();
-      return;
-    }
-
-    this.$()
-      .one('shown.bs.modal', readyCallback)
-      // @ts-expect-error: No typings available for Bootstrap modals.
-      .modal({
-        backdrop: dismissible || 'static',
-        keyboard: dismissible,
-      })
-      .modal('show');
+  /**
+   * Get current active dialog
+   */
+  private get activeDialogElement(): HTMLElement {
+    return document.body.querySelector(`.ModalManager[data-modal-key="${this.attrs.state.modal?.key}"] .Modal`) as HTMLElement;
   }
 
-  animateHide(): void {
-    // @ts-expect-error: No typings available for Bootstrap modals.
-    this.$().modal('hide');
+  /**
+   * Get current active dialog
+   */
+  private get activeDialogManagerElement(): HTMLElement {
+    return document.body.querySelector(`.ModalManager[data-modal-key="${this.attrs.state.modal?.key}"]`) as HTMLElement;
+  }
 
-    this.modalShown = false;
+  animateShow(readyCallback: () => void = () => {}): void {
+    if (!this.attrs.state.modal) return;
+
+    this.activeDialogElement.addEventListener(
+      'transitionend',
+      () => {
+        readyCallback();
+      },
+      { once: true }
+    );
+
+    requestAnimationFrame(() => {
+      this.activeDialogElement.classList.add('in');
+    });
+  }
+
+  animateHide(closedCallback: () => void = () => {}): void {
+    if (this.modalClosing) return;
+    this.modalClosing = true;
+
+    const afterModalClosedCallback = () => {
+      this.modalClosing = false;
+
+      // Close the dialog
+      this.attrs.state.close();
+
+      closedCallback();
+    };
+
+    this.activeDialogElement.addEventListener('transitionend', afterModalClosedCallback, { once: true });
+
+    this.activeDialogElement.classList.remove('in');
+    this.activeDialogElement.classList.add('out');
+  }
+
+  protected handleEscPress(e: KeyboardEvent): void {
+    if (!this.attrs.state.modal) return;
+
+    const dismissibleState = this.attrs.state.modal.componentClass.dismissibleOptions;
+
+    // Close the dialog if the escape key was pressed
+    // Check if closing via escape key is enabled
+    if (e.key === 'Escape' && dismissibleState.viaEscKey) {
+      e.preventDefault();
+
+      this.animateHide();
+    }
+  }
+
+  protected handlePossibleBackdropClick(e: MouseEvent): void {
+    if (!this.attrs.state.modal || !this.attrs.state.modal.componentClass.dismissibleOptions.viaBackdropClick) return;
+
+    this.animateHide();
+  }
+
+  protected onBackdropTransitionEnd(e: TransitionEvent) {
+    if (e.propertyName === 'opacity') {
+      const backdrop = e.currentTarget as HTMLDivElement;
+
+      if (backdrop.getAttribute('data-showing') === null) {
+        // Backdrop is fading out
+        this.attrs.state.backdropShown = false;
+        m.redraw();
+      }
+    }
   }
 }
--- framework/core/js/src/common/states/ModalManagerState.ts
+++ framework/core/js/src/common/states/ModalManagerState.ts
@@ -1,5 +1,5 @@
 import type Component from '../Component';
-import Modal from '../components/Modal';
+import Modal, { IDismissibleOptions } from '../components/Modal';
 
 /**
  * Ideally, `show` would take a higher-kinded generic, ala:
@@ -8,7 +8,13 @@ import Modal from '../components/Modal';
  * https://github.com/Microsoft/TypeScript/issues/1213
  * Therefore, we have to use this ugly, messy workaround.
  */
-type UnsafeModalClass = ComponentClass<any, Modal> & { isDismissible: boolean; component: typeof Component.component };
+type UnsafeModalClass = ComponentClass<any, Modal> & { get dismissibleOptions(): IDismissibleOptions; component: typeof Component.component };
+
+type ModalItem = {
+  componentClass: UnsafeModalClass;
+  attrs?: Record<string, unknown>;
+  key: number;
+};
 
 /**
  * Class used to manage modal state.
@@ -19,11 +25,17 @@ export default class ModalManagerState {
   /**
    * @internal
    */
-  modal: null | {
-    componentClass: UnsafeModalClass;
-    attrs?: Record<string, unknown>;
-    key: number;
-  } = null;
+  modal: ModalItem | null = null;
+
+  /**
+   * @internal
+   */
+  modalList: ModalItem[] = [];
+
+  /**
+   * @internal
+   */
+  backdropShown: boolean = false;
 
   /**
    * Used to force re-initialization of modals if a modal
@@ -31,12 +43,13 @@ export default class ModalManagerState {
    */
   private key = 0;
 
-  private closeTimeout?: NodeJS.Timeout;
-
   /**
    * Shows a modal dialog.
    *
-   * If a modal is already open, the existing one will close and the new modal will replace it.
+   * If `stackModal` is `true`, the modal will be shown on top of the current modal.
+   *
+   * If a value for `stackModal` is not provided, opening a new modal will close
+   * any others currently being shown for backwards compatibility.
    *
    * @example <caption>Show a modal</caption>
    * app.modal.show(MyCoolModal, { attr: 'value' });
@@ -44,8 +57,11 @@ export default class ModalManagerState {
    * @example <caption>Show a modal from a lifecycle method (`oncreate`, `view`, etc.)</caption>
    * // This "hack" is needed due to quirks with nested redraws in Mithril.
    * setTimeout(() => app.modal.show(MyCoolModal, { attr: 'value' }), 0);
+   *
+   * @example <caption>Stacking modals</caption>
+   * app.modal.show(MyCoolStackedModal, { attr: 'value' }, true);
    */
-  show(componentClass: UnsafeModalClass, attrs: Record<string, unknown> = {}): void {
+  show(componentClass: UnsafeModalClass, attrs: Record<string, unknown> = {}, stackModal: boolean = false): void {
     if (!(componentClass.prototype instanceof Modal)) {
       // This is duplicated so that if the error is caught, an error message still shows up in the debug console.
       const invalidModalWarning = 'The ModalManager can only show Modals.';
@@ -53,28 +69,52 @@ export default class ModalManagerState {
       throw new Error(invalidModalWarning);
     }
 
-    if (this.closeTimeout) clearTimeout(this.closeTimeout);
-
-    this.modal = { componentClass, attrs, key: this.key++ };
-
+    this.backdropShown = true;
     m.redraw.sync();
+
+    // We use requestAnimationFrame here, since we need to wait for the backdrop to be added
+    // to the DOM before actually adding the modal to the modal list.
+    //
+    // This is because we use RAF inside the ModalManager onupdate lifecycle hook, and if we
+    // skip this RAF call, the hook will attempt to add a focus trap as well as lock scroll
+    // onto the newly added modal before it's in the DOM, creating an extra scrollbar.
+    requestAnimationFrame(() => {
+      // Set current modal
+      this.modal = { componentClass, attrs, key: this.key++ };
+
+      // We want to stack this modal
+      if (stackModal) {
+        // Remember previously opened modal and add new modal to the modal list
+        this.modalList.push(this.modal);
+      } else {
+        // Override last modals
+        this.modalList = [this.modal];
+      }
+
+      m.redraw();
+    });
   }
 
   /**
-   * Closes the currently open dialog, if one is open.
+   * Closes the topmost currently open dialog, if one is open.
    */
   close(): void {
     if (!this.modal) return;
 
-    // Don't hide the modal immediately, because if the consumer happens to call
-    // the `show` method straight after to show another modal dialog, it will
-    // cause Bootstrap's modal JS to misbehave. Instead we will wait for a tiny
-    // bit to give the `show` method the opportunity to prevent this from going
-    // ahead.
-    this.closeTimeout = setTimeout(() => {
+    // If there are two modals, remove the most recent one
+    if (this.modalList.length > 1) {
+      // Remove last modal from list
+      this.modalList.pop();
+
+      // Open last modal from list
+      this.modal = this.modalList[this.modalList.length - 1];
+    } else {
+      // Reset state
       this.modal = null;
-      m.redraw();
-    });
+      this.modalList = [];
+    }
+
+    m.redraw();
   }
 
   /**
