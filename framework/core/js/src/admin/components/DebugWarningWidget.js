79d51a6e844b3bcaa48b515b571df83fab45c62d
Sami Mazouz <ilyasmazouz@gmail.com>
2022-08-11 18:22:41 +0100

    feat: add `title` and `icon` attributes to the `Alert` component
    Signed-off-by: Sami Mazouz <ilyasmazouz@gmail.com>

--- framework/core/js/src/admin/components/DebugWarningWidget.js
+++ framework/core/js/src/admin/components/DebugWarningWidget.js
@@ -1,6 +1,5 @@
 import app from '../../admin/app';
 import Alert from '../../common/components/Alert';
-import icon from '../../common/helpers/icon';
 import DashboardWidget from './DashboardWidget';
 
 export default class DebugWarningWidget extends DashboardWidget {
@@ -10,13 +9,12 @@ export default class DebugWarningWidget extends DashboardWidget {
 
   content() {
     return (
-      <Alert type="warning" dismissible={false}>
-        <div>
-          <h4 className="DebugWarningWidget-title">
-            {icon('fas fa-exclamation-triangle')} {app.translator.trans('core.admin.debug-warning.label')}
-          </h4>
-        </div>
-        <p className="DebugWarningWidget-detail">{app.translator.trans('core.admin.debug-warning.detail')}</p>
+      <Alert
+        type="error"
+        dismissible={false}
+        title={app.translator.trans('core.admin.debug-warning.label')}
+        icon="fas fa-exclamation-triangle">
+        {app.translator.trans('core.admin.debug-warning.detail')}
       </Alert>
     );
   }
