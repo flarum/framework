3620461221c35fd02b700af40029b6ad418904fb
Ian Morland <ian@morland.me>
2022-08-11 22:31:06 +0200

    chore: add docs link

--- framework/core/js/src/admin/components/DebugWarningWidget.tsx
+++ framework/core/js/src/admin/components/DebugWarningWidget.tsx
@@ -1,5 +1,6 @@
 import app from '../../admin/app';
 import Alert from '../../common/components/Alert';
+import Link from '../../common/components/Link';
 import DashboardWidget from './DashboardWidget';
 
 export default class DebugWarningWidget extends DashboardWidget {
@@ -10,7 +11,9 @@ export default class DebugWarningWidget extends DashboardWidget {
   content() {
     return (
       <Alert type="warning" dismissible={false} title={app.translator.trans('core.admin.debug-warning.label')} icon="fas fa-exclamation-triangle">
-        {app.translator.trans('core.admin.debug-warning.detail')}
+        {app.translator.trans('core.admin.debug-warning.detail', {
+          link: <Link href="https://docs.flarum.org/troubleshoot/#step-0-activate-debug-mode" external={true} target="_blank" />,
+        })}
       </Alert>
     );
   }
