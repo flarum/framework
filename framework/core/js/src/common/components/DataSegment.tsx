ba947349a4a71a9e93bd79c0430832ea2882d4f4
Sami Mazouz <ilyasmazouz@gmail.com>
2022-08-29 19:14:46 +0100

    chore(review): use translation for data segment label
    Signed-off-by: Sami Mazouz <ilyasmazouz@gmail.com>

--- framework/core/js/src/common/components/DataSegment.tsx
+++ framework/core/js/src/common/components/DataSegment.tsx
@@ -1,5 +1,6 @@
 import Component, { ComponentAttrs } from '../Component';
 import type Mithril from 'mithril';
+import app from "../app";
 
 export interface IDataSegmentAttrs extends ComponentAttrs {
   label: Mithril.Children;
@@ -16,7 +17,11 @@ export default class DataSegment<CustomAttrs extends IDataSegmentAttrs = IDataSe
   view(vnode: Mithril.Vnode<CustomAttrs, this>): Mithril.Children {
     return (
       <div className="DataSegment">
-        <div className="DataSegment-label">{this.attrs.label}:</div>
+        <div className="DataSegment-label">
+          {app.translator.trans('core.lib.data_segment.label', {
+            label: this.attrs.label
+          })}
+        </div>
         <div className="DataSegment-value">{this.attrs.value}</div>
       </div>
     );
