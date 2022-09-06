6e53566234084e0c206eb4051a583d028808798a
Sami Mazouz <sychocouldy@gmail.com>
2022-09-06 22:47:58 +0100

    chore(review): improve `InfoCommand` code readability
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/src/Foundation/Console/InfoCommand.php
+++ framework/core/src/Foundation/Console/InfoCommand.php
@@ -19,6 +19,7 @@ use Illuminate\Contracts\Queue\Queue;
 use Illuminate\Database\ConnectionInterface;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
+use InvalidArgumentException;
 use PDO;
 use SessionHandlerInterface;
 use Symfony\Component\Console\Helper\Table;
@@ -188,25 +189,46 @@ class InfoCommand extends AbstractCommand
     }
 
     /**
-     * Some extensions/packages might be overriding the session.handler binding.
-     * So we have to check if the driver used is configured or hardcoded.
+     * Reports on the session driver in use based on three scenarios:
+     *  1. If the configured session driver is valid and in use, it will be returned.
+     *  2. If the configured session driver is invalid, fallback to the default one and mention it.
+     *  3. If the actual used driver (i.e `session.handler`) is different from the current one (configured or default), mention it.
      */
     private function identifySessionDriver(): string
     {
+        /*
+         * Get the configured driver and fallback to the default one.
+         */
         $defaultDriver = $this->session->getDefaultDriver();
-        $driver = Arr::get($this->config, 'session.driver', $defaultDriver);
-        $this->session->driver($driver);
-        $configuredDriver = isset($this->session->getDrivers()[$driver]) ? $driver : $defaultDriver;
+        $configuredDriver = Arr::get($this->config, 'session.driver', $defaultDriver);
+        $driver = $configuredDriver;
+        try {
+            // Try to get the configured driver instance.
+            // Driver instances are created on demand.
+            $this->session->driver($configuredDriver);
+        } catch (InvalidArgumentException $e) {
+            // An exception is thrown if the configured driver is not a valid driver.
+            // So we fallback to the default driver.
+            $driver = $defaultDriver;
+        }
 
+        /*
+         * Get actual driver name from its class name.
+         * And compare that to the current configured driver.
+         */
         // Get class name
         $handlerName = get_class($this->sessionHandler);
         // Drop the namespace
         $handlerName = Str::afterLast($handlerName, '\\');
         // Lowercase the class name
         $handlerName = strtolower($handlerName);
-        // Drop everything like queue SyncQueue, RedisQueue
+        // Drop everything like sessionhandler FileSessionHandler, DatabaseSessionHandler ..etc
         $handlerName = str_replace('sessionhandler', '', $handlerName);
 
-        return $configuredDriver !== $handlerName ? "$handlerName <comment>(code override, configured to <options=bold,underscore>$configuredDriver</>)</comment>" : $configuredDriver;
+        return $driver !== $handlerName
+            ? "$handlerName <comment>(Code override. Configured to <options=bold,underscore>$configuredDriver</>)</comment>"
+            : ($driver !== $configuredDriver
+                ? "$driver <comment>(Fallback default driver. Configured to invalid driver <options=bold,underscore>$configuredDriver</>)</comment>"
+                : $driver);
     }
 }
