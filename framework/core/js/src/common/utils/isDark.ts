ebcad439edbfd160b29709b772ab3de59a379371
protoclown <ornano@felinn.org>
2022-09-26 13:01:46 +0200

    fix: adjust snippet description

--- framework/core/js/src/common/utils/isDark.ts
+++ framework/core/js/src/common/utils/isDark.ts
@@ -1,7 +1,8 @@
 /**
- * The `getContrast` utility converts a hex color to rgb, and then calcul a YIQ
- * value in order to get the appropriate contrast value (is it dark or is it
- * light?) See https://www.w3.org/TR/AERT/#color-contrast for references
+ * The `isDark` utility converts a hex color to rgb, and then calcul a YIQ
+ * value in order to get the appropriate brightness value (is it dark or is it
+ * light?) See https://www.w3.org/TR/AERT/#color-contrast for references. A YIQ
+ * value >= 128 is a light color.
  */
 
 export default function isDark(hexcolor: String) {
