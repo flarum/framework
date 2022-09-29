6903b84605881b1c1362c65762239f66ddf8cd30
protoclown <ornano@felinn.org>
2022-09-29 16:01:01 +0200

    fix: TagHero contrast

--- extensions/tags/js/src/forum/components/TagHero.js
+++ extensions/tags/js/src/forum/components/TagHero.js
@@ -1,4 +1,5 @@
 import Component from 'flarum/common/Component';
+import isDark from 'flarum/common/utils/isDark';
 import tagIcon from '../../common/helpers/tagIcon';
 
 export default class TagHero extends Component {
@@ -7,7 +8,7 @@ export default class TagHero extends Component {
     const color = tag.color();
 
     return (
-      <header className={'Hero TagHero' + (color ? ' TagHero--colored' : '')} style={color ? { '--hero-bg': color } : ''}>
+      <header className={'Hero TagHero' + (color ? ' TagHero--colored' : '') + (isDark(color) ? ' TagHero--dark' : ' TagHero--light')} style={color ? { '--hero-bg': color } : ''}>
         <div className="container">
           <div className="containerNarrow">
             <h2 className="Hero-title">
