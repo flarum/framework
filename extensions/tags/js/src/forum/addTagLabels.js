bb0f3eab748c931f1f0a641a3fa43f989d916e95
protoclown <ornano@felinn.org>
2022-09-29 16:26:43 +0200

    fix: DiscussionHero contrast

--- extensions/tags/js/src/forum/addTagLabels.js
+++ extensions/tags/js/src/forum/addTagLabels.js
@@ -1,6 +1,7 @@
 import { extend } from 'flarum/common/extend';
 import DiscussionListItem from 'flarum/forum/components/DiscussionListItem';
 import DiscussionHero from 'flarum/forum/components/DiscussionHero';
+import isDark from 'flarum/common/utils/isDark';
 
 import tagsLabel from '../common/helpers/tagsLabel';
 import sortTags from '../common/utils/sortTags';
@@ -23,7 +24,13 @@ export default function () {
       const color = tags[0].color();
       if (color) {
         view.attrs.style = { '--hero-bg': color };
-        view.attrs.className += ' DiscussionHero--colored';
+
+        if (isDark(color)) {
+          view.attrs.className += ' DiscussionHero--dark';
+        }
+        else {
+          view.attrs.className += ' DiscussionHero--light';
+        }
       }
     }
   });
