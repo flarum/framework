5c3a37f05248405d6d836468fc623bc89b23845c
Sami Mazouz <sychocouldy@gmail.com>
2022-10-03 16:56:21 +0100

    chore(review): delay extender implementation
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/js/src/common/extenders/Model.ts
+++ framework/core/js/src/common/extenders/Model.ts
@@ -3,27 +3,34 @@ import Application from '../Application';
 import ActualModel from '../Model';
 
 export default class Model implements IExtender {
-  private model: { new (): ActualModel };
+  private readonly model: { new (): ActualModel };
   private type: string | null = null;
+  private callbacks: Array<() => void> = [];
 
   public constructor(model: { new (): ActualModel }) {
     this.model = model;
   }
 
   public attribute<T, O = unknown>(name: string, transform: ((attr: O) => T) | null = null): Model {
-    this.model.prototype[name] = transform ? ActualModel.attribute<T, O>(name, transform) : ActualModel.attribute<T>(name);
+    this.callbacks.push(() => {
+      this.model.prototype[name] = transform ? ActualModel.attribute<T, O>(name, transform) : ActualModel.attribute<T>(name);
+    });
 
     return this;
   }
 
   public hasOne<M extends ActualModel>(name: string): Model {
-    this.model.prototype[name] = ActualModel.hasOne<M>(name);
+    this.callbacks.push(() => {
+      this.model.prototype[name] = ActualModel.hasOne<M>(name);
+    });
 
     return this;
   }
 
   public hasMany<M extends ActualModel>(name: string): Model {
-    this.model.prototype[name] = ActualModel.hasMany<M>(name);
+    this.callbacks.push(() => {
+      this.model.prototype[name] = ActualModel.hasMany<M>(name);
+    });
 
     return this;
   }
@@ -40,6 +47,10 @@ export default class Model implements IExtender {
   }
 
   extend(app: Application, extension: IExtensionModule): void {
+    for (const callback of this.callbacks) {
+      callback.call(this);
+    }
+
     if (this.type) {
       app.store.models[this.type] = this.model;
     }
