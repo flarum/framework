be1e3e031be3e53eab61218f8b48744a7b8948fd
Sami Mazouz <sychocouldy@gmail.com>
2022-10-03 16:46:41 +0100

    chore(review): attributes can be nullable or undefined
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- extensions/suspend/js/src/forum/extend.ts
+++ extensions/suspend/js/src/forum/extend.ts
@@ -5,7 +5,7 @@ import Model from 'flarum/common/Model';
 export default [
   new Extend.Model(User)
     .attribute<boolean>('canSuspend')
-    .attribute<Date, string>('suspendedUntil', Model.transformDate)
-    .attribute<string>('suspendReason')
-    .attribute<string>('suspendMessage'),
+    .attribute<Date, string | null | undefined>('suspendedUntil', Model.transformDate)
+    .attribute<string | null | undefined>('suspendReason')
+    .attribute<string | null | undefined>('suspendMessage'),
 ];
