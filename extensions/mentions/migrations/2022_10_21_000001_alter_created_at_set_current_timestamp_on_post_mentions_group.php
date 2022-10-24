368b7f22ff21dde02e6a1c182c36667f5e6f9722
Ian Morland <ian@morland.me>
2022-10-24 13:24:31 +0100

    Revert "remove unneeded migration"
    This reverts commit 9347665baa1820ed0875c6b2dd22b14fbc2768e7.

---
+++ extensions/mentions/migrations/2022_10_21_000001_alter_created_at_set_current_timestamp_on_post_mentions_group.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        // do this manually because dbal doesn't recognize timestamp columns
+        $connection = $schema->getConnection();
+        $prefix = $connection->getTablePrefix();
+        $connection->statement("ALTER TABLE `${prefix}post_mentions_group` MODIFY created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP");
+    },
+
+    'down' => function (Builder $schema) {
+        // Nothing to do.
+    }
+];
