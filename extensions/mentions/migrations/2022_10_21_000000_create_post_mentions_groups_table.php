8b5b44682b876feb52a44a0f8e99caaa19aa7f57
Ian Morland <16573496+imorland@users.noreply.github.com>
2022-10-25 13:53:44 +0100

    Update extensions/mentions/migrations/2022_10_21_000000_create_post_mentions_groups_table.php
    Co-authored-by: Sami Mazouz <sychocouldy@gmail.com>

--- extensions/mentions/migrations/2022_10_21_000000_create_post_mentions_groups_table.php
+++ extensions/mentions/migrations/2022_10_21_000000_create_post_mentions_groups_table.php
@@ -7,18 +7,28 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use Flarum\Database\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
 
-return Migration::createTable(
-    'post_mentions_group',
-    function (Blueprint $table) {
-        $table->integer('post_id')->unsigned();
-        $table->integer('mentions_group_id')->unsigned();
-        $table->timestamp('created_at')->nullable();
-        $table->primary(['post_id', 'mentions_group_id']);
+return [
+    'up' => function (Builder $schema) {
+        $schema->create('post_mentions_groups', function (Blueprint $table) {
+            $table->integer('post_id')->unsigned();
+            $table->integer('mentions_group_id')->unsigned();
+            $table->timestamp('created_at')->nullable();
+            $table->primary(['post_id', 'mentions_group_id']);
 
-        $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
-        $table->foreign('mentions_group_id')->references('id')->on('groups')->onDelete('cascade');
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('mentions_group_id')->references('id')->on('groups')->onDelete('cascade');
+        });
+
+        // do this manually because dbal doesn't recognize timestamp columns
+        $connection = $schema->getConnection();
+        $prefix = $connection->getTablePrefix();
+        $connection->statement("ALTER TABLE `${prefix}post_mentions_group` MODIFY created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP");
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->drop('post_mentions_groups');
     }
-);
+];
