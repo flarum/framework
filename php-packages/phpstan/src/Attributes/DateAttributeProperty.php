c952bc6e31deb42f3a5b0506595fadf16d33d995
Sami Mazouz <sychocouldy@gmail.com>
2022-11-02 18:07:39 +0100

    feat(phpstan): pick up extended model date attributes
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

---
+++ php-packages/phpstan/src/Attributes/DateAttributeProperty.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Flarum\PHPStan\Attributes;
+
+use Carbon\Carbon;
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\TrinaryLogic;
+use PHPStan\Type\NullType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+
+class DateAttributeProperty implements PropertyReflection
+{
+    /** @var ClassReflection */
+    private $classReflection;
+
+    public function __construct(ClassReflection $classReflection)
+    {
+        $this->classReflection = $classReflection;
+    }
+
+    public function getDeclaringClass(): ClassReflection
+    {
+        return $this->classReflection;
+    }
+
+    public function isStatic(): bool
+    {
+        return false;
+    }
+
+    public function isPrivate(): bool
+    {
+        return false;
+    }
+
+    public function isPublic(): bool
+    {
+        return true;
+    }
+
+    public function getDocComment(): ?string
+    {
+        return null;
+    }
+
+    public function getReadableType(): Type
+    {
+        return new UnionType([
+            new ObjectType(Carbon::class),
+            new NullType(),
+        ]);
+    }
+
+    public function getWritableType(): Type
+    {
+        return $this->getReadableType();
+    }
+
+    public function canChangeTypeAfterAssignment(): bool
+    {
+        return false;
+    }
+
+    public function isReadable(): bool
+    {
+        return true;
+    }
+
+    public function isWritable(): bool
+    {
+        return true;
+    }
+
+    public function isDeprecated(): TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+
+    public function getDeprecatedDescription(): ?string
+    {
+        return null;
+    }
+
+    public function isInternal(): TrinaryLogic
+    {
+        return TrinaryLogic::createNo();
+    }
+}
