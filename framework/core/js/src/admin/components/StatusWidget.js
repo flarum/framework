5d3dd0d12b93ce5d4d069332bd286b8f3ba4b66f
Ian Morland <ian@morland.me>
2022-11-03 23:08:26 +0000

    add ItemList priorities

--- framework/core/js/src/admin/components/StatusWidget.js
+++ framework/core/js/src/admin/components/StatusWidget.js
@@ -31,21 +31,25 @@ export default class StatusWidget extends DashboardWidget {
       </Dropdown>
     );
 
-    items.add('version-flarum', [<strong>Flarum</strong>, <br />, app.forum.attribute('version')]);
-    items.add('version-php', [<strong>PHP</strong>, <br />, app.data.phpVersion]);
-    items.add('version-mysql', [<strong>MySQL</strong>, <br />, app.data.mysqlVersion]);
+    items.add('version-flarum', [<strong>Flarum</strong>, <br />, app.forum.attribute('version')], 100);
+    items.add('version-php', [<strong>PHP</strong>, <br />, app.data.phpVersion], 90);
+    items.add('version-mysql', [<strong>MySQL</strong>, <br />, app.data.mysqlVersion], 80);
     if (app.data.schedulerStatus) {
-      items.add('schedule-status', [
-        <span>
-          <strong>Scheduler</strong>{' '}
-          <LinkButton href="https://discuss.flarum.org/d/24118" external={true} target="_blank" icon="fas fa-info-circle" />
-        </span>,
-        <br />,
-        app.data.schedulerStatus,
-      ]);
+      items.add(
+        'schedule-status',
+        [
+          <span>
+            <strong>Scheduler</strong>{' '}
+            <LinkButton href="https://discuss.flarum.org/d/24118" external={true} target="_blank" icon="fas fa-info-circle" />
+          </span>,
+          <br />,
+          app.data.schedulerStatus,
+        ],
+        70
+      );
     }
 
-    items.add('queue-driver', [<strong>Queue Driver</strong>, <br />, app.data.queueDriver]);
+    items.add('queue-driver', [<strong>Queue Driver</strong>, <br />, app.data.queueDriver], 60);
 
     return items;
   }
