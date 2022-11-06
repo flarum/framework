e698f3e4592e283fcfaba8513c15d4ce29252f9c
Ian Morland <ian@morland.me>
2022-11-06 20:56:27 +0000

    feat: Allow additional reset password params, introduce 'ForgotPasswordValidator'

---
+++ framework/core/src/Api/ForgotPasswordValidator.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Flarum\Api;
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use Flarum\Foundation\AbstractValidator;
+
+class ForgotPasswordValidator extends AbstractValidator
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $rules = [
+        'email' => ['required','email']
+    ];
+}
