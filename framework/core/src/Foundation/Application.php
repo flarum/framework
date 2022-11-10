bb712693d46e659f8721081c9564463c3912e25c
Daniël Klabbers <daniel@klabbers.email>
2022-11-10 13:48:17 +0100

    chore: update application constant for version

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -21,7 +21,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.6.0-dev';
+    const VERSION = '1.6.0';
 
     /**
      * The IoC container for the Flarum application.
