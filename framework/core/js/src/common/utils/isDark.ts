d82b95923fa5db7d1a42a4e52a74bb499d900fdf
protoclown <ornano@felinn.org>
2022-11-27 00:02:25 +0100

    fix(isDark): increase the yiq threshold

--- framework/core/js/src/common/utils/isDark.ts
+++ framework/core/js/src/common/utils/isDark.ts
@@ -19,5 +19,5 @@ export default function isDark(hexcolor: String) {
   const b = parseInt(hexnumbers.substr(4, 2), 16);
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
-  return yiq >= 128 ? false : true;
+  return yiq >= 138 ? false : true;
 }
