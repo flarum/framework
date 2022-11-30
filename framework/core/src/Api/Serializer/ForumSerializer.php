bbfa1bcd2a62d15240c9e4b8091214fb962e8c76
Sébastien Brunat <sebastien.brunat@clever-cloud.com>
2022-11-30 14:15:28 +0100

    patch an issue with AWS S3 SDK
    patch an issue with AWS S3 SDK
    SDK crashes without this patch and we can't use our S3-like buckets

--- framework/core/src/Api/Serializer/ForumSerializer.php
+++ framework/core/src/Api/Serializer/ForumSerializer.php
@@ -93,7 +93,7 @@ class ForumSerializer extends AbstractSerializer
             'canViewForum' => $this->actor->can('viewForum'),
             'canStartDiscussion' => $this->actor->can('startDiscussion'),
             'canSearchUsers' => $this->actor->can('searchUsers'),
-            'assetsBaseUrl' => rtrim($this->assetsFilesystem->url(''), '/'),
+            'assetsBaseUrl' => rtrim($this->assetsFilesystem->url('/'), '/'),
         ];
 
         if ($this->actor->can('administrate')) {
