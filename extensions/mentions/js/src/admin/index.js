070ed9da622d760b819392ec6e60a394d09980ab
Ian Morland <ian@morland.me>
2022-12-05 12:43:16 +0000

    Move permission to tags ext

--- extensions/mentions/js/src/admin/index.js
+++ extensions/mentions/js/src/admin/index.js
@@ -1,7 +1,7 @@
 import app from 'flarum/admin/app';
 
 app.initializers.add('flarum-mentions', function () {
-  const extData = app.extensionData
+  app.extensionData
     .for('flarum-mentions')
     .registerSetting({
       setting: 'flarum-mentions.allow_username_format',
@@ -17,15 +17,4 @@ app.initializers.add('flarum-mentions', function () {
       },
       'start'
     );
-
-  if (app.initializers.has('flarum-tags')) {
-    extData.registerPermission(
-      {
-        permission: 'mentionTags',
-        label: app.translator.trans('flarum-mentions.admin.permissions.mention_tags_label'),
-        icon: 'fas fa-at',
-      },
-      'start'
-    );
-  }
 });
--- extensions/tags/js/src/admin/index.ts
+++ extensions/tags/js/src/admin/index.ts
@@ -9,7 +9,18 @@ import TagsPage from './components/TagsPage';
 app.initializers.add('flarum-tags', (app) => {
   app.store.models.tags = Tag;
 
-  app.extensionData.for('flarum-tags').registerPage(TagsPage);
+  const extData = app.extensionData.for('flarum-tags').registerPage(TagsPage);
+
+  if (app.initializers.has('flarum-mentions')) {
+    extData.registerPermission(
+      {
+        permission: 'mentionTags',
+        label: app.translator.trans('flarum-tags.admin.permissions.mention_tags_label'),
+        icon: 'fas fa-at',
+      },
+      'start'
+    );
+  }
 
   addTagsPermissionScope();
   addTagPermission();
