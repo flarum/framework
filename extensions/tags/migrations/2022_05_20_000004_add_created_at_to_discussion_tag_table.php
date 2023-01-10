a20c7db3e0eecb28e64a529b4174a744793bdb6d
Ian Morland <ian@morland.me>
2023-01-10 15:35:14 +0000

    tags php82

--- extensions/tags/migrations/2022_05_20_000004_add_created_at_to_discussion_tag_table.php
+++ extensions/tags/migrations/2022_05_20_000004_add_created_at_to_discussion_tag_table.php
@@ -19,7 +19,7 @@ return [
         // do this manually because dbal doesn't recognize timestamp columns
         $connection = $schema->getConnection();
         $prefix = $connection->getTablePrefix();
-        $connection->statement("ALTER TABLE `${prefix}discussion_tag` MODIFY created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP");
+        $connection->statement("ALTER TABLE `{$prefix}discussion_tag` MODIFY created_at TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP");
     },
 
     'down' => function (Builder $schema) {
