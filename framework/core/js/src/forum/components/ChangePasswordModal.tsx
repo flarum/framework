f06659d9925f4772fb0ad8d10c7ce1ed616e28df
Sami Mazouz <sychocouldy@gmail.com>
2023-01-21 16:58:14 +0100

    chore: improve change password modal extensibility
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/js/src/forum/components/ChangePasswordModal.tsx
+++ framework/core/js/src/forum/components/ChangePasswordModal.tsx
@@ -1,6 +1,8 @@
 import app from '../../forum/app';
 import Modal, { IInternalModalAttrs } from '../../common/components/Modal';
 import Button from '../../common/components/Button';
+import Mithril from 'mithril';
+import ItemList from '../../common/utils/ItemList';
 
 /**
  * The `ChangePasswordModal` component shows a modal dialog which allows the
@@ -18,21 +20,26 @@ export default class ChangePasswordModal<CustomAttrs extends IInternalModalAttrs
   content() {
     return (
       <div className="Modal-body">
-        <div className="Form Form--centered">
-          <p className="helpText">{app.translator.trans('core.forum.change_password.text')}</p>
-          <div className="Form-group">
-            {Button.component(
-              {
-                className: 'Button Button--primary Button--block',
-                type: 'submit',
-                loading: this.loading,
-              },
-              app.translator.trans('core.forum.change_password.send_button')
-            )}
-          </div>
-        </div>
+        <div className="Form Form--centered">{this.fields().toArray()}</div>
       </div>
     );
+  }
+
+  fields() {
+    const fields = new ItemList<Mithril.Children>();
+
+    fields.add('help', <p className="helpText">{app.translator.trans('core.forum.change_password.text')}</p>);
+
+    fields.add(
+      'submit',
+      <div className="Form-group">
+        <Button className="Button Button--primary Button--block" type="submit" loading={this.loading}>
+          {app.translator.trans('core.forum.change_password.send_button')}
+        </Button>
+      </div>
+    );
+
+    return fields;
   }
 
   onsubmit(e: SubmitEvent) {
@@ -44,8 +51,12 @@ export default class ChangePasswordModal<CustomAttrs extends IInternalModalAttrs
       .request({
         method: 'POST',
         url: app.forum.attribute('apiUrl') + '/forgot',
-        body: { email: app.session.user!.email() },
+        body: this.requestBody(),
       })
       .then(this.hide.bind(this), this.loaded.bind(this));
+  }
+
+  requestBody() {
+    return { email: app.session.user!.email() };
   }
 }
