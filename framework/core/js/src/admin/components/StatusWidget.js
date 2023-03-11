541488aa0f60ba285a5ea942034608de4d0b4134
Robert Korulczyk <robert@korulczyk.pl>
2023-03-12 00:24:18 +0100

    Fix CS.

--- framework/core/js/src/admin/components/StatusWidget.js
+++ framework/core/js/src/admin/components/StatusWidget.js
@@ -49,8 +49,16 @@ export default class StatusWidget extends DashboardWidget {
       );
     }
 
-    items.add('queue-driver', [<strong>{app.translator.trans('core.admin.dashboard.status.headers.queue-driver')}</strong>, <br />, app.data.queueDriver], 60);
-    items.add('session-driver', [<strong>{app.translator.trans('core.admin.dashboard.status.headers.session-driver')}</strong>, <br />, app.data.sessionDriver], 50);
+    items.add(
+      'queue-driver',
+      [<strong>{app.translator.trans('core.admin.dashboard.status.headers.queue-driver')}</strong>, <br />, app.data.queueDriver],
+      60
+    );
+    items.add(
+      'session-driver',
+      [<strong>{app.translator.trans('core.admin.dashboard.status.headers.session-driver')}</strong>, <br />, app.data.sessionDriver],
+      50
+    );
 
     return items;
   }
