9a890b06be77a5dc0355852dcb33469315aaff23
Robert Korulczyk <robert@korulczyk.pl>
2023-03-11 23:59:31 +0100

    Hide Developer Tokens section in Security view if there is nothing to display or create.

--- framework/core/js/src/forum/components/UserSecurityPage.tsx
+++ framework/core/js/src/forum/components/UserSecurityPage.tsx
@@ -51,17 +51,30 @@ export default class UserSecurityPage<CustomAttrs extends IUserPageAttrs = IUser
   settingsItems() {
     const items = new ItemList<Mithril.Children>();
 
-    ['developerTokens', 'sessions'].forEach((section) => {
-      const sectionName = `${section}Items` as 'developerTokensItems' | 'sessionsItems';
-      const sectionLocale = camelCaseToSnakeCase(section);
-
+    if (
+      app.forum.attribute('canCreateAccessToken') || app.forum.attribute('canModerateAccessTokens')
+      || (this.state.hasLoadedTokens() && this.state.getDeveloperTokens()?.length)
+    ) {
       items.add(
-        section,
-        <FieldSet className={`UserSecurityPage-${section}`} label={app.translator.trans(`core.forum.security.${sectionLocale}_heading`)}>
-          {this[sectionName]().toArray()}
+        'developerTokensItems',
+        <FieldSet
+          className="UserSecurityPage-developerTokensItems"
+          styles={!this.state.hasLoadedTokens() || !this.state.getDeveloperTokens() ? {display: 'none'} : {color: 'red'}}
+          label={app.translator.trans(`core.forum.security.developer_tokens_heading`)}
+        >
+          {this.developerTokensItems().toArray()}
         </FieldSet>
       );
-    });
+    } else if (!this.state.hasLoadedTokens()) {
+      items.add('developerTokensItems', <LoadingIndicator/>);
+    }
+
+    items.add(
+      'sessionsItems',
+      <FieldSet className="UserSecurityPage-sessionsItems" label={app.translator.trans(`core.forum.security.sessions_heading`)}>
+        {this.sessionsItems().toArray()}
+      </FieldSet>
+    );
 
     if (this.user!.id() === app.session.user!.id()) {
       items.add(
