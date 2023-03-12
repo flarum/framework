241eba4d0c6fc393d8c6aa9099f8b1300d713c65
Sami Mazouz <sychocouldy@gmail.com>
2023-03-12 13:56:39 +0100

    chore: mark start of `1.8.0` development
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -21,7 +21,7 @@ class Application
      *
      * @var string
      */
-    const VERSION = '1.7.1';
+    const VERSION = '1.8.0-dev';
 
     /**
      * The IoC container for the Flarum application.
