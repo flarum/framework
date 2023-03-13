1e6eff363133f6331d5ebe76142083f52df9274c
Sami Mazouz <sychocouldy@gmail.com>
2023-03-13 22:34:53 +0100

    feat: conditional extenders
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

---
+++ framework/core/src/Extend/Conditional.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Flarum\Extend;
+
+use Flarum\Extension\Extension;
+use Flarum\Extension\ExtensionManager;
+use Illuminate\Contracts\Container\Container;
+
+class Conditional implements ExtenderInterface
+{
+    /**
+     * @var array<array{condition: bool|callable, extenders: ExtenderInterface[]}>
+     */
+    protected $conditions = [];
+
+    /**
+     * @param ExtenderInterface[] $extenders
+     */
+    public function whenExtensionEnabled(string $extensionId, array $extenders): self
+    {
+        return $this->when(function (ExtensionManager $extensions) use ($extensionId) {
+            return $extensions->isEnabled($extensionId);
+        }, $extenders);
+    }
+
+    /**
+     * @param bool|callable $condition
+     * @param ExtenderInterface[] $extenders
+     */
+    public function when($condition, array $extenders): self
+    {
+        $this->conditions[] = [
+            'condition' => $condition,
+            'extenders' => $extenders,
+        ];
+
+        return $this;
+    }
+
+    public function extend(Container $container, Extension $extension = null)
+    {
+        foreach ($this->conditions as $condition) {
+            if (is_callable($condition['condition'])) {
+                $condition['condition'] = $container->call($condition['condition']);
+            }
+
+            if ($condition['condition']) {
+                foreach ($condition['extenders'] as $extender) {
+                    $extender->extend($container, $extension);
+                }
+            }
+        }
+    }
+}
