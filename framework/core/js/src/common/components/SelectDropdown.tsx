3b72d6d9748c78bad590e563ee672cb21b0caa36
IanM <16573496+imorland@users.noreply.github.com>
2023-03-22 09:31:21 +0000

    Update framework/core/js/src/common/components/SelectDropdown.tsx
    Co-authored-by: David Wheatley <david@davwheat.dev>

--- framework/core/js/src/common/components/SelectDropdown.tsx
+++ framework/core/js/src/common/components/SelectDropdown.tsx
@@ -50,7 +50,7 @@ export default class SelectDropdown<CustomAttrs extends ISelectDropdownAttrs = I
     let label = (activeChild && typeof activeChild === 'object' && 'children' in activeChild && activeChild.children) || this.attrs.defaultLabel;
 
     // @ts-ignore
-    if (label instanceof Array) label = label[0];
+    if (Array.isArray(label)) label = label[0];
 
     return [<span className="Button-label">{label}</span>, this.attrs.caretIcon ? icon(this.attrs.caretIcon, { className: 'Button-caret' }) : null];
   }
