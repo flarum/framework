dc80a609e3f824b10d16c646b51de400328701ba
luk <luk@exside.ch>
2023-03-23 15:08:10 +0100

    Make isDark() not fail as easily with invalid input
    Add early return if input looks fishy, minor refactoring and improvements of the entire method.

--- framework/core/js/src/common/utils/isDark.ts
+++ framework/core/js/src/common/utils/isDark.ts
@@ -8,18 +8,25 @@
  * to preserve design consistency.
  */
 export default function isDark(hexcolor: string): boolean {
+  // return if hexcolor is undefined or shorter than 4 characters, shortest hex form is #333;
+  // decided against regex hex color validation for performance considerations
+  if (!hexcolor || hexcolor.length <= 4) {
+    return false;
+  }
+
   let hexnumbers = hexcolor.replace('#', '');
 
   if (hexnumbers.length == 3) {
     hexnumbers += hexnumbers;
   }
 
-  const r = parseInt(hexnumbers.substr(0, 2), 16);
-  const g = parseInt(hexnumbers.substr(2, 2), 16);
-  const b = parseInt(hexnumbers.substr(4, 2), 16);
+  const r = parseInt(hexnumbers.slice(0, 2), 16);
+  const g = parseInt(hexnumbers.slice(2, 4), 16);
+  const b = parseInt(hexnumbers.slice(4, 6), 16);
   const yiq = (r * 299 + g * 587 + b * 114) / 1000;
 
-  const threshold = parseInt(window.getComputedStyle(document.body).getPropertyValue('--yiq-threshold'));
-
+  const threshold = parseInt(getComputedStyle(document.body).getPropertyValue("--yiq-threshold").trim()) || 128;
+  
   return yiq < threshold;
 }
+
