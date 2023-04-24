245ebcabef025ffa1e649017b6573582c3ff33ea
Sami Mazouz <sychocouldy@gmail.com>
2023-04-24 15:54:49 +0100

    chore: continue
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- extensions/mentions/tests/integration/api/NotificationsTest.php
+++ extensions/mentions/tests/integration/api/NotificationsTest.php
@@ -28,25 +28,17 @@ class NotificationsTest extends TestCase
         $this->prepareDatabase([
             'users' => [
                 $this->normalUser(),
-                ['id' => 3, 'username' => 'acme', 'email' => 'acme@machine.local', 'is_email_confirmed' => 1, 'preferences' => json_encode(['flarum-subscriptions.notify_for_all_posts' => true])],
-                ['id' => 4, 'username' => 'acme2', 'email' => 'acme2@machine.local', 'is_email_confirmed' => 1],
+                ['id' => 3, 'username' => 'acme', 'email' => 'acme@machine.local', 'is_email_confirmed' => 1],
             ],
             'discussions' => [
-                ['id' => 1, 'title' => __CLASS__, 'created_at' => Carbon::now(), 'last_posted_at' => Carbon::now(), 'user_id' => 1, 'first_post_id' => 1, 'comment_count' => 1, 'last_post_number' => 1, 'last_post_id' => 1],
-                ['id' => 2, 'title' => __CLASS__, 'created_at' => Carbon::now(), 'last_posted_at' => Carbon::now(), 'user_id' => 1, 'first_post_id' => 2, 'comment_count' => 1, 'last_post_number' => 1, 'last_post_id' => 2],
-
-                ['id' => 33, 'title' => __CLASS__, 'created_at' => Carbon::now(), 'last_posted_at' => Carbon::now(), 'user_id' => 1, 'first_post_id' => 33, 'comment_count' => 6, 'last_post_number' => 6, 'last_post_id' => 38],
+                ['id' => 1, 'title' => __CLASS__, 'created_at' => Carbon::now(), 'last_posted_at' => Carbon::now(), 'user_id' => 1, 'first_post_id' => 1, 'comment_count' => 1, 'last_post_number' => 2, 'last_post_id' => 2],
             ],
             'posts' => [
                 ['id' => 1, 'discussion_id' => 1, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 1],
-                ['id' => 2, 'discussion_id' => 2, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 1],
-
-                ['id' => 33, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 1],
-                ['id' => 34, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 2],
-                ['id' => 35, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 3],
-                ['id' => 36, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 4],
-                ['id' => 37, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 5],
-                ['id' => 38, 'discussion_id' => 33, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 6],
+                ['id' => 2, 'discussion_id' => 1, 'created_at' => Carbon::createFromDate(1975, 5, 21)->toDateTimeString(), 'user_id' => 3, 'type' => 'comment', 'content' => '<t><p>foo bar</p></t>', 'number' => 2],
+            ],
+            'group_user' => [
+                ['group_id' => Group::MEMBER_ID, 'user_id' => 2],
             ],
         ]);
     }
@@ -65,17 +57,17 @@ class NotificationsTest extends TestCase
             ->delete();
 
         /** @var User $mainUser */
-        $mainUser = User::query()->find(2);
+        $mainUser = User::query()->find(3);
 
         $this->assertEquals(0, $mainUser->getUnreadNotificationCount());
 
         $response = $this->send(
             $this->request('POST', '/api/posts', [
-                'authenticatedAs' => 4,
+                'authenticatedAs' => 2,
                 'json' => [
                     'data' => [
                         'attributes' => [
-                            'content' => 'reply with predetermined content for automated testing - too-obscure',
+                            'content' => '@"mainUser"#p2',
                         ],
                         'relationships' => [
                             'discussion' => ['data' => ['id' => 1]],
@@ -90,7 +82,7 @@ class NotificationsTest extends TestCase
         $json = json_decode($response->getBody()->getContents(), true);
 
         $this->send(
-            $this->request('PATCH', '/api/posts'.$json['data']['id'], [
+            $this->request('PATCH', '/api/posts/'.$json['data']['id'], [
                 'authenticatedAs' => 1,
                 'json' => [
                     'data' => [
@@ -101,5 +93,7 @@ class NotificationsTest extends TestCase
                 ]
             ])
         );
+
+        $this->assertEquals(1, $mainUser->getUnreadNotificationCount());
     }
 }
