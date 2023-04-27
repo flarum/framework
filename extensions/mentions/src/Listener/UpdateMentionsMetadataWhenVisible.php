6e1c22aa0ce2a5796d35a4f2bd4e6272693c478b
Sami Mazouz <sychocouldy@gmail.com>
2023-04-27 17:51:52 +0100

    perf: trigger mentions notifications in a queueable job
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- extensions/mentions/src/Listener/UpdateMentionsMetadataWhenVisible.php
+++ extensions/mentions/src/Listener/UpdateMentionsMetadataWhenVisible.php
@@ -11,6 +11,7 @@ namespace Flarum\Mentions\Listener;
 
 use Flarum\Approval\Event\PostWasApproved;
 use Flarum\Extension\ExtensionManager;
+use Flarum\Mentions\Job\SendMentionsNotificationsJob;
 use Flarum\Mentions\Notification\GroupMentionedBlueprint;
 use Flarum\Mentions\Notification\PostMentionedBlueprint;
 use Flarum\Mentions\Notification\UserMentionedBlueprint;
@@ -21,24 +22,25 @@ use Flarum\Post\Event\Restored;
 use Flarum\Post\Event\Revised;
 use Flarum\Post\Post;
 use Flarum\User\User;
+use Illuminate\Contracts\Queue\Queue;
 use s9e\TextFormatter\Utils;
 
 class UpdateMentionsMetadataWhenVisible
 {
     /**
-     * @var NotificationSyncer
-     */
-    protected $notifications;
-
-    /**
      * @var ExtensionManager
      */
     protected $extensions;
 
-    public function __construct(NotificationSyncer $notifications, ExtensionManager $extensions)
+    /**
+     * @var Queue
+     */
+    protected $queue;
+
+    public function __construct(ExtensionManager $extensions, Queue $queue)
     {
-        $this->notifications = $notifications;
         $this->extensions = $extensions;
+        $this->queue = $queue;
     }
 
     /**
@@ -54,17 +56,17 @@ class UpdateMentionsMetadataWhenVisible
 
         $this->syncUserMentions(
             $event->post,
-            Utils::getAttributeValues($content, 'USERMENTION', 'id')
+            $userMentions = Utils::getAttributeValues($content, 'USERMENTION', 'id')
         );
 
         $this->syncPostMentions(
             $event->post,
-            Utils::getAttributeValues($content, 'POSTMENTION', 'id')
+            $postMentions = Utils::getAttributeValues($content, 'POSTMENTION', 'id')
         );
 
         $this->syncGroupMentions(
             $event->post,
-            Utils::getAttributeValues($content, 'GROUPMENTION', 'id')
+            $groupMentions = Utils::getAttributeValues($content, 'GROUPMENTION', 'id')
         );
 
         if ($this->extensions->isEnabled('flarum-tags')) {
@@ -73,59 +75,26 @@ class UpdateMentionsMetadataWhenVisible
                 Utils::getAttributeValues($content, 'TAGMENTION', 'id')
             );
         }
+
+        $this->queue->push(new SendMentionsNotificationsJob($event->post, $userMentions, $postMentions, $groupMentions));
     }
 
     protected function syncUserMentions(Post $post, array $mentioned)
     {
         $post->mentionsUsers()->sync($mentioned);
         $post->unsetRelation('mentionsUsers');
-
-        $users = User::whereIn('id', $mentioned)
-            ->with('groups')
-            ->get()
-            ->filter(function ($user) use ($post) {
-                return $post->isVisibleTo($user) && $user->id !== $post->user_id;
-            })
-            ->all();
-
-        $this->notifications->sync(new UserMentionedBlueprint($post), $users);
     }
 
     protected function syncPostMentions(Post $reply, array $mentioned)
     {
         $reply->mentionsPosts()->sync($mentioned);
         $reply->unsetRelation('mentionsPosts');
-
-        $posts = Post::with('user')
-            ->whereIn('id', $mentioned)
-            ->with('user.groups')
-            ->get()
-            ->filter(function (Post $post) use ($reply) {
-                return $post->user && $post->user_id !== $reply->user_id && $reply->isVisibleTo($post->user);
-            })
-            ->all();
-
-        foreach ($posts as $post) {
-            $this->notifications->sync(new PostMentionedBlueprint($post, $reply), [$post->user]);
-        }
     }
 
     protected function syncGroupMentions(Post $post, array $mentioned)
     {
         $post->mentionsGroups()->sync($mentioned);
         $post->unsetRelation('mentionsGroups');
-
-        $users = User::whereHas('groups', function ($query) use ($mentioned) {
-            $query->whereIn('groups.id', $mentioned);
-        })
-            ->with('groups')
-            ->get()
-            ->filter(function (User $user) use ($post) {
-                return $post->isVisibleTo($user) && $user->id !== $post->user_id;
-            })
-            ->all();
-
-        $this->notifications->sync(new GroupMentionedBlueprint($post), $users);
     }
 
     protected function syncTagMentions(Post $post, array $mentioned)
