1edfa2a9d790d787dce89a074d0ca72f20ab094c
Sami Mazouz <sychocouldy@gmail.com>
2023-07-20 12:02:02 +0100

    fix: multiple inline async imports not picked up
    Signed-off-by: Sami Mazouz <sychocouldy@gmail.com>

--- js-packages/webpack-config/src/RegisterAsyncChunksPlugin.cjs
+++ js-packages/webpack-config/src/RegisterAsyncChunksPlugin.cjs
@@ -32,49 +32,53 @@ class RegisterAsyncChunksPlugin {
               if (module.resource && module.resource.includes("/src/") && module._source?._value.includes("webpackChunkName: ")) {
                 const reg = [];
 
-                module._source._value.replaceAll(/^(.*) webpackChunkName: '([^']*)'.* \*\/ '(.*)'.*$/gm, (match, _, urlPath, importPath) => {
-                  // Import path is relative to module.resource, so we need to resolve it
-                  const importPathResolved = path.resolve(path.dirname(module.resource), importPath);
-                  const thisComposerJson = require(path.resolve(process.cwd(), '../composer.json'));
-                  const namespace = extensionId(thisComposerJson.name);
+                // Each line that has a webpackChunkName comment.
+                [...module._source._value.matchAll(/.*\/\* webpackChunkName: .* \*\/.*/gm)].forEach(([match]) => {
+                  [...match.matchAll(/(.*?) webpackChunkName: '([^']*)'.*? \*\/ '([^']+)'.*?/gm)]
+                    .forEach(([match, _, urlPath, importPath]) => {
+                      // Import path is relative to module.resource, so we need to resolve it
+                      const importPathResolved = path.resolve(path.dirname(module.resource), importPath);
+                      const thisComposerJson = require(path.resolve(process.cwd(), '../composer.json'));
+                      const namespace = extensionId(thisComposerJson.name);
 
-                  const chunkModules = (c) => Array.from(compilation.chunkGraph.getChunkModulesIterable(c));
+                      const chunkModules = (c) => Array.from(compilation.chunkGraph.getChunkModulesIterable(c));
 
-                  const relevantChunk = chunks.find(
-                    (chunk) => chunkModules(chunk)?.find(
-                      (module) => module.resource?.split('.')[0] === importPathResolved || module.rootModule?.resource?.split('.')[0] === importPathResolved
-                    )
-                  );
+                      const relevantChunk = chunks.find(
+                        (chunk) => chunkModules(chunk)?.find(
+                          (module) => module.resource?.split('.')[0] === importPathResolved || module.rootModule?.resource?.split('.')[0] === importPathResolved
+                        )
+                      );
 
-                  if (! relevantChunk) {
-                    console.error(`Could not find chunk for ${importPathResolved}`);
-                    return match;
-                  }
-
-                  let concatenatedModule;
-                  const registrableModulesUrlPaths = new Map();
-                  registrableModulesUrlPaths.set(urlPath, [relevantChunk.id, namespace, urlPath]);
-
-                  if ((concatenatedModule = chunkModules(relevantChunk)[0])?.rootModule) {
-                    // This is a chunk with many modules, we need to register all of them.
-                    concatenatedModule.modules?.forEach((module) => {
-                      // The path right after the src/ directory, without the extension.
-                      const urlPath = module.resource.replace(/.*\/src\/(.*)\..*/, '$1');
-
-                      if (! registrableModulesUrlPaths.has(urlPath)) {
-                        registrableModulesUrlPaths.set(urlPath, [relevantChunk.id, namespace, urlPath]);
+                      if (! relevantChunk) {
+                        console.error(`Could not find chunk for ${importPathResolved}`);
+                        return match;
                       }
+
+                      let concatenatedModule;
+                      const registrableModulesUrlPaths = new Map();
+                      registrableModulesUrlPaths.set(urlPath, [relevantChunk.id, namespace, urlPath]);
+
+                      if ((concatenatedModule = chunkModules(relevantChunk)[0])?.rootModule) {
+                        // This is a chunk with many modules, we need to register all of them.
+                        concatenatedModule.modules?.forEach((module) => {
+                          // The path right after the src/ directory, without the extension.
+                          const urlPath = module.resource.replace(/.*\/src\/(.*)\..*/, '$1');
+
+                          if (! registrableModulesUrlPaths.has(urlPath)) {
+                            registrableModulesUrlPaths.set(urlPath, [relevantChunk.id, namespace, urlPath]);
+                          }
+                        });
+                      }
+
+                      registrableModulesUrlPaths.forEach(([chunkId, namespace, urlPath]) => {
+                        if (! chunkModuleMemory.includes(urlPath)) {
+                          reg.push(`flarum.reg.addChunkModule('${chunkId}', '${namespace}', '${urlPath}');`);
+                          chunkModuleMemory.push(urlPath);
+                        }
+                      });
+
+                      return match;
                     });
-                  }
-
-                  registrableModulesUrlPaths.forEach(([chunkId, namespace, urlPath]) => {
-                    if (! chunkModuleMemory.includes(urlPath)) {
-                      reg.push(`flarum.reg.addChunkModule('${chunkId}', '${namespace}', '${urlPath}');`);
-                      chunkModuleMemory.push(urlPath);
-                    }
-                  });
-
-                  return match;
                 });
 
                 module._source._value += reg.join('\n');
