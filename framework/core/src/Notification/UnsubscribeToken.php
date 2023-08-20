76604bb3d431232a006cd2a0ff43e22079561b14
IanM <ian@morland.me>
2023-08-20 15:31:32 +0100

    remove docblocks

--- framework/core/src/Notification/UnsubscribeToken.php
+++ framework/core/src/Notification/UnsubscribeToken.php
@@ -15,33 +15,15 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class UnsubscribeToken extends AbstractModel
 {
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
     protected $table = 'unsubscribe_tokens';
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'user_id'        => 'int',
         'unsubscribed_at' => 'datetime'
     ];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['user_id', 'email_type', 'token'];
 
-    /**
-     * Define a relation to the User model.
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
