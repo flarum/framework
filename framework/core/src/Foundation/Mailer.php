4171a0265351cba8282bb7f91e05c541975950d7
IanM <ian@morland.me>
2023-08-21 21:47:11 +0100

    allow config.php to specify outgoing mail format

---
+++ framework/core/src/Foundation/Mailer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Flarum\Foundation;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Mail\Mailer as SymfonyMailer;
+use Symfony\Component\Mailer\Transport\TransportInterface;
+
+
+class Mailer extends SymfonyMailer
+{
+    public function __construct(
+        string $name, 
+        Factory $views, 
+        TransportInterface $transport, 
+        Dispatcher $events = null,
+        protected Config $config
+    ){
+        parent::__construct($name, $views, $transport, $events);
+    }
+
+    public function send($view, array $data = [], $callback = null)
+    {
+        $emailType = $this->config['email_format'] ?? 'multipart';
+
+        switch ($emailType) {
+            case 'html':
+                unset($view['text']);
+                break;
+            case 'text':
+                unset($view['html']);
+                break;
+            // case 'multipart' is the default, where Flarum will send both HTML and text versions of emails, so that the recipient's email client can choose which one to display.
+        }
+        
+        parent::send($view, $data, $callback);
+    }
+}
--- framework/core/src/Mail/MailServiceProvider.php
+++ framework/core/src/Mail/MailServiceProvider.php
@@ -10,11 +10,12 @@
 namespace Flarum\Mail;
 
 use Flarum\Foundation\AbstractServiceProvider;
+use Flarum\Foundation\Config;
+use Flarum\Foundation\Mailer;
 use Flarum\Settings\SettingsRepositoryInterface;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Mail\Mailer as MailerContract;
 use Illuminate\Contracts\Validation\Factory;
-use Illuminate\Mail\Mailer;
 use Illuminate\Support\Arr;
 use Symfony\Component\Mailer\Transport\TransportInterface;
 
@@ -66,7 +67,8 @@ class MailServiceProvider extends AbstractServiceProvider
                 'flarum',
                 $container['view'],
                 $container['symfony.mailer.transport'],
-                $container['events']
+                $container['events'],
+                $container->make(Config::class)
             );
 
             if ($container->bound('queue')) {
