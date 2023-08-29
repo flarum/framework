0a2e1a489ea41402043b939b58c3b907fad7b310
Sami Mazouz <sychocouldy@gmail.com>
2023-08-29 15:26:43 +0100

    chore: misc flexbox/grid changes (#3875)
    * chore: `TagsPage` to tsx
    * chore: `TagsPage` flexbox/grid
    * chore: `IndexPage-toolbar` flexbox
    * chore: `UserCard` flexbox & itemlists
    * fix: `Post` improve spacing logic
    * chore: `Post` grid and proper spacing
    * fix: avatar editor hover layer layout
    * chore: `Button` flex

--- extensions/tags/js/src/forum/components/TagsPage.tsx
+++ extensions/tags/js/src/forum/components/TagsPage.tsx
@@ -1,5 +1,6 @@
 import app from 'flarum/forum/app';
 import Page from 'flarum/common/components/Page';
+import type { IPageAttrs } from 'flarum/common/components/Page';
 import PageStructure from 'flarum/forum/components/PageStructure';
 import WelcomeHero from 'flarum/forum/components/WelcomeHero';
 import IndexSidebar from 'flarum/forum/components/IndexSidebar';
@@ -14,16 +15,24 @@ import extractText from 'flarum/common/utils/extractText';
 import tagIcon from '../../common/helpers/tagIcon';
 import tagLabel from '../../common/helpers/tagLabel';
 import sortTags from '../../common/utils/sortTags';
+import Mithril from 'mithril';
 
-export default class TagsPage extends Page {
-  oninit(vnode) {
+import type Tag from '../../common/models/Tag';
+
+export interface ITagsPageAttrs extends IPageAttrs {}
+
+export default class TagsPage<CustomAttrs extends ITagsPageAttrs = ITagsPageAttrs> extends Page<CustomAttrs> {
+  private tags!: Tag[];
+  private loading!: boolean;
+
+  oninit(vnode: Mithril.Vnode<CustomAttrs, this>) {
     super.oninit(vnode);
 
     app.history.push('tags', extractText(app.translator.trans('flarum-tags.forum.header.back_to_tags_tooltip')));
 
     this.tags = [];
 
-    const preloaded = app.preloadedApiDocument();
+    const preloaded = app.preloadedApiDocument<Tag[]>();
 
     if (preloaded) {
       this.tags = sortTags(preloaded.filter((tag) => !tag.isChild()));
@@ -33,7 +42,7 @@ export default class TagsPage extends Page {
     this.loading = true;
 
     app.tagList.load(['children', 'lastPostedDiscussion', 'parent']).then(() => {
-      this.tags = sortTags(app.store.all('tags').filter((tag) => !tag.isChild()));
+      this.tags = sortTags(app.store.all<Tag>('tags').filter((tag) => !tag.isChild()));
 
       this.loading = false;
 
@@ -41,10 +50,10 @@ export default class TagsPage extends Page {
     });
   }
 
-  oncreate(vnode) {
+  oncreate(vnode: Mithril.VnodeDOM<CustomAttrs, this>) {
     super.oncreate(vnode);
 
-    app.setTitle(app.translator.trans('flarum-tags.forum.all_tags.meta_title_text'));
+    app.setTitle(extractText(app.translator.trans('flarum-tags.forum.all_tags.meta_title_text')));
     app.setTitleCount(0);
   }
 
@@ -83,28 +92,30 @@ export default class TagsPage extends Page {
     return <IndexSidebar />;
   }
 
-  tagTileListView(pinned) {
+  tagTileListView(pinned: Tag[]) {
     return <ul className="TagTiles">{pinned.map(this.tagTileView.bind(this))}</ul>;
   }
 
-  tagTileView(tag) {
+  tagTileView(tag: Tag) {
     const lastPostedDiscussion = tag.lastPostedDiscussion();
-    const children = sortTags(tag.children() || []);
+    const children = sortTags((tag.children() || []) as Tag[]);
 
     return (
       <li className={classList('TagTile', { colored: tag.color() }, textContrastClass(tag.color()))} style={{ '--tag-bg': tag.color() }}>
         <Link className="TagTile-info" href={app.route.tag(tag)}>
-          {tag.icon() && tagIcon(tag, {}, { useColor: false })}
-          <h3 className="TagTile-name">{tag.name()}</h3>
+          <div className="TagTile-heading">
+            {tag.icon() && tagIcon(tag, {}, { useColor: false })}
+            <h3 className="TagTile-name">{tag.name()}</h3>
+          </div>
           <p className="TagTile-description">{tag.description()}</p>
           {!!children && (
             <div className="TagTile-children">{children.map((child) => [<Link href={app.route.tag(child)}>{child.name()}</Link>, ' '])}</div>
           )}
         </Link>
         {lastPostedDiscussion ? (
-          <Link className="TagTile-lastPostedDiscussion" href={app.route.discussion(lastPostedDiscussion, lastPostedDiscussion.lastPostNumber())}>
+          <Link className="TagTile-lastPostedDiscussion" href={app.route.discussion(lastPostedDiscussion, lastPostedDiscussion.lastPostNumber()!)}>
             <span className="TagTile-lastPostedDiscussion-title">{lastPostedDiscussion.title()}</span>
-            {humanTime(lastPostedDiscussion.lastPostedAt())}
+            {humanTime(lastPostedDiscussion.lastPostedAt()!)}
           </Link>
         ) : (
           <span className="TagTile-lastPostedDiscussion" />
@@ -113,7 +124,7 @@ export default class TagsPage extends Page {
     );
   }
 
-  cloudView(cloud) {
+  cloudView(cloud: Tag[]) {
     return <div className="TagCloud">{cloud.map((tag) => [tagLabel(tag, { link: true }), ' '])}</div>;
   }
 }
