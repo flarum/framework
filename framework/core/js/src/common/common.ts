c06236cbba8d2932b508e6826841f5c197c58a97
Sami Mazouz <sychocouldy@gmail.com>
2023-09-13 17:22:50 +0100

    fix: chunk loading fails on admin frontend

--- framework/core/js/src/common/common.ts
+++ framework/core/js/src/common/common.ts
@@ -66,6 +66,7 @@ import './components/ModalManager';
 import './components/Button';
 import './components/Modal';
 import './components/GroupBadge';
+import './components/TextEditor';
 import './components/TextEditorButton';
 import './components/Tooltip';
 
