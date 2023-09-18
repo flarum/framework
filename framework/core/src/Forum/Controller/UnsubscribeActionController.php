b0d688e3db424981edc3b2e517891c0785cea814
IanM <16573496+imorland@users.noreply.github.com>
2023-09-18 13:42:13 +0100

    Update framework/core/src/Forum/Controller/UnsubscribeActionController.php
    Co-authored-by: Sami Mazouz <sychocouldy@gmail.com>

--- framework/core/src/Forum/Controller/UnsubscribeActionController.php
+++ framework/core/src/Forum/Controller/UnsubscribeActionController.php
@@ -34,26 +34,21 @@ class UnsubscribeActionController implements RequestHandlerInterface
         $token = Arr::get($parsedBody, 'token');
         $userId = Arr::get($parsedBody, 'userId');
 
-        // Fetch the unsubscribe token record
         /** @var UnsubscribeToken|null $unsubscribeRecord */
         $unsubscribeRecord = UnsubscribeToken::where('user_id', $userId)
             ->where('token', $token)
             ->first();
 
-        // If record exists and has not been used before
         if ($unsubscribeRecord && empty($unsubscribeRecord->unsubscribed_at)) {
-            // Mark as unsubscribed
             $unsubscribeRecord->unsubscribed_at = Carbon::now();
             $unsubscribeRecord->save();
 
-            // Update user preferences
             /** @var User $user */
             $user = User::find($userId);
             $user->setNotificationPreference($unsubscribeRecord->email_type, 'email', false);
             $user->save();
         }
 
-        // Redirect back to the forum's home page after unsubscribing
         return new RedirectResponse($this->url->to('forum')->base());
     }
 }
