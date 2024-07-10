c883bdbb60c2462ffad15570db2c901675a3f1c6
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-07-10 12:44:07 +0200

    chore(likes): export missing modules in compat API

---
+++ extensions/likes/js/src/forum/compat.ts
@@ -0,0 +1,11 @@
+import LikesUserPage from './components/LikesUserPage';
+import PostLikedNotification from './components/PostLikedNotification';
+import PostLikesModal from './components/PostLikesModal';
+import PostLikesModalState from './states/PostLikesModalState';
+
+export default {
+  'likes/components/LikesUserPage': LikesUserPage,
+  'likes/components/PostLikedNotification': PostLikedNotification,
+  'likes/components/PostLikesModal': PostLikesModal,
+  'likes/states/PostLikesModalState': PostLikesModalState,
+};
