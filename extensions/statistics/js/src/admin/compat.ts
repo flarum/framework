34072a69154de6acffd1e9bb52c65c038fb522f1
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-07-10 12:46:25 +0200

    chore(statistics): export missing modules in compat API

---
+++ extensions/statistics/js/src/admin/compat.ts
@@ -0,0 +1,11 @@
+import MiniStatisticsWidget from './components/MiniStatisticsWidget';
+import StatisticsPage from './components/StatisticsPage';
+import StatisticsWidget from './components/StatisticsWidget';
+import StatisticsWidgetDateSelectionModal from './components/StatisticsWidgetDateSelectionModal';
+
+export default {
+  'statistics/components/MiniStatisticsWidget': MiniStatisticsWidget,
+  'statistics/components/StatisticsPage': StatisticsPage,
+  'statistics/components/StatisticsWidget': StatisticsWidget,
+  'statistics/components/StatisticsWidgetDateSelectionModal': StatisticsWidgetDateSelectionModal,
+};
