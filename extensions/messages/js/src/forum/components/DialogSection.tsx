3b37fca4b6cd176e7976bb290d2bba885094fb53
Sami Mazouz <sychocouldy@gmail.com>
2024-09-14 15:02:57 +0100

    chore

--- extensions/messages/js/src/forum/components/DialogSection.tsx
+++ extensions/messages/js/src/forum/components/DialogSection.tsx
@@ -5,6 +5,8 @@ import MessageStream from './MessageStream';
 import username from 'flarum/common/helpers/username';
 import MessageStreamState from '../states/MessageStreamState';
 import Avatar from 'flarum/common/components/Avatar';
+import Link from 'flarum/common/components/Link';
+import app from 'flarum/forum/app';
 
 export interface IDialogStreamAttrs extends ComponentAttrs {
   dialog: Dialog;
@@ -32,7 +34,11 @@ export default class DialogSection<CustomAttrs extends IDialogStreamAttrs = IDia
       <div className="DialogSection">
         <div className="DialogSection-header">
           <Avatar user={this.attrs.dialog.recipient()} />
-          <h2>{username(this.attrs.dialog.recipient())}</h2>
+          {(this.attrs.dialog.recipient() && (
+            <Link href={app.route.user(this.attrs.dialog.recipient()!)}>
+              <h2>{username(this.attrs.dialog.recipient())}</h2>
+            </Link>
+          )) || <h2>{username(this.attrs.dialog.recipient())}</h2>}
         </div>
         <MessageStream dialog={this.attrs.dialog} state={this.messages} />
       </div>
