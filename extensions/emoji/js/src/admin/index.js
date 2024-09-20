c5b4ff7b27004120f8fbb0d9f214502253cfc3fe
青石 <www@qs5.org>
2024-09-20 16:40:23 +0800

    feat(emoji): allow the user to set the cdn address (#3908)

---
+++ extensions/emoji/js/src/admin/index.js
@@ -0,0 +1,13 @@
+import app from 'flarum/admin/app';
+import { version } from '../common/cdn';
+
+app.initializers.add('flarum-emoji', () => {
+  app.extensionData.for('flarum-emoji').registerSetting({
+    setting: 'flarum-emoji.cdn',
+    type: 'text',
+    label: app.translator.trans('flarum-emoji.admin.settings.cdn_label'),
+    help: app.translator.trans('flarum-emoji.admin.settings.cdn_help', {
+      version: version,
+    }),
+  });
+});
