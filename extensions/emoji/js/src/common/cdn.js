7ccbe0d1e4f06987f43254123b6e5e351ba1578c
Sami Mazouz <sychocouldy@gmail.com>
2024-09-20 09:29:39 +0100

    chore: format

--- extensions/emoji/js/src/common/cdn.js
+++ extensions/emoji/js/src/common/cdn.js
@@ -3,6 +3,5 @@ import twemoji from 'twemoji';
 export const version = /([0-9]+).[0-9]+.[0-9]+/g.exec(twemoji.base)[1];
 
 export default function () {
-  return app.forum.attribute('flarum-emoji.cdn')
-    .replace('[version]', version)
-};
+  return app.forum.attribute('flarum-emoji.cdn').replace('[version]', version);
+}
--- extensions/emoji/js/src/forum/renderEmoji.js
+++ extensions/emoji/js/src/forum/renderEmoji.js
@@ -11,7 +11,7 @@ function options() {
     attributes: () => ({
       loading: 'lazy',
     }),
-  }
+  };
 }
 
 /**
