5262b7204188b1d33ac3442bd319eb80c3f3d17b
YUCLing <luotianyi@luotianyi.me>
2024-09-28 00:47:51 +0800

    chore: fix code style

--- framework/core/js/src/common/utils/dayjsPlugins.ts
+++ framework/core/js/src/common/utils/dayjsPlugins.ts
@@ -1,21 +1,20 @@
 export const customFormats: import('dayjs').PluginFunc = function (_option, c, _factory) {
-    const proto = c.prototype;
-    const oldFormat = proto.format;
+  const proto = c.prototype;
+  const oldFormat = proto.format;
 
-    const t = (format?: string) =>
-        format?.replace(/(\[[^\]]+])|(MMMM|MM|DD|dddd)/g, (_, a, b) => a || b.slice(1))
+  const t = (format?: string) => format?.replace(/(\[[^\]]+])|(MMMM|MM|DD|dddd)/g, (_, a, b) => a || b.slice(1));
 
-    const englishFormats: Record<string, string> = {
-        F: 'DD MMMM',
-        FF: 'MMMM YYYY'
-    };
+  const englishFormats: Record<string, string> = {
+    F: 'DD MMMM',
+    FF: 'MMMM YYYY',
+  };
 
-    proto.format = function(template) {
-        const { formats = {} } = (this as any).$locale();
-        const result = template?.replace(/(\[[^\]]+])|(f{1,2}|F{1,2})/g, (_, a, b) => {
-            const B = b && b.toUpperCase();
-            return a || formats[b] || englishFormats[b] || t(formats[B]) || t(englishFormats[B]);
-        });
-        return oldFormat.call(this, result);
-    }
-}
+  proto.format = function (template) {
+    const { formats = {} } = (this as any).$locale();
+    const result = template?.replace(/(\[[^\]]+])|(f{1,2}|F{1,2})/g, (_, a, b) => {
+      const B = b && b.toUpperCase();
+      return a || formats[b] || englishFormats[b] || t(formats[B]) || t(englishFormats[B]);
+    });
+    return oldFormat.call(this, result);
+  };
+};
