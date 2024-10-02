eb6ea8b4f602b48c67a8cc9fee0e06c02a8d1af8
ksvijayan06 <ksvijayan06@gmail.com>
2024-10-02 07:54:53 +0530

    import Mithril at the top

--- framework/core/js/src/forum/components/HeaderPrimary.tsx
+++ framework/core/js/src/forum/components/HeaderPrimary.tsx
@@ -1,6 +1,7 @@
 import Component from '../../common/Component';
 import ItemList from '../../common/utils/ItemList';
 import listItems from '../../common/helpers/listItems';
+import type Mithril from 'mithril';
 
 /**
  * The `HeaderPrimary` component displays primary header controls. On the
@@ -17,7 +18,7 @@ export default class HeaderPrimary extends Component {
    * @return {ItemList<import('mithril').Children>}
    */
   
-  items(): ItemList<import('mithril').Children> {
-    return new ItemList<import('mithril').Children>();
+  items(): ItemList<Mithril.Children> {
+    return new ItemList<Mithril.Children>();
   }
 }
