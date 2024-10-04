486de8828350e2fe2f0aece6b4ceb3091a4602f5
YUCLing <luotianyi@luotianyi.me>
2024-10-04 23:55:12 +0800

    fix: code style

--- framework/core/js/src/common/helpers/humanTime.tsx
+++ framework/core/js/src/common/helpers/humanTime.tsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
 import type Mithril from 'mithril';
-import app from "../app";
+import app from '../app';
 import humanTimeUtil from '../utils/humanTime';
 
 /**
@@ -12,7 +12,7 @@ export default function humanTime(time: Date): Mithril.Vnode {
   const d = dayjs(time);
 
   const datetime = d.format();
-  const full = app.translator.formatDateTime(d, "core.lib.datetime_formats.full_time");
+  const full = app.translator.formatDateTime(d, 'core.lib.datetime_formats.full_time');
   const ago = humanTimeUtil(time);
 
   return (
