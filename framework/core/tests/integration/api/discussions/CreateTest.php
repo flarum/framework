6d1c8b3e832d3f9d5a07e7cd8e99eb12f3e54b6d
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-10-07 17:44:13 +0200

    test(core): implement test for creating discussion without content

--- framework/core/tests/integration/api/discussions/CreateTest.php
+++ framework/core/tests/integration/api/discussions/CreateTest.php
@@ -10,6 +10,7 @@
 namespace Flarum\Tests\integration\api\discussions;
 
 use Flarum\Discussion\Discussion;
+use Flarum\Extend;
 use Flarum\Testing\integration\RetrievesAuthorizedUsers;
 use Flarum\Testing\integration\TestCase;
 use Illuminate\Support\Arr;
@@ -54,6 +55,97 @@ class CreateTest extends TestCase
         $this->assertEquals(422, $response->getStatusCode());
 
         // The response body should contain details about the failed validation
+        $body = (string) $response->getBody();
+        $this->assertJson($body);
+        $this->assertEquals([
+            'errors' => [
+                [
+                    'status' => '422',
+                    'code' => 'validation_error',
+                    'detail' => 'The content field is required.',
+                    'source' => ['pointer' => '/data/attributes/content'],
+                ],
+            ],
+        ], json_decode($body, true));
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_discussion_without_content_property()
+    {
+        $this->extend(
+            (new Extend\Formatter)
+                ->unparse(function ($context, string $content) {
+                    return $content;
+                }),
+            (new Extend\Event())
+                ->listen(\Flarum\Post\Event\Saving::class, function ($event) {
+                    $event->post->content;
+                })
+        );
+
+        $response = $this->send(
+            $this->request('POST', '/api/discussions', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [
+                            'title' => 'Test post',
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertJson($body);
+        $this->assertEquals([
+            'errors' => [
+                [
+                    'status' => '422',
+                    'code' => 'validation_error',
+                    'detail' => 'The content field is required.',
+                    'source' => ['pointer' => '/data/attributes/content'],
+                ],
+            ],
+        ], json_decode($body, true));
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_create_discussion_with_content_set_to_null()
+    {
+        $this->extend(
+            (new Extend\Formatter)
+                ->unparse(function ($context, string $content) {
+                    return $content;
+                }),
+            (new Extend\Event())
+                ->listen(\Flarum\Post\Event\Saving::class, function ($event) {
+                    $event->post->content;
+                })
+        );
+
+        $response = $this->send(
+            $this->request('POST', '/api/discussions', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [
+                            'title' => 'Test post',
+                            'content' => null,
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+
         $body = (string) $response->getBody();
         $this->assertJson($body);
         $this->assertEquals([
