2e3a516ec5cd42501dee5331154e4595b4fe431f
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-10-07 18:50:02 +0200

    test(mentions): implement test for post editing with content empty

---
+++ extensions/mentions/tests/integration/api/EditPostTest.php
@@ -0,0 +1,142 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Mentions\Tests\integration\api;
+
+use Flarum\Extend;
+use Flarum\Testing\integration\TestCase;
+
+class EditPostTest extends TestCase
+{
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->extension('flarum-mentions');
+
+        $this->prepareDatabase([
+            'discussions' => [
+                ['id' => 1, 'title' => '', 'user_id' => 1, 'comment_count' => 1],
+            ],
+            'posts' => [
+                ['id' => 1, 'discussion_id' => 1, 'user_id' => 1, 'type' => 'comment', 'content' => '<t><p></p></t>'],
+            ]
+        ]);
+
+        $this->extend(
+            (new Extend\Event())
+            ->listen(\Flarum\Post\Event\Saving::class, function ($event) {
+                $event->post->content;
+            })
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_update_post_with_empty_string()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/posts/1', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [
+                            'content' => '',
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertJson($body);
+        $this->assertEquals([
+            'errors' => [
+                [
+                    'status' => '422',
+                    'code' => 'validation_error',
+                    'detail' => 'The content field is required.',
+                    'source' => ['pointer' => '/data/attributes/content'],
+                ],
+            ],
+        ], json_decode($body, true));
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_update_post_without_content_property()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/posts/1', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertJson($body);
+        $this->assertEquals([
+            'errors' => [
+                [
+                    'status' => '422',
+                    'code' => 'validation_error',
+                    'detail' => 'The content field is required.',
+                    'source' => ['pointer' => '/data/attributes/content'],
+                ],
+            ],
+        ], json_decode($body, true));
+    }
+
+    /**
+     * @test
+     */
+    public function cannot_update_post_with_content_set_to_null()
+    {
+        $response = $this->send(
+            $this->request('PATCH', '/api/posts/1', [
+                'authenticatedAs' => 1,
+                'json' => [
+                    'data' => [
+                        'attributes' => [
+                            'content' => null,
+                        ],
+                    ],
+                ],
+            ])
+        );
+
+        $this->assertEquals(422, $response->getStatusCode());
+
+        $body = (string) $response->getBody();
+        $this->assertJson($body);
+        $this->assertEquals([
+            'errors' => [
+                [
+                    'status' => '422',
+                    'code' => 'validation_error',
+                    'detail' => 'The content field is required.',
+                    'source' => ['pointer' => '/data/attributes/content'],
+                ],
+            ],
+        ], json_decode($body, true));
+    }
+}
