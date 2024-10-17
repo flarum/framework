876a964e1e77be111bfdbc8c36538b2b890742f4
Sami Mazouz <sychocouldy@gmail.com>
2024-10-17 13:26:30 +0100

    feat: add option for filtering read stickied on all discussions page (#4073)
    Co-authored-by: Adrian McCay <2762877+adrianmccay@users.noreply.github.com>

--- extensions/sticky/js/src/admin/extend.tsx
+++ extensions/sticky/js/src/admin/extend.tsx
@@ -5,13 +5,21 @@ import commonExtend from '../common/extend';
 export default [
   ...commonExtend,
 
-  new Extend.Admin().permission(
-    () => ({
-      icon: 'fas fa-thumbtack',
-      label: app.translator.trans('flarum-sticky.admin.permissions.sticky_discussions_label'),
-      permission: 'discussion.sticky',
-    }),
-    'moderate',
-    95
-  ),
+  new Extend.Admin()
+    .permission(
+      () => ({
+        icon: 'fas fa-thumbtack',
+        label: app.translator.trans('flarum-sticky.admin.permissions.sticky_discussions_label'),
+        permission: 'discussion.sticky',
+      }),
+      'moderate',
+      95
+    )
+    .setting(() => ({
+      setting: 'flarum-sticky.only_sticky_unread_discussions',
+      name: 'onlyStickyUnreadDiscussions',
+      type: 'boolean',
+      label: app.translator.trans('flarum-sticky.admin.settings.only_sticky_unread_discussions_label'),
+      help: app.translator.trans('flarum-sticky.admin.settings.only_sticky_unread_discussions_help'),
+    })),
 ];
