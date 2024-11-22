99be3005cc5837f7a8f90ff4d40831d8a630eb49
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2024-11-22 10:54:43 +0100

    fix: increase email field length in `email_tokens` as well

---
+++ framework/core/migrations/2024_11_22_000000_increase_email_field_length_in_email_tokens.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('email_tokens', function (Blueprint $table) {
+            $table->string('email', 254)->change();
+        });
+    },
+
+    'down' => function (Builder $schema) {
+        $schema->table('email_tokens', function (Blueprint $table) {
+            $table->string('email', 150)->change();
+        });
+    }
+];
