29a549b17c27c317513f0cfac9cee4e4a263bb8b
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-02-10 09:19:57 +0100

    refactor(core): improve extensibility of `IndexPage`

--- framework/core/js/src/forum/components/IndexPage.tsx
+++ framework/core/js/src/forum/components/IndexPage.tsx
@@ -63,13 +63,27 @@ export default class IndexPage<CustomAttrs extends IIndexPageAttrs = IIndexPageA
   view() {
     return (
       <PageStructure className="IndexPage" hero={this.hero.bind(this)} sidebar={this.sidebar.bind(this)}>
-        <div className="IndexPage-toolbar">
-          <ul className="IndexPage-toolbar-view">{listItems(this.viewItems().toArray())}</ul>
-          <ul className="IndexPage-toolbar-action">{listItems(this.actionItems().toArray())}</ul>
-        </div>
-        <DiscussionList state={app.discussions} />
+        {this.contentItems().toArray()}
       </PageStructure>
     );
+  }
+
+  contentItems(): ItemList<Mithril.Children> {
+    const items = new ItemList<Mithril.Children>();
+
+    items.add('toolbar', <div className="IndexPage-toolbar">{this.toolbarItems().toArray()}</div>, 100);
+    items.add('discussionList', <DiscussionList state={app.discussions} />, 90);
+
+    return items;
+  }
+
+  toolbarItems(): ItemList<Mithril.Children> {
+    const items = new ItemList<Mithril.Children>();
+
+    items.add('view', <ul className="IndexPage-toolbar-view">{listItems(this.viewItems().toArray())}</ul>, 100);
+    items.add('action', <ul className="IndexPage-toolbar-action">{listItems(this.actionItems().toArray())}</ul>, 90);
+
+    return items;
   }
 
   setTitle() {
