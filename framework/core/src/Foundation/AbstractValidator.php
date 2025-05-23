957cf0d1e1ed87d71fe012b118783118f804f09d
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-05-23 16:31:49 +0200

    refactor: use string interpolation

--- framework/core/src/Foundation/AbstractValidator.php
+++ framework/core/src/Foundation/AbstractValidator.php
@@ -98,11 +98,7 @@ abstract class AbstractValidator
     {
         $cache = resolve(Cache::class);
 
-        $cacheKey = sprintf(
-            'core.validation.attributes.%s.%s',
-            $this->translator->getLocale(),
-            static::class
-        );
+        $cacheKey = 'core.validation.attributes.' . $this->translator->getLocale() . '.' . static::class;
 
         if ($cached = $cache->get($cacheKey)) {
             return $cached;
