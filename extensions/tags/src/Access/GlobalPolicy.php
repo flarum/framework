0d7a5ddca6f08ec78a80e6ce22f74295ef356d07
Davide Iadeluca <davide.iadeluca@glowingblue.com>
2025-11-10 15:38:49 +0100

    fix(tags): defer policy if min primary & secondary tags 0
    Deferring it rather then force allowing in that case allows other policies to take action. Consider this more of a POC

--- extensions/tags/src/Access/GlobalPolicy.php
+++ extensions/tags/src/Access/GlobalPolicy.php
@@ -42,6 +42,15 @@ class GlobalPolicy extends AbstractPolicy
             return $this->allow();
         }
 
+        if ($ability === 'startDiscussion') {
+            $minPrimaryTags = (int) $this->settings->get('flarum-tags.min_primary_tags');
+            $minSecondaryTags = (int) $this->settings->get('flarum-tags.min_secondary_tags');
+
+            if (($minPrimaryTags === 0 && $minSecondaryTags === 0)) {
+                return;
+            }
+        }
+
         if (in_array($ability, ['viewForum', 'startDiscussion'])) {
             if (! isset($enoughPrimary[$actor->id][$ability])) {
                 $primaryTagsWhereNeedsPermission = $this->settings->get('flarum-tags.min_primary_tags');
