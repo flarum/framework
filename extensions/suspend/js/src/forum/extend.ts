b47060301663dabcdd03a0e01f5950a841f7db95
IanM <ian@morland.me>
2026-01-07 10:01:06 +0000

    chore: fix typo

--- extensions/suspend/js/src/forum/extend.ts
+++ extensions/suspend/js/src/forum/extend.ts
@@ -5,7 +5,7 @@ import Model from 'flarum/common/Model';
 export default [
   new Extend.Model(User)
     .attribute<boolean>('canSuspend')
-    .attribute<Date, string | null | undefined>('suspendedUntil', Model.transformDate)
+    .attribute<Date | null, string | null>('suspendedUntil', Model.transformDate)
     .attribute<string | null | undefined>('suspendReason')
     .attribute<string | null | undefined>('suspendMessage'),
 ];
