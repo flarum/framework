631d25281a973c507b89b276a907e33a4f8f7b16
IanM <ian@morland.me>
2026-02-21 13:30:18 +0000

    chore: prep for beta.7

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -27,7 +27,7 @@ class Application extends IlluminateContainer implements LaravelApplication
      *
      * @var string
      */
-    public const VERSION = '2.0.0-beta.6';
+    public const VERSION = '2.0.0-beta.7';
 
     protected bool $booted = false;
 
