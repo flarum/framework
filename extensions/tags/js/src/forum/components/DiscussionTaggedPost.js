d29b5a91b227225b84a512236de43a472ae293b0
EdgeInfinity <jiahaohe39@gmail.com>
2026-02-27 15:05:56 +0800

    Dynamic loading tags list

--- extensions/tags/js/src/forum/components/DiscussionTaggedPost.js
+++ extensions/tags/js/src/forum/components/DiscussionTaggedPost.js
@@ -2,18 +2,43 @@ import EventPost from 'flarum/forum/components/EventPost';
 import tagsLabel from '../../common/helpers/tagsLabel';
 
 export default class DiscussionTaggedPost extends EventPost {
-  static initAttrs(attrs) {
-    super.initAttrs(attrs);
+  oninit(vnode) {
+    super.oninit(vnode);
 
-    const oldTags = attrs.post.content()[0];
-    const newTags = attrs.post.content()[1];
+    const oldTags = this.attrs.post.content()[0];
+    const newTags = this.attrs.post.content()[1];
 
-    function diffTags(tags1, tags2) {
-      return tags1.filter((tag) => tags2.indexOf(tag) === -1).map((id) => app.store.getById('tags', id));
+    this.tagsAdded = [];
+    this.tagsRemoved = [];
+
+    this.fetchRequired = false;
+    this.loading = true;
+
+    const tagsMutation = [...this.diffTags(newTags, oldTags), ...this.diffTags(oldTags, newTags)];
+    if (tagsMutation.includes(undefined)) {
+      this.fetchRequired = true;
     }
 
-    attrs.tagsAdded = diffTags(newTags, oldTags);
-    attrs.tagsRemoved = diffTags(oldTags, newTags);
+    const afterFetch = () => {
+      this.tagsAdded = this.diffTags(newTags, oldTags);
+      this.tagsRemoved = this.diffTags(oldTags, newTags);
+
+      this.loading = false;
+      m.redraw();
+    };
+
+    if (this.fetchRequired) {
+      app.store.find('tags').then(afterFetch);
+    } else {
+      afterFetch();
+    }
+  }
+
+  diffTags(tags1, tags2) {
+    return tags1
+      .filter((tag) => tags2.indexOf(tag) === -1)
+      .map((id) => app.store.getById('tags', id))
+      // .filter(Boolean);
   }
 
   icon() {
@@ -21,8 +46,8 @@ export default class DiscussionTaggedPost extends EventPost {
   }
 
   descriptionKey() {
-    if (this.attrs.tagsAdded.length) {
-      if (this.attrs.tagsRemoved.length) {
+    if (this.tagsAdded.length) {
+      if (this.tagsRemoved.length) {
         return 'flarum-tags.forum.post_stream.added_and_removed_tags_text';
       }
 
@@ -35,17 +60,17 @@ export default class DiscussionTaggedPost extends EventPost {
   descriptionData() {
     const data = {};
 
-    if (this.attrs.tagsAdded.length) {
+    if (this.tagsAdded.length) {
       data.tagsAdded = app.translator.trans('flarum-tags.forum.post_stream.tags_text', {
-        tags: tagsLabel(this.attrs.tagsAdded, { link: true }),
-        count: this.attrs.tagsAdded.length,
+        tags: tagsLabel(this.tagsAdded, { link: true }),
+        count: this.tagsAdded.length,
       });
     }
 
-    if (this.attrs.tagsRemoved.length) {
+    if (this.tagsRemoved.length) {
       data.tagsRemoved = app.translator.trans('flarum-tags.forum.post_stream.tags_text', {
-        tags: tagsLabel(this.attrs.tagsRemoved, { link: true }),
-        count: this.attrs.tagsRemoved.length,
+        tags: tagsLabel(this.tagsRemoved, { link: true }),
+        count: this.tagsRemoved.length,
       });
     }
 
