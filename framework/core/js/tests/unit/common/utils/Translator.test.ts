flarum/framework

commit 73a029641ae0997258a7ec7f4d23fe05c193418c

chore: switch formatter to `format-message` (#4088)
Co-authored-by: Robert Korulczyk <robert@korulczyk.pl>

framework/core/js/src/common/Translator.tsx
@@ -1,13 +1,13 @@
-import type { Dayjs } from 'dayjs';
-import { RichMessageFormatter, mithrilRichHandler, NestedStringArray } from '@askvortsov/rich-icu-message-formatter';
-import { pluralTypeHandler, selectTypeHandler } from '@ultraq/icu-message-formatter';
 import username from './helpers/username';
+import type { Dayjs } from 'dayjs';
 import User from './models/User';
 import extract from './utils/extract';
+import formatMessage, { Translation } from 'format-message';
+import fireDebugWarning from './helpers/fireDebugWarning';
 import extractText from './utils/extractText';
 import ItemList from './utils/ItemList';
 
-type Translations = Record<string, string>;
+type Translations = { [key: string]: string | Translation };
 type TranslatorParameters = Record<string, unknown>;
 type DateTimeFormatCallback = (id?: string) => string | void;
 
@@ -15,7 +15,9 @@ export default class Translator {
   /**
    * A map of translation keys to their translated values.
    */
-  translations: Translations = {};
+  get translations(): Translations {
+    return this.formatter.setup().translations[this.getLocale()] ?? {};
+  }
 
   /**
    * A item list of date time format callbacks.
@@ -25,44 +27,44 @@ export default class Translator {
   /**
    * The underlying ICU MessageFormatter util.
    */
-  protected formatter = new RichMessageFormatter(null, this.formatterTypeHandlers(), mithrilRichHandler);
+  protected formatter = formatMessage;
 
   /**
    * Sets the formatter's locale to the provided value.
    */
   setLocale(locale: string) {
-    this.formatter.locale = locale;
+    this.formatter.setup({
+      locale,
+      translations: {
+        [locale]: this.formatter.setup().translations[locale] ?? {},
+      },
+    });
   }
 
   /**
    * Returns the formatter's current locale.
    */
-  getLocale() {
-    return this.formatter.locale;
+  getLocale(): string {
+    return (Array.isArray(this.formatter.setup().locale) ? this.formatter.setup().locale[0] : this.formatter.setup().locale) as string;
   }
 
   addTranslations(translations: Translations) {
-    Object.assign(this.translations, translations);
-  }
+    const locale = this.getLocale();
 
-  /**
-   * An extensible entrypoint for extenders to register type handlers for translations.
-   */
-  protected formatterTypeHandlers() {
-    return {
-      plural: pluralTypeHandler,
-      select: selectTypeHandler,
-    };
+    this.formatter.setup({
+      translations: {
+        [locale]: Object.assign(this.translations, translations),
+      },
+    });
   }
 
   /**
    * A temporary system to preprocess parameters.
    * Should not be used by extensions.
-   * TODO: An extender will be added in v1.x.
    *
    * @internal
    */
-  protected preprocessParameters(parameters: TranslatorParameters) {
+  protected preprocessParameters(parameters: TranslatorParameters, translation: string | Translation) {
     // If we've been given a user model as one of the input parameters, then
     // we'll extract the username and use that for the translation. In the
     // future there should be a hook here to inspect the user and change the
@@ -75,23 +77,66 @@ export default class Translator {
       if (!parameters.username) parameters.username = username(user);
     }
 
+    // To maintain backwards compatibility, we will catch HTML elements and
+    // push the tags as mithril children to the parameters keyed by the tag name.
+    // Will be removed in v2.0
+    translation = typeof translation === 'string' ? translation : translation.message;
+    const elements = translation.match(/<(\w+)[^>]*>.*?<\/\1>/g);
+    const tags = elements?.map((element) => element.match(/^<(\w+)/)![1]) || [];
+
+    for (const tag of tags) {
+      if (!parameters[tag]) {
+        fireDebugWarning(
+          `Any HTML tags used within translations must have corresponding mithril component parameters.\nCaught in translation: \n\n"""\n${translation}\n"""`,
+          '',
+          'v2.0',
+          'flarum/framework'
+        );
+
+        parameters[tag] = ({ children }: any) => m(tag, children);
+      }
+    }
+
+    // The old formatter allowed rich parameters as such:
+    // { link: <Link href="https://flarum.org"/> }
+    // The new formatter dictates that the rich parameter must be a function,
+    // like so: { link: ({ children }) => <Link href="https://flarum.org">{children}</Link> }
+    // This layer allows the old format to be used, and converts it to the new format.
+    for (const key in parameters) {
+      const value: any = parameters[key];
+
+      if (tags.includes(key) && typeof value === 'object' && value.attrs && value.tag) {
+        parameters[key] = ({ children }: any) => {
+          return m(value.tag, value.attrs, children);
+        };
+      }
+    }
+
     return parameters;
   }
 
-  trans(id: string, parameters: TranslatorParameters): NestedStringArray;
-  trans(id: string, parameters: TranslatorParameters, extract: false): NestedStringArray;
+  trans(id: string, parameters: TranslatorParameters): any[];
+  trans(id: string, parameters: TranslatorParameters, extract: false): any[];
   trans(id: string, parameters: TranslatorParameters, extract: true): string;
-  trans(id: string): NestedStringArray | string;
+  trans(id: string): any[] | string;
   trans(id: string, parameters: TranslatorParameters = {}, extract = false) {
-    const translation = this.translations[id];
+    const translation = this.preprocessTranslation(this.translations[id]);
 
     if (translation) {
-      parameters = this.preprocessParameters(parameters);
-      const locale = this.formatter.rich(translation, parameters);
+      parameters = this.preprocessParameters(parameters, translation);
+
+      this.translations[id] = translation;
+
+      let locale = this.formatter.rich({ id, default: id }, parameters);
+
+      // convert undefined args to {undefined}.
+      locale = locale instanceof Array ? locale.map((arg) => (arg === undefined ? '{undefined}' : arg)) : locale;
 
       if (extract) return extractText(locale);
 
       return locale;
+    } else {
+      fireDebugWarning(`Missing translation for key: "${id}"`);
     }
 
     return id;
@@ -113,6 +158,32 @@ export default class Translator {
       if (result) return result;
     }
 
-    return time.format(this.translations[id]);
+    return time.format(this.preprocessTranslation(this.translations[id]));
+  }
+
+  /**
+   * Backwards compatibility for translations such as `<a href='{href}'>`, the old
+   * formatter supported that, but the new one doesn't, so attributes are auto dropped
+   * to avoid errors.
+   *
+   * @private
+   */
+  private preprocessTranslation(translation: string | Translation | undefined): string | undefined {
+    if (!translation) return;
+
+    translation = typeof translation === 'string' ? translation : translation.message;
+
+    // If the translation contains a <x ...attrs> tag, then we'll need to
+    // remove the attributes for backwards compatibility. Will be removed in v2.0.
+    // And if it did have attributes, then we'll fire a warning
+    if (translation.match(/<\w+ [^>]+>/g)) {
+      fireDebugWarning(
+        `Any HTML tags used within translations must be simple tags, without attributes.\nCaught in translation: \n\n"""\n${translation}\n"""`
+      );
+
+      return translation.replace(/<(\w+)([^>]*)>/g, '<$1>');
+    }
+
+    return translation;
   }
 }

framework/core/js/tests/unit/common/utils/Translator.test.ts
@@ -1,0 +1,69 @@
+import Translator from '../../../../src/common/Translator';
+import extractText from '../../../../src/common/utils/extractText';
+
+/*
+ * These tests should be in sync with PHP tests in `tests/unit/Locale/TranslatorTest.php`, to make sure that JS
+ * translator works in the same way as JS translator.
+ */
+
+test('placeholders encoding', () => {
+  const translator = new Translator();
+  translator.addTranslations({
+    test1: 'test1 {placeholder} test1',
+    test2: 'test2 {placeholder} test2',
+  });
+
+  expect(extractText(translator.trans('test1', { placeholder: "'" }))).toBe("test1 ' test1");
+  expect(extractText(translator.trans('test1', { placeholder: translator.trans('test2', { placeholder: "'" }) }))).toBe("test1 test2 ' test2 test1");
+});
+
+// This is how the backend translator behaves. The only discrepancy with the frontend translator.
+// test('missing placeholders', () => {
+//   const translator = new Translator();
+//   translator.addTranslations({
+//     test1: 'test1 {placeholder} test1',
+//   });
+//
+//   expect(extractText(translator.trans('test1', {}))).toBe('test1 {placeholder} test1');
+// });
+
+test('missing placeholders', () => {
+  const translator = new Translator();
+  translator.addTranslations({
+    test1: 'test1 {placeholder} test1',
+  });
+
+  expect(extractText(translator.trans('test1', {}))).toBe('test1 {undefined} test1');
+});
+
+test('escaped placeholders', () => {
+  const translator = new Translator();
+  translator.addTranslations({
+    test3: "test1 {placeholder} '{placeholder}' test1",
+  });
+
+  expect(extractText(translator.trans('test3', { placeholder: "'" }))).toBe("test1 ' {placeholder} test1");
+});
+
+test('plural rules', () => {
+  const translator = new Translator();
+  translator.addTranslations({
+    test4: '{pageNumber, plural, =1 {{forumName}} other {Page # - {forumName}}}',
+  });
+
+  expect(extractText(translator.trans('test4', { forumName: 'A & B', pageNumber: 1 }))).toBe('A & B');
+  expect(extractText(translator.trans('test4', { forumName: 'A & B', pageNumber: 2 }))).toBe('Page 2 - A & B');
+});
+
+test('plural rules 2', () => {
+  const translator = new Translator();
+  translator.setLocale('pl');
+  translator.addTranslations({
+    test5: '{count, plural, one {# post} few {# posty} many {# postów} other {# posta}}',
+  });
+
+  expect(extractText(translator.trans('test5', { count: 1 }))).toBe('1 post');
+  expect(extractText(translator.trans('test5', { count: 2 }))).toBe('2 posty');
+  expect(extractText(translator.trans('test5', { count: 5 }))).toBe('5 postów');
+  expect(extractText(translator.trans('test5', { count: 1.5 }))).toBe('1,5 posta');
+});

framework/core/js/src/@types/translator-icu-rich.d.ts
@@ -1,26 +1,0 @@
-declare module '@askvortsov/rich-icu-message-formatter' {
-  type IValues = Record<string, any>;
-
-  type ITypeHandler = (
-    value: string,
-    matches: string,
-    locale: string,
-    values: IValues,
-    format: (message: string, values: IValues) => string
-  ) => string;
-  type IRichHandler = (tag: any, values: IValues, contents: string) => any;
-
-  type ValueOrArray<T> = T | ValueOrArray<T>[];
-  export type NestedStringArray = ValueOrArray<string>;
-
-  export class RichMessageFormatter {
-    locale: string | null;
-    constructor(locale: string | null, typeHandlers: Record<string, ITypeHandler>, richHandler: IRichHandler);
-
-    format(message: string, values: IValues): string;
-    process(message: string, values: IValues): NestedStringArray;
-    rich(message: string, values: IValues): NestedStringArray;
-  }
-
-  export function mithrilRichHandler(tag: any, values: IValues, contents: string): any;
-}

framework/core/js/src/@types/translator-icu.d.ts
@@ -1,17 +1,0 @@
-declare module '@ultraq/icu-message-formatter' {
-  export function pluralTypeHandler(
-    value: string,
-    matches: string,
-    locale: string,
-    values: Record<string, any>,
-    format: (text: string, values: Record<string, any>) => string
-  ): string;
-
-  export function selectTypeHandler(
-    value: string,
-    matches: string,
-    locale: string,
-    values: Record<string, any>,
-    format: (text: string, values: Record<string, any>) => string
-  ): string;
-}

framework/core/js/src/common/helpers/fireDebugWarning.ts
@@ -12,7 +12,7 @@ import app from '../app';
  * can fix.
  */
 export default function fireDebugWarning(...args: Parameters<typeof console.warn>): void {
-  if (!app.forum.attribute('debug')) return;
+  if (!app.data.resources.find((r) => r.type === 'forums')?.attributes?.debug) return;
 
   console.warn(...args);
 }

framework/core/js/src/common/utils/abbreviateNumber.ts
@@ -9,7 +9,6 @@ import extractText from './extractText';
  * // "1.2K"
  */
 export default function abbreviateNumber(number: number): string {
-  // TODO: translation
   if (number >= 1000000) {
     return Math.floor(number / 1000000) + extractText(app.translator.trans('core.lib.number_suffix.mega_text'));
   } else if (number >= 1000) {

framework/core/js/src/forum/components/AccessTokensList.tsx
@@ -9,7 +9,6 @@ import classList from '../../common/utils/classList';
 import Tooltip from '../../common/components/Tooltip';
 import type Mithril from 'mithril';
 import type AccessToken from '../../common/models/AccessToken';
-import { NestedStringArray } from '@askvortsov/rich-icu-message-formatter';
 import Icon from '../../common/components/Icon';
 
 export interface IAccessTokensListAttrs extends ComponentAttrs {
@@ -187,7 +186,7 @@ export default class AccessTokensList<CustomAttrs extends IAccessTokensListAttrs
     m.redraw();
   }
 
-  generateTokenTitle(token: AccessToken): NestedStringArray {
+  generateTokenTitle(token: AccessToken): any[] | string {
     const name = token.title() || app.translator.trans('core.forum.security.token_title_placeholder');
     const value = this.tokenValueDisplay(token);
 

framework/core/tests/unit/Locale/TranslatorTest.php
@@ -1,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of Flarum.
+ *
+ * For detailed copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Flarum\Tests\unit\Locale;
+
+use Flarum\Locale\Translator;
+use Flarum\Testing\unit\TestCase;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+use Symfony\Component\Translation\MessageCatalogueInterface;
+
+class TranslatorTest extends TestCase
+{
+    private const DOMAIN = 'messages'.MessageCatalogueInterface::INTL_DOMAIN_SUFFIX;
+
+    /*
+     * These tests should be in sync with JS tests in `js/tests/unit/common/utils/Translator.test.ts`, to make sure that JS
+     * translator works in the same way as JS translator.
+     */
+
+    /** @test */
+    public function placeholders_encoding()
+    {
+        $translator = new Translator('en');
+        $translator->addLoader('array', new ArrayLoader());
+        $translator->addResource('array', [
+            'test1' => 'test1 {placeholder} test1',
+            'test2' => 'test2 {placeholder} test2',
+        ], 'en', self::DOMAIN);
+
+        $this->assertSame("test1 ' test1", $translator->trans('test1', ['placeholder' => "'"]));
+        $this->assertSame("test1 test2 ' test2 test1", $translator->trans('test1', ['placeholder' => $translator->trans('test2', ['placeholder' => "'"])]));
+    }
+
+    /** @test */
+    public function missing_placeholders()
+    {
+        $translator = new Translator('en');
+        $translator->addLoader('array', new ArrayLoader());
+        $translator->addResource('array', [
+            'test1' => 'test1 {placeholder} test1',
+        ], 'en', self::DOMAIN);
+
+        $this->assertSame('test1 {placeholder} test1', $translator->trans('test1', []));
+    }
+
+    /** @test */
+    public function escaped_placeholders()
+    {
+        $translator = new Translator('en');
+        $translator->addLoader('array', new ArrayLoader());
+        $translator->addResource('array', [
+            'test3' => "test1 {placeholder} '{placeholder}' test1",
+        ], 'en', self::DOMAIN);
+
+        $this->assertSame("test1 ' {placeholder} test1", $translator->trans('test3', ['placeholder' => "'"]));
+    }
+
+    /** @test */
+    public function plural_rules()
+    {
+        $translator = new Translator('en');
+        $translator->addLoader('array', new ArrayLoader());
+        $translator->addResource('array', [
+            'test4' => '{pageNumber, plural, =1 {{forumName}} other {Page # - {forumName}}}',
+        ], 'en', self::DOMAIN);
+
+        $this->assertSame('A & B', $translator->trans('test4', ['forumName' => 'A & B', 'pageNumber' => 1]));
+        $this->assertSame('Page 2 - A & B', $translator->trans('test4', ['forumName' => 'A & B', 'pageNumber' => 2]));
+    }
+
+    /** @test */
+    public function plural_rules_2()
+    {
+        $translator = new Translator('pl');
+        $translator->addLoader('array', new ArrayLoader());
+        $translator->addResource('array', [
+            'test4' => '{count, plural, one {# post} few {# posty} many {# postów} other {# posta}}',
+        ], 'pl', self::DOMAIN);
+
+        $this->assertSame('1 post', $translator->trans('test4', ['count' => 1]));
+        $this->assertSame('2 posty', $translator->trans('test4', ['count' => 2]));
+        $this->assertSame('5 postów', $translator->trans('test4', ['count' => 5]));
+        $this->assertSame('1,5 posta', $translator->trans('test4', ['count' => 1.5]));
+    }
+}

commit dd45c0d00b8b76309eb3761381958d83fdc81809

fix(translator): preserve translations when switching locale
setLocale() was replacing the entire translation store with only entries
for the new locale. Since locale JS files (e.g. forum-pl.js) execute
before app.load() calls setLocale(), their translations were stored under
format-message's default locale ('en'). When setLocale('pl') ran, it set
translations to { pl: {} }, discarding the Polish translations entirely.

English worked by coincidence because its locale matched the default.

Fix: when switching locale, merge any translations loaded under the
previous locale into the new locale key so nothing is lost. A test
covering the pre-setLocale addTranslations() order is also added.

Regression introduced in #4088.

Co-Authored-By: Claude Sonnet 4.6 <noreply@anthropic.com>

framework/core/js/src/common/Translator.tsx
@@ -33,10 +33,13 @@ export default class Translator {
    * Sets the formatter's locale to the provided value.
    */
   setLocale(locale: string) {
+    const previousLocale = this.getLocale();
+    const allTranslations = this.formatter.setup().translations;
+
     this.formatter.setup({
       locale,
       translations: {
-        [locale]: this.formatter.setup().translations[locale] ?? {},
+        [locale]: Object.assign({}, allTranslations[previousLocale] ?? {}, allTranslations[locale] ?? {}),
       },
     });
   }

framework/core/js/tests/unit/common/utils/Translator.test.ts
@@ -67,3 +67,15 @@ test('plural rules 2', () => {
   expect(extractText(translator.trans('test5', { count: 5 }))).toBe('5 postów');
   expect(extractText(translator.trans('test5', { count: 1.5 }))).toBe('1,5 posta');
 });
+
+test('translations added before setLocale are retained', () => {
+  // Simulates the real page load order: locale JS files execute and call
+  // addTranslations() before app.load() calls setLocale().
+  const translator = new Translator();
+  translator.addTranslations({
+    test6: 'Witaj świecie',
+  });
+  translator.setLocale('pl');
+
+  expect(extractText(translator.trans('test6', {}))).toBe('Witaj świecie');
+});