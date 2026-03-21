ddae22a7275f71120c1cb241ff70be67b0714403
IanM <ian@morland.me>
2026-03-21 08:30:10 +0000

    prep for beta.8

--- framework/core/src/Foundation/Application.php
+++ framework/core/src/Foundation/Application.php
@@ -27,7 +27,7 @@ class Application extends IlluminateContainer implements LaravelApplication
      *
      * @var string
      */
-    public const VERSION = '2.0.0-beta.7';
+    public const VERSION = '2.0.0-beta.8';
 
     protected bool $booted = false;
 
