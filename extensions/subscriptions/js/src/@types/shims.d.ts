e03a3a8b9c234e5b7f31c420c7fe8ecfd6392507
huoxin233 <23447157+huoxin233@users.noreply.github.com>
2026-03-21 16:01:34 +0800

    fix: explicitly declare SettingsPage loading generic properties in shims

--- extensions/subscriptions/js/src/@types/shims.d.ts
+++ extensions/subscriptions/js/src/@types/shims.d.ts
@@ -1,7 +1,16 @@
 import 'flarum/common/models/Discussion';
+import 'flarum/forum/components/SettingsPage';
 
 declare module 'flarum/common/models/Discussion' {
   export default interface Discussion {
     subscription(): string;
+  }
+}
+
+declare module 'flarum/forum/components/SettingsPage' {
+  export default interface SettingsPage {
+    followAfterReplyLoading: boolean;
+    followAfterCreateLoading: boolean;
+    notifyForAllPostsLoading: boolean;
   }
 }
