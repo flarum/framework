786f843ef0e37fc97d58faa06aa374059ae8db96
Daniel Klabbers <daniel+git@klabbers.email>
2026-04-14 20:45:24 +0200

    feat: support opcache clears in installation

--- framework/core/src/Http/Middleware/ClearOPCache.php
+++ framework/core/src/Http/Middleware/ClearOPCache.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Flarum\Http\Middleware;
 
 use Flarum\Foundation\Paths;
+use Illuminate\Contracts\Container\Container;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface as Middleware;
@@ -14,7 +15,7 @@ class ClearOPCache implements Middleware
 {
     const PATH = '/cache/clear-opcache';
 
-    public function __construct(private readonly Paths $paths)
+    public function __construct(private readonly Container $container)
     {}
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
@@ -32,6 +33,20 @@ class ClearOPCache implements Middleware
 
     public function path(): string
     {
-        return $this->paths->storage . static::PATH;
+        if ($this->container->bound(Paths::class)) {
+            return $this->container->make(Paths::class)->storage . static::PATH;
+        }
+
+        // Fallback when Paths is not available during installation.
+        $path = dirname(__DIR__, 3);
+
+        while(true) {
+            if ($path === '.') throw new \Exception('Could not find storage directory');
+
+            if (is_dir("$path/storage")) break;
+            $path = dirname($path);
+        }
+
+        return "$path/storage" . static::PATH;
     }
 }
