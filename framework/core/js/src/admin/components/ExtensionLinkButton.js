b4f6387621747c43b2eb4a7d58c6d01f6690c596
Daniel Klabbers <daniel+git@klabbers.email>
2026-04-17 09:53:56 +0200

    fix: admin nav dropdown flickers on mobile, fixes #4577

--- framework/core/js/src/admin/components/ExtensionLinkButton.js
+++ framework/core/js/src/admin/components/ExtensionLinkButton.js
@@ -1,23 +1,10 @@
 import app from '../../admin/app';
 import isExtensionEnabled from '../utils/isExtensionEnabled';
 import LinkButton from '../../common/components/LinkButton';
-import Tooltip from '../../common/components/Tooltip';
 import ItemList from '../../common/utils/ItemList';
 import Icon from '../../common/components/Icon';
 
 export default class ExtensionLinkButton extends LinkButton {
-  view(vnode) {
-    const extension = app.data.extensions[this.attrs.extensionId];
-    const title = extension?.extra?.['flarum-extension']?.title || this.attrs.extensionId;
-    const tooltipText = extension?.version ? `${title}<br>${extension.version}` : title;
-
-    return (
-      <Tooltip text={tooltipText} position="right" container="body" html={true}>
-        {super.view(vnode)}
-      </Tooltip>
-    );
-  }
-
   getButtonContent(children) {
     const content = super.getButtonContent(children);
     const extension = app.data.extensions[this.attrs.extensionId];
